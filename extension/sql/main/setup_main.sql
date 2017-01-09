5bee8d7ee4830f02d107ee1b53cc96822d6d972b
Rob Kiefer <rob.kiefer@gmail.com>
2017-01-09 16:50:12 -0500

    Format SQL setup files

--- extension/sql/main/setup_main.sql
+++ extension/sql/main/setup_main.sql
@@ -1,96 +1,95 @@
+-- Initializes a data node in the cluster.
 CREATE OR REPLACE FUNCTION setup_main()
     RETURNS void LANGUAGE PLPGSQL AS
 $BODY$
 DECLARE
 BEGIN
 
-DROP TRIGGER IF EXISTS trigger_on_create_chunk_replica_node_index
-ON chunk_replica_node_index;
-CREATE TRIGGER trigger_on_create_chunk_replica_node_index AFTER INSERT OR UPDATE OR DELETE ON chunk_replica_node_index
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_chunk_replica_node_index();
+    DROP TRIGGER IF EXISTS trigger_on_create_chunk_replica_node_index
+    ON chunk_replica_node_index;
+    CREATE TRIGGER trigger_on_create_chunk_replica_node_index AFTER INSERT OR UPDATE OR DELETE ON chunk_replica_node_index
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_chunk_replica_node_index();
 
-DROP TRIGGER IF EXISTS trigger_on_create_chunk_replica_node
-ON chunk_replica_node;
-CREATE TRIGGER trigger_on_create_chunk_replica_node AFTER INSERT OR UPDATE OR DELETE ON chunk_replica_node
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_chunk_replica_node();
+    DROP TRIGGER IF EXISTS trigger_on_create_chunk_replica_node
+    ON chunk_replica_node;
+    CREATE TRIGGER trigger_on_create_chunk_replica_node AFTER INSERT OR UPDATE OR DELETE ON chunk_replica_node
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_chunk_replica_node();
 
-DROP TRIGGER IF EXISTS trigger_on_create_chunk
-ON chunk;
-CREATE TRIGGER trigger_on_create_chunk AFTER INSERT OR UPDATE OR DELETE ON chunk
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_chunk();
+    DROP TRIGGER IF EXISTS trigger_on_create_chunk
+    ON chunk;
+    CREATE TRIGGER trigger_on_create_chunk AFTER INSERT OR UPDATE OR DELETE ON chunk
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_chunk();
 
-DROP TRIGGER IF EXISTS trigger_on_create_cluster_user
-ON cluster_user;
-CREATE TRIGGER trigger_on_create_cluster_user AFTER INSERT OR UPDATE OR DELETE ON cluster_user
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_cluster_user();
+    DROP TRIGGER IF EXISTS trigger_on_create_cluster_user
+    ON cluster_user;
+    CREATE TRIGGER trigger_on_create_cluster_user AFTER INSERT OR UPDATE OR DELETE ON cluster_user
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_cluster_user();
 
-DROP TRIGGER IF EXISTS trigger_on_create_distinct_replica_node
-ON distinct_replica_node;
-CREATE TRIGGER trigger_on_create_distinct_replica_node AFTER INSERT OR UPDATE OR DELETE ON distinct_replica_node
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_distinct_replica_node();
+    DROP TRIGGER IF EXISTS trigger_on_create_distinct_replica_node
+    ON distinct_replica_node;
+    CREATE TRIGGER trigger_on_create_distinct_replica_node AFTER INSERT OR UPDATE OR DELETE ON distinct_replica_node
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_distinct_replica_node();
 
-DROP TRIGGER IF EXISTS trigger_on_modify_field
-ON field;
-CREATE TRIGGER trigger_on_modify_field AFTER INSERT OR UPDATE OR DELETE ON field
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_modify_field();
+    DROP TRIGGER IF EXISTS trigger_on_modify_field
+    ON field;
+    CREATE TRIGGER trigger_on_modify_field AFTER INSERT OR UPDATE OR DELETE ON field
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_modify_field();
 
-DROP TRIGGER IF EXISTS trigger_on_deleted_field
-ON deleted_field;
-CREATE TRIGGER trigger_on_deleted_field AFTER INSERT OR UPDATE OR DELETE ON deleted_field
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_deleted_field();
+    DROP TRIGGER IF EXISTS trigger_on_deleted_field
+    ON deleted_field;
+    CREATE TRIGGER trigger_on_deleted_field AFTER INSERT OR UPDATE OR DELETE ON deleted_field
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_deleted_field();
 
-DROP TRIGGER IF EXISTS trigger_on_create_hypertable_replica
-ON hypertable_replica;
-CREATE TRIGGER trigger_on_create_hypertable_replica AFTER INSERT OR UPDATE OR DELETE ON hypertable_replica
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_hypertable_replica();
+    DROP TRIGGER IF EXISTS trigger_on_create_hypertable_replica
+    ON hypertable_replica;
+    CREATE TRIGGER trigger_on_create_hypertable_replica AFTER INSERT OR UPDATE OR DELETE ON hypertable_replica
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_hypertable_replica();
 
-DROP TRIGGER IF EXISTS trigger_on_create_hypertable
-ON hypertable;
-CREATE TRIGGER trigger_on_create_hypertable AFTER INSERT OR UPDATE OR DELETE ON hypertable
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_hypertable();
+    DROP TRIGGER IF EXISTS trigger_on_create_hypertable
+    ON hypertable;
+    CREATE TRIGGER trigger_on_create_hypertable AFTER INSERT OR UPDATE OR DELETE ON hypertable
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_hypertable();
 
-DROP TRIGGER IF EXISTS trigger_on_create_meta
-ON meta;
-CREATE TRIGGER trigger_on_create_meta AFTER INSERT OR UPDATE OR DELETE ON meta
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_meta();
+    DROP TRIGGER IF EXISTS trigger_on_create_meta
+    ON meta;
+    CREATE TRIGGER trigger_on_create_meta AFTER INSERT OR UPDATE OR DELETE ON meta
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_meta();
 
-DROP TRIGGER IF EXISTS trigger_on_create_node
-ON node;
-CREATE TRIGGER trigger_on_create_node AFTER INSERT OR UPDATE OR DELETE ON node
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_node();
+    DROP TRIGGER IF EXISTS trigger_on_create_node
+    ON node;
+    CREATE TRIGGER trigger_on_create_node AFTER INSERT OR UPDATE OR DELETE ON node
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_node();
 
-DROP TRIGGER IF EXISTS trigger_on_create_partition_replica_table
-ON partition_replica;
-CREATE TRIGGER trigger_on_create_partition_replica_table AFTER INSERT OR UPDATE OR DELETE ON partition_replica
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_partition_replica_table();
+    DROP TRIGGER IF EXISTS trigger_on_create_partition_replica_table
+    ON partition_replica;
+    CREATE TRIGGER trigger_on_create_partition_replica_table AFTER INSERT OR UPDATE OR DELETE ON partition_replica
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_create_partition_replica_table();
 
-DROP TRIGGER IF EXISTS trigger_on_modify_hypertable_index
-ON hypertable_index;
-CREATE TRIGGER trigger_on_modify_hypertable_index AFTER INSERT OR UPDATE OR DELETE ON hypertable_index
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_modify_hypertable_index();
+    DROP TRIGGER IF EXISTS trigger_on_modify_hypertable_index
+    ON hypertable_index;
+    CREATE TRIGGER trigger_on_modify_hypertable_index AFTER INSERT OR UPDATE OR DELETE ON hypertable_index
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_modify_hypertable_index();
 
-DROP TRIGGER IF EXISTS trigger_on_deleted_hypertable_index
-ON deleted_hypertable_index;
-CREATE TRIGGER trigger_on_deleted_hypertable_index AFTER INSERT OR UPDATE OR DELETE ON deleted_hypertable_index
-FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_deleted_hypertable_index();
+    DROP TRIGGER IF EXISTS trigger_on_deleted_hypertable_index
+    ON deleted_hypertable_index;
+    CREATE TRIGGER trigger_on_deleted_hypertable_index AFTER INSERT OR UPDATE OR DELETE ON deleted_hypertable_index
+    FOR EACH ROW EXECUTE PROCEDURE _sysinternal.on_deleted_hypertable_index();
 
-  
-CREATE EVENT TRIGGER ddl_create_index ON ddl_command_end
-   WHEN tag IN ('create index')
-   EXECUTE PROCEDURE _sysinternal.ddl_process_create_index();
+    CREATE EVENT TRIGGER ddl_create_index ON ddl_command_end
+        WHEN tag IN ('create index')
+        EXECUTE PROCEDURE _sysinternal.ddl_process_create_index();
 
-CREATE EVENT TRIGGER ddl_alter_index ON ddl_command_end
-   WHEN tag IN ('alter index')
-   EXECUTE PROCEDURE _sysinternal.ddl_process_alter_index();
+    CREATE EVENT TRIGGER ddl_alter_index ON ddl_command_end
+        WHEN tag IN ('alter index')
+        EXECUTE PROCEDURE _sysinternal.ddl_process_alter_index();
 
-CREATE EVENT TRIGGER ddl_drop_index ON sql_drop
-   WHEN tag IN ('drop index')
-  EXECUTE PROCEDURE _sysinternal.ddl_process_drop_index();
+    CREATE EVENT TRIGGER ddl_drop_index ON sql_drop
+        WHEN tag IN ('drop index')
+        EXECUTE PROCEDURE _sysinternal.ddl_process_drop_index();
 
-CREATE EVENT TRIGGER ddl_create_column ON ddl_command_end
-   WHEN tag IN ('alter table')
-   EXECUTE PROCEDURE _sysinternal.ddl_process_alter_table();
-
+    CREATE EVENT TRIGGER ddl_create_column ON ddl_command_end
+       WHEN tag IN ('alter table')
+       EXECUTE PROCEDURE _sysinternal.ddl_process_alter_table();
 
 END
 $BODY$;
--- extension/sql/meta/setup_meta.sql
+++ extension/sql/meta/setup_meta.sql
@@ -1,3 +1,4 @@
+-- Initializes a meta node in the cluster
 CREATE OR REPLACE FUNCTION setup_meta()
     RETURNS void LANGUAGE PLPGSQL AS
 $BODY$
@@ -5,74 +6,72 @@ DECLARE
     table_name NAME;
 BEGIN
 
-DROP TRIGGER IF EXISTS trigger_on_create_chunk_replica_node
-ON chunk_replica_node;
-CREATE TRIGGER trigger_on_create_chunk_replica_node AFTER INSERT OR UPDATE OR DELETE ON chunk_replica_node
-FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_chunk_replica_node_meta();
+    DROP TRIGGER IF EXISTS trigger_on_create_chunk_replica_node
+    ON chunk_replica_node;
+    CREATE TRIGGER trigger_on_create_chunk_replica_node AFTER INSERT OR UPDATE OR DELETE ON chunk_replica_node
+    FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_chunk_replica_node_meta();
 
-DROP TRIGGER IF EXISTS trigger_on_create_chunk
-ON chunk;
-CREATE TRIGGER trigger_on_create_chunk AFTER INSERT OR UPDATE OR DELETE ON chunk
-FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_chunk();
+    DROP TRIGGER IF EXISTS trigger_on_create_chunk
+    ON chunk;
+    CREATE TRIGGER trigger_on_create_chunk AFTER INSERT OR UPDATE OR DELETE ON chunk
+    FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_chunk();
 
-DROP TRIGGER IF EXISTS trigger_create_hypertable
-ON hypertable;
-CREATE TRIGGER trigger_create_hypertable AFTER INSERT OR UPDATE OR DELETE ON hypertable
-FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_hypertable();
+    DROP TRIGGER IF EXISTS trigger_create_hypertable
+    ON hypertable;
+    CREATE TRIGGER trigger_create_hypertable AFTER INSERT OR UPDATE OR DELETE ON hypertable
+    FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_hypertable();
 
-DROP TRIGGER IF EXISTS trigger_create_node
-ON node;
-CREATE TRIGGER trigger_create_node BEFORE INSERT OR UPDATE OR DELETE ON node
-FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_node();
+    DROP TRIGGER IF EXISTS trigger_create_node
+    ON node;
+    CREATE TRIGGER trigger_create_node BEFORE INSERT OR UPDATE OR DELETE ON node
+    FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_node();
 
-DROP TRIGGER IF EXISTS trigger_sync_node
-ON node;
-CREATE TRIGGER trigger_sync_node AFTER INSERT ON node
-FOR EACH ROW EXECUTE PROCEDURE _meta.sync_node();
+    DROP TRIGGER IF EXISTS trigger_sync_node
+    ON node;
+    CREATE TRIGGER trigger_sync_node AFTER INSERT ON node
+    FOR EACH ROW EXECUTE PROCEDURE _meta.sync_node();
 
-DROP TRIGGER IF EXISTS trigger_create_partition
-ON partition;
-CREATE TRIGGER trigger_create_partition AFTER INSERT OR UPDATE OR DELETE ON partition
-FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_partition();
-
-
-FOREACH table_name IN ARRAY ARRAY ['cluster_user', 'hypertable', 'hypertable_index', 'deleted_hypertable_index', 'hypertable_replica',
-'distinct_replica_node', 'partition_epoch', 'partition', 'partition_replica',
-'chunk_replica_node', 'field', 'deleted_field', 'meta', 'default_replica_node'] :: NAME [] LOOP
-    EXECUTE format(
-        $$
-            DROP TRIGGER IF EXISTS trigger_0_sync_insert_%1$s ON %1$s;
-            DROP TRIGGER IF EXISTS trigger_0_sync_update_%1$s ON %1$s;
-            DROP TRIGGER IF EXISTS trigger_0_sync_delete_%1$s ON %1$s;
-        $$,
-        table_name);
-    EXECUTE format(
-        $$
-            CREATE TRIGGER trigger_0_sync_insert_%1$s AFTER INSERT ON %1$s
-            FOR EACH ROW EXECUTE PROCEDURE _sysinternal.sync_insert();
-            CREATE TRIGGER trigger_0_sync_update_%1$s AFTER UPDATE ON %1$s
-            FOR EACH ROW EXECUTE PROCEDURE _sysinternal.sync_update();
-            CREATE TRIGGER trigger_0_sync_delete_%1$s AFTER DELETE ON %1$s
-            FOR EACH ROW EXECUTE PROCEDURE _sysinternal.sync_delete();
+    DROP TRIGGER IF EXISTS trigger_create_partition
+    ON partition;
+    CREATE TRIGGER trigger_create_partition AFTER INSERT OR UPDATE OR DELETE ON partition
+    FOR EACH ROW EXECUTE PROCEDURE _meta.on_create_partition();
 
 
-        $$,
-        table_name);
-END LOOP;
+    FOREACH table_name IN ARRAY ARRAY ['cluster_user', 'hypertable', 'hypertable_index', 'deleted_hypertable_index', 'hypertable_replica',
+    'distinct_replica_node', 'partition_epoch', 'partition', 'partition_replica',
+    'chunk_replica_node', 'field', 'deleted_field', 'meta', 'default_replica_node'] :: NAME [] LOOP
+        EXECUTE format(
+            $$
+                DROP TRIGGER IF EXISTS trigger_0_sync_insert_%1$s ON %1$s;
+                DROP TRIGGER IF EXISTS trigger_0_sync_update_%1$s ON %1$s;
+                DROP TRIGGER IF EXISTS trigger_0_sync_delete_%1$s ON %1$s;
+            $$,
+            table_name);
+        EXECUTE format(
+            $$
+                CREATE TRIGGER trigger_0_sync_insert_%1$s AFTER INSERT ON %1$s
+                FOR EACH ROW EXECUTE PROCEDURE _sysinternal.sync_insert();
+                CREATE TRIGGER trigger_0_sync_update_%1$s AFTER UPDATE ON %1$s
+                FOR EACH ROW EXECUTE PROCEDURE _sysinternal.sync_update();
+                CREATE TRIGGER trigger_0_sync_delete_%1$s AFTER DELETE ON %1$s
+                FOR EACH ROW EXECUTE PROCEDURE _sysinternal.sync_delete();
+            $$,
+            table_name);
+    END LOOP;
 
-FOREACH table_name IN ARRAY ARRAY ['field', 'hypertable_index'] :: NAME [] LOOP
-    EXECUTE format(
-        $$
-            DROP TRIGGER IF EXISTS trigger_0_deleted_%1$s ON %1$s
-        $$,
-        table_name);
-    EXECUTE format(
-        $$
-            CREATE TRIGGER trigger_0_deleted_%1$s AFTER DELETE ON %1$s
-            FOR EACH ROW EXECUTE PROCEDURE _meta.log_delete();
-        $$,
-        table_name);
-END LOOP;
+    FOREACH table_name IN ARRAY ARRAY ['field', 'hypertable_index'] :: NAME [] LOOP
+        EXECUTE format(
+            $$
+                DROP TRIGGER IF EXISTS trigger_0_deleted_%1$s ON %1$s
+            $$,
+            table_name);
+        EXECUTE format(
+            $$
+                CREATE TRIGGER trigger_0_deleted_%1$s AFTER DELETE ON %1$s
+                FOR EACH ROW EXECUTE PROCEDURE _meta.log_delete();
+            $$,
+            table_name);
+    END LOOP;
 
 END
 $BODY$;
