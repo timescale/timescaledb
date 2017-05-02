b128ac2a8792f8ed1923d05571f3be268e651943
Matvey Arye <cevian@gmail.com>
2017-05-02 13:22:53 -0400

    Fix bug with INSERT INTO...SELECT
    Previously, INSERT INTO...SELECT FROM hypertable was broken. This was
    caused by incorrectly tracking the commandType in change_name_walker.
    Fixes #24.

--- test/sql/insert_single.sql
+++ test/sql/insert_single.sql
@@ -10,3 +10,13 @@ INSERT INTO "1dim" VALUES('2017-01-20T09:00:01', 22.5);
 INSERT INTO "1dim" VALUES('2017-01-20T09:00:21', 21.2);
 INSERT INTO "1dim" VALUES('2017-01-20T09:00:47', 25.1);
 SELECT * FROM "1dim";
+
+CREATE TABLE regular_table (time timestamp, temp float);
+INSERT INTO regular_table SELECT * FROM "1dim";
+SELECT * FROM regular_table;
+
+TRUNCATE TABLE regular_table;
+INSERT INTO regular_table VALUES('2017-01-20T09:00:59', 29.2);
+INSERT INTO "1dim" SELECT * FROM regular_table;
+SELECT * FROM "1dim";
+
