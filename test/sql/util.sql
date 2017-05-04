384a8fb0d18b9403d388cef33e517cd5db336a48
Matvey Arye <mat@timescale.com>
2017-05-04 16:37:15 -0400

    Add regression tests for deleted unit tests

---
+++ test/sql/util.sql
@@ -0,0 +1,13 @@
+\unset ECHO
+\o /dev/null
+\ir include/create_single_db.sql
+\o
+\set ECHO errors
+\set VERBOSITY default
+
+DO $$
+BEGIN
+  ASSERT( _timescaledb_catalog.get_partition_for_key('', 16:: INT4) = 13 );
+  ASSERT( _timescaledb_catalog.get_partition_for_key('dev1', 16:: INT4) = 4 );
+  ASSERT( _timescaledb_catalog.get_partition_for_key('longlonglonglongpartitionkey', 16:: INT4) = 6 );
+END$$;
