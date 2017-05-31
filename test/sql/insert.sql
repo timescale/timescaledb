87a5491f9067a31d04b5dcc63292fd3c7ec43a06
Matvey Arye <mat@timescale.com>
2017-05-31 19:21:54 -0400

    Fix problems with partitioning logic for padded fields
    Padded fields such as character(20) did not work correctly before
    because of differences between type output and coercion/casting.

--- test/sql/insert.sql
+++ test/sql/insert.sql
@@ -20,3 +20,14 @@ INSERT INTO error_test VALUES ('Mon Mar 20 09:18:22.3 2017', 21.1, NULL);
 \set ON_ERROR_STOP 1
 INSERT INTO error_test VALUES ('Mon Mar 20 09:18:25.7 2017', 22.4, 'dev2');
 SELECT * FROM error_test;
+
+--test character(9) partition keys since there were issues with padding causing partitioning errors
+CREATE TABLE tick_character (
+    symbol      character(9) NOT NULL,                                                                                                      mid       REAL NOT NULL,                                                                                                                spread      REAL NOT NULL,
+    time        TIMESTAMPTZ       NOT NULL
+);
+
+SELECT create_hypertable ('tick_character', 'time', 'symbol', 2);
+INSERT INTO tick_character ( symbol, mid, spread, time ) VALUES ( 'GBPJPY', 142.639000, 5.80, 'Mon Mar 20 09:18:22.3 2017');
+SELECT * FROM tick_character;
+
