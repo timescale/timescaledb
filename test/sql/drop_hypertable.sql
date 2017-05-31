5f9f371948e5614e70f86894567bbad2ecace7bd
Erik Nordström <erik@timescaledb.com>
2017-05-31 13:48:18 +0200

    Reference the correct column when scanning partition epochs
    This fixes a bug that caused the wrong column to be referenced when
    scanning the partition_epoch table. The bug did not usually manifest
    itself because the referenced (hypertable) ID was typically
    incremented in unison with the desired (partition epoch) ID.

---
+++ test/sql/drop_hypertable.sql
@@ -0,0 +1,22 @@
+\ir include/create_single_db.sql
+
+SELECT * from _timescaledb_catalog.hypertable;
+SELECT * from _timescaledb_catalog.partition_epoch;
+
+CREATE TABLE should_drop (time timestamp, temp float8);
+SELECT create_hypertable('should_drop', 'time');
+
+-- Calling create hypertable again will increment hypertable ID
+-- although no new hypertable is created. Make sure we can handle this.
+SELECT create_hypertable('should_drop', 'time', if_not_exists => true);
+SELECT * from _timescaledb_catalog.hypertable;
+SELECT * from _timescaledb_catalog.partition_epoch;
+DROP TABLE should_drop;
+
+CREATE TABLE should_drop (time timestamp, temp float8);
+SELECT create_hypertable('should_drop', 'time');
+
+INSERT INTO should_drop VALUES (now(), 1.0);
+SELECT * from _timescaledb_catalog.hypertable;
+SELECT * from _timescaledb_catalog.partition_epoch;
+
