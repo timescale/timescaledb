f103fc7de320890ec1da622c3931e4130a9a6408
Erik Nordström <erik@timescale.com>
2017-08-10 11:00:38 +0200

    Make generate-typedefs.sh work on Linux

--- scripts/generate_typedef.sh
+++ scripts/generate_typedef.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-gobjdump -W src/*.o |egrep -A3 DW_TAG_typedef |perl -e 'while (<>) { chomp; @flds = split;next unless (1 < @flds);\
+
+OBJDUMP=${OBJDUMP:-gobjdump}
+
+case "$OSTYPE" in
+  linux*)   OBJDUMP=objdump ;;
+  darwin*)  ;;
+  bsd*)     ;;
+  *)        ;;
+esac
+
+${OBJDUMP} -W src/*.o |egrep -A3 DW_TAG_typedef |perl -e 'while (<>) { chomp; @flds = split;next unless (1 < @flds);\
      next if $flds[0]  ne "DW_AT_name" && $flds[1] ne "DW_AT_name";\
      next if $flds[-1] =~ /^DW_FORM_str/;\
      print $flds[-1],"\n"; }' |sort |uniq > typedef.list.local
