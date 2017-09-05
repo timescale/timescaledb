85022feb7baa9c724dd026504d4e521e786a8494
Rob Kiefer <rob@timescale.com>
2017-09-05 16:19:48 -0400

    Add backup/restore scripts for single hypertables
    Using pg_dump/pg_restore for a database with hypertables will back
    up all internal tables too, which is sometimes not desired. These
    scripts allow one to backup regular PostgreSQL tables separately
    and then pick and choose which hypertables to backup and restore.
    This also provides a forward-compatible way of backing up and
    restoring hypertables, since data is dumped to CSV and restored
    by re-creating and re-inserting the data.

--- sql/util_internal_table_ddl.sql
+++ sql/util_internal_table_ddl.sql
@@ -142,3 +142,69 @@ BEGIN
     END LOOP;
 END
 $BODY$;
+
+-- Outputs the create_hypertable command to recreate the given hypertable.
+--
+-- This is currently used internally for our single hypertable backup tool
+-- so that it knows how to restore the hypertable without user intervention.
+--
+-- It only works for hypertables with up to 2 dimensions.
+CREATE OR REPLACE FUNCTION _timescaledb_internal.get_create_command(
+    table_name NAME
+)
+    RETURNS TEXT LANGUAGE PLPGSQL VOLATILE AS
+$BODY$
+DECLARE
+    h_id             INTEGER;
+    schema_name      NAME;
+    time_column      NAME;
+    time_interval    BIGINT;
+    space_column     NAME;
+    space_partitions INTEGER;
+    dimension_cnt    INTEGER;
+    dimension_row    record;
+    ret              TEXT;
+BEGIN
+    SELECT h.id, h.schema_name
+    FROM _timescaledb_catalog.hypertable AS h
+    WHERE h.table_name = get_create_command.table_name
+    INTO h_id, schema_name;
+
+    IF h_id IS NULL THEN
+        RAISE EXCEPTION 'hypertable % not found', table_name
+        USING ERRCODE = 'IO101';
+    END IF;
+
+    SELECT COUNT(*)
+    FROM _timescaledb_catalog.dimension d
+    WHERE d.hypertable_id = h_id
+    INTO STRICT dimension_cnt;
+
+    IF dimension_cnt > 2 THEN
+        RAISE EXCEPTION 'get_create_command only supports hypertables with up to 2 dimensions'
+        USING ERRCODE = 'IO101';
+    END IF;
+
+    FOR dimension_row IN
+        SELECT *
+        FROM _timescaledb_catalog.dimension d
+        WHERE d.hypertable_id = h_id
+        LOOP
+        IF dimension_row.interval_length IS NOT NULL THEN
+            time_column := dimension_row.column_name;
+            time_interval := dimension_row.interval_length;
+        ELSIF dimension_row.num_slices IS NOT NULL THEN
+            space_column := dimension_row.column_name;
+            space_partitions := dimension_row.num_slices;
+        END IF;
+    END LOOP;
+
+    ret := format($$SELECT create_hypertable('%I.%I', '%I'$$, schema_name, table_name, time_column);
+    IF space_column IS NOT NULL THEN
+        ret := ret || format($$, '%I', %s$$, space_column, space_partitions);
+    END IF;
+    ret := ret || format($$, chunk_time_interval => %s, create_default_indexes=>FALSE);$$, time_interval);
+
+    RETURN ret;
+END
+$BODY$;
