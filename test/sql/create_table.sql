025b683e703a048ff7ae0dfe1cf8d620d4132384
Erik Nordström <erik@timescale.com>
2017-09-29 14:49:27 +0200

    Fix constraint validation on regular tables
    Constraints on regular PostgreSQL tables need to be validatated so
    that they don't have a foreign key constraint that references a
    hypertable, as this is not yet supported. This change fixes a bug in
    this validation caused by not checking for the proper node types when
    parsing a CreateStmt node. The CreateStmt is now also parsed in the
    DDL end hook, instead of the processUtility hook, which ensures that
    the CreateStmt node has been run through parse analysis. The parse
    analysis changes the contents of the node to be more consistent across
    different CREATE statements. For instance, a CREATE LIKE statement
    will look more similar to a regular CREATE statement after parse
    analysis.

---
+++ test/sql/create_table.sql
@@ -0,0 +1,13 @@
+\ir include/create_single_db.sql
+
+-- Test that we can verify constraints on regular tables
+CREATE TABLE test_hyper_pk(time TIMESTAMPTZ PRIMARY KEY, temp FLOAT, device INT);
+CREATE TABLE test_pk(device INT PRIMARY KEY);
+CREATE TABLE test_like(LIKE test_pk);
+
+SELECT create_hypertable('test_hyper_pk', 'time');
+
+\set ON_ERROR_STOP 0
+-- Foreign key constraints that reference hypertables are currently unsupported
+CREATE TABLE test_fk(time TIMESTAMPTZ REFERENCES test_hyper_pk(time));
+\set ON_ERROR_STOP 1
