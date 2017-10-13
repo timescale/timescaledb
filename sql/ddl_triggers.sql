f97d6243595bf15c3d87ab238c9d02ab15756734
Rob Kiefer <rob@timescale.com>
2017-10-13 10:33:45 -0400

    Make event trigger creation idempotent

--- sql/ddl_triggers.sql
+++ sql/ddl_triggers.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE FUNCTION _timescaledb_internal.ddl_command_end() RETURNS event_trigger
 AS '$libdir/timescaledb', 'timescaledb_ddl_command_end' LANGUAGE C IMMUTABLE STRICT;
 
+DROP EVENT TRIGGER IF EXISTS timescaledb_ddl_command_end;
 CREATE EVENT TRIGGER timescaledb_ddl_command_end ON ddl_command_end
 EXECUTE PROCEDURE _timescaledb_internal.ddl_command_end();
