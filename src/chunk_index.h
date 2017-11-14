963ba4e6ceee7bb5c10508a9fb1b5027374239c9
Erik Nordström <erik@timescale.com>
2017-11-14 12:14:02 +0100

    Recurse CLUSTER command to chunks
    Clustering a table means reordering it according to an index.
    This operation requires an exclusive lock and extensive
    processing time. On large hypertables with many chunks, CLUSTER
    risks blocking a lot of operations by holding locks for a long time.
    This is alleviated by processing each chunk in a new transaction,
    ensuring locks are only held on one chunk at a time.

--- src/chunk_index.h
+++ src/chunk_index.h
@@ -7,6 +7,13 @@
 typedef struct Chunk Chunk;
 typedef struct Hypertable Hypertable;
 
+typedef struct ChunkIndexMapping
+{
+	Oid			chunkoid;
+	Oid			parent_indexoid;
+	Oid			indexoid;
+} ChunkIndexMapping;
+
 extern void chunk_index_create_all(int32 hypertable_id, Oid hypertable_relid, int32 chunk_id, Oid chunkrelid);
 extern Oid	chunk_index_create_from_stmt(IndexStmt *stmt, int32 chunk_id, Oid chunkrelid, int32 hypertable_id, Oid hypertable_indexrelid);
 extern int	chunk_index_delete_children_of(Hypertable *ht, Oid hypertable_indexrelid, bool should_drop);
@@ -15,4 +22,7 @@ extern int	chunk_index_rename(Chunk *chunk, Oid chunk_indexrelid, const char *ne
 extern int	chunk_index_rename_parent(Hypertable *ht, Oid hypertable_indexrelid, const char *newname);
 extern int	chunk_index_set_tablespace(Hypertable *ht, Oid hypertable_indexrelid, const char *tablespace);
 extern void chunk_index_create_from_constraint(int32 hypertable_id, Oid hypertable_constaint, int32 chunk_id, Oid chunk_constraint);
+extern List *chunk_index_get_mappings(Hypertable *ht, Oid hypertable_indexrelid);
+extern void chunk_index_mark_clustered(Oid chunkrelid, Oid indexrelid);
+
 #endif   /* TIMESCALEDB_CHUNK_INDEX_H */
