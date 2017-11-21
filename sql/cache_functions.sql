bee321b9f6c6ff10fe64c5180cf4e7e4cda27060
Erik Nordström <erik@timescale.com>
2017-11-21 22:44:48 +0100

    Refactor and fix cache invalidation
    TimescaleDB cache invalidation happens as a side effect of doing a
    full SQL statement (INSERT/UPDATE/DELETE) on a catalog table (via
    table triggers). However, triggers aren't invoked when using
    PostgreSQL's internal catalog API for updates, since PostgreSQL's
    catalog tables don't have triggers that require full statement
    parsing, planning, and execution.
    
    Since we are now using the regular PostgreSQL catalog update API for
    some TimescaleDB catalog operations, we need to do cache invalidation
    also on such operations.
    
    This change adds cache invalidation when updating catalogs using the
    internal (C) API and also makes the cache invalidation more fine
    grained. For instance, caches are no longer invalidated on some
    INSERTS that do not affect the validity of objects already in the
    cache, such as adding a new chunk.

--- sql/cache_functions.sql
+++ sql/cache_functions.sql
@@ -1,10 +1,8 @@
 -- this trigger function causes an invalidation event on the table whose name is
 -- passed in as the first element.
 CREATE OR REPLACE FUNCTION _timescaledb_cache.invalidate_relcache_trigger()
- RETURNS TRIGGER AS '$libdir/timescaledb', 'invalidate_relcache_trigger' LANGUAGE C;
+RETURNS TRIGGER AS '$libdir/timescaledb', 'invalidate_relcache_trigger' LANGUAGE C STRICT;
 
 -- This function is only used for debugging
-CREATE OR REPLACE FUNCTION _timescaledb_cache.invalidate_relcache(proxy_oid OID)
- RETURNS BOOLEAN AS '$libdir/timescaledb', 'invalidate_relcache' LANGUAGE C;
-
-
+CREATE OR REPLACE FUNCTION _timescaledb_cache.invalidate_relcache(catalog_table REGCLASS)
+RETURNS BOOLEAN AS '$libdir/timescaledb', 'invalidate_relcache' LANGUAGE C STRICT;
