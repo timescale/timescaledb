717299f9e10f85376b7c5652d439a345cd813236
Matvey Arye <mat@timescale.com>
2017-11-24 21:37:06 -0500

    Change time handling in drop_chunks for TIMESTAMP times
    This PR fixes the handling of drop_chunks when the hypertable's
    time field is a TIMESTAMP or DATE field. Previously, such
    hypertables needed drop_chunks to be given a timestamptz in UTC.
    Now, drop_chunks can take a DATE or TIMESTAMP. Also, the INTERVAL
    version of drop_chunks correctly handles these cases.
    
    A consequence of this change is that drop_chunks cannot be called
    on multiple tables (with table_name = NULL or schema_name = NULL)
    if the tables have different time column types.

--- sql/updates/pre-0.7.0--0.8.0-dev.sql
+++ sql/updates/pre-0.7.0--0.8.0-dev.sql
@@ -1,2 +1,5 @@
 DROP FUNCTION _timescaledb_internal.create_hypertable_row(REGCLASS, NAME, NAME, NAME, NAME, INTEGER, NAME, NAME, BIGINT, NAME, REGPROC);
 DROP FUNCTION _timescaledb_internal.rename_hypertable(NAME, NAME, NAME, NAME);
+
+DROP FUNCTION drop_chunks(bigint,name,name,boolean);
+DROP FUNCTION drop_chunks(timestamptz,name,name,boolean);
