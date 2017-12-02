0509ca622092977fdc5ff7641131cd39918e9c02
jwdeitch <jwdeitch@gmail.com>
2017-12-01 22:20:16 -0500

    [PG10] Prevent native partitioning attachment of hypertables
    - Raise an exception upon an attach partition event on a hypertable to a native postgres partition

--- src/process_utility.c
+++ src/process_utility.c
@@ -692,13 +692,10 @@ process_rename(Node *parsetree)
 static void
 process_altertable_change_owner(Hypertable *ht, AlterTableCmd *cmd)
 {
-	RoleSpec   *role;
-
 	Assert(IsA(cmd->newowner, RoleSpec));
-	role = (RoleSpec *) cmd->newowner;
 
 	process_utility_set_expect_chunk_modification(true);
-	process_change_hypertable_owner(ht, role->rolename);
+	process_change_hypertable_owner(ht, cmd->newowner->rolename);
 	process_utility_set_expect_chunk_modification(false);
 }
 
@@ -1369,6 +1366,22 @@ process_altertable_end_subcmd(Hypertable *ht, Node *parsetree, ObjectAddress *ob
 			Assert(IsA(cmd->def, ColumnDef));
 			process_alter_column_type_end(ht, cmd);
 			break;
+#if PG10
+		case AT_AttachPartition:
+		{
+			RangeVar   *relation;
+			PartitionCmd  *stmt;
+			stmt = (PartitionCmd *) cmd->def;
+			relation = stmt->name;
+			Assert(NULL != relation);
+
+			if (InvalidOid != hypertable_relid(relation)) {
+				ereport(ERROR,
+						(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+								errmsg("Hypertables do not support native postgres partitioning")));
+			}
+		}
+#endif
 		default:
 			break;
 	}
@@ -1417,19 +1430,16 @@ process_altertable_end_table(Node *parsetree, CollectedCommand *cmd)
 
 	ht = hypertable_cache_get_entry(hcache, relid);
 
-	if (NULL != ht)
+	switch (cmd->type)
 	{
-		switch (cmd->type)
-		{
-			case SCT_Simple:
-				process_altertable_end_simple_cmd(ht, cmd);
-				break;
-			case SCT_AlterTable:
-				process_altertable_end_subcmds(ht, cmd->d.alterTable.subcmds);
-				break;
-			default:
-				break;
-		}
+		case SCT_Simple:
+			process_altertable_end_simple_cmd(ht, cmd);
+			break;
+		case SCT_AlterTable:
+			process_altertable_end_subcmds(ht, cmd->d.alterTable.subcmds);
+			break;
+		default:
+			break;
 	}
 
 	cache_release(hcache);
