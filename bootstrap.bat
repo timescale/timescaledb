55d87f3feaa1f21d36af5f44a730d1fd04cee758
Rob Kiefer <rob@timescale.com>
2018-01-04 10:59:33 -0500

    Bring Windows bootstrap in line with Unix version.
    Flags are now passed on to CMake, the source directory is correctly
    resolved, and support for removing the old build directory added.
    
    Additionally this will limit the VS solution file to only having
    Release targets unless overriden with a flag.

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,20 +1,30 @@
-echo off
-SET BUILD_TYPE=%1
-IF "%BUILD_TYPE%" == "" (SET BUILD_TYPE=Release)
+@echo off
+:: This bootstrap scripts set up the build environment for TimescaleDB
+:: Any flags will be passed on to CMake, e.g.,
+:: ./bootstrap.bat -DCMAKE_BUILD_TYPE="Debug"
 
-SET BUILD_ARCH=%2
-IF "%BUILD_ARCH%" == "" (SET BUILD_ARCH=x64)
+
+:: Get source directory to build from
+set ORIG=%0
+for %%F in (%ORIG%) do set SRC_DIR=%%~dpF
 
 SET BUILD_DIR=./build
-IF NOT EXIST "%BUILD_DIR%" (
-	mkdir "%BUILD_DIR%"
-) ELSE (
+
+IF EXIST "%BUILD_DIR%" (
+	setlocal EnableDelayedExpansion
 	ECHO Build system already initialized in %BUILD_DIR%
-	EXIT
+	SET /P resp="Do you want to remove it (this is IMMEDIATE and PERMANENT), y/n? "
+	IF "!resp!" == "y" (
+		rd /s /q "%BUILD_DIR%"
+	) ELSE (
+		ECHO Exiting
+		EXIT
+	)
 )
 
-cd %BUILD_DIR%
-cmake .. -A %BUILD_ARCH% -B. -DCMAKE_BUILD_TYPE=%BUILD_TYPE%
+mkdir "%BUILD_DIR%"
+cd "%BUILD_DIR%"
+cmake %SRC_DIR% -B. -A x64 %*
 
 ECHO ---
 ECHO TimescaleDB build system initialized in %BUILD_DIR%.
