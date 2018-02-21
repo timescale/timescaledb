d298a02eb78c5d78635c1e7f2323ac7232fcbefb
Erik Nordström <erik@timescale.com>
2018-02-21 17:53:43 +0100

    Remove unused cache invalidation triggers
    The cache invalidation triggers on our catalog tables
    aren't used anymore as all modifications to catalog tables
    happen using the C API, which doesn't invoke triggers and
    has its own cache invalidation functionality.

--- sql/cache_functions.sql
+++
@@ -1,4 +0,0 @@
--- This function is only used for debugging
-CREATE OR REPLACE FUNCTION _timescaledb_cache.invalidate_relcache(catalog_table REGCLASS)
-RETURNS BOOLEAN AS '@MODULE_PATHNAME@', 'invalidate_relcache' LANGUAGE C STRICT;
-
