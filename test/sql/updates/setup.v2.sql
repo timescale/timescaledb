0f895ed66eedd42528bee24448b7e4a140a33fd7
Erik Nordström <erik@timescale.com>
2018-02-28 16:22:54 +0100

    Fix update to 0.9.0
    This fixes a bug in the upgrade script to 0.9.0 that was caused by a
    bad query to delete orphaned dimension slices.

--- test/sql/updates/setup.v2.sql
+++ test/sql/updates/setup.v2.sql
@@ -15,6 +15,7 @@ CREATE TABLE PUBLIC."two_Partitions" (
   series_1 DOUBLE PRECISION NULL,
   series_2 DOUBLE PRECISION NULL,
   series_bool BOOLEAN NULL,
+  PRIMARY KEY("timeCustom", device_id),
   UNIQUE("timeCustom", device_id, series_2)
 );
 CREATE INDEX ON PUBLIC."two_Partitions" (device_id, "timeCustom" DESC NULLS LAST) WHERE device_id IS NOT NULL;
@@ -28,16 +29,17 @@ SELECT * FROM create_hypertable('"public"."two_Partitions"'::regclass, 'timeCust
 
 INSERT INTO devices(id,floor) VALUES
 ('dev1', 1),
-('dev2', 2);
+('dev2', 2),
+('dev3', 3);
 
 INSERT INTO public."two_Partitions"("timeCustom", device_id, series_0, series_1, series_2) VALUES
-(1257987600000000000, 'dev1', 1.5, 1, 1),
 (1257987600000000000, 'dev1', 1.5, 2, 2),
 (1257894000000000000, 'dev2', 1.5, 1, 3),
+(1257987600000000000, 'dev3', 1.5, 1, 1),
 (1257894002000000000, 'dev1', 2.5, 3, 4);
 
 INSERT INTO "two_Partitions"("timeCustom", device_id, series_0, series_1, series_2) VALUES
-(1257894000000000000, 'dev2', 1.5, 2, 6);
+(1257894100000000000, 'dev2', 1.5, 2, 6);
 
 CREATE TABLE PUBLIC.hyper_timestamp (
   time timestamp NOT NULL,
@@ -48,13 +50,17 @@ CREATE TABLE PUBLIC.hyper_timestamp (
    ) WHERE (value > 0)
 );
 
-SELECT * FROM create_hypertable('hyper_timestamp'::regclass, 'time'::name, 'device_id'::name, number_partitions => 2, 
+SELECT * FROM create_hypertable('hyper_timestamp'::regclass, 'time'::name, 'device_id'::name, number_partitions => 2,
     chunk_time_interval=> _timescaledb_internal.interval_to_usec('1 minute'));
 
 --some old versions use more slice_ids than newer ones. Make this uniform
 ALTER SEQUENCE _timescaledb_catalog.dimension_slice_id_seq RESTART WITH 100;
 
-INSERT INTO hyper_timestamp VALUES 
+INSERT INTO hyper_timestamp VALUES
 ('2017-01-20T09:00:01', 'dev1', 1),
 ('2017-01-20T08:00:01', 'dev2', 2),
 ('2016-01-20T09:00:01', 'dev1', 3);
+
+-- DROP some chunks to test metadata cleanup
+DROP TABLE _timescaledb_internal._hyper_1_2_chunk;
+DROP TABLE _timescaledb_internal._hyper_1_3_chunk;
