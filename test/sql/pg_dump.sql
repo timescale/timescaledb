498fb612f692c2ddcb1f532376928db3d65d708b
Rob Kiefer <rob@timescale.com>
2018-03-01 16:46:23 -0500

    Make pg_dump regression test more robust
    Prior to PostgreSQL 10.3, the output for a trigger function did
    not include the implied 'public' schema in its output when
    outputting the function. In 10.3 this was changed causing the
    regression test to fail. To make this more robust, the table is
    now put into an explicit schema so the test does not rely on
    the inconsistent behavior across versions as to printing out
    'public'.

--- test/sql/pg_dump.sql
+++ test/sql/pg_dump.sql
@@ -1,9 +1,13 @@
 \o /dev/null
 \ir include/insert_two_partitions.sql
 \o
+\c single :ROLE_SUPERUSER
+CREATE SCHEMA test_schema AUTHORIZATION :ROLE_DEFAULT_PERM_USER;
+\c single
+ALTER TABLE PUBLIC."two_Partitions" SET SCHEMA "test_schema";
 
 -- Test that we can restore constraints
-ALTER TABLE PUBLIC."two_Partitions"
+ALTER TABLE "test_schema"."two_Partitions"
 ADD CONSTRAINT timeCustom_device_id_series_2_key
 UNIQUE ("timeCustom", device_id, series_2);
 
@@ -16,7 +20,7 @@ BEGIN
 END
 $BODY$;
 
-CREATE TRIGGER restore_trigger BEFORE INSERT ON PUBLIC."two_Partitions"
+CREATE TRIGGER restore_trigger BEFORE INSERT ON "test_schema"."two_Partitions"
 FOR EACH ROW EXECUTE PROCEDURE test_trigger();
 
 SELECT count(*)
@@ -24,16 +28,16 @@ SELECT count(*)
  WHERE refclassid = 'pg_extension'::regclass
      AND refobjid = (SELECT oid FROM pg_extension WHERE extname = 'timescaledb');
 
-SELECT * FROM test.show_columns('public."two_Partitions"');
+SELECT * FROM test.show_columns('"test_schema"."two_Partitions"');
 SELECT * FROM test.show_columns('_timescaledb_internal._hyper_1_1_chunk');
-SELECT * FROM test.show_indexes('public."two_Partitions"');
+SELECT * FROM test.show_indexes('"test_schema"."two_Partitions"');
 SELECT * FROM test.show_indexes('_timescaledb_internal._hyper_1_1_chunk');
-SELECT * FROM test.show_constraints('public."two_Partitions"');
+SELECT * FROM test.show_constraints('"test_schema"."two_Partitions"');
 SELECT * FROM test.show_constraints('_timescaledb_internal._hyper_1_1_chunk');
-SELECT * FROM test.show_triggers('public."two_Partitions"');
+SELECT * FROM test.show_triggers('"test_schema"."two_Partitions"');
 SELECT * FROM test.show_triggers('_timescaledb_internal._hyper_1_1_chunk');
 
-SELECT * FROM "two_Partitions" ORDER BY "timeCustom", device_id;
+SELECT * FROM "test_schema"."two_Partitions" ORDER BY "timeCustom", device_id;
 SELECT * FROM _timescaledb_internal._hyper_1_1_chunk ORDER BY "timeCustom", device_id;
 SELECT * FROM _timescaledb_internal._hyper_1_2_chunk ORDER BY "timeCustom", device_id;
 
@@ -58,17 +62,17 @@ SELECT count(*)
      AND refobjid = (SELECT oid FROM pg_extension WHERE extname = 'timescaledb');
 
 --main table and chunk schemas should be the same
-SELECT * FROM test.show_columns('public."two_Partitions"');
+SELECT * FROM test.show_columns('"test_schema"."two_Partitions"');
 SELECT * FROM test.show_columns('_timescaledb_internal._hyper_1_1_chunk');
-SELECT * FROM test.show_indexes('public."two_Partitions"');
+SELECT * FROM test.show_indexes('"test_schema"."two_Partitions"');
 SELECT * FROM test.show_indexes('_timescaledb_internal._hyper_1_1_chunk');
-SELECT * FROM test.show_constraints('public."two_Partitions"');
+SELECT * FROM test.show_constraints('"test_schema"."two_Partitions"');
 SELECT * FROM test.show_constraints('_timescaledb_internal._hyper_1_1_chunk');
-SELECT * FROM test.show_triggers('public."two_Partitions"');
+SELECT * FROM test.show_triggers('"test_schema"."two_Partitions"');
 SELECT * FROM test.show_triggers('_timescaledb_internal._hyper_1_1_chunk');
 
 --data should be the same
-SELECT * FROM "two_Partitions" ORDER BY "timeCustom", device_id;
+SELECT * FROM "test_schema"."two_Partitions" ORDER BY "timeCustom", device_id;
 SELECT * FROM _timescaledb_internal._hyper_1_1_chunk ORDER BY "timeCustom", device_id;
 SELECT * FROM _timescaledb_internal._hyper_1_2_chunk ORDER BY "timeCustom", device_id;
 
@@ -76,8 +80,8 @@ SELECT * FROM _timescaledb_catalog.chunk_index;
 SELECT * FROM _timescaledb_catalog.chunk_constraint;
 
 --check simple ddl still works
-ALTER TABLE "two_Partitions" ADD COLUMN series_3 integer;
-INSERT INTO "two_Partitions"("timeCustom", device_id, series_0, series_1, series_3) VALUES
+ALTER TABLE "test_schema"."two_Partitions" ADD COLUMN series_3 integer;
+INSERT INTO "test_schema"."two_Partitions"("timeCustom", device_id, series_0, series_1, series_3) VALUES
 (1357894000000000000, 'dev5', 1.5, 2, 4);
 
 --query for the extension tables/sequences that will not be dumped by pg_dump (should be empty except for views)
