fc056377923c3e827472e64abbf17245733b4a7c
Matvey Arye <mat@timescale.com>
2018-03-14 17:01:23 -0400

    Add no preload error to versioned library.
    Previously, only the loader would complain if it was dynamically
    loaded after preload. This PR adds a FATAL error message if the
    versioned .so is not preloaded AND the loader has not been previously
    loaded. This is a common case if the user forgets to edit
    shared_preload_libraries to add the loader.
    
    This PR also fixes the error in cases when it's encountered by a
    non-superuser.

--- src/extension_utils.c
+++ src/extension_utils.c
@@ -13,9 +13,11 @@
 #include <commands/extension.h>
 #include <access/relscan.h>
 #include <catalog/pg_extension.h>
+#include <catalog/pg_authid.h>
 #include <utils/fmgroids.h>
 #include <utils/builtins.h>
 #include <utils/rel.h>
+#include <utils/guc.h>
 #include <catalog/indexing.h>
 
 #include "extension.h"
@@ -23,6 +25,8 @@
 #define EXTENSION_PROXY_TABLE "cache_inval_extension"
 #define CACHE_SCHEMA_NAME "_timescaledb_cache"
 #define MAX_SO_NAME_LEN NAMEDATALEN+NAMEDATALEN+1+1 /* extname+"-"+version */
+
+#define GUC_LOADER_PRESENT_NAME "timescaledb.loader_present"
 
 enum ExtensionState
 {
@@ -171,4 +175,57 @@ extension_current_state()
 	}
 
 	return EXTENSION_STATE_NOT_INSTALLED;
+}
+
+static void
+extension_load_without_preload()
+{
+	/* cannot use GUC variable here since extension not yet loaded */
+	char	   *allow_install_without_preload = GetConfigOptionByName("timescaledb.allow_install_without_preload", NULL, true);
+
+	if (allow_install_without_preload == NULL ||
+		strcmp(allow_install_without_preload, "on") != 0)
+	{
+		/*
+		 * These are FATAL because otherwise the loader ends up in a weird
+		 * half-loaded state after an ERROR
+		 */
+		/* Only privileged users can get the value of `config file` */
+#if PG10
+		if (is_member_of_role(GetUserId(), DEFAULT_ROLE_READ_ALL_SETTINGS))
+#else
+		if (superuser())
+#endif
+		{
+			char	   *config_file = GetConfigOptionByName("config_file", NULL, false);
+
+			ereport(FATAL,
+					(errmsg("The timescaledb library is not preloaded"),
+					 errhint("Please preload the timescaledb library via shared_preload_libraries.\n\n"
+							 "This can be done by editing the config file at: %1$s\n"
+							 "and adding 'timescaledb' to the list in the shared_preload_libraries config.\n"
+							 "	# Modify postgresql.conf:\n	shared_preload_libraries = 'timescaledb'\n\n"
+							 "Another way to do this, if not preloading other libraries, is with the command:\n"
+							 "	echo \"shared_preload_libraries = 'timescaledb'\" >> %1$s \n\n"
+							 "(Will require a database restart.)\n\n"
+							 "If you REALLY know what you are doing and would like to load the library without preloading, you can disable this check with: \n"
+							 "	SET timescaledb.allow_install_without_preload = 'on';", config_file)));
+		}
+		else
+		{
+			ereport(FATAL,
+					(errmsg("The timescaledb library is not preloaded"),
+					 errhint("Please preload the timescaledb library via shared_preload_libraries.\n\n"
+							 "This can be done by editing the postgres config file \n"
+							 "and adding 'timescaledb' to the list in the shared_preload_libraries config.\n"
+							 "	# Modify postgresql.conf:\n	shared_preload_libraries = 'timescaledb'\n\n"
+							 "Another way to do this, if not preloading other libraries, is with the command:\n"
+							 "	echo \"shared_preload_libraries = 'timescaledb'\" >> /path/to/config/file \n\n"
+							 "(Will require a database restart.)\n\n"
+							 "If you REALLY know what you are doing and would like to load the library without preloading, you can disable this check with: \n"
+							 "	SET timescaledb.allow_install_without_preload = 'on';")));
+		}
+
+		return;
+	}
 }
--- src/loader/loader.c
+++ src/loader/loader.c
@@ -212,33 +212,21 @@ post_analyze_hook(ParseState *pstate, Query *query)
 	}
 }
 
+static void
+extension_mark_loader_present()
+{
+	SetConfigOption(GUC_LOADER_PRESENT_NAME, "on", PGC_USERSET, PGC_S_SESSION);
+}
+
 void
 _PG_init(void)
 {
 	if (!process_shared_preload_libraries_in_progress)
 	{
-		/* cannot use GUC variable here since extension not yet loaded */
-		char	   *allow_install_without_preload = GetConfigOptionByName("timescaledb.allow_install_without_preload", NULL, true);
-
-		if (allow_install_without_preload == NULL ||
-			strcmp(allow_install_without_preload, "on") != 0)
-		{
-			char	   *config_file = GetConfigOptionByName("config_file", NULL, false);
-
-			ereport(ERROR,
-					(errmsg("The timescaledb library is not preloaded"),
-					 errhint("Please preload the timescaledb library via shared_preload_libraries.\n\n"
-							 "This can be done by editing the config file at: %1$s\n"
-							 "and adding 'timescaledb' to the list in the shared_preload_libraries config.\n"
-							 "	# Modify postgresql.conf:\n	shared_preload_libraries = 'timescaledb'\n\n"
-							 "Another way to do this, if not preloading other libraries, is with the command:\n"
-							 "	echo \"shared_preload_libraries = 'timescaledb'\" >> %1$s \n\n"
-							 "(Will require a database restart.)\n\n"
-							 "If you REALLY know what you are doing and would like to load the library without preloading, you can disable this check with: \n"
-							 "	SET timescaledb.allow_install_without_preload = 'on';", config_file)));
-			return;
-		}
+		extension_load_without_preload();
 	}
+	extension_mark_loader_present();
+
 	elog(INFO, "timescaledb loaded");
 
 	/* This is a safety-valve variable to prevent loading the full extension */
