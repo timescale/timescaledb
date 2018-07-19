a7c5a44ca3806e1dcb6175fdff59c14f86a9619b
Matvey Arye <mat@timescale.com>
2018-07-19 12:09:26 -0400

    Update statistics on parent table when doing ANALYZE
    Previously, when running a VACUUM ANALYZE or ANALYZE on
    a hypertable the statics on the parent table (hypertable)
    were not correctly updated. This fixes that problem
    and adds tests.

--- test/sql/vacuum.sql
+++ test/sql/vacuum.sql
@@ -15,11 +15,55 @@ SELECT tablename, attname, histogram_bounds, n_distinct FROM pg_stats
 WHERE schemaname = '_timescaledb_internal' AND tablename LIKE '_hyper_%_chunk'
 ORDER BY schemaname, tablename;
 
+SELECT tablename, attname, histogram_bounds, n_distinct FROM pg_stats
+WHERE schemaname = 'public' AND tablename LIKE 'vacuum_test'
+ORDER BY schemaname, tablename;
+
 VACUUM (VERBOSE, ANALYZE) vacuum_test;
 
 -- stats should exist for all three chunks
 SELECT tablename, attname, histogram_bounds, n_distinct FROM pg_stats
 WHERE schemaname = '_timescaledb_internal' AND tablename LIKE '_hyper_%_chunk'
+ORDER BY schemaname, tablename;
+
+-- stats should exist on parent hypertable
+SELECT tablename, attname, histogram_bounds, n_distinct FROM pg_stats
+WHERE schemaname = 'public' AND tablename LIKE 'vacuum_test'
+ORDER BY schemaname, tablename;
+
+DROP TABLE vacuum_test;
+
+--test plain analyze (no_vacuum)
+CREATE TABLE analyze_test(time timestamp, temp float);
+
+SELECT create_hypertable('analyze_test', 'time', chunk_time_interval => 2628000000000);
+
+INSERT INTO analyze_test VALUES ('2017-01-20T16:00:01', 17.5),
+                               ('2017-01-21T16:00:01', 19.1),
+                               ('2017-04-20T16:00:01', 89.5),
+                               ('2017-04-21T16:00:01', 17.1),
+                               ('2017-06-20T16:00:01', 18.5),
+                               ('2017-06-21T16:00:01', 11.0);
+
+-- no stats
+SELECT tablename, attname, histogram_bounds, n_distinct FROM pg_stats
+WHERE schemaname = '_timescaledb_internal' AND tablename LIKE '_hyper_%_chunk'
+ORDER BY schemaname, tablename;
+
+SELECT tablename, attname, histogram_bounds, n_distinct FROM pg_stats
+WHERE schemaname = 'public' AND tablename LIKE 'analyze_test'
+ORDER BY schemaname, tablename;
+
+ANALYZE VERBOSE analyze_test;
+
+-- stats should exist for all three chunks
+SELECT tablename, attname, histogram_bounds, n_distinct FROM pg_stats
+WHERE schemaname = '_timescaledb_internal' AND tablename LIKE '_hyper_%_chunk'
+ORDER BY schemaname, tablename;
+
+-- stats should exist on parent hypertable
+SELECT tablename, attname, histogram_bounds, n_distinct FROM pg_stats
+WHERE schemaname = 'public' AND tablename LIKE 'analyze_test'
 ORDER BY schemaname, tablename;
 
 -- Run vacuum on a normal (non-hypertable) table
