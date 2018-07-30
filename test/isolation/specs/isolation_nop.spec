790d423a889d21f83d0868d52edf3e5695c95ec7
Joshua Lockerman <>
2018-07-30 12:08:14 -0400

    Add pg_isolation_regress support to the timescale build system
    Postgres has a tool pg_isolation_regress which allows testing the behavior of
    concurrent commands. Despite not being installed by default, the tool is still
    build with the rest of postgres, and can run outside of the postgres src
    directory. This commit adds hooks within the timescale build system to call
    pg_isolation_regress from the postgres source tree, and run timescale tests
    using it.
    
    This commit also adds some basic tests for the behavior of concurrent INSERTS
    that create chunks.

---
+++ test/isolation/specs/isolation_nop.spec
@@ -0,0 +1,11 @@
+setup{
+    CREATE TABLE ts_cluster_test(time timestamptz, temp float, location int);
+    SELECT create_hypertable('ts_cluster_test', 'time', chunk_time_interval => interval '1 day');
+}
+
+teardown {
+    DROP TABLE ts_cluster_test;
+}
+
+session "s1"
+step "s1a"	{ SELECT pg_sleep(0); }
--- test/pg_isolation_regress.sh
+++ test/pg_isolation_regress.sh
@@ -3,34 +3,34 @@
 # Wrapper around pg_regress to be able to override the tests to run via the
 # TESTS environment variable
 
-# NB this script mirrors the adjacent pg_isolation_regress.sh, and they should
+# NB this script mirrors the adjacent pg_regress.sh, and they should
 #    kept in synch
 
 EXE_DIR=$(dirname $0)
-PG_REGRESS=${PG_REGRESS:-pg_regress}
-TEST_SCHEDULE=${TEST_SCHEDULE:-}
+PG_ISOLATION_REGRESS=${PG_ISOLATION_REGRESS:-pg_isolation_regress}
+ISOLATION_TEST_SCHEDULE=${ISOLATION_TEST_SCHEDULE:-}
 TESTS=${TESTS:-}
 
 if [[ -z ${TESTS} ]]; then
-    if [[ -z ${TEST_SCHEDULE} ]]; then
-        for t in ${EXE_DIR}/sql/*.sql; do
-            t=${t##${EXE_DIR}/sql/}
-            t=${t%.sql}
+    if [[ -z ${ISOLATION_TEST_SCHEDULE} ]]; then
+        for t in ${EXE_DIR}/isolation/specs/*.spec; do
+            t=${t##${EXE_DIR}/isolation/specs/}
+            t=${t%.spec}
             TESTS="${TESTS} $t"
         done
     else
-        PG_REGRESS_OPTS="${PG_REGRESS_OPTS} --schedule=${TEST_SCHEDULE}"
+        PG_ISOLATION_REGRESS_OPTS="${PG_ISOLATION_REGRESS_OPTS} --schedule=${ISOLATION_TEST_SCHEDULE}"
     fi
 else
-    # Both this and pg_isolation_regress.sh use the same TESTS env var to decide which tests to run.
+    # Both this and pg_regress.sh use the same TESTS env var to decide which tests to run.
     # Since we only want to pass the test runner the kind of tests it can understand,
     # and only those which actually exist, we use TESTS as a filter for the test folder,
     # passing in only those tests from the directory which are found in TESTS
     FILTER=${TESTS}
     TESTS=
-    for t in ${EXE_DIR}/sql/*.sql; do
-        t=${t##${EXE_DIR}/sql/}
-        t=${t%.sql}
+    for t in ${EXE_DIR}/isolation/specs/*.spec; do
+        t=${t##${EXE_DIR}/isolation/specs/}
+        t=${t%.spec}
         # we use the following chain of comparisons to properly handle the case
         # where a test name is a substring of another
         if [[ $FILTER = "$t" ]]; then # one test
@@ -45,4 +45,4 @@ else
     done
 fi
 
-${PG_REGRESS} $@ ${PG_REGRESS_OPTS} ${TESTS}
+${PG_ISOLATION_REGRESS} $@ ${PG_ISOLATION_REGRESS_OPTS} ${TESTS}
