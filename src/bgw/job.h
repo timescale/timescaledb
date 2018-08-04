6770695e1e0fe51aefee727c9aa1254f6e932ea2
Matvey Arye <mat@timescale.com>
2018-08-03 20:02:43 -0400

    Add a scheduler for background jobs
    TimescaleDB will want to run multiple background jobs. This PR
    adds a simple scheduler so that jobs inserted into a jobs table
    could be run on a schedule. This first implementation has two limitations:
    
    1) The list of jobs to be run is read from the database when the scheduler
    is first started. We do not update this list if the jobs table changes.
    
    2) There is no prioritization for when to run jobs.
    
    There design of the scheduler is as follows:
    The scheduler itself is a background job that continuously runs and waits
    for a time when  jobs need to be scheduled. It then launches jobs as new
    background workers that it controls through the background worker handle.
    
    Aggregate statistics about a job are kept in the job_stat catalog table.
    These statistics include the start and finish times of the last run of the job
    as well as whether or not the job succeeded. The next_start is used to
    figure out when next to run a job after a scheduler is restarted.
    
    The statistics table also tracks consecutive failures and crashes for the job
    which is used for calculating the exponential backoff after a crash or failure
    (which is used to set the next_start after the crash/failure). Note also that
    there is a minimum time after the db scheduler starts up and a crashed job
    is restarted. This is to allow the operator enough time to disable the job
    if needed.
    
    Note that the number of crashes is an overestimate of the actual number of crashes
    for a job. This is so that we are conservative and never miss a crash and fail to
    use the appropriate backoff logic.  Note that there is some complexity
    in ensuring that all crashes are counted since a crash in Postgres causes /all/
    processes to SIGQUIT: we must commit changes to the stats
    table /before/ a job starts so that we can then deduce after a job has crashed
    and the scheduler comes back up that a job was started, and not finished before
    the crash (meaning that it could have been the crashing process).

---
+++ src/bgw/job.h
@@ -0,0 +1,41 @@
+#ifndef BGW_JOB_H
+#define BGW_JOB_H
+
+#include "catalog.h"
+#include <postmaster/bgworker.h>
+
+typedef enum JobType
+{
+	JOB_TYPE_VERSION_CHECK = 0,
+	JOB_TYPE_UNKNOWN,
+	_MAX_JOB_TYPE
+} JobType;
+
+typedef struct BgwJob
+{
+	FormData_bgw_job fd;
+	JobType		bgw_type;
+
+} BgwJob;
+
+typedef bool (*unknown_job_type_hook_type) (BgwJob *job);
+
+BackgroundWorkerHandle *bgw_start_worker(const char *function, const char *name, const char *extra);
+
+BackgroundWorkerHandle *bgw_job_start(BgwJob *job);
+
+extern List *bgw_job_get_all(size_t alloc_size, MemoryContext mctx);
+
+extern BgwJob *bgw_job_find(int job_id, MemoryContext mctx);
+
+extern bool bgw_job_has_timeout(BgwJob *job);
+extern TimestampTz bgw_job_timeout_at(BgwJob *job, TimestampTz start_time);
+
+
+bool		bgw_job_execute(BgwJob *job);
+
+PGDLLEXPORT extern Datum ts_bgw_job_entrypoint(PG_FUNCTION_ARGS);
+extern void bgw_job_set_unknown_job_type_hook(unknown_job_type_hook_type hook);
+extern void bgw_job_set_job_entrypoint_function_name(char *func_name);
+
+#endif							/* BGW_JOB_H */
---
+++ src/bgw/scheduler.h
@@ -0,0 +1,18 @@
+#ifndef BGW_SCHEDULER_H
+#define BGW_SCHEDULER_H
+#include <postgres.h>
+#include <fmgr.h>
+#include "compat.h"
+#include <postmaster/bgworker.h>
+
+#include "timer.h"
+
+/* callback used in testing */
+typedef void (*register_background_worker_callback_type) (BackgroundWorkerHandle *);
+
+void		bgw_scheduler_process(int32 run_for_interval_ms, register_background_worker_callback_type bgw_register);
+
+/* exposed for access by mock */
+void		bgw_scheduler_setup_callbacks(void);
+
+#endif							/* BGW_SCHEDULER_H */
