0dc5bbb447869b67fe9403f6419b69e1c2c8ccb0
didier <didier@users.sourceforge.net>
2018-09-10 19:52:49 +0200

    Use pg_config bindir directory for pg executables
    In case pg_dump, createdb, dropdb and pg_restore aren't in system default path.

--- test/sql/utils/pg_dump_aux_dump.sh
+++ test/sql/utils/pg_dump_aux_dump.sh
@@ -1,3 +1,3 @@
-pg_dump -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} -Fc single > dump/single.sql
-dropdb -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} single
-createdb -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} single
+${PG_BINDIR}/pg_dump -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} -Fc single > dump/single.sql
+${PG_BINDIR}/dropdb -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} single
+${PG_BINDIR}/createdb -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} single
--- test/sql/utils/pg_dump_aux_restore.sh
+++ test/sql/utils/pg_dump_aux_restore.sh
@@ -1,1 +1,1 @@
-pg_restore -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} -d single dump/single.sql
+${PG_BINDIR}/pg_restore -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} -d single dump/single.sql
