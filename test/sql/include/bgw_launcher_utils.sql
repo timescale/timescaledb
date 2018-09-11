acebaea40c5feb0d5584b55d13067cdfed03cc94
David Kohn <david@timescale.com>
2018-09-11 12:47:51 -0400

    Don't start schedulers for template databases.

---
+++ test/sql/include/bgw_launcher_utils.sql
@@ -0,0 +1,34 @@
+/*
+ * Note on testing: need a couple wrappers that pg_sleep in a loop to wait for changes
+ * to appear in pg_stat_activity.
+ * Further Note: PG 9.6 changed what appeared in pg_stat_activity, so the launcher doesn't actually show up. 
+ * we can still test its interactions with its children, but can't test some of the things specific to the launcher. 
+ * So we've added some bits about the version number as needed. 
+ */
+
+CREATE VIEW worker_counts as SELECT count(*) filter (WHERE application_name = 'TimescaleDB Background Worker Launcher') as launcher,
+count(*) filter (WHERE application_name = 'TimescaleDB Background Worker Scheduler' AND datname = 'single') as single_scheduler,
+count(*) filter (WHERE application_name = 'TimescaleDB Background Worker Scheduler' AND datname = 'single_2') as single_2_scheduler, 
+count(*) filter (WHERE application_name = 'TimescaleDB Background Worker Scheduler' AND datname = 'template1') as template1_scheduler
+FROM pg_stat_activity;
+
+CREATE FUNCTION wait_worker_counts(launcher_ct INTEGER,  scheduler1_ct INTEGER, scheduler2_ct INTEGER, template1_ct INTEGER) RETURNS BOOLEAN LANGUAGE PLPGSQL AS
+$BODY$
+DECLARE
+r INTEGER;
+BEGIN
+FOR i in 1..10
+LOOP
+SELECT COUNT(*) from worker_counts where (launcher = launcher_ct OR current_setting('server_version_num')::int < 100000) 
+	AND single_scheduler = scheduler1_ct AND single_2_scheduler = scheduler2_ct and template1_scheduler = template1_ct into r;
+if(r < 1) THEN
+  PERFORM pg_sleep(0.1);
+  PERFORM pg_stat_clear_snapshot();
+ELSE
+	--We have the correct counts!
+  RETURN TRUE;
+END IF;
+END LOOP;
+RETURN FALSE;
+END
+$BODY$;
---
+++ test/sql/include/bgw_launcher_utils_cleanup.sql
@@ -0,0 +1,3 @@
+
+DROP FUNCTION wait_worker_counts(integer, integer, integer, integer);
+DROP VIEW worker_counts;
