1a9ae17f992d7ec61fcda5987b536ea95a3b166a
Kevin Bowling <kevin.bowling@kev009.com>
2018-09-11 21:45:51 -0700

    Fix build on FreeBSD wrt sockets

--- src/net/conn_plain.c
+++ src/net/conn_plain.c
@@ -2,6 +2,10 @@
 #include <postgres.h>
 #include <pg_config.h>
 
+#ifndef WIN32
+#include <sys/socket.h>
+#endif
+
 #include "conn_internal.h"
 #include "conn_plain.h"
 
@@ -25,7 +29,7 @@ plain_connect(Connection *conn, const char *host, const char *servname, int port
 	char		strport[6];
 	struct addrinfo *ainfo,
 				hints = {
-		.ai_family = PF_UNSPEC,
+		.ai_family = AF_UNSPEC,
 		.ai_socktype = SOCK_STREAM,
 	};
 	int			ret;
