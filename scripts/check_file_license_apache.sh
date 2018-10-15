18e82be6f7782a55cf55a80862a71d960522f748
Joshua Lockerman <josh@timescale.com>
2018-10-15 17:18:19 -0400

    Add automated license check to travis

---
+++ scripts/check_file_license_apache.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+FLAG=${1}
+FILE=${2}
+SCRIPTPATH="$( cd "$(dirname "${0}")" ; pwd -P )"
+TIMESCALE_LOCATION=`dirname ${SCRIPTPATH}`
+
+LICENSE_FILE=
+LICENSE_STRING=
+FIRST_COMMENT=
+
+if [[ "${FLAG}" == '-c' ]]; then
+    LICENSE_FILE=${SCRIPTPATH}/c_license_header.h
+    LICENSE_STRING=`awk 'BEGIN {ORS="\0"}{if($1 == "*/") {print; exit;}} {print}' ${LICENSE_FILE}`
+    FIRST_COMMENT=`awk 'BEGIN {ORS="\0"}{if($1 == "*/") {print; exit;}} {print}' ${FILE}`
+elif [[ "${FLAG}" == '-s' ]]; then
+    LICENSE_FILE=${SCRIPTPATH}/sql_license.sql
+    LICENSE_STRING=`awk 'BEGIN {ORS="\0"}{if($1 == "") {print; exit;}} {print}' ${SCRIPTPATH}/sql_license.sql`
+    FIRST_COMMENT=`awk 'BEGIN {ORS="\0"}{if($1 == "") {print; exit;}} {print}' ${FILE}`
+elif [[ "${FLAG}" == '-t' ]]; then
+    LICENSE_FILE=${SCRIPTPATH}/test_license.sql
+    LICENSE_STRING=`awk 'BEGIN {ORS="\0"}{if($1 == "") {print; exit;}} {print}' ${SCRIPTPATH}/test_license.sql`
+    FIRST_COMMENT=`awk 'BEGIN {ORS="\0"}{if($1 == "") {print; exit;}} {print}' ${FILE}`
+else
+    echo "Unkown flag" ${1}
+    exit 1;
+fi
+
+if [[ "${FIRST_COMMENT}" != "${LICENSE_STRING}" ]]; then
+    echo ${FILE#"$TIMESCALE_LOCATION/"} "lacks a license header. Add";
+    echo
+    cat ${LICENSE_FILE}
+    echo
+    echo "to the top of the file";
+    exit 1;
+fi
+
+exit 0;
