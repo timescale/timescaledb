9ccda0df0074d1d7cb2b7a18afa4996e14efc9a9
David Kohn <david@timescale.com>
2018-10-17 13:39:45 -0400

    Start stopped workers on restart message
    Modify the restart action to start schedulers if they do not exist, this fixes a
    potential race condition where a scheduler could be started for a given
    database, but before it has shut down (because the extension does not exist) a
    create extension command is run, the start action then would not change the
    state of the worker but it would be waiting on the wrong vxid, so not see that
    the extension exists. This also makes it so the start action can be truly
    idempotent and not set the vxid on its startup, thereby respecting any restart
    action that has taken place before and better defining how each interacts with
    the system.
    
    Additionally, we decided that the previous behavior in which launchers were not
    started up on, say, alter extension update actions was not all that desirable as
    it worked if the stop action had happened, but the database had not restarted,
    if the database restarted, then the stop action would have no effect. We decided
    that if we desire the ability to disable schedulers for a particular database,
    we will implement it in the future as a standalone feature that takes effect
    across server restarts rather than having somewhat ill-defined behavior with an
    implicit feature of the stop action.

--- sql/bgw_scheduler.sql
+++ sql/bgw_scheduler.sql
@@ -13,8 +13,6 @@ RETURNS BOOL
 AS '@LOADER_PATHNAME@', 'ts_bgw_db_workers_start'
 LANGUAGE C VOLATILE;
 
-SELECT _timescaledb_internal.start_background_workers();
-
 INSERT INTO _timescaledb_config.bgw_job (id, application_name, job_type, schedule_INTERVAL, max_runtime, max_retries, retry_period) VALUES
 (1, 'Telemetry Reporter', 'telemetry_and_version_check_if_enabled', INTERVAL '24h', INTERVAL '100s', -1, INTERVAL '1h')
 ON CONFLICT (id) DO NOTHING;
