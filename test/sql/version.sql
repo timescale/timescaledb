3754ae423f8760c0afe5100566c8a47e5ab90f56
Sven Klemm <sven@timescale.com>
2018-10-23 13:24:11 +0200

    Test get_git_commit, get_os_info and get_version returned types

--- test/sql/version.sql
+++ test/sql/version.sql
@@ -1,4 +1,9 @@
--- Just test that the function does not error out as the output
--- will change.
-select count(*) from _timescaledb_internal.get_git_commit();
+-- Test that get_git_commit returns text
+select pg_typeof(git) from _timescaledb_internal.get_git_commit() AS git;
+
+-- Test that get_os_info returns 3 x text
+select pg_typeof(sysname) AS sysname_type,pg_typeof(version) AS version_type,pg_typeof(release) AS release_type from _timescaledb_internal.get_os_info();
+
+-- Test that get_version major, minor and patch are int
+select pg_typeof(major) AS major_type,pg_typeof(minor) AS minor_type,pg_typeof(patch) AS patch_type from _timescaledb_internal.get_version();
 
