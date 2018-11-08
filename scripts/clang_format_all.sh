cc52a78b9bf3b611993f33f93594a2c503e7aeec
Matvey Arye <mat@timescale.com>
2018-11-08 15:52:44 -0500

    Add scripts for clang-format
    We are switching from pgindent to clang-format for several
    reasons:
    1) pgindent is harder to set up and depends on other tools
    like gobjdump that differ between OSes making it harder
    to set up a proper development environment.
    2) clang-format is more standard and works better
    with various IDEs and editors.
    
    Our clang-format wrapper script is transactional in that this
    version of the script copies all files to a temporary directory,
    runs clang-format in the temp dir, and only then copies the files
    back. This should prevent mis-formatting when the script is cancled
    in the middle of running.
    
    We changes travis to use clang-format instead of pgindent
    and thus it uses a different docker image to do the format test.
    We also changes travis to use the new clang docker
    image for the license check and prefix checks for consistency.
    This also paves the way for us to use more clang features for tests
    in the future.

---
+++ scripts/clang_format_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# we need to convert script dir to an absolute path
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+BASE_DIR=$(dirname $SCRIPT_DIR)
+
+find ${BASE_DIR} \( -path "${BASE_DIR}/src/*" -or -path "${BASE_DIR}/test/*" -or -path "${BASE_DIR}/tsl/*" \) \
+    -and \( -name '*.c' -or -name '*.h' \) -print | xargs ${SCRIPT_DIR}/clang_format_wrapper.sh -style=file -i
