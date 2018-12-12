1b41b1499440f775bd0267814d125234c53ec2e6
Amy Tai <amy.tai.2009@gmail.com>
2018-12-11 20:13:16 -0500

    Update licensecheck to look for correct string
    Previously, files under tsl/test/ were expected to have "Apache" rather than "Timescale" license in their license header. This has been fixed.

--- scripts/ts_test_license.sql
+++ scripts/ts_test_license.sql
@@ -1,5 +1,5 @@
 -- Copyright (c) 2016-2018  Timescale, Inc. All Rights Reserved.
 --
--- This file is licensed under the Apache License,
+-- This file is licensed under the Timescale License,
 -- see LICENSE-TIMESCALE at the top of the tsl directory.
 
