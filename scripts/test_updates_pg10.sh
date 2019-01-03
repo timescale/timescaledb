192409257e329fa826db97d1b1ce5bcf2b48350e
Sven Klemm <sven@timescale.com>
2019-01-03 13:02:55 +0100

    Add update test for PostgreSQL 10
    Add an update test for updating the extension on PostgreSQL 10.
    Change PostgreSQL 9.6 update test to only run triggered by cron.

---
+++ scripts/test_updates_pg10.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+SCRIPT_DIR=$(dirname $0)
+
+TAGS="0.7.0-pg10 0.7.1-pg10 0.8.0-pg10 0.9.0-pg10 0.9.1-pg10 0.9.2-pg10 0.10.0-pg10 0.10.1-pg10 0.11.0-pg10 0.12.0-pg10 1.0.0-pg10 1.0.1-pg10 1.1.0-pg10 1.1.1-pg10"
+TEST_VERSION="v2"
+
+. ${SCRIPT_DIR}/test_updates.sh
