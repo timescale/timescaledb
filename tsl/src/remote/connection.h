b89cfff9fc566f686705dc2a4e564a7277d4cb84
Matvey Arye <mat@timescale.com>
2019-01-03 18:54:16 -0500

    Change license header statement
    Update to the new license header statement

--- tsl/src/remote/connection.h
+++ tsl/src/remote/connection.h
@@ -1,8 +1,7 @@
 /*
- * Copyright (c) 2018  Timescale, Inc. All Rights Reserved.
- *
- * This file is licensed under the Timescale License,
- * see LICENSE-TIMESCALE at the top of the tsl directory.
+ * This file and its contents are licensed under the Timescale License.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-TIMESCALE for a copy of the license.
  */
 #ifndef TIMESCALEDB_TSL_REMOTE_CONNECTION_H
 #define TIMESCALEDB_TSL_REMOTE_CONNECTION_H
