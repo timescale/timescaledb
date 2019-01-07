98e14710c6a05de4d7b516d3ae7446e4e869ac46
Sven Klemm <sven@timescale.com>
2019-01-07 12:38:50 +0100

    Execute regression tests in parallel
    Change the schedule generation for the regression tests to generate
    a parallel schedule to improve test runtime. Also moving tablespace
    directory creation to pg_regress.sh to prevent redundant work and
    race conditions between parallel runs.

--- test/runner.sh
+++ test/runner.sh
@@ -7,9 +7,19 @@ EXE_DIR=${EXE_DIR:-${CURRENT_DIR}}
 PG_REGRESS_PSQL=$1
 PSQL=${PSQL:-$PG_REGRESS_PSQL}
 TEST_PGUSER=${TEST_PGUSER:-postgres}
-TEST_DBNAME=${TEST_DBNAME:-single}
 TEST_INPUT_DIR=${TEST_INPUT_DIR:-${EXE_DIR}}
 TEST_OUTPUT_DIR=${TEST_OUTPUT_DIR:-${EXE_DIR}}
+
+# PGAPPNAME will be 'pg_regress/test' so we cut off the prefix
+# to get the name of the test (PG 10 and 11 only)
+if [[ ${PGAPPNAME} = pg_regress/* ]]; then
+  CURRENT_TEST=${PGAPPNAME##pg_regress/}
+else
+  # PG 9.6 pg_regress does not pass in testname
+  # so we generate unique name from pid
+  CURRENT_TEST="test_$$"
+fi
+TEST_DBNAME="db_${CURRENT_TEST}"
 
 # Read the extension version from version.config
 read -r VERSION < ${CURRENT_DIR}/../version.config
@@ -17,9 +27,6 @@ EXT_VERSION=${VERSION##version = }
 
 #docker doesn't set user
 USER=${USER:-`whoami`}
-# This mktemp line will work on both OSX and GNU systems
-TEST_TABLESPACE1_PATH=${TEST_TABLESPACE1_PATH:-$(mktemp -d 2>/dev/null || mktemp -d -t 'timescaledb_regress')}
-TEST_TABLESPACE2_PATH=${TEST_TABLESPACE2_PATH:-$(mktemp -d 2>/dev/null || mktemp -d -t 'timescaledb_regress')}
 
 TEST_SPINWAIT_ITERS=${TEST_SPINWAIT_ITERS:-10}
 
@@ -30,28 +37,26 @@ TEST_ROLE_DEFAULT_PERM_USER_2=${TEST_ROLE_DEFAULT_PERM_USER_2:-default_perm_user
 shift
 
 function cleanup {
-    rm -rf ${TEST_TABLESPACE1_PATH}
-    rm -rf ${TEST_TABLESPACE2_PATH}
+  ${PSQL} $@ -U $TEST_ROLE_SUPERUSER -d postgres -v ECHO=none -c "DROP DATABASE \"${TEST_DBNAME}\";" >/dev/null
 }
 
 trap cleanup EXIT
 
-# Setup directories required by tests
-cd ${EXE_DIR}/sql
-mkdir -p ${TEST_TABLESPACE1_PATH}
-mkdir -p ${TEST_TABLESPACE2_PATH}
-mkdir -p dump
+# setup clusterwide settings on first run
+if [[ ! -f ${TEST_OUTPUT_DIR}/.pg_init ]]; then
+  touch ${TEST_OUTPUT_DIR}/.pg_init
+  ${PSQL} $@ -U ${USER} -d postgres -v ECHO=none -c "ALTER USER ${TEST_ROLE_SUPERUSER} WITH SUPERUSER;" >/dev/null
+fi
 
-# set role permissions and reset database
-${PSQL} $@ -U $USER -d postgres -v ECHO=none -c "ALTER USER $TEST_ROLE_SUPERUSER WITH SUPERUSER;"
-${PSQL} $@ -U $TEST_ROLE_SUPERUSER -d postgres -v ECHO=none -c "DROP DATABASE IF EXISTS \"${TEST_DBNAME}\";" >/dev/null 2>&1 || :
+cd ${EXE_DIR}/sql
+
+# create database and install timescaledb
 ${PSQL} $@ -U $TEST_ROLE_SUPERUSER -d postgres -v ECHO=none -c "CREATE DATABASE \"${TEST_DBNAME}\";"
 ${PSQL} $@ -U $TEST_ROLE_SUPERUSER -d ${TEST_DBNAME} -v ECHO=none -c "set client_min_messages=error; CREATE EXTENSION timescaledb;"
-${PSQL} $@ -U $TEST_ROLE_SUPERUSER -d ${TEST_DBNAME} -v ECHO=none -v MODULE_PATHNAME="'timescaledb-${EXT_VERSION}'" < ${CURRENT_DIR}/sql/utils/testsupport.sql >/dev/null 2>&1 || :
+${PSQL} $@ -U $TEST_ROLE_SUPERUSER -d ${TEST_DBNAME} -v ECHO=none -v MODULE_PATHNAME="'timescaledb-${EXT_VERSION}'" < ${CURRENT_DIR}/sql/utils/testsupport.sql >/dev/null 2>&1
 
 export TEST_DBNAME
 
-#Add the TSL_MODULE_PATHNAME so we can reference C functions in tsl/test/src
 ${PSQL} -U ${TEST_PGUSER} \
      -v ON_ERROR_STOP=1 \
      -v VERBOSITY=terse \
