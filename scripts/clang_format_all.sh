f8c365422d6d47cbb1c05dcb7416a293bcec1a8a
Erik Nordström <erik@timescale.com>
2019-02-07 11:08:00 +0100

    Exclude hidden and CMake directories when formatting code
    The script for running clang-format on the source code now properly
    excludes hidden directories and CMake directories from the source code
    scan. Including these directories may otherwise break clang-format
    because hidden directories are often used for IDE/tools caches (e.g.,
    `cquery`) where the cache files have the same ending as the source
    code.

--- scripts/clang_format_all.sh
+++ scripts/clang_format_all.sh
@@ -5,4 +5,5 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BASE_DIR=$(dirname $SCRIPT_DIR)
 
 find ${BASE_DIR} \( -path "${BASE_DIR}/src/*" -or -path "${BASE_DIR}/test/*" -or -path "${BASE_DIR}/tsl/*" \) \
+    -and -not \( -path "*/.*" -or -path "*CMake*" \) \
     -and \( -name '*.c' -or -name '*.h' \) -print | xargs ${SCRIPT_DIR}/clang_format_wrapper.sh -style=file -i
