ce219fdcbc142ab7fc9be432ac979ccc7e5239a6
Sven Klemm <sven@timescale.com>
2019-03-06 19:26:00 +0100

    Adjust ordered append plan cost
    Ordered append used to only consider startup cost of the first child.
    This patch changes the cost for ordered append to also include the total
    cost of the first child

--- test/sql/include/plan_ordered_append_load.sql
+++ test/sql/include/plan_ordered_append_load.sql
@@ -16,6 +16,7 @@ $BODY$;
 CREATE TABLE ordered_append(time timestamptz NOT NULL, device_id INT, value float);
 SELECT create_hypertable('ordered_append','time');
 CREATE index on ordered_append(time DESC,device_id);
+CREATE index on ordered_append(device_id,time DESC);
 
 INSERT INTO ordered_append VALUES('2000-01-01',1,1.0);
 INSERT INTO ordered_append VALUES('2000-01-08',1,2.0);
