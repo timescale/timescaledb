8baaa98698f95ca16eb10d49fa36cee1285ba085
Matvey Arye <mat@timescale.com>
2019-03-17 18:04:45 -0400

    Add foreach interface to scanner
    This PR adds a foreach style interface to our catalog scanner API.
    Previously, we only had a callback-style interface where for every
    tuple found by the scanner a callback function was called. The new
    foreach style interface has several advantages:
    
    1) Because the processing of the found tuples is inline with the calling
    function, the code becomes easier to follow since the flow of the code
    isn't interrupted by having to find the callback function. This
    also reduces the verbosity of the code.
    2) Since data does not have to be passed to a callback, we reduce the
    amount of unsafe void pointers and also the structs that need to be
    defined for the sole purpose of passing data to the callbacks.
    
    Note that this API change is fully backwards-compatible. We still allow
    the scanner to use the callback-style interface, it just adds an
    alternative API.

---
+++ src/scan_iterator.h
@@ -0,0 +1,64 @@
+/*
+ * This file and its contents are licensed under the Apache License 2.0.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-APACHE for a copy of the license.
+ */
+#ifndef TIMESCALEDB_SCAN_ITERATOR_H
+#define TIMESCALEDB_SCAN_ITERATOR_H
+#include <postgres.h>
+
+#include "scanner.h"
+
+#define EMBEDDED_SCAN_KEY_SIZE 5
+
+typedef struct ScanIterator
+{
+	ScannerCtx ctx;
+	TupleInfo *tinfo;
+	InternalScannerCtx ictx;
+	ScanKeyData scankey[EMBEDDED_SCAN_KEY_SIZE];
+} ScanIterator;
+
+#define ts_scan_iterator_create(catalog_table_id, lock_mode, mctx)                                 \
+	(ScanIterator)                                                                                 \
+	{                                                                                              \
+		.ctx = {                                                                                   \
+			.table = catalog_get_table_id(ts_catalog_get(), catalog_table_id),                     \
+			.nkeys = 0,                                                                            \
+			.scandirection = ForwardScanDirection,                                                 \
+			.lockmode = lock_mode,                                                                 \
+			.result_mctx = mctx,                                                                   \
+		}                                                                                          \
+	}
+
+static inline TupleInfo *
+ts_scan_iterator_tuple_info(ScanIterator *iterator)
+{
+	return iterator->tinfo;
+}
+
+static inline HeapTuple
+ts_scan_iterator_tuple(ScanIterator *iterator)
+{
+	return iterator->tinfo->tuple;
+}
+
+static inline TupleDesc
+ts_scan_iterator_tupledesc(ScanIterator *iterator)
+{
+	return iterator->tinfo->desc;
+}
+
+void ts_scan_iterator_close(ScanIterator *iterator);
+
+void ts_scan_iterator_scan_key_init(ScanIterator *iterator, AttrNumber attributeNumber,
+									StrategyNumber strategy, RegProcedure procedure,
+									Datum argument);
+
+/* You must use `ts_scan_iterator_close` if terminating this loop early */
+#define ts_scanner_foreach(scan_iterator)                                                          \
+	for (ts_scanner_start_scan(&(scan_iterator)->ctx, &(scan_iterator)->ictx);                     \
+		 ((scan_iterator)->tinfo =                                                                 \
+			  ts_scanner_next(&(scan_iterator)->ctx, &(scan_iterator)->ictx)) != NULL;)
+
+#endif /* TIMESCALEDB_SCAN_ITERATOR_H */
