7a9a28a5ab347bea9e1448f8cd352f962ec7a0cc
Sven Klemm <sven@timescale.com>
2019-03-20 00:37:28 +0100

    Combine hashagg plan and results test
    Since generating test data for plan_hashagg_test takes quite a long
    time compared to other tests, combine plan test and results diff test
    so we only need to load data once. This patch also removes the foreign
    key reference from the test tables since they are not required for
    the tests.

--- test/sql/include/plan_hashagg_load.sql
+++ test/sql/include/plan_hashagg_load.sql
@@ -3,8 +3,8 @@
 -- LICENSE-APACHE for a copy of the license.
 
 CREATE TABLE metric (id SERIAL PRIMARY KEY, value INT);
-CREATE TABLE hyper(time TIMESTAMP, time_int BIGINT, time_broken DATE, metricid int REFERENCES metric(id), VALUE double precision);
-CREATE TABLE regular(time TIMESTAMP, time_int BIGINT, time_date DATE, metricid int REFERENCES metric(id), VALUE double precision);
+CREATE TABLE hyper(time TIMESTAMP NOT NULL, time_int BIGINT, time_broken DATE, metricid int, value double precision);
+CREATE TABLE regular(time TIMESTAMP NOT NULL, time_int BIGINT, time_date DATE, metricid int, value double precision);
 
 SELECT create_hypertable('hyper', 'time', chunk_time_interval => interval '20 day', create_default_indexes=>FALSE);
 
@@ -14,9 +14,9 @@ ADD COLUMN time_date DATE;
 
 INSERT INTO metric(value) SELECT random()*100 FROM generate_series(0,10);
 
-INSERT INTO hyper SELECT t,  EXTRACT(EPOCH FROM t), (random() * 10)+1, 1.0, t::date FROM generate_series('2001-01-01', '2001-01-10', INTERVAL '1 second') t;
+INSERT INTO hyper SELECT t,  EXTRACT(EPOCH FROM t), (EXTRACT(EPOCH FROM t)::int % 10)+1, 1.0, t::date FROM generate_series('2001-01-01', '2001-01-10', INTERVAL '1 second') t;
 INSERT INTO regular(time, time_int, time_date, metricid, value)
-  SELECT t,  EXTRACT(EPOCH FROM t), t::date, (random() * 10)+1, 1.0 FROM generate_series('2001-01-01', '2001-01-02', INTERVAL '1 second') t;
+  SELECT t,  EXTRACT(EPOCH FROM t), t::date, (EXTRACT(EPOCH FROM t)::int % 10) + 1, 1.0 FROM generate_series('2001-01-01', '2001-01-02', INTERVAL '1 second') t;
 
 --test some queries before analyze;
 EXPLAIN (costs off) SELECT time_bucket('1 minute', time) AS MetricMinuteTs, AVG(value) as avg
@@ -37,11 +37,12 @@ CREATE OR REPLACE FUNCTION unix_to_timestamp(unixtime float8)
 $BODY$
     SELECT to_timestamp(unixtime);
 $BODY$;
-CREATE TABLE hyper_timefunc(time float8, metricid int REFERENCES metric(id), VALUE double precision, time_date DATE);
+CREATE TABLE hyper_timefunc(time float8 NOT NULL, metricid int, VALUE double precision, time_date DATE);
 
 SELECT create_hypertable('hyper_timefunc', 'time', chunk_time_interval => interval '20 day', create_default_indexes=>FALSE, time_partitioning_func => 'unix_to_timestamp');
 INSERT INTO hyper_timefunc SELECT time_int, metricid, VALUE, time_date FROM hyper;
 
-SET client_min_messages = 'error';
-ANALYZE;
-RESET client_min_messages;
+ANALYZE metric;
+ANALYZE hyper;
+ANALYZE regular;
+ANALYZE hyper_timefunc;
