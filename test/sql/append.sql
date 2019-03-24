0893eb265389d7fabbc1ea216eb4189c1bd97b2e
Sven Klemm <sven@timescale.com>
2019-03-24 18:02:53 +0100

    Refactor append test
    Change append test to merge append plan output and result test
    similar to make them similar to other tests.

--- test/sql/append.sql
+++ test/sql/append.sql
@@ -2,5 +2,35 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
-SET timescaledb.disable_optimizations = OFF;
-\ir include/append.sql
+\set TEST_BASE_NAME append
+SELECT format('include/%s_load.sql', :'TEST_BASE_NAME') as "TEST_LOAD_NAME",
+       format('include/%s_query.sql', :'TEST_BASE_NAME') as "TEST_QUERY_NAME",
+       format('%s/results/%s_results_optimized.out', :'TEST_OUTPUT_DIR', :'TEST_BASE_NAME') as "TEST_RESULTS_OPTIMIZED",
+       format('%s/results/%s_results_unoptimized.out', :'TEST_OUTPUT_DIR', :'TEST_BASE_NAME') as "TEST_RESULTS_UNOPTIMIZED"
+\gset
+SELECT format('\! diff %s %s', :'TEST_RESULTS_OPTIMIZED', :'TEST_RESULTS_UNOPTIMIZED') as "DIFF_CMD"
+\gset
+-- use input redirect here because filenames are absolute and would be different across installations
+SELECT format('\! wc -l <%s; wc -l <%s', :'TEST_RESULTS_OPTIMIZED', :'TEST_RESULTS_UNOPTIMIZED') as "WC_CMD"
+\gset
+
+\set PREFIX 'EXPLAIN (costs OFF)'
+\ir :TEST_LOAD_NAME
+\ir :TEST_QUERY_NAME
+
+--generate the results into two different files
+\set ECHO errors
+SET client_min_messages TO error;
+--make output contain query results
+\set PREFIX ''
+\o :TEST_RESULTS_OPTIMIZED
+SET timescaledb.disable_optimizations = 'off';
+\ir :TEST_QUERY_NAME
+\o
+\o :TEST_RESULTS_UNOPTIMIZED
+SET timescaledb.disable_optimizations = 'on';
+\ir :TEST_QUERY_NAME
+\o
+
+:DIFF_CMD
+:WC_CMD
