d48baad946442a1887ccafbf57083827aa531af3
gayyappan <gayathri@timescale.com>
2019-04-09 16:25:48 -0400

    Add timescaledb_information views for continuous aggregates
    Add timescaledb_information.continuous_aggregate_settings and timescaledb_information.continuous_aggregate_job_stats views

--- sql/views.sql
+++ sql/views.sql
@@ -53,5 +53,43 @@ CREATE OR REPLACE VIEW timescaledb_information.policy_stats as
     INNER JOIN _timescaledb_internal.bgw_job_stat js on p.job_id = js.job_id
   ORDER BY ht.schema_name, ht.table_name;
 
+-- views for continuous aggregate queries ---
+CREATE OR REPLACE VIEW timescaledb_information.continuous_aggregates as
+  SELECT format('%1$I.%2$I', cagg.user_view_schema, cagg.user_view_name)::regclass as view_name,
+    viewinfo.viewowner as view_owner,
+    cagg.refresh_lag,
+    bgwjob.schedule_interval as refresh_interval,
+    format('%1$I.%2$I', ht.schema_name, ht.table_name)::regclass as materialization_hypertable
+  FROM  _timescaledb_catalog.continuous_agg cagg, 
+        _timescaledb_catalog.hypertable ht, LATERAL
+        ( select C.oid, pg_get_userbyid( C.relowner) as viewowner
+          FROM pg_class C LEFT JOIN pg_namespace N on (N.oid = C.relnamespace)
+          where C.relkind = 'v' and C.relname = cagg.user_view_name
+          and N.nspname = cagg.user_view_schema ) viewinfo, LATERAL
+        ( select schedule_interval
+          FROM  _timescaledb_config.bgw_job
+          where id = cagg.job_id ) bgwjob
+  WHERE cagg.mat_hypertable_id = ht.id;
+
+CREATE OR REPLACE VIEW timescaledb_information.continuous_aggregate_stats as
+  SELECT format('%1$I.%2$I', cagg.user_view_schema, cagg.user_view_name)::regclass as view_name,
+    ct.watermark as completed_threshold,
+    it.watermark as invalidation_threshold,
+    bgw_job_stat.last_start as last_run_started_at,
+    case when bgw_job_stat.last_finish < '4714-11-24 00:00:00+00 BC' then 'running'
+       when bgw_job_stat.next_start is not null then 'scheduled'
+    end as job_status,
+    case when bgw_job_stat.last_finish > bgw_job_stat.last_start then (bgw_job_stat.last_finish - bgw_job_stat.last_start) 
+    end as last_run_duration,
+    bgw_job_stat.next_start as next_scheduled_run
+  FROM
+    _timescaledb_catalog.continuous_agg as cagg JOIN
+    _timescaledb_internal.bgw_job_stat as bgw_job_stat
+    ON  ( cagg.job_id = bgw_job_stat.job_id )
+    LEFT JOIN _timescaledb_catalog.continuous_aggs_invalidation_threshold as it
+    ON ( cagg.raw_hypertable_id = it.hypertable_id)
+    LEFT JOIN _timescaledb_catalog.continuous_aggs_completed_threshold as ct
+    ON ( cagg.mat_hypertable_id = ct.materialization_id);
+
 GRANT USAGE ON SCHEMA timescaledb_information TO PUBLIC;
 GRANT SELECT ON ALL TABLES IN SCHEMA timescaledb_information TO PUBLIC;
