828ffcd62275894f23f444120e97cb305504513c
Matvey Arye <mat@timescale.com>
2019-05-29 14:59:49 -0400

    Fix telemetry test
    The current telemetry test fails in ApacheOnly mode because it uses
    community functions. The PR separates out the community from the
    apache only testing and add a telemetry_community test that only
    runs if not in ApacheOnly mode.
    
    Also adjust the travis file to print telemetry output for the
    telemetry test run without openssl.

--- test/sql/telemetry.sql
+++ test/sql/telemetry.sql
@@ -127,28 +127,19 @@ SET timescaledb.telemetry_level=off;
 SELECT * FROM json_object_keys(get_telemetry_report()::json) AS key
 WHERE key != 'os_name_pretty';
 
-SELECT json_object_field(get_telemetry_report()::json,'num_continuous_aggs');
-
 -- check telemetry picks up flagged content from metadata
 SELECT json_object_field(get_telemetry_report()::json,'db_metadata');
 
 -- check timescaledb_telemetry.cloud
 SELECT json_object_field(get_telemetry_report()::json,'instance_metadata');
 
+SELECT json_object_field(get_telemetry_report()::json,'num_hypertables');
 
---create a continuous agg
+SELECT json_object_field(get_telemetry_report()::json,'num_continuous_aggs');
+
 CREATE TABLE device_readings (
       observation_time  TIMESTAMPTZ       NOT NULL
 );
 SELECT table_name FROM create_hypertable('device_readings', 'observation_time');
-CREATE VIEW device_summary
-WITH (timescaledb.continuous) AS
-SELECT
-  time_bucket('1 hour', observation_time) as bucket,
-  min(observation_time)
-FROM
-  device_readings
-GROUP BY bucket;
 
-SELECT json_object_field(get_telemetry_report()::json,'num_continuous_aggs');
 SELECT json_object_field(get_telemetry_report()::json,'num_hypertables');
