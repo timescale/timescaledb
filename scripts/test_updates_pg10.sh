498497991080a8ddd252612d0e32aa51ee1e4b4e
Matvey Arye <mat@timescale.com>
2019-06-11 15:24:54 -0400

    Test continuous aggs with extension updates
    Add a v3 update test that makes sure that continuous aggregates
    continue to work after extensions are updated.

--- scripts/test_updates_pg10.sh
+++ scripts/test_updates_pg10.sh
@@ -5,7 +5,21 @@ set -o pipefail
 
 SCRIPT_DIR=$(dirname $0)
 
-TAGS="0.7.0-pg10 0.7.1-pg10 0.8.0-pg10 0.9.0-pg10 0.9.1-pg10 0.9.2-pg10 0.10.0-pg10 0.10.1-pg10 0.11.0-pg10 0.12.0-pg10 1.0.0-pg10 1.0.1-pg10 1.1.0-pg10 1.1.1-pg10 1.2.0-pg10 1.2.1-pg10 1.2.2-pg10 1.3.0-pg10 1.3.1-pg10"
+TAGS="0.7.0-pg10 0.7.1-pg10 0.8.0-pg10 0.9.0-pg10 0.9.1-pg10 0.9.2-pg10 0.10.0-pg10 0.10.1-pg10 0.11.0-pg10 0.12.0-pg10 1.0.0-pg10 1.0.1-pg10 1.1.0-pg10 1.1.1-pg10 1.2.0-pg10 1.2.1-pg10 1.2.2-pg10"
 TEST_VERSION="v2"
 
-. ${SCRIPT_DIR}/test_updates.sh
+TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  exit $EXIT_CODE
+fi
+
+
+TAGS="1.3.0-pg10 1.3.1-pg10"
+TEST_VERSION="v3"
+
+TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  exit $EXIT_CODE
+fi
