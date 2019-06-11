33d28d2217df648eb528fa0dca902d7233d2626d
Matvey Arye <mat@timescale.com>
2019-06-11 15:24:54 -0400

    Test continuous aggs with extension updates
    Add a v3 update test that makes sure that continuous aggregates
    continue to work after extensions are updated.

--- scripts/test_updates_with_constraints.sh
+++ scripts/test_updates_with_constraints.sh
@@ -12,7 +12,20 @@ set -o pipefail
 
 SCRIPT_DIR=$(dirname $0)
 
-TAGS="0.5.0 0.6.0 0.6.1 0.7.0-pg9.6 0.7.1-pg9.6 0.8.0-pg9.6 0.9.0-pg9.6 0.9.1-pg9.6 0.9.2-pg9.6 0.10.0-pg9.6 0.10.1-pg9.6 0.11.0-pg9.6 0.12.0-pg9.6 1.0.0-pg9.6 1.0.1-pg9.6 1.1.0-pg9.6 1.1.1-pg9.6 1.2.0-pg9.6 1.2.1-pg9.6 1.2.2-pg9.6 1.3.0-pg9.6 1.3.1-pg9.6"
+TAGS="0.5.0 0.6.0 0.6.1 0.7.0-pg9.6 0.7.1-pg9.6 0.8.0-pg9.6 0.9.0-pg9.6 0.9.1-pg9.6 0.9.2-pg9.6 0.10.0-pg9.6 0.10.1-pg9.6 0.11.0-pg9.6 0.12.0-pg9.6 1.0.0-pg9.6 1.0.1-pg9.6 1.1.0-pg9.6 1.1.1-pg9.6 1.2.0-pg9.6 1.2.1-pg9.6 1.2.2-pg9.6"
 TEST_VERSION="v2"
 
-. ${SCRIPT_DIR}/test_updates.sh
+TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  exit $EXIT_CODE
+fi
+
+TAGS="1.3.0-pg9.6 1.3.1-pg9.6"
+TEST_VERSION="v3"
+
+TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  exit $EXIT_CODE
+fi
---
+++ test/sql/updates/post.continuous_aggs.sql
@@ -0,0 +1,15 @@
+-- This file and its contents are licensed under the Apache License 2.0.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-APACHE for a copy of the license.
+
+SELECT * FROM mat_before;
+
+INSERT INTO conditions_before
+SELECT generate_series('2018-12-01 00:00'::timestamp, '2018-12-31 00:00'::timestamp, '1 day'), 'POR', 165, 75, 40, 70, NULL, (1,2)::custom_type, 2, true;
+
+SELECT * FROM mat_before;
+
+REFRESH MATERIALIZED VIEW mat_before;
+
+--the max of the temp for the POR should now be 165
+SELECT * FROM mat_before;
