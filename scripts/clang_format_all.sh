d8ed007fe74cd5fbaef0eb035991957d2a3b2395
Mats Kindahl <mats@timescale.com>
2019-06-24 11:52:31 +0200

    Support different clang-format names
    On some distributions, the default version of clang-format is named
    'clang-format' while other versions are available as 'clang-format-N',
    where 'N' is the major version number of clang-format.
    
    Current custom rules and scripts to format the entire repository expect
    the name to be 'clang-format', which causes either lookup to fail or
    execution to fail.
    
    This commit changes the custom rule to look for clang-format-7 and
    clang-format, in that order, before checking the version. In addition,
    the scripts that execute clang-format are updated to support whatever
    name the CMakeLists.txt found.

--- scripts/clang_format_all.sh
+++ scripts/clang_format_all.sh
@@ -4,6 +4,9 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 BASE_DIR=$(dirname $SCRIPT_DIR)
 
+export CLANG_FORMAT
+CLANG_FORMAT=${1:-clang-format}
+
 find ${BASE_DIR} \( -path "${BASE_DIR}/src/*" -or -path "${BASE_DIR}/test/*" -or -path "${BASE_DIR}/tsl/*" \) \
     -and -not \( -path "*/.*" -or -path "*CMake*" \) \
     -and \( -name '*.c' -or -name '*.h' \) -print | xargs ${SCRIPT_DIR}/clang_format_wrapper.sh -style=file -i
