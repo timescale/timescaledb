dcc1d902d1953c2a42d53cc45790b22e9daf519d
Matvey Arye <mat@timescale.com>
2019-08-21 11:50:53 -0400

    Add more compression tests
    Adds several test features:
    - Add a tests on compression of an altered hypertable
    - Adds tests for transparent decompression in hypertables tests
    - Add dump/restore in hypertable test

--- tsl/test/sql/compression_hypertable.sql
+++ tsl/test/sql/compression_hypertable.sql
@@ -19,3 +19,29 @@ SELECT
 SELECT 'test1' AS "HYPERTABLE_NAME" \gset
 
 \ir include/compression_test_hypertable.sql
+
+--add test for altered hypertable
+CREATE TABLE test2 ("Time" timestamptz, i integer, b bigint, t text);
+SELECT table_name from create_hypertable('test2', 'Time', chunk_time_interval=> INTERVAL '1 day');
+
+--create some chunks with the old column numbers
+INSERT INTO test2 SELECT t,  gen_rand_minstd(), gen_rand_minstd(), gen_rand_minstd()::text FROM generate_series('2018-03-02 1:00'::TIMESTAMPTZ, '2018-03-04 1:00', '1 hour') t;
+
+ALTER TABLE test2 DROP COLUMN b;
+--add default a value
+ALTER TABLE test2 ADD COLUMN c INT DEFAULT -15;
+--add default NULL
+ALTER TABLE test2 ADD COLUMN d INT;
+
+--write to both old chunks and new chunks with different column #s
+INSERT INTO test2 SELECT t, gen_rand_minstd(), gen_rand_minstd()::text, gen_rand_minstd(), gen_rand_minstd() FROM generate_series('2018-03-02 1:00'::TIMESTAMPTZ, '2018-03-06 1:00', '1 hour') t;
+
+ALTER TABLE test2 set (timescaledb.compress, timescaledb.compress_segmentby = '', timescaledb.compress_orderby = 'c, "Time" DESC');
+
+SELECT
+  $$
+  SELECT * FROM test2 ORDER BY c, "Time"
+  $$ AS "QUERY" \gset
+
+SELECT 'test2' AS "HYPERTABLE_NAME" \gset
+\ir include/compression_test_hypertable.sql
