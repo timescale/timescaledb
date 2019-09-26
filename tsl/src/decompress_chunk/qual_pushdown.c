b79db68b3a420d3e0db6aa977a1093c862c90ed2
Sven Klemm <sven@timescale.com>
2019-09-26 13:49:00 +0200

    Improve qual pushdown error handling
    Improve transparent decompression qual pushdown error handling by
    preventing pushdown if the expression is volatile or when operators
    are not member of btree family.

--- tsl/src/decompress_chunk/qual_pushdown.c
+++ tsl/src/decompress_chunk/qual_pushdown.c
@@ -14,6 +14,7 @@
 #include <optimizer/tlist.h>
 #include <optimizer/var.h>
 #include <parser/parsetree.h>
+#include <parser/parse_func.h>
 #include <utils/builtins.h>
 #include <utils/typcache.h>
 
@@ -56,6 +57,10 @@ pushdown_quals(PlannerInfo *root, RelOptInfo *chunk_rel, RelOptInfo *compressed_
 	{
 		RestrictInfo *ri = lfirst(lc);
 		Expr *expr;
+
+		/* pushdown is not safe for volatile expressions */
+		if (contain_volatile_functions((Node *) ri->clause))
+			continue;
 
 		context.can_pushdown = true;
 		context.needs_recheck = false;
@@ -104,43 +109,41 @@ get_compression_info_from_var(QualPushdownContext *context, Var *var)
 	return get_column_compressioninfo(context->compression_info, column_name);
 }
 
-static OpExpr *
-make_op_comparison_expr(TypeCacheEntry *tce, Expr *left, StrategyNumber strategy, Expr *right,
-						Oid comparison_collation)
-{
-	OpExpr *opexpr;
-	Oid opr = get_opfamily_member(tce->btree_opf, tce->type_id, tce->type_id, strategy);
-
-	opexpr = makeNode(OpExpr);
-	opexpr->opno = opr;
-	opexpr->opfuncid = InvalidOid;
-	opexpr->opresulttype = BOOLOID;
-	opexpr->opretset = false;
-	opexpr->opcollid = InvalidOid;				/* result is a bool */
-	opexpr->inputcollid = comparison_collation; /* collation to use for comparison*/
-	opexpr->args = list_make2(left, right);
-	return opexpr;
-}
-
 static FuncExpr *
-make_segment_meta_accessor_function_expr(char *accessor_name, Index compressed_rel_index,
-										 AttrNumber meta_column_attno, Oid uncompressed_type_oid,
-										 Oid result_collation)
+make_segment_meta_accessor_funcexpr(int strategy, Index compressed_rel_index,
+									AttrNumber meta_column_attno, Oid uncompressed_type_oid,
+									Oid result_collation)
 {
-	Oid any = ANYELEMENTOID;
-	Oid accessor_function_oid =
-		lookup_proc_filtered(INTERNAL_SCHEMA_NAME, accessor_name, &any, NULL, NULL);
+	List *func_name;
+	Oid segment_meta_type = ts_custom_type_cache_get(CUSTOM_TYPE_SEGMENT_META_MIN_MAX)->type_oid;
+	Oid accessor_function_oid;
+	Oid argtypes[] = { segment_meta_type, ANYELEMENTOID };
 	Var *meta_var;
 	Const *null_const;
 
-	if (!OidIsValid(accessor_function_oid))
-		elog(ERROR, "couldn't find SegmentMetaMinMax accessor function %s", accessor_name);
-	meta_var = makeVar(compressed_rel_index,
-					   meta_column_attno,
-					   ts_custom_type_cache_get(CUSTOM_TYPE_SEGMENT_META_MIN_MAX)->type_oid,
-					   -1,
-					   InvalidOid,
-					   0);
+	switch (strategy)
+	{
+		case BTGreaterStrategyNumber:
+		case BTGreaterEqualStrategyNumber:
+			/* var > expr  implies max > expr */
+			func_name = list_make2(makeString(INTERNAL_SCHEMA_NAME),
+								   makeString(SEGMENT_META_ACCESSOR_MAX_SQL_FUNCTION));
+			break;
+		case BTLessStrategyNumber:
+		case BTLessEqualStrategyNumber:
+			/* var < expr  implies min < expr */
+			func_name = list_make2(makeString(INTERNAL_SCHEMA_NAME),
+								   makeString(SEGMENT_META_ACCESSOR_MIN_SQL_FUNCTION));
+			break;
+		default:
+			elog(ERROR, "invalid strategy");
+			break;
+	}
+
+	accessor_function_oid = LookupFuncName(func_name, lengthof(argtypes), argtypes, false);
+
+	meta_var =
+		makeVar(compressed_rel_index, meta_column_attno, segment_meta_type, -1, InvalidOid, 0);
 	null_const = makeNullConst(uncompressed_type_oid, -1, InvalidOid);
 
 	return makeFuncExpr(accessor_function_oid,
@@ -152,45 +155,22 @@ make_segment_meta_accessor_function_expr(char *accessor_name, Index compressed_r
 }
 
 static OpExpr *
-make_segment_meta_min_less_expr(Index compressed_rel_index, AttrNumber meta_column_attno,
-								TypeCacheEntry *tce, Var *uncompressed_var, Expr *compare_to_expr,
-								StrategyNumber strategy)
+make_segment_meta_opexpr(QualPushdownContext *context, Oid opno, AttrNumber meta_column_attno,
+						 Var *uncompressed_var, Expr *compare_to_expr, StrategyNumber strategy)
 {
-	FuncExpr *accessor_function_expr =
-		make_segment_meta_accessor_function_expr(SEGMENT_META_ACCESSOR_MIN_SQL_FUNCTION,
-												 compressed_rel_index,
-												 meta_column_attno,
-												 tce->type_id,
-												 uncompressed_var->varcollid);
+	FuncExpr *func = make_segment_meta_accessor_funcexpr(strategy,
+														 context->compressed_rel->relid,
+														 meta_column_attno,
+														 uncompressed_var->vartype,
+														 uncompressed_var->varcollid);
 
-	Assert(strategy == BTLessEqualStrategyNumber || BTLessStrategyNumber);
-
-	return make_op_comparison_expr(tce,
-								   (Expr *) accessor_function_expr,
-								   strategy,
-								   copyObject(compare_to_expr),
-								   uncompressed_var->varcollid);
-}
-
-static OpExpr *
-make_segment_meta_max_greater_expr(Index compressed_rel_index, AttrNumber meta_column_attno,
-								   TypeCacheEntry *tce, Var *uncompressed_var,
-								   Expr *compare_to_expr, StrategyNumber strategy)
-{
-	FuncExpr *accessor_function_expr =
-		make_segment_meta_accessor_function_expr(SEGMENT_META_ACCESSOR_MAX_SQL_FUNCTION,
-												 compressed_rel_index,
-												 meta_column_attno,
-												 tce->type_id,
-												 uncompressed_var->varcollid);
-
-	Assert(strategy == BTGreaterEqualStrategyNumber || strategy == BTGreaterStrategyNumber);
-
-	return make_op_comparison_expr(tce,
-								   (Expr *) accessor_function_expr,
-								   strategy,
-								   copyObject(compare_to_expr),
-								   uncompressed_var->varcollid);
+	return (OpExpr *) make_opclause(opno,
+									BOOLOID,
+									false,
+									(Expr *) func,
+									copyObject(compare_to_expr),
+									InvalidOid,
+									uncompressed_var->varcollid);
 }
 
 static AttrNumber
@@ -247,7 +227,6 @@ pushdown_op_to_segment_meta_min_max(QualPushdownContext *context, List *expr_arg
 	Var *var_with_segment_meta;
 	TypeCacheEntry *tce;
 	int strategy;
-	Oid lefttype, righttype;
 	FormData_hypertable_compression *compression_info;
 
 	if (list_length(expr_args) != 2)
@@ -289,10 +268,9 @@ pushdown_op_to_segment_meta_min_max(QualPushdownContext *context, List *expr_arg
 
 	tce = lookup_type_cache(var_with_segment_meta->vartype, TYPECACHE_BTREE_OPFAMILY);
 
-	if (!op_in_opfamily(op_oid, tce->btree_opf))
+	strategy = get_op_opfamily_strategy(op_oid, tce->btree_opf);
+	if (strategy == InvalidStrategy)
 		return NULL;
-
-	get_op_opfamily_properties(op_oid, tce->btree_opf, false, &strategy, &lefttype, &righttype);
 
 	expr = get_pushdownsafe_expr(context, expr);
 
@@ -306,48 +284,50 @@ pushdown_op_to_segment_meta_min_max(QualPushdownContext *context, List *expr_arg
 			/* var = expr implies min < expr and max > expr */
 			AttrNumber meta_attno =
 				get_segment_meta_attr_number(compression_info, context->compressed_rte->relid);
+			Oid opno_le = get_opfamily_member(tce->btree_opf,
+											  tce->type_id,
+											  tce->type_id,
+											  BTLessEqualStrategyNumber);
+			Oid opno_ge = get_opfamily_member(tce->btree_opf,
+											  tce->type_id,
+											  tce->type_id,
+											  BTGreaterEqualStrategyNumber);
+
+			if (!OidIsValid(opno_le) || !OidIsValid(opno_ge))
+				return NULL;
 
 			return make_andclause(
-				list_make2(make_segment_meta_min_less_expr(context->compressed_rel->relid,
-														   meta_attno,
-														   tce,
-														   var_with_segment_meta,
-														   expr,
-														   BTLessEqualStrategyNumber),
-						   make_segment_meta_max_greater_expr(context->compressed_rel->relid,
-															  meta_attno,
-															  tce,
-															  var_with_segment_meta,
-															  expr,
-															  BTGreaterEqualStrategyNumber)));
+				list_make2(make_segment_meta_opexpr(context,
+													opno_le,
+													meta_attno,
+													var_with_segment_meta,
+													expr,
+													BTLessEqualStrategyNumber),
+						   make_segment_meta_opexpr(context,
+													opno_ge,
+													meta_attno,
+													var_with_segment_meta,
+													expr,
+													BTGreaterEqualStrategyNumber)));
 		}
 		case BTLessStrategyNumber:
 		case BTLessEqualStrategyNumber:
-		{
-			/* var < expr  implies min < expr */
-			AttrNumber meta_attno =
-				get_segment_meta_attr_number(compression_info, context->compressed_rte->relid);
-
-			return (Expr *) make_segment_meta_min_less_expr(context->compressed_rel->relid,
-															meta_attno,
-															tce,
-															var_with_segment_meta,
-															expr,
-															strategy);
-		}
 		case BTGreaterStrategyNumber:
 		case BTGreaterEqualStrategyNumber:
 		{
-			/* var > expr  implies max > expr */
 			AttrNumber meta_attno =
 				get_segment_meta_attr_number(compression_info, context->compressed_rte->relid);
+			Oid opno = get_opfamily_member(tce->btree_opf, tce->type_id, tce->type_id, strategy);
 
-			return (Expr *) make_segment_meta_max_greater_expr(context->compressed_rel->relid,
-															   meta_attno,
-															   tce,
-															   var_with_segment_meta,
-															   expr,
-															   strategy);
+			if (!OidIsValid(opno))
+				return NULL;
+
+			return (Expr *) make_segment_meta_opexpr(context,
+													 opno,
+													 meta_attno,
+													 var_with_segment_meta,
+													 expr,
+													 strategy);
 		}
 		default:
 			return NULL;
