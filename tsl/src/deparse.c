8f5aaa0f712ed1f824f201272b246cba81112274
Erik Nordström <erik@timescale.com>
2019-10-25 09:31:22 +0200

    Fix variuos compiler warnings and nits
    This change fixes a number of compiler/code warnings.

--- tsl/src/deparse.c
+++ tsl/src/deparse.c
@@ -542,8 +542,6 @@ deparse_result_type(StringInfo sql, FunctionCallInfo fcinfo)
 			break;
 		case TYPEFUNC_RECORD:
 			/* indeterminate rowtype result */
-			elog(NOTICE, "record return type");
-			break;
 		case TYPEFUNC_COMPOSITE_DOMAIN:
 			/* domain over determinable rowtype result */
 		case TYPEFUNC_OTHER:
