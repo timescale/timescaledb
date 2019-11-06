12d904c711dca68e831ec60de2d42fc01591ace1
Matvey Arye <mat@timescale.com>
2019-11-06 17:56:56 -0500

    Add tests for updates of catalog tables
    PG11 added an optimization where columns that were added by
    an ALTER TABLE that had a DEFAULT value did not cause a table
    re-write. Instead, those columns are filled with the default
    value on read.
    
    But, this mechanism does not apply to catalog tables and does
    not work with our catalog scanning code. This tests makes
    sure we never have such alters in our updates.

---
+++ test/sql/updates/setup.v4.sql
@@ -0,0 +1,6 @@
+-- This file and its contents are licensed under the Apache License 2.0.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-APACHE for a copy of the license.
+
+\ir setup.v3.sql
+
