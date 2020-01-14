38654b2158174d30248de567474a05188b539145
Mats Kindahl <mats@timescale.com>
2020-01-14 15:12:26 +0100

    Print notice for COPY TO on hypertable
    When using `COPY TO` on a hypertable (which copies from the hypertable
    to some other destination), nothing will be printed and nothing will be
    copied. Since this can be potentially confusing for users, this commit
    print a notice when an attempt is made to copy from a hypertable
    directly (not using a query) to some other destination.

--- test/sql/utils/pg_dump_aux_dump.sh
+++ test/sql/utils/pg_dump_aux_dump.sh
@@ -1,4 +1,7 @@
+export PGOPTIONS
+
 DUMPFILE=$1
+PGOPTIONS='--client-min-messages=warning'
 
 ${PG_BINDIR}/pg_dump -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} -Fc ${TEST_DBNAME} > ${DUMPFILE}
 ${PG_BINDIR}/dropdb -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} ${TEST_DBNAME}
