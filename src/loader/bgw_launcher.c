d13993485bb474283cb50315c1635a4dd577afeb
David Kohn <david@timescale.com>
2020-02-27 10:28:59 -0500

    Fix restoring/scheduler entrypoint to avoid BGW death
    There was a race condition between the post_restore function
    restarting the background worker and the setting of the
    restoring flag to "off". If the worker started before the
    change to the restoring flag had been committed, it would not
    see the change and then die because the worker should exit
    when the db is in a restoring state. This modifies the
    post_restore function to use a restart instead of a start
    so that it waits on the commit to start up. It also adds
    logic to the entrypoint to reload config changes caused
    by an `ALTER DATABASE SET` command. These changes are
    normally only seen at connection startup but we have to
    wait until after our lock on the modifying transaction is
    released to know whether we should adopt them.

--- src/loader/bgw_launcher.c
+++ src/loader/bgw_launcher.c
@@ -40,6 +40,9 @@
 /* for allocating the htab storage */
 #include <utils/memutils.h>
 
+/* for getting settings correct before loading the versioned scheduler */
+#include "catalog/pg_db_role_setting.h"
+
 #include "../compat.h"
 #include "../extension_constants.h"
 #include "loader.h"
@@ -843,6 +846,39 @@ database_is_template_check(void)
 }
 
 /*
+ * Before we morph into the scheduler, we also need to reload configs from their
+ * defaults if the database default has changed. Defaults are changed in the
+ * post_restore function where we change the db default for the restoring guc
+ * wait until the txn commits and then must see if the txn made the change.
+ * Checks for changes are normally run at connection startup, but because we
+ * have to connect in order to wait on the txn we have to re-run after the wait.
+ * This function is based on the postgres function in postinit.c by the same
+ * name.
+ */
+
+static void
+process_settings(Oid databaseid)
+{
+	Relation relsetting;
+	Snapshot snapshot;
+
+	if (!IsUnderPostmaster)
+		return;
+
+	relsetting = heap_open(DbRoleSettingRelationId, AccessShareLock);
+
+	/* read all the settings under the same snapshot for efficiency */
+	snapshot = RegisterSnapshot(GetCatalogSnapshot(DbRoleSettingRelationId));
+
+	/* Later settings are ignored if set earlier. */
+	ApplySetting(snapshot, databaseid, InvalidOid, relsetting, PGC_S_DATABASE);
+	ApplySetting(snapshot, InvalidOid, InvalidOid, relsetting, PGC_S_GLOBAL);
+
+	UnregisterSnapshot(snapshot);
+	heap_close(relsetting, AccessShareLock);
+}
+
+/*
  * This can be run either from the cluster launcher at db_startup time, or
  * in the case of an install/uninstall/update of the extension, in the
  * first case, we have no vxid that we're waiting on. In the second case,
@@ -890,6 +926,8 @@ ts_bgw_db_scheduler_entrypoint(PG_FUNCTION_ARGS)
 	 * so, as we don't want to run in template dbs.
 	 */
 	database_is_template_check();
+	/*  Process any config changes caused by an ALTER DATABASE */
+	process_settings(MyDatabaseId);
 	ts_installed = ts_loader_extension_exists();
 	if (ts_installed)
 		StrNCpy(version, ts_loader_extension_version(), MAX_VERSION_LEN);
