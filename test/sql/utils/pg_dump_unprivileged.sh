d1c089c5ebc1b54136d1c3f0c1f2749158436a0a
Erik Nordström <erik@timescale.com>
2020-03-05 15:35:12 +0100

    Make tests using pg_dump work for PG12
    PostgreSQL 12 changed the log level in client tools, such as
    `pg_dump`, which makes some of our tests fail due to different log
    level labels.
    
    This change filters and modifies the log level output of `pg_dump` in
    earlier PostgreSQL versions to adopt the new PostgreSQL 12 format.

--- test/sql/utils/pg_dump_unprivileged.sh
+++ test/sql/utils/pg_dump_unprivileged.sh
@@ -1,9 +1,10 @@
 export PGOPTIONS
 PGOPTIONS='--client-min-messages=warning'
 
-${PG_BINDIR}/pg_dump -h ${PGHOST} -U dump_unprivileged dump_unprivileged > /dev/null
+# PG12 changed client logging so NOTICE messages are now warning.
+# We adopt this also for older PG versions to make tests compatible.
+${PG_BINDIR}/pg_dump -h ${PGHOST} -U dump_unprivileged dump_unprivileged 2>&1 > /dev/null | sed 's/NOTICE/warning/g'
 
 if [ $? -eq 0 ]; then
   echo "Database dumped successfully"
 fi
-
