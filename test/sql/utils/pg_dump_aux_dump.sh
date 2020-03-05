21dc7b9f7bee23153845662e06af2f19122bfd0a
Erik Nordström <erik@timescale.com>
2020-03-05 15:35:12 +0100

    Make tests using pg_dump work for PG12
    PostgreSQL 12 changed the log level in client tools, such as
    `pg_dump`, which makes some of our tests fail due to different log
    level labels.
    
    This change filters and modifies the log level output of `pg_dump` in
    earlier PostgreSQL versions to adopt the new PostgreSQL 12 format.

--- test/sql/utils/pg_dump_aux_dump.sh
+++ test/sql/utils/pg_dump_aux_dump.sh
@@ -3,6 +3,8 @@ export PGOPTIONS
 DUMPFILE=$1
 PGOPTIONS='--client-min-messages=warning'
 
-${PG_BINDIR}/pg_dump -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} -Fc ${TEST_DBNAME} > ${DUMPFILE}
+# PG12 changed client logging so NOTICE messages are now warning.
+# We adopt this also for older PG versions to make tests compatible.
+${PG_BINDIR}/pg_dump -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} -Fc ${TEST_DBNAME} 2>&1 > ${DUMPFILE} | sed 's/NOTICE/warning/g'
 ${PG_BINDIR}/dropdb -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} ${TEST_DBNAME}
 ${PG_BINDIR}/createdb -h ${PGHOST} -U ${TEST_ROLE_SUPERUSER} ${TEST_DBNAME}
