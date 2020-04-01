e2da5606cf649a47bf933cb2e107661ce10d8e64
Dmitry Simonenko <dmitry@timescale.com>
2020-04-01 14:15:44 +0300

    Add test for jit compilation support
    This test also covers a failure case descibed in the issue #1262.

---
+++ tsl/test/sql/include/jit_cleanup.sql
@@ -0,0 +1,7 @@
+-- This file and its contents are licensed under the Timescale License.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-TIMESCALE for a copy of the license.
+
+DROP TABLE IF EXISTS jit_test CASCADE;
+DROP TABLE IF EXISTS jit_test_interval CASCADE;
+DROP TABLE IF EXISTS jit_test_contagg CASCADE;
