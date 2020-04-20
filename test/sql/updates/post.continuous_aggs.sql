8e45d5e39ee14f561c37836ee4d1cb769fbe68d2
Sven Klemm <sven@timescale.com>
2020-04-20 21:40:53 +0200

    Add missing ORDER BY clause to update test queries

--- test/sql/updates/post.continuous_aggs.sql
+++ test/sql/updates/post.continuous_aggs.sql
@@ -2,7 +2,7 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
-SELECT * FROM mat_before;
+SELECT * FROM mat_before ORDER BY bucket, location;
 
 INSERT INTO conditions_before
 SELECT generate_series('2018-12-01 00:00'::timestamp, '2018-12-31 00:00'::timestamp, '1 day'), 'POR', 165, 75, 40, 70, NULL, (1,2)::custom_type, 2, true;
@@ -11,9 +11,9 @@ SELECT generate_series('2018-12-01 00:00'::timestamp, '2018-12-31 00:00'::timest
 INSERT INTO conditions_before
 SELECT generate_series('2017-12-01 00:00'::timestamp, '2017-12-31 00:00'::timestamp, '1 day'), 'POR', 1065, 75, 40, 70, NULL, (1,2)::custom_type, 2, true;
 
-SELECT * FROM mat_before;
+SELECT * FROM mat_before ORDER BY bucket, location;
 
 REFRESH MATERIALIZED VIEW mat_before;
 
 --the max of the temp for the POR should now be 165
-SELECT * FROM mat_before;
+SELECT * FROM mat_before ORDER BY bucket, location;
