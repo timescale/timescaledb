379690fd89cbae5c889473f76c56d036a2b30c22
Sven Klemm <sven@timescale.com>
2020-05-23 12:11:11 +0200

    Fix multiple definitions of ts_mock_timer

--- test/src/bgw/timer_mock.h
+++ test/src/bgw/timer_mock.h
@@ -3,8 +3,8 @@
  * Please see the included NOTICE for copyright information and
  * LICENSE-APACHE for a copy of the license.
  */
-#ifndef TIME_MOCK_H
-#define TIME_MOCK_H
+#ifndef TIMESCALEDB_BGW_TIMER_MOCK_H
+#define TIMESCALEDB_BGW_TIMER_MOCK_H
 
 #include <postgres.h>
 #include <postmaster/bgworker.h>
@@ -13,6 +13,6 @@
 
 extern void ts_timer_mock_register_bgw_handle(BackgroundWorkerHandle *handle);
 
-const Timer ts_mock_timer;
+extern const Timer ts_mock_timer;
 
-#endif /* tTIME_MOCK_H */
+#endif /* TIMESCALEDB_BGW_TIMER_MOCK_H */
