112c566ff77f33cc11d22105dac3e9f69a101c0e
Sven Klemm <sven@timescale.com>
2020-07-20 12:37:17 +0200

    Add isolation test files to license check
    This patch enhances the license check script to check for a
    license header in isolation test files

--- scripts/check_license_all.sh
+++ scripts/check_license_all.sh
@@ -2,9 +2,9 @@
 SCRIPT_DIR=$(dirname $0)
 BASE_DIR=$(dirname ${SCRIPT_DIR})
 
-SRC_DIR=$BASE_DIR ${SCRIPT_DIR}/check_license.sh -c ${BASE_DIR}/src -s ${BASE_DIR}/sql -c ${BASE_DIR}/test -t ${BASE_DIR}/test
+SRC_DIR=$BASE_DIR ${SCRIPT_DIR}/check_license.sh -c ${BASE_DIR}/src -s ${BASE_DIR}/sql -c ${BASE_DIR}/test -s ${BASE_DIR}/test -i ${BASE_DIR}/test
 exit_apache=$?
-SRC_DIR=$BASE_DIR ${SCRIPT_DIR}/check_license.sh -e ${BASE_DIR}/tsl/src -u ${BASE_DIR}/tsl/test/sql -u ${BASE_DIR}/tsl/test/shared/sql -e ${BASE_DIR}/tsl/test/src
+SRC_DIR=$BASE_DIR ${SCRIPT_DIR}/check_license.sh -e ${BASE_DIR}/tsl/src -t ${BASE_DIR}/tsl/test -e ${BASE_DIR}/tsl/test -j ${BASE_DIR}/tsl/test
 exit_tsl=$?
 
 if [ ${exit_apache} -ne 0 ] || [ ${exit_tsl} -ne 0 ]; then
