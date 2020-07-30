0712c606146c87b1e5f0b79ac0dd508d074478a2
Sven Klemm <sven@timescale.com>
2020-07-30 02:12:26 +0200

    Remove obsolete sql update files
    Since the minimum version we can update from on PG11 is 1.1.0 we
    can get rid of all the previous versions update files as they
    are not a valid update source for any current version.

--- sql/updates/0.10.0--0.10.1.sql
+++
@@ -1,6 +0,0 @@
---Fix any potential catalog issues that may have been introduced if a
---trigger was dropped on a hypertable before the current bugfix
---Only deletes orphaned rows from pg_depend.
-DELETE FROM pg_depend d 
-WHERE d.classid = 'pg_trigger'::regclass 
-AND NOT EXISTS (SELECT 1 FROM pg_trigger WHERE oid = d.objid);
