2a70cf440b868505ef00d693f002a4f62a310e89
Sven Klemm <sven@timescale.com>
2020-08-09 13:48:42 +0200

    Add check for unreferenced test files
    This patch adds a check for test files not referenced in the
    CMakeLists.txt file to CI.

---
+++ scripts/check_unreferenced_files.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname ${0})
+BASE_DIR=$(pwd)/$SCRIPT_DIR/..
+
+unreferenced=0
+
+function get_filenames {
+  echo *.$2 *.$2.in | xargs git ls-files | xargs -IFILE basename FILE
+}
+
+function check_directory {
+  cd $BASE_DIR/$1
+  test_files=$(get_filenames $1 $2)
+
+  for file in $test_files; do
+    output=$(grep --files-without-match $file CMakeLists.txt)
+
+    # return value from grep --files-without-match seems to differ
+    # between grep versions so we use output instead of return value
+    if [ "$output" != "" ]; then
+      echo -e "\nUnreferenced file in $1: $file\n"
+      unreferenced=1
+    fi
+  done
+}
+
+check_directory test/sql sql
+check_directory tsl/test/sql sql
+check_directory tsl/test/shared/sql sql
+
+check_directory test/isolation/specs spec
+check_directory tsl/test/isolation/specs spec
+
+exit $unreferenced
