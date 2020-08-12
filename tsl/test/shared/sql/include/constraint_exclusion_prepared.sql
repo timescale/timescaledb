f61818f3a75ba2ca92ddf326bc978ed30d0c0950
Sven Klemm <sven@timescale.com>
2020-08-12 14:45:20 +0200

    Limit resultsets for constraint_exclusion_prepared test
    This patches limits the resultset for the constraint exclusion test
    with prepared statements to make them run in a more reasonable time.

--- tsl/test/shared/sql/include/constraint_exclusion_prepared.sql
+++ tsl/test/shared/sql/include/constraint_exclusion_prepared.sql
@@ -4,7 +4,15 @@
 
 -- test prepared statements
 -- executor startup exclusion with no chunks excluded
-PREPARE prep AS SELECT time FROM :TEST_TABLE WHERE time < now() AND device_id = 1 ORDER BY time;
+
+PREPARE prep AS
+SELECT time
+FROM :TEST_TABLE
+WHERE time < now()
+  AND device_id = 1
+ORDER BY time
+LIMIT 100;
+
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
@@ -13,7 +21,14 @@ PREPARE prep AS SELECT time FROM :TEST_TABLE WHERE time < now() AND device_id = 
 DEALLOCATE prep;
 
 -- executor startup exclusion with chunks excluded
-PREPARE prep AS SELECT time FROM :TEST_TABLE WHERE time < '2000-01-10'::text::timestamptz AND device_id = 1 ORDER BY time;
+PREPARE prep AS
+SELECT time
+FROM :TEST_TABLE
+WHERE time < '2000-01-10'::text::timestamptz
+  AND device_id = 1
+ORDER BY time
+LIMIT 100;
+
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
@@ -22,7 +37,19 @@ PREPARE prep AS SELECT time FROM :TEST_TABLE WHERE time < '2000-01-10'::text::ti
 DEALLOCATE prep;
 
 -- runtime exclusion with LATERAL and 2 hypertables
-PREPARE prep AS SELECT m1.time, m2.time FROM :TEST_TABLE m1 LEFT JOIN LATERAL(SELECT time FROM :TEST_TABLE m2 WHERE m1.time = m2.time LIMIT 1) m2 ON true WHERE device_id = 2 ORDER BY m1.time;
+PREPARE prep AS
+SELECT m1.time,
+  m2.time
+FROM :TEST_TABLE m1
+  LEFT JOIN LATERAL (
+    SELECT time
+    FROM :TEST_TABLE m2
+    WHERE m1.time = m2.time
+    LIMIT 1) m2 ON TRUE
+WHERE device_id = 2
+ORDER BY m1.time
+LIMIT 100;
+
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
@@ -31,7 +58,15 @@ PREPARE prep AS SELECT m1.time, m2.time FROM :TEST_TABLE m1 LEFT JOIN LATERAL(SE
 DEALLOCATE prep;
 
 -- executor startup exclusion with subquery
-PREPARE prep AS SELECT time FROM (SELECT time FROM :TEST_TABLE WHERE time < '2000-01-10'::text::timestamptz ORDER BY time LIMIT 100) m;
+PREPARE prep AS
+SELECT time
+FROM (
+  SELECT time
+  FROM :TEST_TABLE
+  WHERE time < '2000-01-10'::text::timestamptz
+  ORDER BY time
+  LIMIT 100) m;
+
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
@@ -40,13 +75,26 @@ PREPARE prep AS SELECT time FROM (SELECT time FROM :TEST_TABLE WHERE time < '200
 DEALLOCATE prep;
 
 -- test constraint exclusion for subqueries with ConstraintAwareAppend
-SET timescaledb.enable_chunk_append TO false;
-PREPARE prep AS SELECT device_id, time FROM (SELECT device_id, time FROM :TEST_TABLE WHERE time < '2000-01-10'::text::timestamptz ORDER BY device_id, time LIMIT 100) m;
+SET timescaledb.enable_chunk_append TO FALSE;
+
+PREPARE prep AS
+SELECT device_id,
+  time
+FROM (
+  SELECT device_id,
+    time
+  FROM :TEST_TABLE
+  WHERE time < '2000-01-10'::text::timestamptz
+  ORDER BY device_id,
+    time
+  LIMIT 100) m;
+
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
 :PREFIX EXECUTE prep;
 DEALLOCATE prep;
+
 RESET timescaledb.enable_chunk_append;
 
