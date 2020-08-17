ab996a4ea757fe381ba8e0eaa3ac1ecd01cc762b
Mats Kindahl <mats@timescale.com>
2020-08-17 19:11:49 +0200

    Add debug waitpoint implementation
    Add implementation for debug waitpoints. Debug waitpoints can be added
    to code and can be enabled using the `ts_debug_waitpoint_enable`
    function. Once execution reaches this point, the session will block
    waiting for a call to `ts_debug_waitpoint_release`.
    
    Waitpoints are added to code by using the macro `DEBUG_WAITPOINT` with
    a string, for example:
    
        DEBUG_WAITPOINT("references_fetched");
    
    The string is hashed to compute a 32-bit number that is then used as an
    shared advisory lock. The waitpoint can be enabled with the function
    `ts_debug_waitpoint_enable`. This function takes a string and computes
    a hash and will use the hash to take an exclusive advisory lock. This
    will cause all sessions reaching the waitpoint to block until the lock
    is released with `ts_debug_waitpoint_release`.

---
+++ src/debug_wait.c
@@ -0,0 +1,109 @@
+/*
+ * This file and its contents are licensed under the Apache License 2.0.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-APACHE for a copy of the license.
+ */
+
+#include "debug_wait.h"
+
+#include <postgres.h>
+
+#include <fmgr.h>
+
+#include <access/hash.h>
+#include <storage/lock.h>
+#include <miscadmin.h>
+#include <utils/builtins.h>
+
+#include "export.h"
+
+void
+ts_debug_waitpoint_init(DebugWait *waitpoint, const char *tagname)
+{
+	/* Use 64-bit hashing to get two independent 32-bit hashes */
+	uint64 hash = DatumGetUInt32(hash_any((const unsigned char *) tagname, strlen(tagname)));
+
+	SET_LOCKTAG_ADVISORY(waitpoint->tag, MyDatabaseId, (uint32)(hash >> 32), (uint32) hash, 1);
+	waitpoint->tagname = pstrdup(tagname);
+	ereport(DEBUG1, (errmsg("initializing waitpoint '%s' to use %lu", waitpoint->tagname, hash)));
+}
+
+/*
+ * Wait for the waitpoint to be released.
+ *
+ * This is handled by first trying to get a shared lock, which will not block
+ * other sessions that try to grab the same lock but will block if an
+ * exclusive lock is already taken, and then release the lock immediately
+ * after.
+ */
+void
+ts_debug_waitpoint_wait(DebugWait *waitpoint)
+{
+	LockAcquireResult lock_acquire_result pg_attribute_unused();
+	bool lock_release_result pg_attribute_unused();
+
+	ereport(DEBUG1, (errmsg("waiting on waitpoint '%s'", waitpoint->tagname)));
+
+	/* Take the lock. This should always succeed, anything else is a bug. */
+	lock_acquire_result = LockAcquire(&waitpoint->tag, ShareLock, true, false);
+	Assert(lock_acquire_result == LOCKACQUIRE_OK);
+
+	lock_release_result = LockRelease(&waitpoint->tag, ShareLock, true);
+	Assert(lock_release_result);
+
+	ereport(DEBUG1, (errmsg("proceeding after waitpoint '%s'", waitpoint->tagname)));
+}
+
+static void
+debug_waitpoint_enable(DebugWait *waitpoint)
+{
+	ereport(DEBUG1, (errmsg("enabling waitpoint \"%s\"", waitpoint->tagname)));
+	if (LockAcquire(&waitpoint->tag, ExclusiveLock, true, true) == LOCKACQUIRE_NOT_AVAIL)
+		ereport(NOTICE, (errmsg("debug waitpoint \"%s\" already enabled", waitpoint->tagname)));
+}
+
+static void
+debug_waitpoint_release(DebugWait *waitpoint)
+{
+	ereport(DEBUG1, (errmsg("releasing waitpoint \"%s\"", waitpoint->tagname)));
+	if (!LockRelease(&waitpoint->tag, ExclusiveLock, true))
+		elog(ERROR, "cannot release waitpoint \"%s\"", waitpoint->tagname);
+}
+
+/*
+ * Enable a waitpoint to block when being reached.
+ *
+ * This function will always succeed since we will not lock the waitpoint if
+ * it is already locked. A notice will be printed if the waitpoint is already
+ * enabled.
+ */
+TS_FUNCTION_INFO_V1(ts_debug_waitpoint_enable);
+Datum ts_debug_waitpoint_enable(PG_FUNCTION_ARGS)
+{
+	text *tag = PG_GETARG_TEXT_PP(0);
+	DebugWait waitpoint;
+
+	if (PG_ARGISNULL(0))
+		ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("no tag provided")));
+
+	ts_debug_waitpoint_init(&waitpoint, text_to_cstring(tag));
+	debug_waitpoint_enable(&waitpoint);
+	PG_RETURN_VOID();
+}
+
+/*
+ * Release a waitpoint allowing execution to proceed.
+ */
+TS_FUNCTION_INFO_V1(ts_debug_waitpoint_release);
+Datum ts_debug_waitpoint_release(PG_FUNCTION_ARGS)
+{
+	text *tag = PG_GETARG_TEXT_PP(0);
+	DebugWait waitpoint;
+
+	if (PG_ARGISNULL(0))
+		ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE), errmsg("no tag provided")));
+
+	ts_debug_waitpoint_init(&waitpoint, text_to_cstring(tag));
+	debug_waitpoint_release(&waitpoint);
+	PG_RETURN_VOID();
+}
---
+++ src/debug_wait.h
@@ -0,0 +1,52 @@
+/*
+ * This file and its contents are licensed under the Apache License 2.0.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-APACHE for a copy of the license.
+ */
+
+#ifndef TIMESCALEDB_DEBUG_WAIT_H_
+#define TIMESCALEDB_DEBUG_WAIT_H_
+
+#include <postgres.h>
+
+#include <storage/lock.h>
+
+/* Debug waitpoint tag for debug waitpoints.
+ *
+ * Debug waitpoints only exists in debug-built code and is intended to allow
+ * more controlled testing of the code by creating wait-points where execution
+ * will halt until explicitly released.
+ *
+ * Each debug waitpoint is identified by a string that is hashed to a 8-byte
+ * number and used with the normal advisory locks available in PostgreSQL.
+ *
+ * When blocking on a waitpoint, there is an attempt to take a shared lock on
+ * the waitpoint. If the waitpoint is enabled by locking using an exclusive
+ * lock, this will block all waiters. Once the exclusive lock is released, all
+ * waiters will be able to proceed.
+ */
+typedef struct DebugWait
+{
+	const char *tagname;
+	LOCKTAG tag;
+} DebugWait;
+
+void ts_debug_waitpoint_init(DebugWait *waitpoint, const char *tagname);
+void ts_debug_waitpoint_wait(DebugWait *waitpoint);
+
+#ifdef TS_DEBUG
+#define DEBUG_WAITPOINT(TAG)                                                                       \
+	do                                                                                             \
+	{                                                                                              \
+		DebugWait waitpoint;                                                                       \
+		ts_debug_waitpoint_init(&waitpoint, (TAG));                                                \
+		ts_debug_waitpoint_wait(&waitpoint);                                                       \
+	} while (0)
+#else
+#define DEBUG_WAITPOINT(TAG)                                                                       \
+	do                                                                                             \
+	{                                                                                              \
+	} while (0)
+#endif
+
+#endif /* TIMESCALEDB_DEBUG_WAIT_H_ */
