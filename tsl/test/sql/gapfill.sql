f5f6d7754e912ef2e8abff1de885817967b77aaf
Sven Klemm <sven@timescale.com>
2020-08-29 17:41:02 +0200

    Merge gapfill tests into single test

--- tsl/test/sql/gapfill.sql
+++ tsl/test/sql/gapfill.sql
@@ -2,6 +2,142 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-TIMESCALE for a copy of the license.
 
+\set EXPLAIN 'EXPLAIN (COSTS OFF)'
+
+CREATE TABLE gapfill_plan_test(time timestamptz NOT NULL, value float);
+SELECT table_name FROM create_hypertable('gapfill_plan_test','time',chunk_time_interval=>'4 weeks'::interval);
+
+INSERT INTO gapfill_plan_test SELECT generate_series('2018-01-01'::timestamptz,'2018-04-01'::timestamptz,'1m'::interval), 1.0;
+
+-- simple example
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,now(),now()),
+  avg(c2)
+FROM (VALUES (now(),1),(now(),NULL),(now(),NULL)) as t(time,c2)
+GROUP BY 1
+ORDER BY 1;
+
+-- test sorting
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,now(),now()),
+  avg(c2)
+FROM (VALUES (now(),1),(now(),NULL),(now(),NULL)) as t(time,c2)
+GROUP BY 1
+ORDER BY 2;
+
+-- test sort direction
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,now(),now()),
+  avg(c2)
+FROM (VALUES (now(),1),(now(),NULL),(now(),NULL)) as t(time,c2)
+GROUP BY 1
+ORDER BY 1 DESC;
+
+-- test order by aggregate function
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,now(),now()),
+  avg(c2)
+FROM (VALUES (now(),1),(now(),NULL),(now(),NULL)) as t(time,c2)
+GROUP BY 1
+ORDER BY 2,1;
+
+-- test query without order by
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,now(),now()),
+  avg(c2)
+FROM (VALUES (now(),1),(now(),NULL),(now(),NULL)) as t(time,c2)
+GROUP BY 1;
+
+-- test parallel query
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,to_timestamp(0),to_timestamp(0)),
+  avg(value)
+FROM gapfill_plan_test
+GROUP BY 1
+ORDER BY 1;
+
+-- test parallel query with locf
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,to_timestamp(0),to_timestamp(0)),
+  locf(avg(value))
+FROM gapfill_plan_test
+GROUP BY 1
+ORDER BY 1;
+
+-- test parallel query with interpolate
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,to_timestamp(0),to_timestamp(0)),
+  interpolate(avg(value))
+FROM gapfill_plan_test
+GROUP BY 1
+ORDER BY 1;
+
+-- make sure we can run gapfill in parallel workers
+-- ensure this plan runs in parallel
+:EXPLAIN
+SELECT
+  time_bucket_gapfill('5m',time,to_timestamp(0),to_timestamp(0)),
+  interpolate(avg(value))
+FROM gapfill_plan_test
+GROUP BY 1
+ORDER BY 2
+LIMIT 1;
+
+-- actually run a parallel gapfill
+SELECT
+  time_bucket_gapfill('5m',time,to_timestamp(0),to_timestamp(0)),
+  interpolate(avg(value))
+FROM gapfill_plan_test
+GROUP BY 1
+ORDER BY 2
+LIMIT 1;
+
+-- test sort optimizations
+
+-- test sort optimization with single member order by,
+-- should use index scan (no GapFill node for this one since we're not gapfilling)
+:EXPLAIN SELECT time_bucket_gapfill('5m',time),value
+FROM gapfill_plan_test
+ORDER BY 1;
+
+SET max_parallel_workers_per_gather TO 0;
+
+-- test sort optimizations with locf
+:EXPLAIN SELECT time_bucket_gapfill('5m',time,to_timestamp(0),to_timestamp(0)), locf(avg(value))
+FROM gapfill_plan_test
+GROUP BY 1
+ORDER BY 1;
+
+-- test sort optimizations with interpolate
+:EXPLAIN SELECT time_bucket_gapfill('5m',time,to_timestamp(0),to_timestamp(0)), interpolate(avg(value))
+FROM gapfill_plan_test
+GROUP BY 1
+ORDER BY 1;
+
+RESET max_parallel_workers_per_gather;
+
+CREATE INDEX ON gapfill_plan_test(value, time);
+
+-- test sort optimization with ordering by multiple columns and time_bucket_gapfill not last,
+-- must not use index scan
+:EXPLAIN  SELECT time_bucket_gapfill('5m',time),value
+FROM gapfill_plan_test
+ORDER BY 1,2;
+
+-- test sort optimization with ordering by multiple columns and time_bucket as last member,
+-- should use index scan
+:EXPLAIN SELECT time_bucket_gapfill('5m',time),value
+FROM gapfill_plan_test
+ORDER BY 2,1;
+
 CREATE TABLE metrics_int(time int,device_id int, sensor_id int, value float);
 
 INSERT INTO metrics_int VALUES
@@ -932,7 +1068,7 @@ GROUP BY 1 ORDER BY 2 NULLS LAST,1;
 
 -- test queries on hypertable
 CREATE TABLE metrics_tstz(time timestamptz, device_id INT, v1 float, v2 int);
-SELECT create_hypertable('metrics_tstz','time');
+SELECT table_name FROM create_hypertable('metrics_tstz','time');
 INSERT INTO metrics_tstz VALUES
 (timestamptz '2018-01-01 05:00:00 PST', 1, 0.5, 10),
 (timestamptz '2018-01-01 05:00:00 PST', 2, 0.7, 20),
@@ -1497,9 +1633,6 @@ SELECT
 FROM (VALUES (1,1),(2,2)) v(time,device_id)
 GROUP BY 1,device_id;
 
-
-
-
 --test interpolation with big diifferences in values (test overflows in calculations)
 --we use the biggest possible difference in time(x) and the value(y).
 --For bigints we also test values of smaller than bigintmax/min to avoid
@@ -1517,3 +1650,4 @@ SELECT
 FROM (values (:big_int_min,(-32768)::smallint,(-2147483648)::int,:big_int_min,-2147483648::bigint, '-Infinity'::double precision),
              (:big_int_max, 32767::smallint, 2147483647::int,:big_int_max, 2147483647::bigint, 'Infinity'::double precision)) v(time,s,i,b,b2,d)
 GROUP BY 1 ORDER BY 1;
+
