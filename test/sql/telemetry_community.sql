84f6c26df5bbdde7d5d9215fdd03f5e35ccca4be
Sven Klemm <sven@timescale.com>
2020-08-31 15:11:46 +0200

    Merge telemetry_community and telemetry_compression

--- test/sql/telemetry_community.sql
+++ test/sql/telemetry_community.sql
@@ -13,7 +13,6 @@ SELECT json_object_field(get_telemetry_report(always_display_report := true)::js
 -- check timescaledb_telemetry.cloud
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'instance_metadata');
 
-
 --create a continuous agg
 CREATE TABLE device_readings (
       observation_time  TIMESTAMPTZ       NOT NULL
@@ -37,3 +36,19 @@ SELECT json_object_field(get_telemetry_report(always_display_report := true)::js
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'uncompressed_heap_size');
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'uncompressed_index_size');
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'uncompressed_toast_size');
+
+--test compression
+ALTER TABLE device_readings SET(timescaledb.compress);
+insert into device_readings select  generate_series('2018-01-01 00:00'::timestamp, '2018-01-02 00:00'::timestamp, '1 hour');
+select  count(compress_chunk(ch1.schema_name|| '.' || ch1.table_name))
+FROM _timescaledb_catalog.chunk ch1, _timescaledb_catalog.hypertable ht where ch1.hypertable_id = ht.id and ht.table_name like 'device_readings';
+
+SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'num_hypertables');
+SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'num_compressed_hypertables');
+SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'compressed_heap_size');
+SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'compressed_index_size');
+SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'compressed_toast_size');
+SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'uncompressed_heap_size');
+SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'uncompressed_index_size');
+SELECT json_object_field(get_telemetry_report(always_display_report := true)::json,'uncompressed_toast_size');
+
