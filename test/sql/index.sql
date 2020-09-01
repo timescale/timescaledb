4bc88cb6948f660bc8ec2f038fa35e38f24f6519
Sven Klemm <sven@timescale.com>
2020-09-01 16:51:27 +0200

    Merge index and reindex test
    Combine index and reindex test to reduce number of test cases.

--- test/sql/index.sql
+++ test/sql/index.sql
@@ -259,5 +259,54 @@ CREATE INDEX IF NOT EXISTS idx_exists_time_asc_idx ON idx_exists(time ASC);
 -- should be skipped since it was created in previous command
 CREATE INDEX IF NOT EXISTS idx_exists_time_asc_idx ON idx_exists(time ASC);
 
+-- test reindex
+CREATE TABLE reindex_test(time timestamp, temp float, PRIMARY KEY(time, temp));
+CREATE UNIQUE INDEX reindex_test_time_unique_idx ON reindex_test(time);
 
+-- create hypertable with three chunks
+SELECT create_hypertable('reindex_test', 'time', chunk_time_interval => 2628000000000);
+
+INSERT INTO reindex_test VALUES ('2017-01-20T09:00:01', 17.5),
+                                ('2017-01-21T09:00:01', 19.1),
+                                ('2017-04-20T09:00:01', 89.5),
+                                ('2017-04-21T09:00:01', 17.1),
+                                ('2017-06-20T09:00:01', 18.5),
+                                ('2017-06-21T09:00:01', 11.0);
+
+SELECT * FROM test.show_columns('reindex_test');
+SELECT * FROM test.show_subtables('reindex_test');
+
+-- show reindexing
+REINDEX (VERBOSE) TABLE reindex_test;
+
+\set ON_ERROR_STOP 0
+-- this one currently doesn't recurse to chunks and instead gives an
+-- error
+REINDEX (VERBOSE) INDEX reindex_test_time_unique_idx;
+\set ON_ERROR_STOP 1
+
+-- show reindexing on a normal table
+CREATE TABLE reindex_norm(time timestamp, temp float);
+CREATE UNIQUE INDEX reindex_norm_time_unique_idx ON reindex_norm(time);
+
+INSERT INTO reindex_norm VALUES ('2017-01-20T09:00:01', 17.5),
+                                ('2017-01-21T09:00:01', 19.1),
+                                ('2017-04-20T09:00:01', 89.5),
+                                ('2017-04-21T09:00:01', 17.1),
+                                ('2017-06-20T09:00:01', 18.5),
+                                ('2017-06-21T09:00:01', 11.0);
+
+REINDEX (VERBOSE) TABLE reindex_norm;
+REINDEX (VERBOSE) INDEX reindex_norm_time_unique_idx;
+
+SELECT * FROM test.show_constraintsp('_timescaledb_internal._hyper_12%');
+SELECT * FROM reindex_norm;
+
+SELECT * FROM test.show_indexes('_timescaledb_internal._hyper_12_12_chunk');
+SELECT chunk_index_clone::regclass::text
+FROM _timescaledb_internal.chunk_index_clone('_timescaledb_internal."12_3_reindex_test_pkey"'::regclass);
+SELECT * FROM test.show_indexes('_timescaledb_internal._hyper_12_12_chunk');
+SELECT * FROM _timescaledb_internal.chunk_index_replace('_timescaledb_internal."12_3_reindex_test_pkey"'::regclass, '_timescaledb_internal."_hyper_12_12_chunk_12_3_reindex_test_pkey"'::regclass);
+
+SELECT * FROM test.show_indexes('_timescaledb_internal._hyper_12_12_chunk');
 
