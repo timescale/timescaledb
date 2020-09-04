9730a5aa521bf2fd1aa39ddf16516aba6c2431c2
Sven Klemm <sven@timescale.com>
2020-09-04 11:13:04 +0200

    Fix copy/paste error in AT_CookedColumnDefault handling

--- src/build-defs.cmake
+++ src/build-defs.cmake
@@ -41,7 +41,7 @@ endif()
 # To be able to build against snapshots and REL_12_STABLE branch we check header file
 # for existance. Once PG 12.5 is out this can safely be turned into a version check.
 file(READ ${PG_INCLUDEDIR_SERVER}/nodes/parsenodes.h PG_PARSENODES_H)
-string(REGEX MATCH "AT_CookedColumnDefault" PG_HAS_COOKEDCOLUMNDEFAULT ${PG_CONFIG_H})
+string(REGEX MATCH "AT_CookedColumnDefault" PG_HAS_COOKEDCOLUMNDEFAULT ${PG_PARSENODES_H})
 if (PG_HAS_COOKEDCOLUMNDEFAULT)
   add_definitions(-DPG_HAS_COOKEDCOLUMNDEFAULT)
 endif()
