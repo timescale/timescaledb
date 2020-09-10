6251a3906cc07107cbff77899956ed1c24a9f531
Erik Nordström <erik@timescale.com>
2020-09-10 20:42:58 +0200

    Remove completed threshold
    The completed threshold in the TimescaleDB catalog is no longer used
    by the refactored continuous aggregates, so it is removed.
    
    Fixes #2178

--- src/continuous_agg.h
+++ src/continuous_agg.h
@@ -63,7 +63,6 @@ ts_continuous_agg_find_by_mat_hypertable_id(int32 mat_hypertable_id);
 extern TSDLLEXPORT ContinuousAggHypertableStatus
 ts_continuous_agg_hypertable_status(int32 hypertable_id);
 extern TSDLLEXPORT List *ts_continuous_aggs_find_by_raw_table_id(int32 raw_hypertable_id);
-extern TSDLLEXPORT int64 ts_continuous_agg_get_completed_threshold(int32 materialization_id);
 extern TSDLLEXPORT ContinuousAgg *ts_continuous_agg_find_by_view_name(const char *schema,
 																	  const char *name,
 																	  ContinuousAggViewType type);
