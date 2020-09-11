e28ce96141cb3dcd8f9de30ab1ac852d887fb86c
Sven Klemm <sven@timescale.com>
2020-09-11 18:09:34 +0200

    Check sequence values don't get reset in update test

--- test/sql/updates/post.sequences.sql
+++ test/sql/updates/post.sequences.sql
@@ -2,10 +2,13 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
-\ir post.v2.sql
-\ir catalog_missing_columns.sql
-\ir post.compression.sql
-\ir post.continuous_aggs.v2.sql
-\ir post.policies.sql
-\ir post.sequences.sql
+-- check sequences did not get reset
+SELECT seqrelid::regclass,
+  nextval(seqrelid),
+  seqstart,
+  seqincrement,
+  seqmax,
+  seqmin
+FROM pg_sequence
+ORDER BY seqrelid::regclass::text;
 
