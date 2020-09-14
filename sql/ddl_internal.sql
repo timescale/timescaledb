1068188128622d7facf89e9cefe765edaeb7bc45
Erik Nordström <erik@timescale.com>
2020-09-14 08:34:02 +0200

    Make block chunks API internal
    This change moves the block chunks functionality to the internal
    namespace since it won't be part of the public API for the 2.0
    release.
    
    Fixes #2236

--- sql/ddl_internal.sql
+++ sql/ddl_internal.sql
@@ -8,3 +8,15 @@ AS '@MODULE_PATHNAME@', 'ts_chunk_index_clone' LANGUAGE C VOLATILE STRICT;
 
 CREATE OR REPLACE FUNCTION _timescaledb_internal.chunk_index_replace(chunk_index_oid_old OID, chunk_index_oid_new OID) RETURNS VOID
 AS '@MODULE_PATHNAME@', 'ts_chunk_index_replace' LANGUAGE C VOLATILE STRICT;
+
+-- Block new chunk creation on a data node for a distributed
+-- hypertable. NULL hypertable means it will block chunks for all
+-- distributed hypertables
+CREATE OR REPLACE FUNCTION _timescaledb_internal.block_new_chunks(data_node_name NAME, hypertable REGCLASS = NULL, force BOOLEAN = FALSE) RETURNS INTEGER
+AS '@MODULE_PATHNAME@', 'ts_data_node_block_new_chunks' LANGUAGE C VOLATILE;
+
+-- Allow chunk creation on a blocked data node for a distributed
+-- hypertable. NULL hypertable means it will allow chunks for all
+-- distributed hypertables
+CREATE OR REPLACE FUNCTION _timescaledb_internal.allow_new_chunks(data_node_name NAME, hypertable REGCLASS = NULL) RETURNS INTEGER
+AS '@MODULE_PATHNAME@', 'ts_data_node_allow_new_chunks' LANGUAGE C VOLATILE;
