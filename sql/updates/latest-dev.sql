453ee02cf24ea1c92417ad04515816d2473feffe
Mats Kindahl <mats@timescale.com>
2020-09-30 10:00:47 +0200

    Show more information in get_git_commit
    The command `get_git_commit` shows quite a little information and
    especially if mistakes are made it is hard to verify what the binary is
    actually based on.
    
    This commit extends the function to provide some more information:
    specifically the SHA of the HEAD regardless of whether there is a tag
    on it or not and the time for the HEAD commit.
    
    ```
    postgres=# select * from _timescaledb_internal.get_git_commit();
            commit_tag        | commit_hash |      commit_time
    --------------------------+-------------+------------------------
     1.7.4-10-g09b0b778-dirty | 09b0b778    | 2020-09-13 17:50:38+02
    (1 row)
    ```
    
    If git is not installed, the function `get_git_commit` will return an
    error indicating that no git information is available. If some of the
    fields are available, they will be emitted and the remaining fields
    will be NULL.
    
    Fixes #2457

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,2 +1,4 @@
 DROP VIEW IF EXISTS timescaledb_information.continuous_aggregates;
 DROP VIEW IF EXISTS timescaledb_information.job_stats;
+DROP FUNCTION IF EXISTS _timescaledb_internal.get_git_commit;
+
