dbd707b17ee1087ea9a651179fb62f759c66707d
Sven Klemm <sven@timescale.com>
2020-10-05 17:39:47 +0200

    Fix datanode sql function script
    This patch merge data_node and remote_txn sql scripts and fixes
    the CREATE FUNCTION statement to use CREATE OR REPLACE FUNCTION.

--- sql/remote_txn.sql
+++
@@ -1,8 +0,0 @@
--- This file and its contents are licensed under the Apache License 2.0.
--- Please see the included NOTICE for copyright information and
--- LICENSE-APACHE for a copy of the license.
-
-CREATE FUNCTION _timescaledb_internal.remote_txn_heal_data_node(foreign_server_oid oid)
-RETURNS INT
-AS '@MODULE_PATHNAME@', 'ts_remote_txn_heal_data_node'
-LANGUAGE C STRICT;
