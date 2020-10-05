43ef211ae676dd14aa33f76e65afe32dc5f8d568
Sven Klemm <sven@timescale.com>
2020-10-05 17:18:33 +0200

    Fix timescaledb_fdw sql script
    Since CREATE FOREIGN DATA WRAPPER is not idempotent it must not be
    grouped with the normal sql scripts but has to be in the pre_install
    group.

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -407,3 +407,17 @@ BEGIN
 END
 $$;
 
+CREATE OR REPLACE FUNCTION timescaledb_fdw_handler()
+RETURNS fdw_handler
+AS '@MODULE_PATHNAME@', 'ts_timescaledb_fdw_handler'
+LANGUAGE C STRICT;
+
+CREATE OR REPLACE FUNCTION timescaledb_fdw_validator(text[], oid)
+RETURNS void
+AS '@MODULE_PATHNAME@', 'ts_timescaledb_fdw_validator'
+LANGUAGE C STRICT;
+
+CREATE FOREIGN DATA WRAPPER timescaledb_fdw
+  HANDLER timescaledb_fdw_handler
+  VALIDATOR timescaledb_fdw_validator;
+
