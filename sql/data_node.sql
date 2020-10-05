17d3fb1e0dd6ea7303b50245421d599f0d9ee59e
Sven Klemm <sven@timescale.com>
2020-10-05 17:39:47 +0200

    Fix datanode sql function script
    This patch merge data_node and remote_txn sql scripts and fixes
    the CREATE FUNCTION statement to use CREATE OR REPLACE FUNCTION.

--- sql/data_node.sql
+++ sql/data_node.sql
@@ -3,5 +3,10 @@
 -- LICENSE-APACHE for a copy of the license.
 
 -- Check if a data node is up
-CREATE FUNCTION _timescaledb_internal.ping_data_node(node_name NAME) RETURNS BOOLEAN
+CREATE OR REPLACE FUNCTION _timescaledb_internal.ping_data_node(node_name NAME) RETURNS BOOLEAN
 AS '@MODULE_PATHNAME@', 'ts_data_node_ping' LANGUAGE C VOLATILE;
+
+CREATE OR REPLACE FUNCTION _timescaledb_internal.remote_txn_heal_data_node(foreign_server_oid oid)
+RETURNS INT
+AS '@MODULE_PATHNAME@', 'ts_remote_txn_heal_data_node'
+LANGUAGE C STRICT;
