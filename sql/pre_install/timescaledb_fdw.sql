5a5cbba38386b2a24075970876e3478590214b6d
Sven Klemm <sven@timescale.com>
2020-10-12 16:23:46 +0200

    Fix timescaledb_fdw function handling in update script
    This patch splits the timescaledb_fdw sql file into two parts to
    separate the idempotent parts from the non-idempotent ones so
    the function definitions can be included in the regular update
    script.

--- sql/pre_install/timescaledb_fdw.sql
+++ sql/pre_install/timescaledb_fdw.sql
@@ -2,16 +2,6 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
-CREATE OR REPLACE FUNCTION timescaledb_fdw_handler()
-RETURNS fdw_handler
-AS '@MODULE_PATHNAME@', 'ts_timescaledb_fdw_handler'
-LANGUAGE C STRICT;
-
-CREATE OR REPLACE FUNCTION timescaledb_fdw_validator(text[], oid)
-RETURNS void
-AS '@MODULE_PATHNAME@', 'ts_timescaledb_fdw_validator'
-LANGUAGE C STRICT;
-
 CREATE FOREIGN DATA WRAPPER timescaledb_fdw
   HANDLER timescaledb_fdw_handler
   VALIDATOR timescaledb_fdw_validator;
