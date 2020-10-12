437873e3b0722bd93e1f4bf5a8c99866d3e31435
Sven Klemm <sven@timescale.com>
2020-10-12 16:30:03 +0200

    Check function linkage in update test
    This patch adds a check that all c functions link to the correct
    library after an update.

--- test/sql/updates/post.functions.sql
+++ test/sql/updates/post.functions.sql
@@ -2,11 +2,14 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
-\ir post.v2.sql
-\ir catalog_missing_columns.sql
-\ir post.compression.sql
-\ir post.continuous_aggs.v2.sql
-\ir post.policies.sql
-\ir post.sequences.sql
-\ir post.functions.sql
+-- check all functions point to the correct library
+SELECT
+  oid::REGPROCEDURE,
+  probin
+FROM
+  pg_proc
+WHERE
+  probin LIKE '%timescale%'
+ORDER BY
+  oid::REGPROCEDURE::TEXT;
 
