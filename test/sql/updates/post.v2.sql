9f93fe506a0e145d3db4dbb6b33c02f8f7a31ae8
Sven Klemm <sven@timescale.com>
2020-10-13 15:37:09 +0200

    Clean up update tests
    Refactor update test files and remove obsolete test files.

--- test/sql/updates/post.v2.sql
+++ test/sql/updates/post.v2.sql
@@ -2,12 +2,7 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
-\ir post.pre_insert.sql
-
--- INSERT data to create a new chunk after update or restore.
-INSERT INTO devices(id,floor) VALUES
-('dev5', 5);
-INSERT INTO "two_Partitions"("timeCustom", device_id, device_id_2, series_0, series_1, series_2) VALUES
-(1258894000000000000, 'dev5', 'dev1', 2.2, 1, 2);
-
-\ir post.post_insert.sql
+\ir post.catalog.sql
+\ir post.insert.sql
+\ir post.integrity_test.sql
+\ir post.functions.sql
