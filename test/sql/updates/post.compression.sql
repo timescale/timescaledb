f545b6d7e251a4f29ed08c01c642aaee197aebc1
Sven Klemm <sven@timescale.com>
2020-10-15 22:48:02 +0200

    Add test for statistics on compressed chunks
    Add a test for updating statistics on chunks compressed before
    statistics where kept. This patch also sets metadata for previously
    compressed chunks so running ANALYZE on them will set the correct
    tuplecount.

--- test/sql/updates/post.compression.sql
+++ test/sql/updates/post.compression.sql
@@ -8,9 +8,42 @@ INSERT INTO compress
 SELECT g, 'QW', g::text, 2, 0, (100,4)::custom_type_for_compression, false
 FROM generate_series('2019-11-01 00:00'::timestamp, '2019-12-15 00:00'::timestamp, '1 day') g;
 
-SELECT count(compress_chunk(chunk.schema_name|| '.' || chunk.table_name)) as count_compressed
-FROM _timescaledb_catalog.chunk chunk
-INNER JOIN _timescaledb_catalog.hypertable hypertable ON (chunk.hypertable_id = hypertable.id)
-WHERE hypertable.table_name = 'compress' and chunk.compressed_chunk_id IS NULL;
+SELECT
+  count(compress_chunk(chunk.schema_name || '.' || chunk.table_name)) AS count_compressed
+FROM
+  _timescaledb_catalog.chunk chunk
+  INNER JOIN _timescaledb_catalog.hypertable hypertable ON (chunk.hypertable_id = hypertable.id)
+WHERE
+  hypertable.table_name = 'compress'
+  AND chunk.compressed_chunk_id IS NULL;
 
 SELECT * FROM compress ORDER BY time DESC, small_cardinality;
+
+-- check count and approximate_row_count are the same after analyze
+ANALYZE compress;
+SELECT
+  count,
+  approximate,
+  CASE WHEN count != approximate THEN
+    'counts not matching' || random()::TEXT
+  ELSE
+    'match'
+  END AS MATCH
+FROM (
+  SELECT
+    count(*)
+  FROM
+    compress) AS count,
+  approximate_row_count('compress') AS approximate;
+
+SELECT
+  hypertable_schema,
+  hypertable_name,
+  approximate_row_count(format('%I.%I', hypertable_schema, hypertable_name)::REGCLASS)
+FROM
+  timescaledb_information.hypertables
+WHERE
+  compression_enabled = true
+ORDER BY
+  1,
+  2;
