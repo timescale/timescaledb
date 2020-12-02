ccc1d08e9efa3b42b4612e5cd09750559741a031
Mats Kindahl <mats@timescale.com>
2020-12-02 21:27:45 +0100

    Fix ABI check build dependencies and tests
    Some tests contain code that only work if the build is based on a Git
    clone, which caused these tests to fail when Git was not available.
    This commit splits out those tests and only enabling them if Git is
    found and Git information can be retrieved.

--- test/sql/version.sql
+++ test/sql/version.sql
@@ -2,11 +2,5 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
--- Check what get_git_commit returns
-SELECT pg_typeof(commit_tag) AS commit_tag_type,
-       pg_typeof(commit_hash) AS commit_hash_type,
-       pg_typeof(commit_time) AS commit_time_type
-  FROM _timescaledb_internal.get_git_commit();
-
 -- Test that get_os_info returns 3 x text
 select pg_typeof(sysname) AS sysname_type,pg_typeof(version) AS version_type,pg_typeof(release) AS release_type from _timescaledb_internal.get_os_info();
