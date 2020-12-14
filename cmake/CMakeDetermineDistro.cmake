9cd6b56e945048c31440f490d7fbe28eb2e01d77
Mats Kindahl <mats@timescale.com>
2020-12-14 07:51:48 +0100

    Make lsb_release optional

--- cmake/CMakeDetermineDistro.cmake
+++ cmake/CMakeDetermineDistro.cmake
@@ -10,24 +10,24 @@ if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
     lsb_release
     PATHS /usr/bin /usr/local/bin /usr/local/opt)
   if(NOT LSB_RELEASE)
-    message(FATAL_ERROR "Cannot find lsb_release")
-  endif()
-
-  execute_process(
-    COMMAND ${LSB_RELEASE} -is
-    OUTPUT_VARIABLE _identifier
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-  string(TOLOWER "${_identifier}" CMAKE_DISTRO_NAME)
+    message(WARNING "Cannot find lsb_release, not possible to build packages")
+  else()
+    execute_process(
+      COMMAND ${LSB_RELEASE} -is
+      OUTPUT_VARIABLE _identifier
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    string(TOLOWER "${_identifier}" CMAKE_DISTRO_NAME)
     
-  execute_process(
-    COMMAND ${LSB_RELEASE} -rs
-    OUTPUT_VARIABLE _release
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
-  set(CMAKE_DISTRO_RELEASE ${_release})
-endif()
+    execute_process(
+      COMMAND ${LSB_RELEASE} -rs
+      OUTPUT_VARIABLE _release
+      OUTPUT_STRIP_TRAILING_WHITESPACE)
+    set(CMAKE_DISTRO_RELEASE ${_release})
 
-message(STATUS "Distribution identifier: ${CMAKE_DISTRO_NAME}")
-message(STATUS "Distribution release: ${CMAKE_DISTRO_RELEASE}")
+    message(STATUS "Distribution identifier: ${CMAKE_DISTRO_NAME}")
+    message(STATUS "Distribution release: ${CMAKE_DISTRO_RELEASE}")
+  endif()
+endif()
 
 # Variables to map distribution name to generator
 set(CPACK_debian_GENERATOR "DEB")
@@ -35,6 +35,11 @@ set(CPACK_ubuntu_GENERATOR "DEB")
 set(CPACK_centos_GENERATOR "RPM")
 
 macro(get_distr_generator VAR)
-  set(${VAR} ${CPACK_${CMAKE_DISTRO_NAME}_GENERATOR})
+  if(CMAKE_DISTRO_NAME)
+    set(${VAR} ${CPACK_${CMAKE_DISTRO_NAME}_GENERATOR})
+  else()
+    message(FATAL_ERROR
+      "No distro name available, cannot build packages (have you installed lsb_release?)")
+  endif()
 endmacro()
 
