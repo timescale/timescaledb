afd05e0a40219dfcc0c9155ca012c8ef23860682
Sven Klemm <sven@timescale.com>
2020-12-17 13:47:35 +0100

    Add 2.0.0-rc4 to update test scripts

--- scripts/test_updates_pg12.sh
+++ scripts/test_updates_pg12.sh
@@ -6,7 +6,7 @@ set -o pipefail
 SCRIPT_DIR=$(dirname $0)
 echo $SCRIPT_DIR
 
-TAGS="1.7.0-pg12 1.7.1-pg12 1.7.2-pg12 1.7.3-pg12 1.7.4-pg12 2.0.0-rc1-pg12 2.0.0-rc2-pg12 2.0.0-rc3-pg12"
+TAGS="1.7.0-pg12 1.7.1-pg12 1.7.2-pg12 1.7.3-pg12 1.7.4-pg12 2.0.0-rc1-pg12 2.0.0-rc2-pg12 2.0.0-rc3-pg12 2.0.0-rc4-pg12"
 TEST_VERSION="v6"
 
 TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh
