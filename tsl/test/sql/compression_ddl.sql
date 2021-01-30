bc78ff11f559e5d1ae047489df8f61db7f008c49
Sven Klemm <sven@timescale.com>
2021-01-30 01:04:39 +0100

    Fix compressed chunk check when disabling compression
    The check for existence of compressed chunks when disabling
    compression would not ignore dropped chunks making it impossible
    to disable compression on hypertables with continuous aggregates
    that had dropped chunks.
    This patch ignores dropped chunks in this check and also sets
    compressed_chunk_id to NULL in the metadata for deleted chunks.

--- tsl/test/sql/compression_ddl.sql
+++ tsl/test/sql/compression_ddl.sql
@@ -445,8 +445,18 @@ FROM _timescaledb_catalog.chunk chunk
 INNER JOIN _timescaledb_catalog.hypertable hypertable ON (chunk.hypertable_id = hypertable.id)
 WHERE hypertable.table_name like 'test1'  ORDER BY chunk.id ) as subq;
 
+-- test disabling compression on hypertables with caggs and dropped chunks
+-- github issue 2844
+CREATE TABLE i2844 (created_at timestamptz NOT NULL,c1 float);
+SELECT create_hypertable('i2844', 'created_at', chunk_time_interval => '6 hour'::interval);
+INSERT INTO i2844 SELECT generate_series('2000-01-01'::timestamptz, '2000-01-02'::timestamptz,'1h'::interval);
 
+CREATE VIEW test_agg WITH (timescaledb.continuous) AS SELECT time_bucket('1 hour', created_at) AS bucket, AVG(c1) AS avg_c1 FROM i2844 GROUP BY bucket;
 
+ALTER TABLE i2844 SET (timescaledb.compress);
 
+SELECT compress_chunk(show_chunks) AS compressed_chunk FROM show_chunks('i2844');
+SELECT drop_chunks(table_name => 'i2844', older_than => '2000-01-01 18:00'::timestamptz, cascade_to_materializations => true);
+SELECT decompress_chunk(show_chunks, if_compressed => TRUE) AS decompressed_chunks FROM show_chunks('i2844');
 
-
+ALTER TABLE i2844 SET (timescaledb.compress = FALSE);
