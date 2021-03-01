e2564b26e6c9ae7a2d86e5a02902069ba31f458f
Mats Kindahl <mats@timescale.com>
2021-03-01 14:48:44 +0100

    Break out multinode update tests
    This commit is part of a longer sequence to refactor the update tests
    for use with smoke testing.
    
    The multinode parts of the tests are broken out into a new version to
    make the logic simpler.
    
    - All multinode tests are now added to a new version, `v7` which allow
      us to create multinode objects only for versions that support
      multinode.
    - Test script `test_updates_pg11` and `test_updates_pg12` are changed
      to use `v6` for updates from versions preceeding 2.0 and `v7` for
      versions 2.0 and later.
    - Setup file `post.update.sql` is not needed any more since it was used
      to "fix" a pre-2.0 updated version to add data nodes so that it
      matched the clean setup. This is not necessary any more since v6 does
      not add data nodes for some versions and not for others.

--- scripts/test_updates_pg11.sh
+++ scripts/test_updates_pg11.sh
@@ -11,7 +11,7 @@ TEST_VERSION="v2"
 TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh "$@"
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
-  exit $EXIT_CODE
+    exit $EXIT_CODE
 fi
 
 TAGS="1.3.0-pg11 1.3.1-pg11 1.3.2-pg11 1.4.0-pg11 1.4.1-pg11 1.4.2-pg11"
@@ -20,7 +20,7 @@ TEST_VERSION="v4"
 TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh "$@"
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
-  exit $EXIT_CODE
+    exit $EXIT_CODE
 fi
 
 TAGS="1.5.0-pg11 1.5.1-pg11 1.6.0-pg11 1.6.1-pg11"
@@ -29,14 +29,23 @@ TEST_VERSION="v5"
 TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh "$@"
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
-  exit $EXIT_CODE
+    exit $EXIT_CODE
 fi
 
-TAGS="1.7.0-pg11 1.7.1-pg11 1.7.2-pg11 1.7.3-pg11 1.7.4-pg11 1.7.5-pg11 2.0.0-rc1-pg11 2.0.0-rc2-pg11 2.0.0-rc3-pg11 2.0.0-rc4-pg11 2.0.0-pg11 2.0.1-pg11 2.0.2-pg11 2.1.0-pg11"
+TAGS="1.7.0-pg11 1.7.1-pg11 1.7.2-pg11 1.7.3-pg11 1.7.4-pg11 1.7.5-pg11"
 TEST_VERSION="v6"
 
 TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh "$@"
 EXIT_CODE=$?
 if [ $EXIT_CODE -ne 0 ]; then
-  exit $EXIT_CODE
+    exit $EXIT_CODE
+fi
+
+TAGS="2.0.0-rc1-pg11 2.0.0-rc2-pg11 2.0.0-rc3-pg11 2.0.0-rc4-pg11 2.0.0-pg11 2.0.1-pg11 2.0.2-pg11 2.1.0-pg11"
+TEST_VERSION="v7"
+
+TAGS=$TAGS TEST_VERSION=$TEST_VERSION bash ${SCRIPT_DIR}/test_updates.sh "$@"
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+    exit $EXIT_CODE
 fi
