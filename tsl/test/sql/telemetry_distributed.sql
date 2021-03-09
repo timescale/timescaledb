04d7b3993297b18babf3466b5acb8a5f0440d5b6
Sven Klemm <sven@timescale.com>
2021-03-09 09:33:23 +0100

    Adjust telemetry_distributed database names
    Change the telemetry_distributed test to not use hardcoded database
    names but instead derive it from test name. This patch also changes
    the test to drop any databases created by the test itself.

--- tsl/test/sql/telemetry_distributed.sql
+++ tsl/test/sql/telemetry_distributed.sql
@@ -3,27 +3,22 @@
 -- LICENSE-TIMESCALE for a copy of the license.
 
 \c :TEST_DBNAME :ROLE_CLUSTER_SUPERUSER;
-
-SET client_min_messages TO ERROR;
-DROP DATABASE IF EXISTS data_node_1;
-DROP DATABASE IF EXISTS data_node_2;
-SET client_min_messages TO NOTICE;
+\set DN_DBNAME_1 :TEST_DBNAME _1
+\set DN_DBNAME_2 :TEST_DBNAME _2
 
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json, 'distributed_db');
 
 -- Become an access node
-SELECT * FROM add_data_node('data_node_1', host => 'localhost',
-                            database => 'data_node_1');
+SELECT * FROM add_data_node('data_node_1', host => 'localhost', database => :'DN_DBNAME_1');
 
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json, 'distributed_db');
 
 -- See telemetry report from a data node
-\c data_node_1 :ROLE_CLUSTER_SUPERUSER;
+\c :DN_DBNAME_1 :ROLE_CLUSTER_SUPERUSER;
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json, 'distributed_db');
 
 \c :TEST_DBNAME :ROLE_CLUSTER_SUPERUSER;
-SELECT * FROM add_data_node('data_node_2', 'localhost',
-                            database => 'data_node_2');
+SELECT * FROM add_data_node('data_node_2', host => 'localhost', database => :'DN_DBNAME_2');
 
 -- Add hypertables 
 CREATE TABLE test_ht(time timestamptz, device int, PRIMARY KEY (time, device));
@@ -36,9 +31,12 @@ CREATE TABLE disttable2(time timestamptz, device int, PRIMARY KEY (time, device)
 SELECT * FROM create_distributed_hypertable('disttable2', 'time', 'device', 2, replication_factor => 2);
 
 -- See telemetry report update from the data node
-\c data_node_1 :ROLE_CLUSTER_SUPERUSER;
+\c :DN_DBNAME_1 :ROLE_CLUSTER_SUPERUSER;
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json, 'distributed_db');
 
 \c :TEST_DBNAME :ROLE_CLUSTER_SUPERUSER;
 -- See a number of distributed and distributed and replicated hypertables update
 SELECT json_object_field(get_telemetry_report(always_display_report := true)::json, 'distributed_db');
+
+DROP DATABASE :DN_DBNAME_1;
+DROP DATABASE :DN_DBNAME_2;
