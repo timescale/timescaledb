dbbbce6b44014005c74d3b4bc5e3c7d5e1fc1292
Sven Klemm <sven@timescale.com>
2021-04-03 18:37:20 +0200

    Make VACUUM tests more resilient
    PG13 changes index vacuum behaviour in versions after 13.2 so
    index vacuum is no longer triggered when inserting only.
    This lead to test failures in the ABI test which was running
    PG13 snapshot but will also lead to test differences between
    13.2 and 13.3 when that is released.
    This patch removes the indexes from the vacuum and vacuum multi
    tests and makes them smaller for vacuum_parallel test to make
    the size stable.
    
    https://github.com/postgres/postgres/commit/9663d12

--- test/sql/vacuum_parallel.sql
+++ test/sql/vacuum_parallel.sql
@@ -2,23 +2,38 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
-
 -- PG13 introduced parallel VACUUM functionality. It gets invoked when a table
 -- has two or more indexes on it. Read up more at
 -- https://www.postgresql.org/docs/13/sql-vacuum.html#PARALLEL
 
-CREATE TABLE vacuum_test(time timestamp, temp1 float, temp2 int);
+CREATE TABLE vacuum_test(time timestamp NOT NULL, temp1 float, temp2 int);
 
--- create hypertable with chunks
-SELECT create_hypertable('vacuum_test', 'time', chunk_time_interval => 2628000000000);
+-- create hypertable
+-- we create chunks in public schema cause otherwise we would need
+-- elevated privileges to create indexes directly
+SELECT create_hypertable('vacuum_test', 'time', create_default_indexes => false, associated_schema_name => 'public');
 
--- create additional indexes on the temp columns
-CREATE INDEX vt_temp1 on vacuum_test (temp1);
-CREATE INDEX vt_temp2 on vacuum_test (temp2);
+-- parallel vacuum needs the index size to be larger than min_parallel_index_scan_size to kick in
+SET min_parallel_index_scan_size TO 0;
+INSERT INTO vacuum_test SELECT TIMESTAMP 'epoch' + (i * INTERVAL '4h'),
+                i, i+1 FROM generate_series(1, 100) as T(i);
 
--- parallel vacuum needs the index size to be larger than 512KB to kick in
-INSERT INTO vacuum_test SELECT TIMESTAMP 'epoch' + (i * INTERVAL '100 second'),
-                i, i+1 FROM generate_series(1, 100000) as T(i);
+-- create indexes on the temp columns
+-- we create indexes manually because otherwise vacuum verbose output
+-- would be different between 13.2 and 13.3+
+-- 13.2 would try to vacuum the parent table index too while 13.3+ wouldn't
+CREATE INDEX ON _hyper_1_1_chunk(time);
+CREATE INDEX ON _hyper_1_1_chunk(temp1);
+CREATE INDEX ON _hyper_1_1_chunk(temp2);
+CREATE INDEX ON _hyper_1_2_chunk(time);
+CREATE INDEX ON _hyper_1_2_chunk(temp1);
+CREATE INDEX ON _hyper_1_2_chunk(temp2);
+CREATE INDEX ON _hyper_1_3_chunk(time);
+CREATE INDEX ON _hyper_1_3_chunk(temp1);
+CREATE INDEX ON _hyper_1_3_chunk(temp2);
+
+-- INSERT only will not trigger vacuum on indexes for PG13.3+
+UPDATE vacuum_test SET time = time + '1s'::interval, temp1 = random(), temp2 = random();
 
 -- we should see two parallel workers for each chunk
 VACUUM (PARALLEL 3, VERBOSE) vacuum_test;
