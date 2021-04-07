699fa7fc1fd889bfd0d1d6d283724d230131727d
Ruslan Fomkin <ruslan.fomkin@gmail.com>
2021-04-07 08:54:21 +0200

    Create an empty chunk table
    Adds an internal API function to create an empty chunk table according
    the given hypertable for the given chunk table name and dimension
    slices. This functions creates a chunk table inheriting from the
    hypertable, so it guarantees the same schema. No TimescaleDB's
    metadata is updated.
    
    To be able to create the chunk table in a tablespace attached to the
    hyeprtable, this commit allows calculating the tablespace id without
    the dimension slice to exist in the catalog.
    
    If there is already a chunk, which collides on dimension slices, the
    function fails to create the chunk table.
    
    The function will be used internally in multi-node to be able to
    replicate a chunk from one data node to another.

--- src/cross_module_fn.c
+++ src/cross_module_fn.c
@@ -83,6 +83,7 @@ CROSSMODULE_WRAPPER(data_node_detach);
 CROSSMODULE_WRAPPER(chunk_set_default_data_node);
 CROSSMODULE_WRAPPER(chunk_get_relstats);
 CROSSMODULE_WRAPPER(chunk_get_colstats);
+CROSSMODULE_WRAPPER(chunk_create_empty_table);
 
 CROSSMODULE_WRAPPER(timescaledb_fdw_handler);
 CROSSMODULE_WRAPPER(timescaledb_fdw_validator);
@@ -399,6 +400,7 @@ TSDLLEXPORT CrossModuleFunctions ts_cm_functions_default = {
 	.func_call_on_data_nodes = func_call_on_data_nodes_default,
 	.chunk_get_relstats = error_no_default_fn_pg_community,
 	.chunk_get_colstats = error_no_default_fn_pg_community,
+	.chunk_create_empty_table = error_no_default_fn_pg_community,
 	.hypertable_distributed_set_replication_factor = error_no_default_fn_pg_community,
 	.update_compressed_chunk_relstats = update_compressed_chunk_relstats_default,
 };
