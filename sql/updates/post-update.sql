aeb107659bc0815ec5e3cb6b8044aed5dd444ac9
Mats Kindahl <mats@timescale.com>
2021-04-13 10:07:36 +0200

    Copy recreated object permissions on update
    Tables, indexes, and sequences that are recreated as part of an update
    does not propagate permissions to the recreated object. This commit
    fixes that by saving away the permissions in `pg_class` temporarily and
    then copying them back into the `pg_class` table.
    
    If internal objects are created or re-created, they get the wrong
    initial privileges, which result in privileges not being dumped when
    using `pg_dump`. Save away the privileges before starting the update
    and restore them afterwards to make sure that the privileges are
    maintained over the update.
    
    For new objects, we use the initial privileges of the `chunk` metadata
    table, which should always have correct initial privileges.
    
    Fixes #3078

--- sql/updates/post-update.sql
+++ sql/updates/post-update.sql
@@ -30,3 +30,46 @@ $$;
 
 -- can only be dropped after views have been rebuilt
 DROP FUNCTION IF EXISTS _timescaledb_internal.cagg_watermark(oid);
+
+-- For objects that are newly created, we need to set the initprivs to
+-- the initprivs for some table that was created in the installation
+-- of the TimescaleDB extension and not as part of any update.
+--
+-- We chose the "chunk" catalog table for this since that is created
+-- in the first version of TimescaleDB and should have the correct
+-- initprivs, but we could use any other table that existed in the
+-- first installation.
+INSERT INTO saved_privs
+     SELECT nspname, relname, relacl,
+ 	    (SELECT tmpini FROM saved_privs
+	      WHERE tmpnsp = '_timescaledb_catalog' AND tmpname = 'chunk')
+       FROM pg_class JOIN pg_namespace ns ON ns.oid = relnamespace
+		LEFT JOIN saved_privs ON tmpnsp = nspname AND tmpname = relname
+      WHERE nspname IN ('_timescaledb_catalog', '_timescaledb_config')
+ 	 OR nspname = '_timescaledb_internal'
+        AND relname IN ('hypertable_chunk_local_size', 'compressed_chunk_stats',
+                        'bgw_job_stat', 'bgw_policy_chunk_stats')
+ON CONFLICT DO NOTHING;
+
+-- We can now copy back saved initprivs.
+WITH to_update AS (
+     SELECT objoid, tmpini
+     FROM pg_class cl JOIN pg_namespace ns ON ns.oid = relnamespace
+   		      JOIN pg_init_privs ip ON ip.objoid = cl.oid
+		      JOIN saved_privs ON tmpnsp = nspname AND tmpname = relname)
+UPDATE pg_init_privs
+   SET initprivs = tmpini
+  FROM to_update
+ WHERE to_update.objoid = pg_init_privs.objoid
+   AND classoid = 'pg_class'::regclass;
+
+-- Can only restore permissions on views after they have been rebuilt,
+-- so we restore for all types of objects here.
+WITH to_update AS (
+     SELECT cl.oid, tmpacl
+     FROM pg_class cl JOIN pg_namespace ns ON ns.oid = relnamespace
+                      JOIN saved_privs ON tmpnsp = nspname AND tmpname = relname)
+UPDATE pg_class cl SET relacl = tmpacl
+  FROM to_update WHERE cl.oid = to_update.oid;
+
+DROP TABLE saved_privs;
---
+++ test/sql/updates/setup.catalog.sql
@@ -0,0 +1,23 @@
+-- This file and its contents are licensed under the Apache License 2.0.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-APACHE for a copy of the license.
+
+-- Catalog tables are occationally rewritten as part of updates, so
+-- this is to test that privileges are maintained over updates of the
+-- extension. We could verify that other properties (e.g., comments)
+-- are maintained here as well, but this is not something we use right
+-- now.
+--
+-- We do not alter the privileges on _timescaledb_internal since this
+-- affects both internal objects and two tables that are metadata
+-- placed in the _timescaledb_internal schema.
+
+GRANT SELECT ON ALL TABLES IN SCHEMA _timescaledb_catalog TO tsdbadmin;
+GRANT SELECT ON ALL TABLES IN SCHEMA _timescaledb_config TO tsdbadmin;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA _timescaledb_catalog TO tsdbadmin;
+GRANT SELECT ON ALL SEQUENCES IN SCHEMA _timescaledb_config TO tsdbadmin;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA _timescaledb_catalog
+      GRANT SELECT ON tables TO tsdbadmin;
+ALTER DEFAULT PRIVILEGES IN SCHEMA _timescaledb_config
+      GRANT SELECT ON tables TO tsdbadmin;
