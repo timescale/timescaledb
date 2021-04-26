cb5d6883e8bea7ec7ceb1a610a178dc24122add7
Nikhil <nikhil@timescale.com>
2021-04-26 11:01:36 +0530

    Implement drop_chunk_replica API
    This function drops a chunk on a specified data node. It then removes
    the metadata about the datanode, chunk association on the access node.
    
    This function is meant for internal use as part of the "move chunk"
    functionality.
    
    If only one chunk replica remains then this function refuses to drop it
    to avoid data loss.

--- src/cross_module_fn.h
+++ src/cross_module_fn.h
@@ -129,6 +129,7 @@ typedef struct CrossModuleFunctions
 	Datum (*chunk_set_default_data_node)(PG_FUNCTION_ARGS);
 	Datum (*create_chunk)(PG_FUNCTION_ARGS);
 	Datum (*show_chunk)(PG_FUNCTION_ARGS);
+
 	List *(*get_and_validate_data_node_list)(ArrayType *nodearr);
 	void (*hypertable_make_distributed)(Hypertable *ht, List *data_node_names);
 	Datum (*timescaledb_fdw_handler)(PG_FUNCTION_ARGS);
@@ -159,6 +160,7 @@ typedef struct CrossModuleFunctions
 	PGFunction hypertable_distributed_set_replication_factor;
 	PGFunction chunk_create_empty_table;
 	PGFunction chunk_create_replica_table;
+	PGFunction chunk_drop_replica;
 	void (*update_compressed_chunk_relstats)(Oid uncompressed_relid, Oid compressed_relid);
 } CrossModuleFunctions;
 
