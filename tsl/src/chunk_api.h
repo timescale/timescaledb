3e85508f7233f75ab9755e1392033613c2cf4168
Ruslan Fomkin <ruslan.fomkin@gmail.com>
2021-04-26 14:15:49 +0200

    Create chunk table from access node
    Creates a table for chunk replica on the given data node. The table
    gets the same schema and name as the chunk. The created chunk replica
    table is not added into metadata on the access node or data node.
    
    The primary goal is to use it during copy/move chunk.

--- tsl/src/chunk_api.h
+++ tsl/src/chunk_api.h
@@ -8,6 +8,10 @@
 
 #include <postgres.h>
 
+#include <chunk.h>
+
+#include "hypertable_data_node.h"
+
 extern Datum chunk_show(PG_FUNCTION_ARGS);
 extern Datum chunk_create(PG_FUNCTION_ARGS);
 extern void chunk_api_create_on_data_nodes(Chunk *chunk, Hypertable *ht);
@@ -15,5 +19,7 @@ extern Datum chunk_api_get_chunk_relstats(PG_FUNCTION_ARGS);
 extern Datum chunk_api_get_chunk_colstats(PG_FUNCTION_ARGS);
 extern void chunk_api_update_distributed_hypertable_stats(Oid relid);
 extern Datum chunk_create_empty_table(PG_FUNCTION_ARGS);
+extern void chunk_api_call_create_empty_chunk_table(const Hypertable *ht, const Chunk *chunk,
+													const char *node_name);
 
 #endif /* TIMESCALEDB_TSL_CHUNK_API_H */
