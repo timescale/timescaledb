422440a2de9f0d542f5cb38a46761c7a9ab030da
Erik Nordström <erik@timescale.com>
2021-05-06 22:38:09 +0200

    Add GENERATED column support on distributed hypertables
    Add support for deparsing `GENERATED` columns when creating
    distributed hypertables, including support for generating values
    "on-the-fly" when a `RETURNING` clause is used with an `INSERT`
    statement.
    
    Previously, using generated columns on a hypertable led to errors or
    had unpredictible results.

--- tsl/test/sql/dist_hypertable_pg12.sql
+++ tsl/test/sql/dist_hypertable_pg12.sql
@@ -2,6 +2,7 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-TIMESCALE for a copy of the license.
 
+-- Tests using features supported in PG12 and greater.
 -- Need to be super user to create extension and add data nodes
 \c :TEST_DBNAME :ROLE_CLUSTER_SUPERUSER;
 
@@ -29,7 +30,7 @@ CREATE ACCESS METHOD test_am TYPE TABLE HANDLER heap_tableam_handler;
 $$);
 
 -- Create distributed hypertable using non-default access method
-CREATE TABLE disttable(time timestamptz NOT NULL, device int) USING test_am;
+CREATE TABLE disttable(time timestamptz NOT NULL, device int, temp_c float, temp_f float GENERATED ALWAYS AS (temp_c * 9 / 5 + 32) STORED) USING test_am;
 SELECT * FROM create_distributed_hypertable('disttable', 'time', 'device', 3);
 
 -- Make sure that distributed hypertable created on data nodes is
@@ -44,16 +45,35 @@ $$);
 
 -- Check that basic operations are working as expected
 INSERT INTO disttable VALUES
-       ('2017-01-01 06:01', 1),
-       ('2017-01-01 09:11', 3),
-       ('2017-01-01 08:01', 1),
-       ('2017-01-02 08:01', 2),
-       ('2018-07-02 08:01', 87),
-       ('2018-07-01 06:01', 13),
-       ('2018-07-01 09:11', 90),
-       ('2018-07-01 08:01', 29);
+       ('2017-01-01 06:01', 1, -10.0),
+       ('2017-01-01 09:11', 3, -5.0),
+       ('2017-01-01 08:01', 1, 1.0),
+       ('2017-01-02 08:01', 2, 5.0),
+       ('2018-07-02 08:01', 87, 10.0),
+       ('2018-07-01 06:01', 13, 15.0),
+       ('2018-07-01 09:11', 90, 20.0),
+       ('2018-07-01 08:01', 29, 24.0);
 
 SELECT * FROM disttable ORDER BY time;
+
+-- Show that GENERATED columns work for INSERT with RETURNING clause
+TRUNCATE disttable;
+EXPLAIN VERBOSE
+INSERT INTO disttable VALUES ('2017-08-01 06:01', 1, 35.0) RETURNING *;
+INSERT INTO disttable VALUES ('2017-08-01 06:01', 1, 35.0) RETURNING *;
+
+-- Same values returned with SELECT:
+SELECT * FROM disttable ORDER BY 1;
+
+UPDATE disttable SET temp_c=40.0 WHERE device=1;
+SELECT * FROM disttable ORDER BY 1;
+
+-- Insert another value
+INSERT INTO disttable VALUES ('2017-09-01 06:01', 2, 30.0);
+SELECT * FROM disttable ORDER BY 1;
+-- Delete a value based on the generated column
+DELETE FROM disttable WHERE temp_f=104;
+SELECT * FROM disttable ORDER BY 1;
 
 DROP DATABASE :DN_DBNAME_1;
 DROP DATABASE :DN_DBNAME_2;
