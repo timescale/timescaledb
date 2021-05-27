dd3e422964f9a0d8a913d03ae2da2ad64799f3f8
Sven Klemm <sven@timescale.com>
2021-05-27 10:56:46 +0200

    Remove update files for PG11
    Remove update files that were only used for PG11 versions since
    those versions do not support postgres past PG11

--- sql/updates/1.3.1--1.3.2.sql
+++
@@ -1,3 +0,0 @@
-DROP FUNCTION IF EXISTS _timescaledb_internal.dimension_calculate_default_range_open(bigint, bigint);
-DROP FUNCTION IF EXISTS _timescaledb_internal.dimension_calculate_default_range_closed(bigint, smallint);
-
