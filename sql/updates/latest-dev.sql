af90f7c1336c2b872212598085488d8cfbef64f7
Nikhil <nikhil@timescale.com>
2021-05-27 13:05:16 +0530

    Implement wrapper API for copy/move chunk
    The building blocks required for implementing end-to-end copy/move
    chunk functionality have now been wrapped in a procedure.
    
    A procedure is required because multiple transactions are needed to
    carry out the activity across the access node and the involved two data
    nodes.
    
    The following steps are encapsulated in this procedure
    
    1) Create an empty chunk table on the destination data node
    
    2) Copy the data from the src data node chunk to this newly created
    destination node chunk. This is done via inbuilt PostgreSQL logical
    replication functionality
    
    3) Attach this chunk to the hypertable on the dst data node
    
    4) Remove this chunk from the src data node to complete the move if
    requested
    
    A new catalog table "chunk_copy_activity" has been added to track
    the progress of the above stages. A unique id gets assigned to each
    activity and it is updated with the completed stages as things
    progress.

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -3,3 +3,30 @@ GRANT USAGE ON SCHEMA timescaledb_experimental TO PUBLIC;
 DROP FUNCTION IF EXISTS _timescaledb_internal.block_new_chunks;
 DROP FUNCTION IF EXISTS _timescaledb_internal.allow_new_chunks;
 DROP FUNCTION IF EXISTS _timescaledb_internal.create_chunk;
+
+-- Use the experimental schema for ths new procedure
+CREATE OR REPLACE PROCEDURE timescaledb_experimental.move_chunk(
+    chunk REGCLASS,
+    source_node Name = NULL,
+    destination_node Name = NULL,
+    verbose BOOLEAN=FALSE)
+AS '@MODULE_PATHNAME@', 'ts_move_chunk_proc' LANGUAGE C;
+
+CREATE SEQUENCE IF NOT EXISTS _timescaledb_catalog.chunk_copy_activity_id_seq MINVALUE 1;
+
+CREATE TABLE IF NOT EXISTS _timescaledb_catalog.chunk_copy_activity (
+  id integer PRIMARY KEY DEFAULT nextval('_timescaledb_catalog.chunk_copy_activity_id_seq'),
+  operation_id name NOT NULL UNIQUE, -- the publisher/subscriber identifier used
+  backend_pid integer NOT NULL, -- the pid of the backend running this activity
+  completed_stage text NOT NULL, -- the completed stage/step
+  time_start timestamptz NOT NULL DEFAULT NOW(), -- start time of the activity
+  chunk_id integer NOT NULL REFERENCES _timescaledb_catalog.chunk (id) ON DELETE CASCADE,
+  source_node_name name NOT NULL,
+  dest_node_name name NOT NULL,
+  delete_on_source_node bool NOT NULL -- is a move or copy activity
+);
+
+ALTER SEQUENCE _timescaledb_catalog.chunk_copy_activity_id_seq OWNED BY _timescaledb_catalog.chunk_copy_activity.id;
+
+GRANT SELECT ON _timescaledb_catalog.chunk_copy_activity_id_seq TO PUBLIC;
+GRANT SELECT ON _timescaledb_catalog.chunk_copy_activity TO PUBLIC;
