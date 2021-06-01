3e0a93f26c29d8d3c546748b6b37d2361abd8cd5
Sven Klemm <sven@timescale.com>
2021-06-01 20:54:10 +0200

    Allow building against PG14

--- src/compat.h
+++ src/compat.h
@@ -24,15 +24,20 @@
 
 #define is_supported_pg_version_12(version) ((version >= 120000) && (version < 130000))
 #define is_supported_pg_version_13(version) ((version >= 130002) && (version < 140000))
+#define is_supported_pg_version_14(version) ((version >= 140000) && (version < 150000))
 
 #define is_supported_pg_version(version)                                                           \
-	(is_supported_pg_version_12(version) || is_supported_pg_version_13(version))
+	(is_supported_pg_version_12(version) || is_supported_pg_version_13(version) ||                 \
+	 is_supported_pg_version_14(version))
 
 #define PG12 is_supported_pg_version_12(PG_VERSION_NUM)
 #define PG13 is_supported_pg_version_13(PG_VERSION_NUM)
+#define PG14 is_supported_pg_version_14(PG_VERSION_NUM)
 
 #define PG13_LT (PG_VERSION_NUM < 130000)
 #define PG13_GE (PG_VERSION_NUM >= 130000)
+#define PG14_LT (PG_VERSION_NUM < 140000)
+#define PG14_GE (PG_VERSION_NUM >= 140000)
 
 #if !(is_supported_pg_version(PG_VERSION_NUM))
 #error "Unsupported PostgreSQL version"
