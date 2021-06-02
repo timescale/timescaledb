10e339f5915b8cf143a36abc1c13a817b620cd16
Mats Kindahl <mats@timescale.com>
2021-06-02 11:07:11 +0200

    Add experimental schema
    This commit adds an experimental schema where experimental features can
    be added.

--- sql/pre_install/schemas.sql
+++ sql/pre_install/schemas.sql
@@ -6,4 +6,7 @@ CREATE SCHEMA IF NOT EXISTS _timescaledb_catalog;
 CREATE SCHEMA IF NOT EXISTS _timescaledb_internal;
 CREATE SCHEMA IF NOT EXISTS _timescaledb_cache;
 CREATE SCHEMA IF NOT EXISTS _timescaledb_config;
+CREATE SCHEMA IF NOT EXISTS timescaledb_experimental;
 GRANT USAGE ON SCHEMA _timescaledb_cache, _timescaledb_catalog, _timescaledb_internal, _timescaledb_config TO PUBLIC;
+GRANT USAGE ON SCHEMA timescaledb_experimental TO PUBLIC;
+
---
+++ sql/updates/latest-dev.sql
@@ -0,0 +1,2 @@
+CREATE SCHEMA IF NOT EXISTS timescaledb_experimental;
+GRANT USAGE ON SCHEMA timescaledb_experimental TO PUBLIC;
