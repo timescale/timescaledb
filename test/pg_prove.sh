731c03d371fddfecb3723cbe2821f169c372850b
Nikhil <nikhil@timescale.com>
2021-06-08 18:44:27 +0530

    Make provecheck run specific tap tests
    Introduce a shell wrapper around perl prove utility to control running
    of TAP tests. The following control variable is supported:
    
    PROVE_TESTS: only run TAP tests from this list
    
    e.g make provecheck PROVE_TESTS="t/foo.pl t/bar.pl"
    
    Note that you can also use regular expressions to run multiple taps
    tests matching the pattern:
    
    e.g make provecheck PROVE_TESTS="t/*chunk*"
    
    If the existing "TESTS=" option is used along with PROVE_TESTS then
    the subset represented by PROVE_TESTS will also get run. Otherwise tap
    tests will be skipped if "TESTS=" is specified.
    
    e.g make installcheck TESTS=dist_hyper* PROVE_TESTS="t/001_*"

---
+++ test/pg_prove.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# Wrapper around perl prove utility to control running of TAP tests
+#
+# The following control variable is supported:
+#
+# PROVE_TESTS  only run TAP tests from this list
+# e.g make provecheck PROVE_TESTS="t/foo.pl t/bar.pl"
+#
+# Note that you can also use regular expressions to run multiple
+# taps tests matching the pattern:
+#
+# e.g make provecheck PROVE_TESTS="t/*chunk*"
+#
+
+PROVE_TESTS=${PROVE_TESTS:-}
+PROVE=${PROVE:-prove}
+
+# If PROVE_TESTS is specified then run those subset of TAP tests even if
+# TESTS is also specified
+if [ -z "$PROVE_TESTS" ]
+then
+    # Exit early if we are running with TESTS=expr
+    if [ ! -z "$TESTS" ]
+    then
+        exit 0
+    fi
+    FINAL_TESTS="t/*.pl"
+else
+    FINAL_TESTS=$PROVE_TESTS
+fi
+
+${PROVE} -I "${SRC_DIR}/src/test/perl" -I "${CM_SRC_DIR}/test/perl" $FINAL_TESTS
