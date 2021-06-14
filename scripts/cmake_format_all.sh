b22f9e179fff16354352646e094c5c695c4749d2
Mats Kindahl <mats@timescale.com>
2021-06-14 15:23:21 +0200

    Add workflow and CMake support for formatting
    Add a workflow to check that CMake files are correctly formatted as
    well as a custom target to format CMake files in the repository. This
    commit also runs the formatting on all CMake files in the repository.

---
+++ scripts/cmake_format_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPTDIR=$(cd $(dirname $0); pwd)
+BASEDIR=$(dirname $SCRIPTDIR)
+
+find $BASEDIR -name CMakeLists.txt  -exec cmake-format -i {} +
+find $BASEDIR/src $BASEDIR/test $BASEDIR/tsl -name '*.cmake' -exec cmake-format -i {} +
