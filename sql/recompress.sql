d2e07318c098ce7c46f2926d57a2de1541c56d54
gayyappan <gayathri@timescale.com>
2021-06-22 16:16:32 -0400

    revert changes

--- sql/recompress.sql
+++ sql/recompress.sql
@@ -11,16 +11,6 @@ LANGUAGE C IMMUTABLE ;;
 DROP FUNCTION IF EXISTS _timescaledb_internal.recompress_chunk_ffunc;
 
 CREATE OR REPLACE FUNCTION _timescaledb_internal.recompress_chunk_ffunc( tstate internal, compressed_chunk_name regclass, extra anyelement)
-RETURNS SETOF record
+RETURNS anyelement
 AS '@MODULE_PATHNAME@', 'ts_recompress_chunk_ffunc'
 LANGUAGE C IMMUTABLE ;
-
---cannot aggregate with finalfunc that returns set. Not premitted by PG ---
-CREATE AGGREGATE _timescaledb_internal.recompress_tuples(compressed_chunk_name REGCLASS, val anyelement)
-(
-    SFUNC = _timescaledb_internal.recompress_chunk_sfunc2,
-    STYPE = internal,
-    FINALFUNC = _timescaledb_internal.recompress_chunk_ffunc2,
-    FINALFUNC_EXTRA
-);
-
