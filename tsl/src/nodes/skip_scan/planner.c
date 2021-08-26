7b1542242719060957d1235522865e41cf106d0d
Sven Klemm <sven@timescale.com>
2021-08-26 15:15:47 +0200

    Adjust code to PG14 append changes
    PG14 removes partitioned_rels from create_append_path and
    create_merge_append_path
    
    https://github.com/postgres/postgres/commit/f003a7522b

--- tsl/src/nodes/skip_scan/planner.c
+++ tsl/src/nodes/skip_scan/planner.c
@@ -266,12 +266,12 @@ tsl_skip_scan_paths_add(PlannerInfo *root, RelOptInfo *input_rel, RelOptInfo *ou
 			if (!new_paths)
 				continue;
 
-			subpath = (Path *) create_merge_append_path(root,
-														merge_path->path.parent,
-														new_paths,
-														merge_path->path.pathkeys,
-														NULL,
-														merge_path->partitioned_rels);
+			subpath = (Path *) create_merge_append_path_compat(root,
+															   merge_path->path.parent,
+															   new_paths,
+															   merge_path->path.pathkeys,
+															   NULL,
+															   merge_path->partitioned_rels);
 			subpath->pathtarget = copy_pathtarget(merge_path->path.pathtarget);
 		}
 		else if (ts_is_chunk_append_path(subpath))
