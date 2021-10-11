d09eaf42037bb182654589a3877d5bc39b9627af
gayyappan <gayathri@timescale.com>
2021-10-11 11:20:13 -0400

    Expose subtract_integer_from_now as SQL function
    Move subtract_integer_from_now to src directory
    and create a SQL function for it.

--- src/utils.h
+++ src/utils.h
@@ -81,6 +81,7 @@ extern TSDLLEXPORT Oid ts_get_cast_func(Oid source, Oid target);
 typedef struct Dimension Dimension;
 
 extern TSDLLEXPORT Oid ts_get_integer_now_func(const Dimension *open_dim);
+extern TSDLLEXPORT int64 subtract_integer_from_now(int64 interval, Oid time_dim_type, Oid now_func);
 
 extern TSDLLEXPORT void *ts_create_struct_from_slot(TupleTableSlot *slot, MemoryContext mctx,
 													size_t alloc_size, size_t copy_size);
--- tsl/src/bgw_policy/policy_utils.h
+++ tsl/src/bgw_policy/policy_utils.h
@@ -10,7 +10,6 @@
 #include <postgres.h>
 bool policy_config_check_hypertable_lag_equality(Jsonb *config, const char *json_label,
 												 Oid dim_type, Oid lag_type, Datum lag_datum);
-int64 subtract_integer_from_now(int64 interval, Oid time_dim_type, Oid now_func);
 int64 subtract_integer_from_now_internal(int64 interval, Oid time_dim_type, Oid now_func,
 										 bool *overflow);
 Datum subtract_interval_from_now(Interval *interval, Oid time_dim_type);
