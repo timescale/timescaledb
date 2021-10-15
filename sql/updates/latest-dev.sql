9c426103d6e8ec206f55a01284befdc956a1c220
gayyappan <gayathri@timescale.com>
2021-10-15 16:45:47 -0400

    remember upgrade/downgrade

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,3 +1,3 @@
--- Remove old compression policy procedures
-DROP PROCEDURE IF EXISTS _timescaledb_internal.policy_compression_interval(INTEGER, INTEGER, INTERVAL, INTEGER, BOOLEAN, BOOLEAN);
-DROP PROCEDURE IF EXISTS _timescaledb_internal.policy_compression_integer(INTEGER, INTEGER, BIGINT, INTEGER, BOOLEAN, BOOLEAN);
+-- rewrite cagg to ensure selectedCols is set correctly in the view rules --
+-- we do this in post-update.sql
+-- needed for this feature
