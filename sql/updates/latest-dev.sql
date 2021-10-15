da90398e8d03bc2f24864e4cf8aecc722d4cfb07
gayyappan <gayathri@timescale.com>
2021-10-15 16:45:47 -0400

    remember upgrade/downgrade

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,2 +1,5 @@
 DROP FUNCTION IF EXISTS _timescaledb_internal.time_col_name_for_chunk(name,name);
 DROP FUNCTION IF EXISTS _timescaledb_internal.time_col_type_for_chunk(name,name);
+-- rewrite cagg to ensure selectedCols is set correctly in the view rules --
+-- we do this in post-update.sql
+-- needed for this feature
