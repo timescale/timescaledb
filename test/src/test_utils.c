72dcb5f7dc53033dc402a2337baf8359e736049f
Mats Kindahl <mats@timescale.com>
2021-10-25 14:27:45 +0200

    Fix flaky pg_dump
    Calling `CREATE DATABASE` we cannot have any backends connected to the
    source database, except the current one. Since background workers
    connect to any database that has installed the extension, they can
    linger and cause a flaky error.
    
    The test changed to just check that the extension can be implicitly
    installed, but this require waiting for backends to be unregistered
    from `procArray` in `procarray.c`, which the function
    `pg_terminate_backend` does not handle properly and hence results in a
    flaky test. This happen because `pg_terminate_backend` only signals the
    process to terminate, but does not wait for it to actually terminate.
    
    Instead, we add an explicit function that checks the `procArray` and
    waits for it to not have any backends for a database and uses that to
    wait for termination.

--- test/src/test_utils.c
+++ test/src/test_utils.c
@@ -6,6 +6,8 @@
 #include <postgres.h>
 #include <fmgr.h>
 #include <utils/builtins.h>
+#include <storage/procarray.h>
+#include <commands/dbcommands.h>
 
 #include "test_utils.h"
 #include "debug_point.h"
@@ -71,4 +73,41 @@ ts_test_error_injection(PG_FUNCTION_ARGS)
 	text *name = PG_GETARG_TEXT_PP(0);
 	DEBUG_ERROR_INJECTION(text_to_cstring(name));
 	PG_RETURN_VOID();
+}
+
+TS_FUNCTION_INFO_V1(ts_bgw_wait);
+Datum
+ts_bgw_wait(PG_FUNCTION_ARGS)
+{
+	text *datname = PG_GETARG_TEXT_PP(0);
+	/* The timeout is given in seconds, so we compute the number of iterations
+	 * necessary to get a coverage of that time */
+	uint32 iterations = PG_ARGISNULL(1) ? 5 : (PG_GETARG_UINT32(1) + 4) / 5;
+	Oid dboid = get_database_oid(text_to_cstring(datname), false);
+
+	/* This function contains a timeout of 5 seconds, so we iterate a few
+	 * times to make sure that it really has terminated. */
+	int notherbackends;
+	int npreparedxacts;
+	while (iterations-- > 0)
+	{
+		if (!CountOtherDBBackends(dboid, &notherbackends, &npreparedxacts))
+			PG_RETURN_NULL();
+		ereport(NOTICE,
+				(errmsg("source database \"%s\" is being accessed by other users",
+						text_to_cstring(datname)),
+				 errdetail("There are %d other session(s) and %d prepared transaction(s) using the "
+						   "database.",
+						   notherbackends,
+						   npreparedxacts)));
+	}
+	ereport(ERROR,
+			(errcode(ERRCODE_OBJECT_IN_USE),
+			 errmsg("source database \"%s\" is being accessed by other users",
+					text_to_cstring(datname)),
+			 errdetail("There are %d other session(s) and %d prepared transaction(s) using the "
+					   "database.",
+					   notherbackends,
+					   npreparedxacts)));
+	pg_unreachable();
 }
