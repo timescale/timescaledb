8925dd8e15c30ab5bdc1013abb063d4d05f64f06
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2021-10-26 17:23:45 -0300

    Release 2.5.0
    This release adds major new features since the 2.4.2 release.
    We deem it moderate priority for upgrading.
    
    This release includes these noteworthy features:
    
    * Continuous Aggregates for Distributed Hypertables
    * Support for PostgreSQL 14
    * Experimental: Support for timezones in `time_bucket_ng()`, including
    the `origin` argument
    
    This release also includes several bug fixes.
    
    **Features**
    * #3034 Add support for PostgreSQL 14
    * #3435 Add continuous aggregates for distributed hypertables
    * #3505 Add support for timezones in `time_bucket_ng()`
    
    **Bugfixes**
    * #3580 Fix memory context bug executing TRUNCATE
    * #3592 Allow alter column type on distributed hypertable
    * #3598 Improve evaluation of stable functions such as now() on access
    node
    * #3618 Fix execution of refresh_caggs from user actions
    * #3625 Add shared dependencies when creating chunk
    * #3626 Fix memory context bug executing TRUNCATE
    * #3627 Schema qualify UDTs in multi-node
    * #3638 Allow owner change of a data node
    * #3654 Fix index attnum mapping in reorder_chunk
    * #3661 Fix SkipScan path generation with constant DISTINCT column
    * #3667 Fix compress_policy for multi txn handling
    * #3673 Fix distributed hypertable DROP within a procedure
    * #3701 Allow anyone to use size utilities on distributed hypertables
    * #3708 Fix crash in get_aggsplit
    * #3709 Fix ordered append pathkey check
    * #3712 Fix GRANT/REVOKE ALL IN SCHEMA handling
    * #3717 Support transparent decompression on individual chunks
    * #3724 Fix inserts into compressed chunks on hypertables with caggs
    * #3727 Fix DirectFunctionCall crash in distributed_exec
    * #3728 Fix SkipScan with varchar column
    * #3733 Fix ANALYZE crash with custom statistics for custom types
    * #3747 Always reset expr context in DecompressChunk
    
    **Thanks**
    * @binakot and @sebvett for reporting an issue with DISTINCT queries
    * @hardikm10, @DavidPavlicek and @pafiti for reporting bugs on TRUNCATE
    * @mjf for reporting an issue with ordered append and JOINs
    * @phemmer for reporting the issues on multinode with aggregate queries and evaluation of now()
    * @abolognino for reporting an issue with INSERTs into compressed hypertables that have cagg
    * @tanglebones for reporting the ANALYZE crash with custom types on multinode

--- sql/updates/latest-dev.sql
+++
@@ -1,5 +0,0 @@
-DROP FUNCTION IF EXISTS _timescaledb_internal.time_col_name_for_chunk(name,name);
-DROP FUNCTION IF EXISTS _timescaledb_internal.time_col_type_for_chunk(name,name);
-
-CREATE OR REPLACE FUNCTION _timescaledb_internal.subtract_integer_from_now( hypertable_relid REGCLASS, lag INT8 )
-RETURNS INT8 AS '@MODULE_PATHNAME@', 'ts_subtract_integer_from_now' LANGUAGE C STABLE STRICT;
