297b3cb5dd263a9b1505970e316923e085e9b924
gayyappan <gayathri@timescale.com>
2021-11-12 13:31:54 -0500

    upgrade test for enabling compression after upgrade
    Verify with a test on caggs that has significantly
    fewer columns than the table.

---
+++ test/sql/updates/post.v8.sql
@@ -0,0 +1,13 @@
+-- This file and its contents are licensed under the Apache License 2.0.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-APACHE for a copy of the license.
+
+\ir post.catalog.sql
+\ir post.insert.sql
+\ir post.integrity_test.sql
+\ir catalog_missing_columns.sql
+\ir post.compression.sql
+\ir post.continuous_aggs.v3.sql
+\ir post.policies.sql
+\ir post.sequences.sql
+\ir post.functions.sql
