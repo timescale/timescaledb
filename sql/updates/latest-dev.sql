a589c4624a78372454c1f6fec26de47b67e4b80c
gayyappan <gayathri@timescale.com>
2021-11-12 14:19:50 -0500

    fixup latest-dev.sql

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,9 +1,7 @@
-DROP FUNCTION IF EXISTS _timescaledb_internal.time_col_name_for_chunk(name,name);
-DROP FUNCTION IF EXISTS _timescaledb_internal.time_col_type_for_chunk(name,name);
+-- Remove old compression policy procedures
+DROP PROCEDURE IF EXISTS _timescaledb_internal.policy_compression_interval(INTEGER, INTEGER, INTERVAL, INTEGER, BOOLEAN, BOOLEAN);
+DROP PROCEDURE IF EXISTS _timescaledb_internal.policy_compression_integer(INTEGER, INTEGER, BIGINT, INTEGER, BOOLEAN, BOOLEAN);
 
-CREATE OR REPLACE FUNCTION _timescaledb_internal.subtract_integer_from_now( hypertable_relid REGCLASS, lag INT8 )
-RETURNS INT8 AS '@MODULE_PATHNAME@', 'ts_subtract_integer_from_now' LANGUAGE C STABLE STRICT;
-
--- rewrite cagg to ensure selectedCols is set correctly in the view rules --
--- we do this in post-update.sql
--- needed for this feature
+--- rewrite cagg to ensure selectedCols is set correctly in the view rules --
+--- we do this in post-update.sql
+--- needed for this feature
