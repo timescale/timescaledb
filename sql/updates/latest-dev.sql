800b764a1f85aef2c2698769b08d6fa1f705c86c
gayyappan <gayathri@timescale.com>
2021-11-18 17:49:42 -0500

    view defn change

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,1 +1,2 @@
 DROP FUNCTION IF EXISTS recompress_chunk;
+DROP VIEW IF EXISTS timescaledb_information.continuous_aggregates;
--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,2 +1,4 @@
 DROP PROCEDURE IF EXISTS recompress_chunk;
 DROP FUNCTION IF EXISTS _timescaledb_internal.chunk_status;
+-- revert changes to continuous aggregates view definition
+DROP VIEW IF EXISTS timescaledb_information.continuous_aggregates;
