a69815412f8d272d070bafcf47aaf9dd856a68d0
gayyappan <gayathri@timescale.com>
2021-11-22 12:10:11 -0500

    reverse

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,2 +1,6 @@
 -- Remove old compression policy procedures
 DROP PROCEDURE IF EXISTS _timescaledb_internal.policy_compression_execute(INTEGER, INTEGER, ANYELEMENT, INTEGER, BOOLEAN, BOOLEAN);
+
+-- revert changes to continuous aggregates view definition
+DROP VIEW IF EXISTS timescaledb_information.continuous_aggregates;
+
