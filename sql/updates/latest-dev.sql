94dc8373943e01561577df610fcba6dda08301db
Duncan Moore <duncan@timescale.com>
2021-11-30 22:47:40 +0000

    Release 2.5.1

--- sql/updates/latest-dev.sql
+++
@@ -1,3 +0,0 @@
--- Remove old compression policy procedures
-DROP PROCEDURE IF EXISTS _timescaledb_internal.policy_compression_interval(INTEGER, INTEGER, INTERVAL, INTEGER, BOOLEAN, BOOLEAN);
-DROP PROCEDURE IF EXISTS _timescaledb_internal.policy_compression_integer(INTEGER, INTEGER, BIGINT, INTEGER, BOOLEAN, BOOLEAN);
