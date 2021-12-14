0ffc6361c7778cb629b57a867003a456f4e0073f
Alexander Kuzmenkov <akuzmenkov@timescale.com>
2021-12-14 17:15:28 +0300

    fix

--- tsl/src/fdw/relinfo.c
+++ tsl/src/fdw/relinfo.c
@@ -429,7 +429,7 @@ fdw_relinfo_create(PlannerInfo *root, RelOptInfo *rel, Oid server_oid, Oid local
 	fpinfo->fdw_startup_cost = DEFAULT_FDW_STARTUP_COST;
 	fpinfo->fdw_tuple_cost = DEFAULT_FDW_TUPLE_COST;
 	fpinfo->shippable_extensions = list_make1_oid(ts_extension_oid());
-	Assert(lfirst_oid(fpinfo->shippable_extensions) != InvalidOid);
+	Assert(linitial_oid(fpinfo->shippable_extensions) != InvalidOid);
 	fpinfo->fetch_size = DEFAULT_FDW_FETCH_SIZE;
 
 	apply_fdw_and_server_options(fpinfo);
