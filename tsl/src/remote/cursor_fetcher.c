7c024bdb597e7341719b902bde4517204812fe21
Erik Nordström <erik@timescale.com>
2021-12-21 18:56:56 +0100

    Avoid double PQclear() in data fetchers
    The macro `remote_result_elog` is used in the cursor and row-by-row
    fetchers and clears the result value using the function `PQclear()`
    before rethrowing its error. However, this error is later captured to
    clear the wrapping result value that holds a reference to the original
    result. To avoid this double-clearing, set the response to NULL before
    the error is thrown.

--- tsl/src/remote/cursor_fetcher.c
+++ tsl/src/remote/cursor_fetcher.c
@@ -12,6 +12,7 @@
 #include <postgres.h>
 #include <lib/stringinfo.h>
 #include <utils/rel.h>
+#include <utils/guc.h>
 
 #include "utils.h"
 #include "async.h"
@@ -305,7 +306,13 @@ cursor_fetcher_fetch_data_complete(CursorFetcher *cursor)
 
 		/* On error, report the original query, not the FETCH. */
 		if (PQresultStatus(res) != PGRES_TUPLES_OK)
+		{
+			/* remote_result_elog will call PQclear() on the result, so need
+			 * to mark the response as NULL to avoid double PQclear() */
+			pfree(response);
+			response = NULL;
 			remote_result_elog(res, ERROR);
+		}
 
 		/* Convert the data into HeapTuples */
 		numrows = PQntuples(res);
