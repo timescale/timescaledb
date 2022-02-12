72d03e6f7d30cc4794c9263445f14199241e2eb5
Sven Klemm <sven@timescale.com>
2022-02-12 17:02:03 +0100

    Remove search_path modifications from reverse-dev
    Resetting search_path in reverse-dev was necessary before the
    release of 2.5.2 as the previous timescaledb version scripts
    didn't handle locked down search_path. We can remove setting
    search_path too as the downgrade script includes pre-update.sql
    which locks down search_path.

--- sql/updates/pre-update.sql
+++ sql/updates/pre-update.sql
@@ -2,7 +2,7 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
--- This file is always prepended to all upgrade scripts.
+-- This file is always prepended to all upgrade and downgrade scripts.
 SET LOCAL search_path TO pg_catalog;
 
 -- Disable parallel execution for the duration of the update process.
