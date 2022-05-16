557023c3a7c5edf6e45c0f6003b75796fdab4b7a
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2022-05-16 11:35:36 -0300

    Revert "Ignore flaky "dist_gapfill_pushdown" test"
    This reverts commit cf83b3b9c2d899fa0156d5c94dc4848b7584e3c9.

--- scripts/gh_matrix_builder.py
+++ scripts/gh_matrix_builder.py
@@ -48,7 +48,7 @@ def build_debug_config(overrides):
     "build_type": "Debug",
     "pg_build_args": "--enable-debug --enable-cassert",
     "tsdb_build_args": "-DCODECOVERAGE=ON -DWARNINGS_AS_ERRORS=ON",
-    "installcheck_args": "IGNORES='bgw_db_scheduler dist_gapfill_pushdown-12 dist_gapfill_pushdown-13 dist_gapfill_pushdown-14'",
+    "installcheck_args": "IGNORES='bgw_db_scheduler'",
     "coverage": True,
     "extra_packages": "clang-9 llvm-9 llvm-9-dev llvm-9-tools",
     "llvm_config": "llvm-config-9",
@@ -111,7 +111,7 @@ def macos_config(overrides):
     "tsdb_build_args": "-DASSERTIONS=ON -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl",
     "llvm_config": "/usr/local/opt/llvm/bin/llvm-config",
     "coverage": False,
-    "installcheck_args": "IGNORES='bgw_db_scheduler bgw_launcher pg_dump remote_connection compressed_collation dist_gapfill_pushdown-12 dist_gapfill_pushdown-13 dist_gapfill_pushdown-14'",
+    "installcheck_args": "IGNORES='bgw_db_scheduler bgw_launcher pg_dump remote_connection compressed_collation'",
     "extra_packages": "",
   })
   base_config.update(overrides)
