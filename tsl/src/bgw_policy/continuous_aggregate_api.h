16fdb6ca5e3a42c120a45c7dc635e03c12f4b2e9
Rafia Sabih <rafia.sabih@gmail.com>
2022-07-07 21:07:54 +0200

    Checks for policy validation and compatibility
    At the time of adding or updating policies, it is
    checked if the policies are compatible with each
    other and to those already on the CAgg.
    These checks are:
    - refresh and compression policies should not overlap
    - refresh and retention policies should not overlap
    - compression and retention policies should not overlap
    
    Co-authored-by: Markos Fountoulakis <markos@timescale.com>

--- tsl/src/bgw_policy/continuous_aggregate_api.h
+++ tsl/src/bgw_policy/continuous_aggregate_api.h
@@ -26,7 +26,7 @@ bool policy_refresh_cagg_exists(int32 materialization_id);
 Datum policy_refresh_cagg_add_internal(Oid cagg_oid, Oid start_offset_type,
 									   NullableDatum start_offset, Oid end_offset_type,
 									   NullableDatum end_offset, Interval refresh_interval,
-									   bool if_not_exists, bool is_one_step);
+									   bool if_not_exists);
 Datum policy_refresh_cagg_remove_internal(Oid cagg_oid, bool if_exists);
 
 #endif /* TIMESCALEDB_TSL_BGW_POLICY_CAGG_API_H */
