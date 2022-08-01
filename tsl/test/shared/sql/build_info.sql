49b6486dad4458dbab5ead98bd9304208639762c
Sven Klemm <sven@timescale.com>
2022-08-01 09:52:19 +0200

    Change get_git_commit to return full commit hash
    This patch changes get_git_commit to always return the full hash.
    Since different git versions do not agree on the length of the
    abbreviated hash this made the length flaky. To make the length
    consistent change it to always be the full hash.

--- tsl/test/shared/sql/build_info.sql
+++ tsl/test/shared/sql/build_info.sql
@@ -5,6 +5,7 @@
 -- Check what get_git_commit returns
 SELECT pg_typeof(commit_tag) AS commit_tag_type,
        pg_typeof(commit_hash) AS commit_hash_type,
+       length(commit_hash) AS commit_hash_length,
        pg_typeof(commit_time) AS commit_time_type
   FROM _timescaledb_internal.get_git_commit();
 
