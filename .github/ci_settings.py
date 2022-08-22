timescale/timescaledb

commit 0c6f4e24587bfb7ab56642a6df75172ee4472845

Centralize CI settings
This patch adds a new settings file for common github action settings.
Instead of repeating latest pg versions in every github workflow we
can read the settings from this central file.

.github/ci_settings.py
@@ -1,0 +1,16 @@
+#!/usr/bin/env python
+
+#  This file and its contents are licensed under the Apache License 2.0.
+#  Please see the included NOTICE for copyright information and
+#  LICENSE-APACHE for a copy of the license.
+
+# Common settings for our CI jobs
+
+PG12_EARLIEST = "12.0"
+PG12_LATEST = "12.12"
+PG13_EARLIEST = "13.2"
+PG13_LATEST = "13.8"
+PG14_EARLIEST = "14.0"
+PG14_LATEST = "14.5"
+
+PG_LATEST = [PG12_LATEST, PG13_LATEST, PG14_LATEST]

.github/workflows/abi.yaml
@@ -15,13 +15,25 @@ on:
     branches:
       - prerelease_test
 jobs:
+  config:
+    runs-on: ubuntu-latest
+    outputs:
+      pg_latest: ${{ steps.setter.outputs.PG_LATEST }}
+    steps:
+    - name: Checkout source code
+      uses: actions/checkout@v2
+    - name: Read configuration
+      id: setter
+      run: python .github/gh_config_reader.py
+
   abi_test:
     name: ABI Test ${{ matrix.type}} PG${{ matrix.pg }}
     runs-on: ubuntu-latest
+    needs: config
     strategy:
       fail-fast: false
       matrix:
-        pg: ["12.8","13.4","14.0"]
+        pg: ${{ fromJson(needs.config.outputs.pg_latest) }}
         type: ["min","max"]
 
     steps:

.github/gh_config_reader.py
@@ -1,0 +1,14 @@
+#!/usr/bin/env python
+
+#  This file and its contents are licensed under the Apache License 2.0.
+#  Please see the included NOTICE for copyright information and
+#  LICENSE-APACHE for a copy of the license.
+
+import json
+import ci_settings
+
+# generate commands to set github action variables
+for key in dir(ci_settings):
+    if not key.startswith("__"):
+        value = getattr(ci_settings, key)
+        print(str.format("::set-output name={0}::{1}", key, json.dumps(value)))

.github/gh_matrix_builder.py
@@ -17,16 +17,10 @@
 
 import json
 import sys
+from ci_settings import PG12_EARLIEST, PG12_LATEST, PG13_EARLIEST, PG13_LATEST, PG14_EARLIEST, PG14_LATEST
 
 # github event type which is either push, pull_request or schedule
 event_type = sys.argv[1]
-
-PG12_EARLIEST = "12.0"
-PG12_LATEST = "12.12"
-PG13_EARLIEST = "13.2"
-PG13_LATEST = "13.8"
-PG14_EARLIEST = "14.0"
-PG14_LATEST = "14.5"
 
 m = {"include": [],}
 

.github/workflows/coverity.yaml
@@ -7,13 +7,25 @@ on:
     branches:
       - coverity_scan
 jobs:
+  config:
+    runs-on: ubuntu-latest
+    outputs:
+      pg_latest: ${{ steps.setter.outputs.PG_LATEST }}
+    steps:
+    - name: Checkout source code
+      uses: actions/checkout@v2
+    - name: Read configuration
+      id: setter
+      run: python .github/gh_config_reader.py
+
   coverity:
     name: Coverity ${{ matrix.pg }}
     runs-on: ${{ matrix.os }}
+    needs: config
     strategy:
       fail-fast: false
       matrix:
-        pg: ["12.12", "13.8", "14.5"]
+        pg: ${{ fromJson(needs.config.outputs.pg_latest) }}
         os: [ubuntu-20.04]
     env:
       PG_SRC_DIR: pgbuild

.github/workflows/linux-32bit-build-and-test.yaml
@@ -8,9 +8,21 @@ on:
       - prerelease_test
   pull_request:
 jobs:
+  config:
+    runs-on: ubuntu-latest
+    outputs:
+      pg_latest: ${{ steps.setter.outputs.PG_LATEST }}
+    steps:
+    - name: Checkout source code
+      uses: actions/checkout@v2
+    - name: Read configuration
+      id: setter
+      run: python .github/gh_config_reader.py
+
   regress_linux_32bit:
     name: PG${{ matrix.pg }} ${{ matrix.build_type }} linux-i386
     runs-on: ubuntu-latest
+    needs: config
     container:
       image: i386/debian:buster-slim
       options: --privileged --ulimit core=-1
@@ -20,7 +32,7 @@ jobs:
     strategy:
       fail-fast: false
       matrix:
-        pg: ["12.12","13.8","14.5"]
+        pg: ${{ fromJson(needs.config.outputs.pg_latest) }}
         build_type: [ Debug ]
 
     steps:

.github/workflows/linux-build-and-test.yaml
@@ -17,7 +17,7 @@ jobs:
       uses: actions/checkout@v2
     - name: Build matrix
       id: set-matrix
-      run: python scripts/gh_matrix_builder.py ${{ github.event_name }}
+      run: python .github/gh_matrix_builder.py ${{ github.event_name }}
 
   regress:
     name: PG${{ matrix.pg }}${{ matrix.snapshot }} ${{ matrix.name }} ${{ matrix.os }}

.github/workflows/sanitizer-build-and-test.yaml
@@ -36,15 +36,27 @@ env:
   LSAN_OPTIONS: suppressions=${{ github.workspace }}/scripts/suppressions/suppr_leak.txt print_suppressions=0 log_path=${{ github.workspace }}/sanitizer.log log_exe_name=true print_suppressions=false exitcode=27 external_symbolizer_path=/usr/lib/llvm-10/bin/llvm-symbolizer
   UBSAN_OPTIONS: suppressions=${{ github.workspace }}/scripts/suppressions/suppr_ub.txt print_stacktrace=1 halt_on_error=1 log_path=${{ github.workspace }}/sanitizer.log log_exe_name=true print_suppressions=false exitcode=27 external_symbolizer_path=/usr/lib/llvm-10/bin/llvm-symbolizer
 jobs:
+  config:
+    runs-on: ubuntu-latest
+    outputs:
+      pg_latest: ${{ steps.setter.outputs.PG_LATEST }}
+    steps:
+    - name: Checkout source code
+      uses: actions/checkout@v2
+    - name: Read configuration
+      id: setter
+      run: python .github/gh_config_reader.py
+
   sanitizer:
     name: PG${{ matrix.pg }} Sanitizer ${{ matrix.os }}
     runs-on: ${{ matrix.os }}
+    needs: config
     strategy:
       fail-fast: false
       matrix:
         # "os" has to be in the matrix due to a bug in "env": https://github.community/t/how-to-use-env-context/16975
         os: ["ubuntu-20.04"]
-        pg: ["12.12","13.8","14.5"]
+        pg: ${{ fromJson(needs.config.outputs.pg_latest) }}
     steps:
     - name: Install Linux Dependencies
       run: |

commit ca1d4103f1c0a5c848d2ebe813a0f9d38c61b45f

Fix ABI test
Recent CI settings refactoring made in commit 0c6f4e24 broke the
ABI test. This patch fixes the test and adds setting for the minimum
ABI compatible version.

.github/ci_settings.py
@@ -5,12 +5,21 @@
 #  LICENSE-APACHE for a copy of the license.
 
 # Common settings for our CI jobs
+#
+# EARLIEST is the minimum postgres version required when building from source
+# LATEST is the maximum postgres version that is supported
+# ABI_MIN is the minimum postgres version required when the extension was build against LATEST
 
 PG12_EARLIEST = "12.0"
 PG12_LATEST = "12.12"
+PG12_ABI_MIN = "12.8"
+
 PG13_EARLIEST = "13.2"
 PG13_LATEST = "13.8"
+PG13_ABI_MIN = "13.5"
+
 PG14_EARLIEST = "14.0"
 PG14_LATEST = "14.5"
+PG14_ABI_MIN = "14.0"
 
 PG_LATEST = [PG12_LATEST, PG13_LATEST, PG14_LATEST]

.github/workflows/abi.yaml
@@ -18,23 +18,54 @@ jobs:
   config:
     runs-on: ubuntu-latest
     outputs:
-      pg_latest: ${{ steps.setter.outputs.PG_LATEST }}
+      pg12_abi_min: ${{ steps.config.outputs.pg12_abi_min }}
+      pg13_abi_min: ${{ steps.config.outputs.pg13_abi_min }}
+      pg14_abi_min: ${{ steps.config.outputs.pg14_abi_min }}
+      pg12_latest: ${{ steps.config.outputs.pg12_latest }}
+      pg13_latest: ${{ steps.config.outputs.pg13_latest }}
+      pg14_latest: ${{ steps.config.outputs.pg14_latest }}
+
     steps:
     - name: Checkout source code
       uses: actions/checkout@v2
     - name: Read configuration
-      id: setter
+      id: config
       run: python .github/gh_config_reader.py
 
   abi_test:
-    name: ABI Test ${{ matrix.type}} PG${{ matrix.pg }}
+    name: ABI Test ${{ matrix.type}} PG${{ matrix.test }}
     runs-on: ubuntu-latest
     needs: config
     strategy:
       fail-fast: false
       matrix:
-        pg: ${{ fromJson(needs.config.outputs.pg_latest) }}
-        type: ["min","max"]
+        test: [ "12backward", "12forward", "13backward", "13forward", "14backward", "14forward" ]
+        os: [ windows-2019 ]
+        include:
+          - test: 12backward
+            pg: 12
+            builder: ${{ fromJson(needs.config.outputs.pg12_latest) }}
+            tester: ${{ fromJson(needs.config.outputs.pg12_abi_min ) }}
+          - test: 12forward
+            pg: 12
+            builder: ${{ fromJson(needs.config.outputs.pg12_abi_min ) }}
+            tester: ${{ fromJson(needs.config.outputs.pg12_latest) }}
+          - test: 13backward
+            pg: 13
+            builder: ${{ fromJson(needs.config.outputs.pg13_latest) }}
+            tester: ${{ fromJson(needs.config.outputs.pg13_abi_min ) }}
+          - test: 13forward
+            pg: 13
+            builder: ${{ fromJson(needs.config.outputs.pg13_abi_min ) }}
+            tester: ${{ fromJson(needs.config.outputs.pg13_latest) }}
+          - test: 14backward
+            pg: 14
+            builder: ${{ fromJson(needs.config.outputs.pg14_latest) }}
+            tester: ${{ fromJson(needs.config.outputs.pg14_abi_min) }}
+          - test: 14forward
+            pg: 14
+            builder: ${{ fromJson(needs.config.outputs.pg14_abi_min) }}
+            tester: ${{ fromJson(needs.config.outputs.pg14_latest) }}
 
     steps:
 
@@ -43,14 +74,7 @@ jobs:
 
     - name: Build extension
       run: |
-
-        if [[ "${{ matrix.type }}" == "min" ]]; then
-          PG_MAJOR=$(echo "${{matrix.pg}}" | cut -c 1-2 )
-          # this will be latest released version
-          BUILDER_IMAGE="postgres:${PG_MAJOR}-alpine"
-        else
-          BUILDER_IMAGE="postgres:${{matrix.pg}}-alpine"
-        fi
+        BUILDER_IMAGE="postgres:${{matrix.builder}}-alpine"
 
         docker run -i --rm -v $(pwd):/mnt ${BUILDER_IMAGE} bash <<"EOF"
           apk add cmake gcc make build-base krb5-dev openssl-dev git > /dev/null
@@ -65,14 +89,7 @@ jobs:
 
     - name: Run tests
       run: |
-
-        if [[ "${{ matrix.type }}" == "min" ]]; then
-          TEST_IMAGE="postgres:${{matrix.pg}}-alpine"
-        else
-          PG_MAJOR=$(echo "${{matrix.pg}}" | cut -c 1-2 )
-          # this will be latest released version
-          TEST_IMAGE="postgres:${PG_MAJOR}-alpine"
-        fi
+        TEST_IMAGE="postgres:${{ matrix.tester }}-alpine"
 
         docker run -i --rm -v $(pwd):/mnt ${TEST_IMAGE} bash <<"EOF"
           apk add cmake gcc make build-base krb5-dev openssl-dev sudo > /dev/null