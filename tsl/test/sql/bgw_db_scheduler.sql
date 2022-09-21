887bd3e317ba1c4e52067977cc251c2af81318a1
Konstantina Skovola <konstantina@timescale.com>
2022-09-21 09:08:51 +0200

    split scheduler tests

--- tsl/test/sql/bgw_db_scheduler.sql
+++ tsl/test/sql/bgw_db_scheduler.sql
@@ -22,11 +22,11 @@ CREATE OR REPLACE FUNCTION ts_bgw_test_job_sleep(job_id INT, config JSONB) RETUR
 CREATE OR REPLACE FUNCTION ts_bgw_params_reset_time(set_time BIGINT = 0, wait BOOLEAN = false) RETURNS VOID
 AS :MODULE_PATHNAME LANGUAGE C VOLATILE;
 
-CREATE OR REPLACE FUNCTION insert_job(application_name NAME,job_type NAME, schedule_interval INTERVAL, max_runtime INTERVAL, retry_period INTERVAL, owner NAME DEFAULT CURRENT_ROLE, scheduled BOOL DEFAULT true) RETURNS INT LANGUAGE SQL SECURITY DEFINER AS
+CREATE OR REPLACE FUNCTION insert_job(application_name NAME,job_type NAME, schedule_interval INTERVAL, max_runtime INTERVAL, retry_period INTERVAL, owner NAME DEFAULT CURRENT_ROLE, scheduled BOOL DEFAULT true, fixed_schedule DEFAULT false) RETURNS INT LANGUAGE SQL SECURITY DEFINER AS
 $$
   INSERT INTO _timescaledb_config.bgw_job(application_name,schedule_interval,max_runtime,max_retries,
-  retry_period,proc_name,proc_schema,owner,scheduled,initial_start)
-  VALUES($1,$3,$4,5,$5,$2,'public',$6,$7,'2000-01-01 00:00:00+00'::timestamptz) RETURNING id;
+  retry_period,proc_name,proc_schema,owner,scheduled,fixed_schedule)
+  VALUES($1,$3,$4,5,$5,$2,'public',$6,$7,$8) RETURNING id;
 $$;
 
 CREATE OR REPLACE FUNCTION test_toggle_scheduled(job_id INTEGER) RETURNS VOID LANGUAGE SQL SECURITY DEFINER AS
