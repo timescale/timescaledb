080011d767a6e61d09dc83b4d58f8be7dc9ecb31
Alexander Kuzmenkov <akuzmenkov@timescale.com>
2022-10-18 20:26:45 +0400

    Speed up the dist_copy tests
    In some cases we can use less chunks, less data, and not truncate
    tables.

--- tsl/test/sql/dist_copy_format_long.sql
+++ tsl/test/sql/dist_copy_format_long.sql
@@ -25,14 +25,15 @@ SET ROLE :ROLE_1;
 -- Aim to about 100 partitions, the data is from 1995 to 2022.
 create table uk_price_paid(price integer, "date" date, postcode1 text, postcode2 text, type smallint, is_new bool, duration smallint, addr1 text, addr2 text, street text, locality text, town text, district text, country text, category smallint);
 select create_distributed_hypertable('uk_price_paid', 'date', 'postcode2',
-    chunk_time_interval => interval '90 day');
+    chunk_time_interval => interval '270 day');
 
 -- Populate.
-\copy uk_price_paid from program 'zcat < data/prices-100k-random-1.tsv.gz';
-select count(*), sum(price) from uk_price_paid;
+\copy uk_price_paid from program 'zcat < data/prices-10k-random-1.tsv.gz';
+select count(*), sum(price), sum(price) / count(*) from uk_price_paid;
+select count(*) from show_chunks('uk_price_paid');
 
 -- Make binary file.
-\copy (select * from uk_price_paid) to 'prices-100k.pgbinary' with (format binary);
+\copy (select * from uk_price_paid) to 'prices-10k.pgbinary' with (format binary);
 
 -- Binary input with binary data transfer.
 set timescaledb.enable_connection_binary_data = true;
@@ -41,30 +42,28 @@ create table uk_price_paid_bin(like uk_price_paid);
 select create_distributed_hypertable('uk_price_paid_bin', 'date', 'postcode2',
     chunk_time_interval => interval '90 day', replication_factor => 2);
 
-\copy uk_price_paid_bin from 'prices-100k.pgbinary' with (format binary);
-select count(*), sum(price) from uk_price_paid_bin;
+\copy uk_price_paid_bin from 'prices-10k.pgbinary' with (format binary);
+select count(*), sum(price), sum(price) / count(*) from uk_price_paid_bin;
 
 -- Text input with explicit format option and binary data transfer.
-truncate uk_price_paid_bin;
-\copy uk_price_paid_bin from program 'zcat < data/prices-100k-random-1.tsv.gz' with (format text);
-select count(*), sum(price) from uk_price_paid_bin;
+\copy uk_price_paid_bin from program 'zcat < data/prices-10k-random-1.tsv.gz' with (format text);
+select count(*), sum(price), sum(price) / count(*) from uk_price_paid_bin;
 
 -- Binary input with text data transfer. Doesn't work.
 set timescaledb.dist_copy_transfer_format = 'text';
 \set ON_ERROR_STOP off
-\copy uk_price_paid_bin from 'prices-100k.pgbinary' with (format binary);
+\copy uk_price_paid_bin from 'prices-10k.pgbinary' with (format binary);
 \set ON_ERROR_STOP on
 
 -- Text input with text data transfer.
-truncate uk_price_paid_bin;
-\copy uk_price_paid_bin from program 'zcat < data/prices-100k-random-1.tsv.gz';
-select count(*), sum(price) from uk_price_paid_bin;
+\copy uk_price_paid_bin from program 'zcat < data/prices-10k-random-1.tsv.gz';
+select count(*), sum(price), sum(price) / count(*) from uk_price_paid_bin;
 
 -- Nonsensical settings
 set timescaledb.dist_copy_transfer_format = 'binary';
 set timescaledb.enable_connection_binary_data = false;
 \set ON_ERROR_STOP off
-\copy uk_price_paid_bin from 'prices-100k.pgbinary' with (format binary);
+\copy uk_price_paid_bin from 'prices-10k.pgbinary' with (format binary);
 \set ON_ERROR_STOP on
 
 -- Teardown
