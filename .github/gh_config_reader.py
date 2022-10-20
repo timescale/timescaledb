6ada7ed82725e5c42378756635ba874c6699ad2f
Mats Kindahl <mats@timescale.com>
2022-10-20 13:37:26 +0200

    Fix GitHub output action
    To avoid untrusted logged data to use `set-state` and `set-output`
    workflow commands without the intention of the workflow author GitHub
    have introduced a new set of environment files to manage state and
    output.
    
    This commit changes the existing uses of `set-output` to use the new
    environment files instead.
    
    See https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands/

--- .github/gh_config_reader.py
+++ .github/gh_config_reader.py
@@ -4,11 +4,13 @@
 #  Please see the included NOTICE for copyright information and
 #  LICENSE-APACHE for a copy of the license.
 
-import json
 import ci_settings
+import json
+import os
 
 # generate commands to set github action variables
 for key in dir(ci_settings):
     if not key.startswith("__"):
         value = getattr(ci_settings, key)
-        print(str.format("::set-output name={0}::{1}", key, json.dumps(value)))
+        with open(os.environ["GITHUB_OUTPUT"], "a") as output:
+            print(str.format("{0}={1}", key, json.dumps(value)), file=output)
