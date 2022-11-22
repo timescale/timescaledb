43dea3195b110d2f97be6ec306315ab893aa32b3
Lakshmi Narayanan Sreethar <lakshmi@timescale.com>
2022-11-22 21:06:00 +0530

    Debug dist_move_chunk

--- .github/gh_matrix_builder.py
+++ .github/gh_matrix_builder.py
@@ -152,10 +152,8 @@ m["include"].append(
             "pg": 15,
             "snapshot": "snapshot",
             "tsdb_build_args": "-DASSERTIONS=ON -DREQUIRE_ALL_TESTS=ON -DEXPERIMENTAL=ON",
-            # below tests are tracked as part of #4972
-            "installcheck_args": "SKIPS='dist_move_chunk' "
             # below tests are tracked as part of #4835
-            "IGNORES='telemetry_stats partialize_finalize dist_fetcher_type "
+            "installcheck_args": "IGNORES='telemetry_stats partialize_finalize dist_fetcher_type "
             # below tests are tracked as part of #4837
             "remote_txn'",
         }
