b3f0eea73d975474870450d13dbbdf94b48c8fdc
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2023-01-10 15:19:18 -0300

    Fix CAgg on CAgg variable bucket size validation
    Previous attempt to fix it (PR #5130) was not entirely correct because
    the bucket width calculation for interval width was wrong.
    
    Fixed it by properly calculate the bucket width for intervals using the
    Postgres internal function `interval_part` to extract the epoch of the
    interval and execute the validations. For integer widths use the already
    calculated bucket width.
    
    Fixes #5158, #5168

--- tsl/test/sql/cagg_on_cagg_dist_ht.sql
+++ tsl/test/sql/cagg_on_cagg_dist_ht.sql
@@ -22,6 +22,8 @@ GRANT CREATE ON SCHEMA public TO :ROLE_DEFAULT_PERM_USER;
 
 -- Global test variables
 \set IS_DISTRIBUTED TRUE
+\set IS_TIME_DIMENSION_WITH_TIMEZONE_1ST FALSE
+\set IS_TIME_DIMENSION_WITH_TIMEZONE_2TH FALSE
 
 -- ########################################################
 -- ## INTEGER data type tests
@@ -199,6 +201,68 @@ SET timezone TO 'UTC';
 \set BUCKET_WIDTH_1ST 'INTERVAL \'2 hours\''
 \set BUCKET_WIDTH_2TH 'INTERVAL \'1 hour\''
 \set WARNING_MESSAGE '-- SHOULD ERROR because new bucket should be greater than previous'
+\ir include/cagg_on_cagg_validations.sql
+
+--
+-- Validations using time bucket with timezone (ref issue #5126)
+--
+\set TIME_DIMENSION_DATATYPE TIMESTAMPTZ
+\set IS_TIME_DIMENSION_WITH_TIMEZONE_1ST TRUE
+\set IS_TIME_DIMENSION_WITH_TIMEZONE_2TH TRUE
+\set CAGG_NAME_1ST_LEVEL conditions_summary_1_5m
+\set CAGG_NAME_2TH_LEVEL conditions_summary_2_1h
+\set BUCKET_TZNAME_1ST 'US/Pacific'
+\set BUCKET_TZNAME_2TH 'US/Pacific'
+\set BUCKET_WIDTH_1ST 'INTERVAL \'5 minutes\''
+\set BUCKET_WIDTH_2TH 'INTERVAL \'1 hour\''
+\set WARNING_MESSAGE '-- SHOULD WORK'
+\ir include/cagg_on_cagg_validations.sql
+
+\set BUCKET_WIDTH_1ST 'INTERVAL \'5 minutes\''
+\set BUCKET_WIDTH_2TH 'INTERVAL \'16 minutes\''
+\set WARNING_MESSAGE '-- SHOULD ERROR because non-multiple bucket sizes'
+\ir include/cagg_on_cagg_validations.sql
+
+--
+-- Variable bucket size with the same timezones
+--
+\set BUCKET_TZNAME_1ST 'UTC'
+\set BUCKET_TZNAME_2TH 'UTC'
+\set BUCKET_WIDTH_1ST 'INTERVAL \'1 day\''
+\set BUCKET_WIDTH_2TH 'INTERVAL \'1 month\''
+\set WARNING_MESSAGE '-- SHOULD WORK'
+\ir include/cagg_on_cagg_validations.sql
+
+--
+-- Variable bucket size with different timezones
+--
+\set BUCKET_TZNAME_1ST 'US/Pacific'
+\set BUCKET_TZNAME_2TH 'UTC'
+\set BUCKET_WIDTH_1ST 'INTERVAL \'1 day\''
+\set BUCKET_WIDTH_2TH 'INTERVAL \'1 month\''
+\set WARNING_MESSAGE '-- SHOULD WORK'
+\ir include/cagg_on_cagg_validations.sql
+
+--
+-- TZ bucket on top of non-TZ bucket
+--
+\set IS_TIME_DIMENSION_WITH_TIMEZONE_1ST FALSE
+\set IS_TIME_DIMENSION_WITH_TIMEZONE_2TH TRUE
+\set BUCKET_TZNAME_2TH 'UTC'
+\set BUCKET_WIDTH_1ST 'INTERVAL \'1 day\''
+\set BUCKET_WIDTH_2TH 'INTERVAL \'1 month\''
+\set WARNING_MESSAGE '-- SHOULD WORK'
+\ir include/cagg_on_cagg_validations.sql
+
+--
+-- non-TZ bucket on top of TZ bucket
+--
+\set IS_TIME_DIMENSION_WITH_TIMEZONE_1ST TRUE
+\set IS_TIME_DIMENSION_WITH_TIMEZONE_2TH FALSE
+\set BUCKET_TZNAME_1ST 'UTC'
+\set BUCKET_WIDTH_1ST 'INTERVAL \'1 day\''
+\set BUCKET_WIDTH_2TH 'INTERVAL \'1 month\''
+\set WARNING_MESSAGE '-- SHOULD WORK'
 \ir include/cagg_on_cagg_validations.sql
 
 -- Cleanup
