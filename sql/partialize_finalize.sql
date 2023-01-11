05479bf94c4ae742cad66f3901736cb77284b3ff
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2023-01-11 17:29:32 -0300

    Remove parallel safe from partialize_agg
    Previous PR #4307 mark `partialize_agg` and `finalize_agg` as parallel
    safe but this change is leading to incorrect results in some cases.
    
    Those functions are supposed work in parallel but seems is not the case
    and it is not evident yet the root cause and how to properly use it in
    parallel queries so we decided to revert this change and provide correct
    results to users.
    
    Fixes #4922

--- sql/partialize_finalize.sql
+++ sql/partialize_finalize.sql
@@ -2,30 +2,24 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
--- These wrapper functions are used to push down aggregation to data nodes.
--- They can be marked parallel-safe, and the parallel plan will be chosen
--- depending on whether the underlying aggregate function itself is
--- parallel-safe.
-
 CREATE OR REPLACE FUNCTION _timescaledb_internal.partialize_agg(arg ANYELEMENT)
-RETURNS BYTEA AS '@MODULE_PATHNAME@', 'ts_partialize_agg' LANGUAGE C STABLE PARALLEL SAFE;
+RETURNS BYTEA AS '@MODULE_PATHNAME@', 'ts_partialize_agg' LANGUAGE C VOLATILE;
 
 CREATE OR REPLACE FUNCTION _timescaledb_internal.finalize_agg_sfunc(
 tstate internal, aggfn TEXT, inner_agg_collation_schema NAME, inner_agg_collation_name NAME, inner_agg_input_types NAME[][], inner_agg_serialized_state BYTEA, return_type_dummy_val ANYELEMENT)
 RETURNS internal
 AS '@MODULE_PATHNAME@', 'ts_finalize_agg_sfunc'
-LANGUAGE C IMMUTABLE PARALLEL SAFE;
+LANGUAGE C IMMUTABLE;
 
 CREATE OR REPLACE FUNCTION _timescaledb_internal.finalize_agg_ffunc(
 tstate internal, aggfn TEXT, inner_agg_collation_schema NAME, inner_agg_collation_name NAME, inner_agg_input_types NAME[][], inner_agg_serialized_state BYTEA, return_type_dummy_val ANYELEMENT)
 RETURNS anyelement
 AS '@MODULE_PATHNAME@', 'ts_finalize_agg_ffunc'
-LANGUAGE C IMMUTABLE PARALLEL SAFE;
+LANGUAGE C IMMUTABLE;
 
 CREATE OR REPLACE AGGREGATE _timescaledb_internal.finalize_agg(agg_name TEXT,  inner_agg_collation_schema NAME,  inner_agg_collation_name NAME, inner_agg_input_types NAME[][], inner_agg_serialized_state BYTEA, return_type_dummy_val anyelement) (
     SFUNC = _timescaledb_internal.finalize_agg_sfunc,
     STYPE = internal,
     FINALFUNC = _timescaledb_internal.finalize_agg_ffunc,
-    FINALFUNC_EXTRA,
-    PARALLEL = SAFE
+    FINALFUNC_EXTRA
 );
