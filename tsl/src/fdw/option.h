19065bbdf39d2536a227041331adf24ffb38ffdc
Jan Nidzwetzki <jan@timescale.com>
2023-01-17 08:46:21 +0100

    Introduce a FDW option to mark reference tables
    With this patch, the ability to mark reference tables (tables that exist
    on all data nodes of a multi-node installation) via an FDW option has
    been added.

--- tsl/src/fdw/option.h
+++ tsl/src/fdw/option.h
@@ -10,6 +10,7 @@
 
 extern void option_validate(List *options_list, Oid catalog);
 extern List *option_extract_extension_list(const char *extensions_string, bool warn_on_missing);
+extern List *option_extract_join_ref_table_list(const char *join_tables);
 extern bool option_get_from_options_list_int(List *options, const char *optionname, int *value);
 
 #endif /* TIMESCALEDB_TSL_FDW_OPTION_H */
