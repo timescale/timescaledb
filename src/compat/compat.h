22a98176f317b1e1f6d8a7541d91b68af825bee8
Jacob Champion <jchampion@timescale.com>
2023-01-27 15:25:16 -0800

    WIP: deal with _ownercheck changes
    Similar to _aclcheck; see upstream
    afbfc02983f86c4d71825efa6befd547fe81a926.

--- src/compat/compat.h
+++ src/compat/compat.h
@@ -795,8 +795,9 @@ RelationGetSmgr(Relation rel)
 #endif
 
 /*
- * PG16 consolidates (almost) all _aclcheck functions into one:
+ * PG16 consolidates (almost) all _aclcheck and _ownercheck functions into one:
  *   https://github.com/postgres/postgres/commit/c727f511bd
+ *   https://github.com/postgres/postgres/commit/afbfc02983
  */
 #if PG16_GE
 #define pg_database_aclcheck(...) object_aclcheck(DatabaseRelationId, __VA_ARGS__)
@@ -804,6 +805,7 @@ RelationGetSmgr(Relation rel)
 #define pg_namespace_aclcheck(...) object_aclcheck(NamespaceRelationId, __VA_ARGS__)
 #define pg_proc_aclcheck(...) object_aclcheck(ProcedureRelationId, __VA_ARGS__)
 #define pg_tablespace_aclcheck(...) object_aclcheck(TableSpaceRelationId, __VA_ARGS__)
+#define pg_class_ownercheck(...) object_ownercheck(RelationRelationId, __VA_ARGS__)
 #endif
 
 #endif /* TIMESCALEDB_COMPAT_H */
