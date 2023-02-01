2a47462fbc930ac6230a83d07100c8a21d040b70
Sven Klemm <sven@timescale.com>
2023-02-01 07:59:14 +0100

    Remove SECURITY DEFINER from get_telemetry_report
    We should not broadly make functions security definer as that increases
    the attack surface for our extension. Especially for our telemetry we
    should strive to only run it with the minimum required privileges.

--- sql/with_telemetry.sql
+++ sql/with_telemetry.sql
@@ -2,11 +2,9 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
--- Security definer is good to use here since the function computes an
--- aggregate of error codes and does not expose the individual messages.
 CREATE OR REPLACE FUNCTION @extschema@.get_telemetry_report() RETURNS jsonb
     AS '@MODULE_PATHNAME@', 'ts_telemetry_get_report_jsonb'
-	LANGUAGE C STABLE PARALLEL SAFE SECURITY DEFINER;
+	LANGUAGE C STABLE PARALLEL SAFE;
 
 INSERT INTO _timescaledb_config.bgw_job (id, application_name, schedule_interval, max_runtime, max_retries, retry_period, proc_schema, proc_name, owner, scheduled, fixed_schedule) VALUES
 (1, 'Telemetry Reporter [1]', INTERVAL '24h', INTERVAL '100s', -1, INTERVAL '1h', '_timescaledb_internal', 'policy_telemetry', CURRENT_ROLE, true, false)
