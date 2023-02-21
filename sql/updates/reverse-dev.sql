c8c50dad7eca4f7425bfb9980b872a1c44201ceb
Maheedhar PV <mahi@timescale.com>
2023-02-21 23:03:46 +0530

    Post-release fixes for 2.10.0
    Bumping the previous version and adding tests for 2.10.0

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,9 +1,3 @@
-GRANT ALL ON _timescaledb_internal.job_errors TO PUBLIC;
-
-ALTER EXTENSION timescaledb DROP VIEW timescaledb_information.job_errors;
-
-DROP VIEW timescaledb_information.job_errors;
-
 DROP FUNCTION _timescaledb_internal.ping_data_node(NAME, INTERVAL);
 
 CREATE OR REPLACE FUNCTION _timescaledb_internal.ping_data_node(node_name NAME) RETURNS BOOLEAN
