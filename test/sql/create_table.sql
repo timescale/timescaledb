c13ed17fbce56129136239ad369ac5e5f1502821
Bharathy <satish.8483@gmail.com>
2023-03-07 20:11:42 +0530

    Fix DELETE command tag
    DELETE on hypertables always reports 0 as affected rows.
    This patch fixes this issue.

--- test/sql/create_table.sql
+++ test/sql/create_table.sql
@@ -13,3 +13,30 @@ SELECT create_hypertable('test_hyper_pk', 'time');
 -- Foreign key constraints that reference hypertables are currently unsupported
 CREATE TABLE test_fk(time TIMESTAMPTZ REFERENCES test_hyper_pk(time));
 \set ON_ERROR_STOP 1
+
+CREATE TABLE test_delete(time timestamp with time zone PRIMARY KEY, temp float);
+SELECT create_hypertable('test_delete', 'time');
+INSERT INTO test_delete VALUES('2017-01-20T09:00:01', 22.5);
+INSERT INTO test_delete VALUES('2017-01-20T09:00:21', 21.2);
+INSERT INTO test_delete VALUES('2017-01-20T09:00:47', 25.1);
+INSERT INTO test_delete VALUES('2020-01-20T09:00:47', 25.1);
+INSERT INTO test_delete VALUES('2021-01-20T09:00:47', 25.1);
+SELECT * FROM test_delete WHERE temp = 25.1 ORDER BY time;
+
+CREATE OR replace FUNCTION test_delete_row_count()
+RETURNS void AS $$
+DECLARE
+    v_cnt numeric;
+BEGIN
+    v_cnt := 0;
+
+        DELETE FROM test_delete WHERE temp = 25.1;
+        GET DIAGNOSTICS v_cnt = ROW_COUNT;
+
+        IF v_cnt != 3 THEN
+           RAISE EXCEPTION 'unexpected result';
+        END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT test_delete_row_count();
