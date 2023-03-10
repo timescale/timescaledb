356a20777c6f1aab234c5604f0f97de8077b4890
Jan Nidzwetzki <jan@timescale.com>
2023-03-10 14:34:56 +0100

    Handle user-defined FDW options properly
    This patch changes the way user-defined FDW options (e.g., startup
    costs, per-tuple costs) are handled. So far, these values were retrieved
    in apply_fdw_and_server_options() but reset to default values afterward.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@ accidentally triggering the load of a previous DB version.**
 * #5396 Fix SEGMENTBY columns predicates to be pushed down
 * #5410 Fix file trailer handling in the COPY fetcher
 * #5233 Out of on_proc_exit slots on guc license change
+* #5427 Handle user-defined FDW options properly
 
 **Thanks**
 * @nikolaps for reporting an issue with the COPY fetcher
---
+++ tsl/test/sql/include/data_fetcher_fdw_settings.sql
@@ -0,0 +1,28 @@
+-- This file and its contents are licensed under the Timescale License.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-TIMESCALE for a copy of the license.
+
+-- Default settings
+EXPLAIN (COSTS) SELECT * FROM one_batch;
+
+-- Set custom startup cost
+ALTER FOREIGN DATA WRAPPER timescaledb_fdw OPTIONS (ADD fdw_startup_cost '200');
+EXPLAIN (COSTS) SELECT * FROM one_batch;
+
+-- Set custom tuple cost
+ALTER FOREIGN DATA WRAPPER timescaledb_fdw OPTIONS (ADD fdw_tuple_cost '1');
+EXPLAIN (COSTS) SELECT * FROM one_batch;
+
+-- Update startup cost
+ALTER FOREIGN DATA WRAPPER timescaledb_fdw OPTIONS (SET fdw_startup_cost '2');
+EXPLAIN (COSTS) SELECT * FROM one_batch;
+
+-- Update startup cost
+ALTER FOREIGN DATA WRAPPER timescaledb_fdw OPTIONS (SET fdw_tuple_cost '0.5');
+EXPLAIN (COSTS) SELECT * FROM one_batch;
+
+-- Reset custom settings
+ALTER FOREIGN DATA WRAPPER timescaledb_fdw OPTIONS (DROP fdw_startup_cost);
+ALTER FOREIGN DATA WRAPPER timescaledb_fdw OPTIONS (DROP fdw_tuple_cost);
+EXPLAIN (COSTS) SELECT * FROM one_batch;
+
