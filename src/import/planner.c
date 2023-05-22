b2b3acf6ac1f580c0c6737386f6360b912eb6c8f
Lakshmi Narayanan Sreethar <lakshmi@timescale.com>
2023-05-22 21:22:27 +0530

    PG16: No need to pass create_new_ph flag to find_placeholder_info
    PG16 also optimized the PlaceFolderInfo lookups to perform in constant
    time, so there is no need to do an additional cheap/quick test using
    bms_overlap to see if the PHV might be evaluated in the outer rels.
    
    postgres/postgres@6569ca439
    postgres/postgres@b3ff6c742

--- src/import/planner.c
+++ src/import/planner.c
@@ -970,6 +970,7 @@ replace_nestloop_params_mutator(Node *node, PlannerInfo *root)
 		/* Upper-level PlaceHolderVars should be long gone at this point */
 		Assert(phv->phlevelsup == 0);
 
+#if PG16_LT
 		/*
 		 * Check whether we need to replace the PHV.  We use bms_overlap as a
 		 * cheap/quick test to see if the PHV might be evaluated in the outer
@@ -977,6 +978,10 @@ replace_nestloop_params_mutator(Node *node, PlannerInfo *root)
 		 */
 		if (!bms_overlap(phv->phrels, root->curOuterRels) ||
 			!bms_is_subset(find_placeholder_info(root, phv, false)->ph_eval_at, root->curOuterRels))
+#else
+		/* Check whether we need to replace the PHV */
+		if (!bms_is_subset(find_placeholder_info(root, phv)->ph_eval_at, root->curOuterRels))
+#endif
 		{
 			/*
 			 * We can't replace the whole PHV, but we might still need to
