ae76c08f96bbc4d17ffa452840509561b3ac959a
Zoltan Haindrich <zoltan@timescale.com>
2023-06-13 09:22:07 +0000

    add comment

--- src/ts_catalog/continuous_agg.h
+++ src/ts_catalog/continuous_agg.h
@@ -140,6 +140,7 @@ typedef enum ContinuousAggHypertableStatus
 	HypertableIsNotContinuousAgg = 0,
 	HypertableIsMaterialization = 1,
 	HypertableIsRawTable = 2,
+//	HypertableHasNonFinalizedContinousAgg = 4,
 	HypertableIsMaterializationAndRaw = HypertableIsMaterialization | HypertableIsRawTable,
 } ContinuousAggHypertableStatus;
 
