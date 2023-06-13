a651fb15fa0b2416df6021b559c71d98b373764d
Zoltan Haindrich <zoltan@timescale.com>
2023-06-13 13:25:28 +0000

    x

--- tsl/test/sql/chunk_attach_detach.sql
+++ tsl/test/sql/chunk_attach_detach.sql
@@ -10,19 +10,15 @@ ALTER TABLE main_table SET (
 
 INSERT INTO main_table SELECT t, 'dev1'  FROM generate_series('2018-03-02 1:00'::TIMESTAMPTZ, '2018-03-05 3:00', '1 hour') t;
 
--- INSERT INTO main_table VALUES
---     ('2011-11-11 11:11:11', 'foo');
-
-
--- INSERT INTO main_table VALUES
---     ('2011-11-11 11:11:11', 'foo');
-
-
 select show_chunks('main_table');
+
 SELECT (_timescaledb_internal.show_chunk(show_chunks)).*
 FROM show_chunks('main_table')
 ORDER BY slices;
 
+
+with t as (SELECT * FROM show_chunks('main_table') as t(ch) order by ch)
+select (select ch from t limit 1 offset 2), (select ch from t limit 1 offset 3);
 
 select '_timescaledb_internal._hyper_1_3_chunk' as chunk \gset
 select '_timescaledb_internal._hyper_1_4_chunk' as chunk2 \gset
@@ -48,6 +44,7 @@ create table n (like main_table);
 \d n
 insert into n 
 select * from :chunk union all select * from :chunk2;
+
 
 select _timescaledb_internal.create_chunk('main_table','{"time": [1520035200000000, 1520121600000000]}'::jsonb,null,null,'n'::regclass);
 
