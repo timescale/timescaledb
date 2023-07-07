3cb41c5e487b72ec76b89ba7f1d15e1a4cd9bfbb
Lakshmi Narayanan Sreethar <lakshmi@timescale.com>
2023-07-07 14:53:01 +0530

    Remove update files for PG12
    Removed the update files that were used only for PG12.

--- sql/updates/2.0.0-rc4--2.0.0.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE _timescaledb_catalog.hypertable
-  DROP CONSTRAINT hypertable_replication_factor_check,
-  ADD CONSTRAINT hypertable_replication_factor_check CHECK (replication_factor > 0 OR replication_factor = -1);
