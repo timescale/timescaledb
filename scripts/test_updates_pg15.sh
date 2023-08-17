e4b18fa7dd96c4686d1362d5e3f072b7db8248be
Konstantina Skovola <konstantina@timescale.com>
2023-08-17 14:15:21 +0300

    Post-release fixes for 2.11.2
    Bumping the previous version and adding tests for 2.11.2

--- scripts/test_updates_pg15.sh
+++ scripts/test_updates_pg15.sh
@@ -12,4 +12,4 @@ run_tests "$@" -v8 \
 
 # Also run repair tests for >=2.10.x versions due to PR #5441
 run_tests "$@" -r -v8 \
-          2.10.0-pg15 2.10.1-pg15 2.10.2-pg15 2.10.3-pg15 2.11.0-pg15 2.11.1-pg15
+          2.10.0-pg15 2.10.1-pg15 2.10.2-pg15 2.10.3-pg15 2.11.0-pg15 2.11.1-pg15 2.11.2-pg15
