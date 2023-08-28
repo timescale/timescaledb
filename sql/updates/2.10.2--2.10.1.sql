534335fb78353ddbf516fadbdc862a905aa1bc5c
Aryan Agal <aryanagal98@gmail.com>
2023-08-28 20:10:31 +0530

    Fix updates between 2.10.1 and 2.10.2
    The upgrade and downgrade scripts between 2.10.1 and 2.10.2 use DROP
    FUNCTION without using IF EXISTS. This breaks the upgrade and downgrade
    paths for users upgrading from much lower versions (<2.0.0) since the
    function itself will not be available, throwing an error. A similar
    situation may occur in the future, if the API is deprecated in the
    future.

--- sql/updates/2.10.2--2.10.1.sql
+++ sql/updates/2.10.2--2.10.1.sql
@@ -1,4 +1,4 @@
-DROP FUNCTION _timescaledb_internal.ping_data_node(NAME, INTERVAL);
+DROP FUNCTION IF EXISTS _timescaledb_internal.ping_data_node(NAME, INTERVAL);
 
 CREATE OR REPLACE FUNCTION _timescaledb_internal.ping_data_node(node_name NAME) RETURNS BOOLEAN
 AS '@MODULE_PATHNAME@', 'ts_data_node_ping' LANGUAGE C VOLATILE;
