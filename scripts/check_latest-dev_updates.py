38754f163b78161ba7b0d8c8994c2046523676e8
Konstantina Skovola <konstantina@timescale.com>
2023-09-25 13:00:21 +0300

    Include check for func/proc in _timescaledb_schema

--- scripts/check_latest-dev_updates.py
+++ scripts/check_latest-dev_updates.py
@@ -10,10 +10,24 @@ def remove_comments(lines):
 
 
 def unsafe_catalog_modification(s):
-    # catalog tables reside in _timescaledb_catalog, _timescaledb_internal and _timescaledb_config
-    # it is unsafe to simply ADD or DROP columns from catalog tables
+    """catalog tables reside in _timescaledb_catalog, _timescaledb_internal and _timescaledb_config.
+    It is unsafe to simply ADD or DROP columns from catalog tables"""
     matches = re.search(
         r"ALTER\s*TABLE\s*(_timescaledb_catalog|_timescaledb_internal|_timescaledb_config)[\s\S]+(DROP|ADD)\s+COLUMN",
+        s,
+        flags=re.IGNORECASE,
+    )
+    if matches:
+        return True
+    return False
+
+
+def uses_timescaledb_internal_schema(s):
+    """Functions must not be created in the _timescaledb_internal schema anymore but in _timescaledb_functions instead"""
+    # OR REPLACE is not needed because pgspot will complain, and the correct way is
+    # to simply CREATE the function.
+    matches = re.search(
+        r"CREATE\s+(FUNCTION|PROCEDURE)\s+_timescaledb_internal",
         s,
         flags=re.IGNORECASE,
     )
@@ -26,6 +40,7 @@ def main():
     # Open latest-dev.sql
     latest_dev = sys.argv[1]
     unsafe_modification = False
+    wrong_schema = False
     contents = ""
 
     with open(latest_dev, "r", encoding="utf-8") as ldev:
@@ -33,12 +48,20 @@ def main():
         lines = ldev.readlines()
         contents = remove_comments(lines)
         unsafe_modification = unsafe_catalog_modification(contents)
+        wrong_schema = uses_timescaledb_internal_schema(contents)
     if unsafe_modification:
         print(
             """
 ERROR: Attempting to alter timescaledb catalog tables without rebuilding the table.
 Rebuilding catalog tables is required to ensure consistent attribute numbers across versions."""
         )
+    if wrong_schema:
+        print(
+            """
+ERROR: Attempting to create function in the _timescaledb_internal schema, which has been deprecated
+for security reasons. Functions must be created in the _timescaledb_functions schema instead."""
+        )
+    if unsafe_modification or wrong_schema:
         sys.exit(1)
 
 
