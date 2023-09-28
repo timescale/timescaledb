6791e701125c0dca262d114f1181bd7c8e857ff5
Mats Kindahl <mats@timescale.com>
2023-09-28 17:22:03 +0200

    Restart scheduler on error
    If the scheduler receives an error, it will never restart again since
    `bgw_restart_time` is set to `BGW_NEVER_RESTART`, which will prevent
    all jobs from executing.
    
    This commit fixes the issue by adding a GUC that can be set to the
    restart time for the scheduler, and set the default to 30 seconds. It
    also adds some additional variables to be able to shutdown the
    scheduler with a non-zero exit code, which allows the restart
    functionality to be tested, as well as tests.
    
    Fixes #5091

--- src/loader/bgw_launcher.h
+++ src/loader/bgw_launcher.h
@@ -10,7 +10,9 @@
 #include <postgres.h>
 #include <fmgr.h>
 
-extern void ts_bgw_cluster_launcher_register(void);
+extern int ts_guc_bgw_scheduler_restart_time_sec;
+
+extern void ts_bgw_cluster_launcher_init(void);
 
 /*called by postmaster at launcher bgw startup*/
 TSDLLEXPORT extern Datum ts_bgw_cluster_launcher_main(PG_FUNCTION_ARGS);
