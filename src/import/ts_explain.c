8476adc48497a03c7692bdb30b5444cf8e3828a0
Sven Klemm <sven@timescale.com>
2023-10-08 10:19:10 +0200

    Move explain utility functions to src/import
    This moves EXPLAIN utility functions into apache to make it available
    to all custom nodes. Since these are copies of static postgres functions
    there is no reason for them to be TSL licensed.

--- src/import/ts_explain.c
+++ src/import/ts_explain.c
@@ -1,7 +1,7 @@
 /*
- * This file and its contents are licensed under the Timescale License.
+ * This file and its contents are licensed under the Apache License 2.0.
  * Please see the included NOTICE for copyright information and
- * LICENSE-TIMESCALE for a copy of the license.
+ * LICENSE-APACHE for a copy of the license.
  */
 
 /*
