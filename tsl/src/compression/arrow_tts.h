af10f83a48e91eb52fb099d0dcd4e8192050a2bb
Erik Nordström <erik@timescale.com>
2023-10-13 15:43:22 +0200

    Support single index over both compressed and non-compressed relations
    Add ability to have a single index that spans both the compressed and
    non-compressed data. In other words, the single index will point to
    data in two different underlying heaps. By encoding the relation a TID
    belongs to into the TID itself, it is possible to know which relation
    to fetch the tuple from given a TID.

--- tsl/src/compression/arrow_tts.h
+++ tsl/src/compression/arrow_tts.h
@@ -18,13 +18,15 @@
 
 typedef struct ArrowTupleTableSlot
 {
-	BufferHeapTupleTableSlot base;
-	TupleTableSlot *compressed_slot;
+	VirtualTupleTableSlot base;
+	TupleTableSlot *child_slot;
 	ArrowArray **arrow_columns;
-	uint16 tuple_index; /* Index of this particular tuple in the compressed (columnar data) tuple */
+	uint16 tuple_index; /* Index of this particular tuple in the compressed
+						 * (columnar data) child tuple. Note that the first
+						 * value has index 1. If the index is 0 it means the
+						 * child slot points to a non-compressed tuple. */
 	MemoryContext decompression_mcxt;
 	Bitmapset *segmentby_columns;
-	char *data;
 	int16 *attrs_offset_map;
 } ArrowTupleTableSlot;
 
@@ -86,14 +88,13 @@ build_attribute_offset_map(const TupleDesc tupdesc, const TupleDesc ctupdesc,
 
 	return attrs_offset_map;
 }
-
-extern TupleTableSlot *ExecStoreArrowTuple(TupleTableSlot *slot, TupleTableSlot *compressed_slot,
+extern TupleTableSlot *ExecStoreArrowTuple(TupleTableSlot *slot, TupleTableSlot *child_slot,
 										   uint16 tuple_index);
 extern TupleTableSlot *ExecStoreArrowTupleExisting(TupleTableSlot *slot, uint16 tuple_index);
-extern void tts_arrow_set_heaptuple_mode(TupleTableSlot *slot);
 
 #define TTS_IS_ARROWTUPLE(slot) ((slot)->tts_ops == &TTSOpsArrowTuple)
 
+#define InvalidTupleIndex 0
 #define MaxCompressedBlockNumber ((BlockNumber) 0x3FFFFF)
 
 static inline void
@@ -101,10 +102,12 @@ tid_to_compressed_tid(ItemPointer out_tid, ItemPointer in_tid, uint16 tuple_inde
 {
 	BlockNumber blockno = ItemPointerGetBlockNumber(in_tid);
 	OffsetNumber offsetno = ItemPointerGetOffsetNumber(in_tid);
-	BlockNumber index_blockno = (blockno << 10) | (tuple_index & 0x3FF);
+	BlockNumber compressed_blockno;
 
 	Assert(blockno <= MaxCompressedBlockNumber);
-	ItemPointerSet(out_tid, index_blockno, offsetno);
+
+	compressed_blockno = ((tuple_index & 0x3FF) << 22) | blockno;
+	ItemPointerSet(out_tid, compressed_blockno, offsetno);
 }
 
 static inline uint16
@@ -112,12 +115,14 @@ compressed_tid_to_tid(ItemPointer out_tid, ItemPointer in_tid)
 {
 	BlockNumber blockno = ItemPointerGetBlockNumber(in_tid);
 	OffsetNumber offsetno = ItemPointerGetOffsetNumber(in_tid);
-	uint16 tuple_index = (blockno & 0x3FF);
-	BlockNumber orig_blockno = (blockno >> 10);
+	uint16 tuple_index = (blockno >> 22);
+	BlockNumber orig_blockno = (blockno & 0x3FFFFF);
 
 	ItemPointerSet(out_tid, orig_blockno, offsetno);
 
 	return tuple_index;
 }
+
+#define is_compressed_tid(itemptr) ((ItemPointerGetBlockNumber(itemptr) >> 22) != 0)
 
 #endif /* PG_ARROW_TUPTABLE_H */
