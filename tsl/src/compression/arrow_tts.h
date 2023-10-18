1453c78059a5246a348d53a9abb3dce04aa4ea11
Erik Nordström <erik@timescale.com>
2023-10-18 12:19:47 +0200

    Do not return data twice with transparent decompression
    When a chunk is partially compressed, enabling transparent
    decompressing would return duplicate data because it reads
    "non-compressed" data via the new TAM, which returns data from both
    the compressed and non-compressed heap. Fix this by only returning
    non-compressed data in both SeqScans and IndexScans when transparent
    decompression is enabled.

--- tsl/src/compression/arrow_tts.h
+++ tsl/src/compression/arrow_tts.h
@@ -8,6 +8,7 @@
 
 #include <postgres.h>
 #include <access/attnum.h>
+#include <access/tupdesc.h>
 #include <executor/tuptable.h>
 #include <nodes/bitmapset.h>
 #include <utils/builtins.h>
@@ -16,10 +17,13 @@
 #include "compression/create.h"
 #include "nodes/decompress_chunk/detoaster.h"
 
+#include <limits.h>
+
 typedef struct ArrowTupleTableSlot
 {
 	VirtualTupleTableSlot base;
 	TupleTableSlot *child_slot;
+	TupleDesc compressed_tupdesc;
 	ArrowArray **arrow_columns;
 	uint16 tuple_index; /* Index of this particular tuple in the compressed
 						 * (columnar data) child tuple. Note that the first
@@ -97,32 +101,74 @@ extern TupleTableSlot *ExecStoreArrowTupleExisting(TupleTableSlot *slot, uint16 
 #define InvalidTupleIndex 0
 #define MaxCompressedBlockNumber ((BlockNumber) 0x3FFFFF)
 
+/*
+ * The compressed TID is encoded in the following manner, which places a limit
+ * on 1024 rows in a single compressed tuple. Since we are currently storing
+ * 1000 rows that should work.
+ *
+ *         32 bits                16 bits
+ * +-------------------------+-----------------+
+ * |       Block Number      |  Offset Number  |
+ * +------+------------------+---+-------------+
+ * | Flag | Compressed Tuple TID | Tuple Index |
+ * +------+----------------------+-------------+
+ *  1 bit         33 bits            10 bits
+ */
+
+#define BLOCKID_BITS (CHAR_BIT * sizeof(BlockIdData))
+#define COMPRESSED_FLAG (1UL << (BLOCKID_BITS - 1))
+#define OFFSET_BITS (CHAR_BIT * sizeof(OffsetNumber))
+#define OFFSET_MASK (((uint64) 1UL << OFFSET_BITS) - 1)
+#define TUPINDEX_BITS (10U)
+#define TUPINDEX_MASK (((uint64) 1UL << TUPINDEX_BITS) - 1)
+
+/* Compute a 64-bits value from the item pointer data */
+static uint64
+bits_from_tid(ItemPointer tid)
+{
+	return (ItemPointerGetBlockNumber(tid) << OFFSET_BITS) | ItemPointerGetOffsetNumber(tid);
+}
+
+/*
+ * The "compressed TID" consists of the bits of the TID for the compressed row
+ * shifted to insert the tuple index as the least significant bits of the TID.
+ */
 static inline void
 tid_to_compressed_tid(ItemPointer out_tid, ItemPointer in_tid, uint16 tuple_index)
 {
-	BlockNumber blockno = ItemPointerGetBlockNumber(in_tid);
-	OffsetNumber offsetno = ItemPointerGetOffsetNumber(in_tid);
-	BlockNumber compressed_blockno;
+	const uint64 bits = (bits_from_tid(in_tid) << TUPINDEX_BITS) | tuple_index;
 
-	Assert(blockno <= MaxCompressedBlockNumber);
+	Assert(tuple_index != InvalidTupleIndex);
 
-	compressed_blockno = ((tuple_index & 0x3FF) << 22) | blockno;
-	ItemPointerSet(out_tid, compressed_blockno, offsetno);
+	/* Insert the tuple index as the least significant bits and set the most
+	 * significant bit of the block id to mark it as a compressed tuple. */
+	const BlockNumber blockno = COMPRESSED_FLAG | (bits >> OFFSET_BITS);
+	const OffsetNumber offsetno = bits & OFFSET_MASK;
+	ItemPointerSet(out_tid, blockno, offsetno);
 }
 
 static inline uint16
 compressed_tid_to_tid(ItemPointer out_tid, ItemPointer in_tid)
 {
-	BlockNumber blockno = ItemPointerGetBlockNumber(in_tid);
-	OffsetNumber offsetno = ItemPointerGetOffsetNumber(in_tid);
-	uint16 tuple_index = (blockno >> 22);
-	BlockNumber orig_blockno = (blockno & 0x3FFFFF);
+	const uint64 orig_bits = bits_from_tid(in_tid);
+	const uint16 tuple_index = orig_bits & TUPINDEX_MASK;
 
-	ItemPointerSet(out_tid, orig_blockno, offsetno);
+	/* Remove the tuple index bits and clear the compressed flag from the block id. */
+	const uint64 bits = orig_bits >> TUPINDEX_BITS;
+	BlockNumber blockno = ~COMPRESSED_FLAG & (bits >> OFFSET_BITS);
+	OffsetNumber offsetno = bits & OFFSET_MASK;
+
+	Assert(tuple_index != InvalidTupleIndex);
+
+	ItemPointerSet(out_tid, blockno, offsetno);
 
 	return tuple_index;
 }
 
-#define is_compressed_tid(itemptr) ((ItemPointerGetBlockNumber(itemptr) >> 22) != 0)
+static inline bool
+is_compressed_tid(ItemPointer itemptr)
+{
+	return (ItemPointerGetBlockNumber(itemptr) & COMPRESSED_FLAG) != 0;
+}
 
 #endif /* PG_ARROW_TUPTABLE_H */
