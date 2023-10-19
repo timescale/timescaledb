48f42969261006cf06fe051335cde8194170c923
Jan Nidzwetzki <jan@timescale.com>
2023-10-19 16:19:43 +0200

    Support for segment_by filters on vector aggs
    This patch adds support for vectorized aggregation queries with filters
    on the segment_by value of compressed data.

--- tsl/test/sql/vectorized_aggregation.sql
+++ tsl/test/sql/vectorized_aggregation.sql
@@ -38,9 +38,19 @@ SELECT sum(segment_by_value) FROM testtable;
 :EXPLAIN
 SELECT sum(segment_by_value) FROM testtable;
 
--- Vectorization not possible due to a used filter
+-- Vectorization possible - filter on segment_by
 :EXPLAIN
 SELECT sum(segment_by_value) FROM testtable WHERE segment_by_value > 0;
+
+-- Vectorization not possible due to a used filter
+:EXPLAIN
+SELECT sum(segment_by_value) FROM testtable WHERE segment_by_value > 0 AND int_value > 0;
+
+:EXPLAIN
+SELECT sum(segment_by_value) FROM testtable WHERE int_value > 0;
+
+:EXPLAIN
+SELECT sum(segment_by_value) FROM testtable WHERE float_value > 0;
 
 -- Vectorization not possible due grouping
 :EXPLAIN
@@ -190,6 +200,22 @@ SELECT compress_chunk(ch) FROM show_chunks('testtable') ch;
 SELECT sum(segment_by_value) FROM testtable;
 SELECT sum(int_value) FROM testtable;
 
+-- Vectorization possible - filter on segment_by
+:EXPLAIN
+SELECT sum(int_value) FROM testtable WHERE segment_by_value > 5;
+
+SELECT sum(int_value) FROM testtable WHERE segment_by_value > 5;
+
+SET timescaledb.vectorized_aggregation = OFF;
+SELECT sum(int_value) FROM testtable WHERE segment_by_value > 5;
+RESET timescaledb.vectorized_aggregation;
+
+SELECT sum(int_value) FROM testtable WHERE segment_by_value > 10;
+
+SET timescaledb.vectorized_aggregation = OFF;
+SELECT sum(int_value) FROM testtable WHERE segment_by_value > 10;
+RESET timescaledb.vectorized_aggregation;
+
 ---
 -- Tests with parallel plans
 ---
@@ -334,3 +360,24 @@ SELECT sum(segment_by_value1) FROM testtable2;
 SELECT sum(segment_by_value2) FROM testtable2;
 
 SELECT sum(segment_by_value2) FROM testtable2;
+
+
+-- Vectorization possible - filter on segment_by
+:EXPLAIN
+SELECT sum(segment_by_value1) FROM testtable2 WHERE segment_by_value1 > 0;
+
+:EXPLAIN
+SELECT sum(segment_by_value1) FROM testtable2 WHERE segment_by_value1 > 0 AND segment_by_value2 > 0;
+
+:EXPLAIN
+SELECT sum(segment_by_value1) FROM testtable2 WHERE segment_by_value1 > 0 AND segment_by_value2 > 0 AND 2>1;
+
+-- Vectorization not possible filter on segment_by and compressed value
+
+-- Disable parallel worker to get deterministic query plans on i386
+SET max_parallel_workers_per_gather = 0;
+
+:EXPLAIN
+SELECT sum(segment_by_value1) FROM testtable2 WHERE segment_by_value1 > 1000 AND int_value > 1000;
+
+RESET max_parallel_workers_per_gather;
