c19aed84f9cdf7e048a86151a3c08777541fc862
Erik Nordström <erik@timescale.com>
2023-11-09 17:05:10 +0100

    Support speculative insert in compression TAM
    Implement missing functionality to support speculative inserts with
    compression TAM and add tests using ON CONFLICT with and without
    conflicts.
    
    During speculative inserts, decompression currently happens in order
    to populate the unique index that conflicts are checked against.
    
    With the compression TAM, this decompression is no longer necessary
    because a unique index covers both compressed and non-compressed
    data. However, the decompression is not disabled as part of this
    change in order to not affect the current implementation of
    compression. This unnecessary decompression will be removed later.
    
    To handle speculative inserts, it is necessary to implement the
    index_delete_tuples() callback in the TAM. This callback is used by
    the indexAM to ask the tableAM about potential tuples to delete. The
    tuple TIDs are passed to the callback as an array, and may include
    TIDs referencing both compressed and non-compressed tuples. Therfore,
    the compression TAM's version of index_delete_tuples() needs to split
    The TID array into two: one for the compressed relation and one for
    the non-compressed one, and then call the corresponding heapAM
    callback on each relation individually.

--- tsl/test/sql/compression_tam.sql
+++ tsl/test/sql/compression_tam.sql
@@ -2,7 +2,7 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-TIMESCALE for a copy of the license.
 
-CREATE TABLE readings(time timestamptz, location int, device int, temp float, humidity float);
+CREATE TABLE readings(time timestamptz UNIQUE, location int, device int, temp float, humidity float);
 
 SELECT create_hypertable('readings', 'time');
 
@@ -17,9 +17,6 @@ ALTER TABLE readings SET (
 	  timescaledb.compress_orderby = 'time',
 	  timescaledb.compress_segmentby = 'device'
 );
-
-
-SET timescaledb.enable_transparent_decompression TO false;
 
 SELECT format('%I.%I', chunk_schema, chunk_name)::regclass AS chunk
   FROM timescaledb_information.chunks
@@ -46,6 +43,12 @@ WHERE location = 1;
 -- We should be able to set the table access method for a chunk, which
 -- will automatically compress the chunk.
 ALTER TABLE :chunk SET ACCESS METHOD tscompression;
+SET timescaledb.enable_transparent_decompression TO false;
+
+-- Show access method used on chunk
+SELECT c.relname, a.amname FROM pg_class c
+INNER JOIN pg_am a ON (c.relam = a.oid)
+WHERE c.oid = :'chunk'::regclass;
 
 -- This should compress the chunk
 SELECT chunk_name FROM chunk_compression_stats('readings') WHERE compression_status='Compressed';
@@ -79,11 +82,65 @@ WHERE location = 1;
 SELECT count(*) FROM :chunk
 WHERE location = 1;
 
+
+-- Test that a conflict happens when inserting a value that already exists in the
+-- compressed part of the chunk
+SELECT count(*) FROM :chunk WHERE time  = '2022-06-01'::timestamptz;
+
+\set ON_ERROR_STOP 0
+INSERT INTO readings VALUES ('2022-06-01', 1, 1, 1.0, 1.0);
+-- Same result if inserted directly into the compressed chunk
+INSERT INTO :chunk VALUES ('2022-06-01', 1, 1, 1.0, 1.0);
+\set ON_ERROR_STOP 1
+
+-- Test insert of a non-conflicting value into the compressed chunk,
+-- first directly into chunk and then via hypertable. The value should
+-- end up in the non-compressed part in contrast to the conflicting
+-- value above.
+INSERT INTO :chunk VALUES ('2022-06-01 00:00:02', 1, 1, 1.0, 1.0);
+INSERT INTO readings VALUES ('2022-06-01 00:00:03', 1, 1, 1.0, 1.0);
+
+SELECT * FROM readings WHERE time BETWEEN '2022-06-01'::timestamptz AND '2022-06-01 01:00'::timestamptz ORDER BY time ASC LIMIT 10;
+
+-- Inserting the same values again should lead to conflicts
+\set ON_ERROR_STOP 0
+INSERT INTO :chunk VALUES ('2022-06-01 00:00:02', 1, 1, 1.0, 1.0);
+INSERT INTO readings VALUES ('2022-06-01 00:00:03'::timestamptz, 1, 1, 1.0, 1.0);
+\set ON_ERROR_STOP 1
+
+SELECT device, count(*) FROM readings WHERE device=1 GROUP BY device;
+-- Speculative insert when conflicting row is in the non-compressed part
+INSERT INTO :chunk VALUES ('2022-06-01 00:00:02', 2, 1, 1.0, 1.0) ON CONFLICT (time) DO UPDATE SET location = 11;
+-- Show the updated tuple
+SELECT * FROM readings WHERE time = '2022-06-01 00:00:02'::timestamptz;
+INSERT INTO readings VALUES ('2022-06-01 00:00:02', 3, 1, 1.0, 1.0) ON CONFLICT (time) DO UPDATE SET location = 12;
+SELECT * FROM readings WHERE time = '2022-06-01 00:00:02'::timestamptz;
+
+-- Speculative insert when conflicting row is in the compressed part
+\set ON_ERROR_STOP 0
+INSERT INTO :chunk VALUES ('2022-06-01', 2, 1, 1.0, 1.0) ON CONFLICT (time) DO UPDATE SET location = 13;
+\set ON_ERROR_STOP 1
+SELECT * FROM readings WHERE time = '2022-06-01'::timestamptz;
+INSERT INTO readings VALUES ('2022-06-01', 3, 1, 1.0, 1.0) ON CONFLICT (time) DO UPDATE SET location = 14;
+SELECT * FROM readings WHERE time = '2022-06-01'::timestamptz;
+
+-- Speculative insert without a conflicting
+INSERT INTO :chunk VALUES ('2022-06-01 00:00:06', 2, 1, 1.0, 1.0) ON CONFLICT (time) DO UPDATE SET location = 15;
+SELECT * FROM readings WHERE time = '2022-06-01 00:00:06';
+INSERT INTO readings VALUES ('2022-06-01 00:00:07', 3, 1, 1.0, 1.0) ON CONFLICT (time) DO UPDATE SET location = 16;
+SELECT * FROM readings WHERE time = '2022-06-01 00:00:07';
+
 -- We should be able to change it back to heap.
 ALTER TABLE :chunk SET ACCESS METHOD heap;
+
+-- Show access method used on chunk
+SELECT c.relname, a.amname FROM pg_class c
+INNER JOIN pg_am a ON (c.relam = a.oid)
+WHERE c.oid = :'chunk'::regclass;
 
 -- Should give the same result as above
 SELECT device, count(*) INTO decomp FROM readings GROUP BY device;
 
--- Row counts for each device should match, so this should be empty.
-SELECT device FROM orig JOIN decomp USING (device) WHERE orig.count != decomp.count;
+-- Row counts for each device should match, except for the chunk we did inserts on.
+SELECT device, orig.count AS orig_count, decomp.count AS decomp_count, (decomp.count - orig.count) AS diff
+FROM orig JOIN decomp USING (device) WHERE orig.count != decomp.count;
