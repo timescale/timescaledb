4999c8c6b515f00e3ce19998566d650417a99c7b
Erik Nordström <erik@timescale.com>
2023-11-13 09:48:23 +0100

    Add columnar scan for compression TAM
    Add a new custom scan node that implements a columnar scan. Currently,
    the only optimization is the ability to push down filters to the TAM
    in the form of scan keys.
    
    Pushing down scan keys allows the heap scan to remove rows at a much
    lower level, and without having to do qual expression evaluation in
    order to filter tuples in the scan node. However, only filters that
    apply to a segment_by or order_by column can be used as scan keys, so
    in some cases filtering is still necessary. Also, only segment_by
    filtering on simple boolean expressions is implemented as part of this
    change.
    
    Future additions to columnar scan might include vectorized filtering
    and columnar-based projection (only decompressing the projected
    columns). Vectorized aggregations should probably be done in another
    scan node that executes above the columnar scan (similar to an Agg
    node).

--- src/guc.h
+++ src/guc.h
@@ -66,6 +66,7 @@ extern TSDLLEXPORT bool ts_guc_enable_2pc;
 extern TSDLLEXPORT bool ts_guc_enable_compression_indexscan;
 extern TSDLLEXPORT bool ts_guc_enable_bulk_decompression;
 extern TSDLLEXPORT bool ts_guc_auto_sparse_indexes;
+extern TSDLLEXPORT bool ts_guc_enable_columnarscan;
 extern TSDLLEXPORT int ts_guc_bgw_log_level;
 
 #ifdef TS_DEBUG
