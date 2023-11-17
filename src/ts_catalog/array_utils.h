a3aec8c209412ea03b1491cffce544632b32bf34
Sven Klemm <sven@timescale.com>
2023-11-17 14:51:03 +0100

    Add helper functions for arrays
    These will see more use in follow-up patches that adjust how
    compression settings are stored in our catalog.

---
+++ src/ts_catalog/array_utils.h
@@ -0,0 +1,20 @@
+/*
+ * This file and its contents are licensed under the Apache License 2.0.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-APACHE for a copy of the license.
+ */
+#include <postgres.h>
+#include <utils/array.h>
+
+#include "export.h"
+
+/*
+ * Array helper function for internal catalog arrays.
+ * These are not suitable for arbitrary dimension
+ * arrays but only for 1-dimensional arrays as we use
+ * them in our catalog.
+ */
+
+extern TSDLLEXPORT int ts_array_length(ArrayType *arr);
+extern TSDLLEXPORT bool ts_array_is_member(ArrayType *arr, const char *name);
+extern TSDLLEXPORT int ts_array_position(ArrayType *arr, const char *name);
