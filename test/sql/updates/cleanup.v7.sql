d2284c282a1d64640b62b3772c92c7cf0b652f42
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2023-11-29 15:49:45 -0300

    PG16: Enable update/downgrade tests on CI
    Note that multinode support was removed from PG16 so starting now
    update/downgrade tests will be executed only on singlenode setup.

--- test/sql/updates/cleanup.v7.sql
+++ test/sql/updates/cleanup.v7.sql
@@ -2,5 +2,11 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
+SELECT current_setting('server_version_num')::int < 160000 AS has_multinode_support
+\gset
+
 \ir cleanup.v6.sql
-\ir cleanup.multinode.sql
+
+\if :has_multinode_support
+    \ir cleanup.multinode.sql
+\endif
--- test/sql/updates/setup.v7.sql
+++ test/sql/updates/setup.v7.sql
@@ -2,5 +2,11 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
+SELECT current_setting('server_version_num')::int < 160000 AS has_multinode_support
+\gset
+
 \ir setup.v6.sql
-\ir setup.multinode.sql
+
+\if :has_multinode_support
+    \ir setup.multinode.sql
+\endif
