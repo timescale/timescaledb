5d3ab062955e474676a758f92b9042c6ede1a93f
Sven Klemm <sven@timescale.com>
2023-12-12 15:07:36 +0100

    Remove multinode from update/downgrade test

--- test/sql/updates/setup.v7.sql
+++ test/sql/updates/setup.v7.sql
@@ -2,11 +2,5 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
-SELECT current_setting('server_version_num')::int < 160000 AS has_multinode_support
-\gset
-
 \ir setup.v6.sql
 
-\if :has_multinode_support
-    \ir setup.multinode.sql
-\endif
