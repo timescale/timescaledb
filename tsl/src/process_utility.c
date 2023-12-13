641bd0e626934f6d59f35df1f4d0b1efa18502ba
Erik Nordström <erik@timescale.com>
2023-12-13 10:00:45 +0100

    Fix issue when converting from compression TAM
    When converting a table from compression TAM to another TAM (typically
    heap), it is necessary to truncate the compressed relation since all
    its data is written into the new heap (in uncompressed form). If no
    truncation occurs, the data will be duplicated since, after converting
    back to heap, the table is technically still compressed in the "old"
    way where data is also read from the compressed relation.
    
    Ideally, converting back to heap from compression TAM should mean one
    of two things:
    
    - the table/chunk is no longer using compression, so the compressed
      relation should be removed and all data put (uncompressed) in the
      main heap.
    
    - the table/chunk is still compressed and no data is
      re-written/converted. The only change is that it is no longer using
      compression TAM.
    
    However, one of those behaviors will have to be implemented in a
    future change.
    
    This change also disables the optimization to provide ordered sequence
    scans in some cases since it doesn't (yet) produce the right order
    when there is data in both the compressed and non-compressed
    relations.

--- tsl/src/process_utility.c
+++ tsl/src/process_utility.c
@@ -39,16 +39,17 @@ tsl_ddl_command_start(ProcessUtilityArgs *args)
 					case AT_SetAccessMethod:
 					{
 						Oid relid = AlterTableLookupRelation(stmt, NoLock);
+						bool to_compressionam = (strcmp(cmd->name, "tscompression") == 0);
 
-						if (strcmp(cmd->name, "tscompression") == 0)
-							compressionam_handler_start_conversion(relid, false);
+						if (to_compressionam)
+							compressionam_alter_access_method_begin(relid, false);
 						else
 						{
-							Relation rel = table_open(relid, AccessShareLock);
+							Relation rel = RelationIdGetRelation(relid);
 
 							if (rel->rd_tableam == compressionam_routine())
-								compressionam_handler_start_conversion(relid, true);
-							table_close(rel, AccessShareLock);
+								compressionam_alter_access_method_begin(relid, true);
+							RelationClose(rel);
 						}
 						break;
 					}
@@ -124,5 +125,42 @@ tsl_process_rename_cmd(Oid relid, Cache *hcache, const RenameStmt *stmt)
 		{
 			tsl_process_compress_table_rename_column(ht, stmt);
 		}
+	}
+}
+
+void
+tsl_ddl_command_end(EventTriggerData *command)
+{
+	switch (nodeTag(command->parsetree))
+	{
+		case T_AlterTableStmt:
+		{
+			AlterTableStmt *stmt = castNode(AlterTableStmt, command->parsetree);
+			ListCell *lc;
+
+			foreach (lc, stmt->cmds)
+			{
+				AlterTableCmd *cmd = lfirst_node(AlterTableCmd, lc);
+
+				switch (cmd->subtype)
+				{
+#if PG15_GE
+					case AT_SetAccessMethod:
+					{
+						Oid relid = AlterTableLookupRelation(stmt, NoLock);
+						bool to_compressionam = (strcmp(cmd->name, "tscompression") == 0);
+						compressionam_alter_access_method_finish(relid, !to_compressionam);
+						break;
+					}
+#endif
+					default:
+						break;
+				}
+			}
+
+			break;
+		}
+		default:
+			break;
 	}
 }
