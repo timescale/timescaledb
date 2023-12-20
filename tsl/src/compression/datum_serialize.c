cfad3821a04f513633a4933e5eeb6f8ccfccf1ba
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2023-12-20 11:44:21 +0100

    More tests for decompression
    Add separate testing for bulk and row-by-row decompression, so that the
    errors in one don't mask the errors in the other.
    
    Also add fuzzing for row-by-row decompression, for text columns as well.

--- tsl/src/compression/datum_serialize.c
+++ tsl/src/compression/datum_serialize.c
@@ -20,6 +20,8 @@
 #include "datum_serialize.h"
 #include "compat/compat.h"
 
+#include "compression.h"
+
 typedef struct DatumSerializer
 {
 	Oid type_oid;
@@ -305,6 +307,22 @@ bytes_to_datum_and_advance(DatumDeserializer *deserializer, const char **ptr)
 
 	*ptr =
 		(Pointer) att_align_pointer(*ptr, deserializer->type_align, deserializer->type_len, *ptr);
+	if (deserializer->type_len == -1)
+	{
+		/*
+		 * Check for potentially corrupt varlena headers since we're reading them
+		 * directly from compressed data. We can only have a plain datum
+		 * with 1-byte or 4-byte header here, no TOAST or compressed data.
+		 */
+		CheckCompressedData(VARATT_IS_4B_U(*ptr) || (VARATT_IS_1B(*ptr) && !VARATT_IS_1B_E(*ptr)));
+
+		/*
+		 * Full varsize must be larger or equal than the header size so that the
+		 * calculation of size without header doesn't overflow.
+		 */
+		CheckCompressedData((VARATT_IS_1B(*ptr) && VARSIZE_1B(*ptr) >= VARHDRSZ_SHORT) ||
+							(VARSIZE_4B(*ptr) > VARHDRSZ));
+	}
 	res = fetch_att(*ptr, deserializer->type_by_val, deserializer->type_len);
 	*ptr = att_addlength_pointer(*ptr, deserializer->type_len, *ptr);
 	return res;
@@ -343,8 +361,7 @@ binary_string_get_type(StringInfo buffer)
 							   Anum_pg_type_oid,
 							   PointerGetDatum(element_type_name),
 							   ObjectIdGetDatum(namespace_oid));
-	if (!OidIsValid(type_oid))
-		elog(ERROR, "could not find type %s.%s", element_type_namespace, element_type_name);
+	CheckCompressedData(OidIsValid(type_oid));
 
 	return type_oid;
 }
---
+++ tsl/src/compression/decompress_text_test_impl.c
@@ -0,0 +1,152 @@
+/*
+ * This file and its contents are licensed under the Timescale License.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-TIMESCALE for a copy of the license.
+ */
+#include <postgres.h>
+
+#include <libpq/pqformat.h>
+
+#include "compression.h"
+
+#include "compression_test.h"
+
+/*
+ * Try to decompress the given compressed data. Used for fuzzing and for checking
+ * the examples found by fuzzing. For fuzzing we do less checks to keep it
+ * faster and the coverage space smaller. This is a generic implementation
+ * for arithmetic types.
+ */
+static int
+decompress_generic_text(const uint8 *Data, size_t Size, bool bulk, int requested_algo)
+{
+	if (bulk)
+	{
+		elog(ERROR, "bulk decompression not supported for text");
+	}
+
+	StringInfoData si = { .data = (char *) Data, .len = Size };
+
+	const int data_algo = pq_getmsgbyte(&si);
+
+	CheckCompressedData(data_algo > 0 && data_algo < _END_COMPRESSION_ALGORITHMS);
+
+	if (data_algo != requested_algo)
+	{
+		/*
+		 * It's convenient to fuzz only one algorithm at a time. We specialize
+		 * the fuzz target for one algorithm, so that the fuzzer doesn't waste
+		 * time discovering others from scratch.
+		 */
+		return -1;
+	}
+
+	const CompressionAlgorithmDefinition *def = algorithm_definition(data_algo);
+	Datum compressed_data = def->compressed_data_recv(&si);
+
+	/*
+	 * Test row-by-row decompression.
+	 */
+	DecompressionIterator *iter = def->iterator_init_forward(compressed_data, TEXTOID);
+	DecompressResult results[GLOBAL_MAX_ROWS_PER_COMPRESSION];
+	int n = 0;
+	for (DecompressResult r = iter->try_next(iter); !r.is_done; r = iter->try_next(iter))
+	{
+		if (n >= GLOBAL_MAX_ROWS_PER_COMPRESSION)
+		{
+			elog(ERROR, "too many compressed rows");
+		}
+
+		results[n++] = r;
+	}
+
+	/*
+	 * For row-by-row decompression, check that the result is still the same
+	 * after we compress and decompress back.
+	 * Don't perform this check for other types of tests.
+	 */
+	if (bulk)
+	{
+		return n;
+	}
+
+	/*
+	 * 1) Compress.
+	 */
+	Compressor *compressor = def->compressor_for_type(TEXTOID);
+
+	for (int i = 0; i < n; i++)
+	{
+		if (results[i].is_null)
+		{
+			compressor->append_null(compressor);
+		}
+		else
+		{
+			compressor->append_val(compressor, results[i].val);
+		}
+	}
+
+	compressed_data = (Datum) compressor->finish(compressor);
+	if (compressed_data == 0)
+	{
+		/* Some compressors return NULL when all rows are null. */
+		return n;
+	}
+
+	/*
+	 * 2) Decompress and check that it's the same.
+	 */
+	iter = def->iterator_init_forward(compressed_data, TEXTOID);
+	int nn = 0;
+	for (DecompressResult r = iter->try_next(iter); !r.is_done; r = iter->try_next(iter))
+	{
+		if (r.is_null != results[nn].is_null)
+		{
+			elog(ERROR, "the repeated decompression result doesn't match");
+		}
+
+		if (!r.is_null)
+		{
+			const Datum old_value = results[nn].val;
+			const Datum new_value = r.val;
+
+			/*
+			 * Floats can also be NaN/infinite and the comparison doesn't
+			 * work in that case.
+			 */
+			if (VARSIZE_ANY_EXHDR(old_value) != VARSIZE_ANY_EXHDR(new_value))
+			{
+				elog(ERROR, "the repeated decompression result doesn't match");
+			}
+
+			if (strncmp(VARDATA_ANY(old_value),
+						VARDATA_ANY(new_value),
+						VARSIZE_ANY_EXHDR(new_value)))
+			{
+				elog(ERROR, "the repeated decompression result doesn't match");
+			}
+		}
+
+		nn++;
+
+		if (nn > n)
+		{
+			elog(ERROR, "the repeated recompression result doesn't match");
+		}
+	}
+
+	return n;
+}
+
+int
+decompress_ARRAY_TEXT(const uint8 *Data, size_t Size, bool bulk)
+{
+	return decompress_generic_text(Data, Size, bulk, COMPRESSION_ALGORITHM_ARRAY);
+}
+
+int
+decompress_DICTIONARY_TEXT(const uint8 *Data, size_t Size, bool bulk)
+{
+	return decompress_generic_text(Data, Size, bulk, COMPRESSION_ALGORITHM_DICTIONARY);
+}
