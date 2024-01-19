48db1a50e699e8b8f056c8b948fc30d9777c481a
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2024-01-19 16:23:13 +0100

    fixup

--- tsl/test/sql/decompress_vector_qual.sql
+++ tsl/test/sql/decompress_vector_qual.sql
@@ -354,8 +354,8 @@ select count(compress_chunk(x, true)) from show_chunks('t') x;
 
 set timescaledb.debug_require_vector_qual to 'only';
 -- Uncomment to generate the test reference w/o the vector optimizations.
- set timescaledb.enable_bulk_decompression to off;
- set timescaledb.debug_require_vector_qual to 'forbid';
+-- set timescaledb.enable_bulk_decompression to off;
+-- set timescaledb.debug_require_vector_qual to 'forbid';
 
 select count(*), min(ts), max(ts), min(d), max(d) from t where a = 'default';
 select count(*), min(ts), max(ts), min(d), max(d) from t where a = '';
