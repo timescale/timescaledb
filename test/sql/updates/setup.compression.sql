507761a50579bd7eeaf7a95733ae50413410bac4
Sven Klemm <sven@timescale.com>
2024-02-12 11:40:21 +0100

    Fix update script for tables with dropped columns
    (cherry picked from commit fa5c0a9b22ae7bd07c8a9e5578038a2dccd380a4)

--- test/sql/updates/setup.compression.sql
+++ test/sql/updates/setup.compression.sql
@@ -8,6 +8,7 @@ CREATE TABLE compress (
       time                  TIMESTAMPTZ         NOT NULL,
       small_cardinality     TEXT                NULL,
       large_cardinality     TEXT                NULL,
+      dropped               TEXT                NULL,
       some_double           DOUBLE PRECISION    NULL,
       some_int              integer             NULL,
       some_custom           custom_type_for_compression         NULL,
@@ -17,20 +18,36 @@ CREATE TABLE compress (
 SELECT table_name FROM create_hypertable( 'compress', 'time');
 
 INSERT INTO compress
-SELECT g, 'POR', g::text, 75.0, 40, (1,2)::custom_type_for_compression, true
+SELECT g, 'POR', g::text, 'lint', 75.0, 40, (1,2)::custom_type_for_compression, true
 FROM generate_series('2018-12-01 00:00'::timestamp, '2018-12-31 00:00'::timestamp, '1 day') g;
 
 INSERT INTO compress
-SELECT g, 'POR', NULL, NULL, NULL, NULL, NULL
+SELECT g, 'POR', NULL, 'lint', NULL, NULL, NULL, NULL
 FROM generate_series('2018-11-01 00:00'::timestamp, '2018-12-31 00:00'::timestamp, '1 day') g;
 
 INSERT INTO compress
-SELECT g, 'POR', g::text, 94.0, 45, (3,4)::custom_type_for_compression, true
+SELECT g, 'POR', g::text, 'lint', 94.0, 45, (3,4)::custom_type_for_compression, true
 FROM generate_series('2018-11-01 00:00'::timestamp, '2018-12-15 00:00'::timestamp, '1 day') g;
 
-ALTER TABLE compress SET (timescaledb.compress, timescaledb.compress_segmentby='small_cardinality');
+DO $$
+DECLARE
+  ts_minor int := (SELECT (string_to_array(extversion,'.'))[2]::int FROM pg_extension WHERE extname = 'timescaledb');
+BEGIN
 
-SELECT count(compress_chunk(ch, true)) FROM show_chunks('compress') ch;
+  -- support for dropping columns on compressed hypertables was added in 2.10.0
+  -- so we drop before compressing if the version is before 2.10.0
+  IF ts_minor < 10 THEN
+    ALTER TABLE compress DROP COLUMN dropped;
+  END IF;
+
+  ALTER TABLE compress SET (timescaledb.compress, timescaledb.compress_segmentby='small_cardinality');
+  PERFORM count(compress_chunk(ch, true)) FROM show_chunks('compress') ch;
+
+  IF ts_minor >= 10 THEN
+    ALTER TABLE compress DROP COLUMN dropped;
+  END IF;
+END
+$$;
 
 \if :WITH_ROLES
 GRANT SELECT ON compress TO tsdbadmin;
