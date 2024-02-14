8d8f1583025bb8229352a2f4f1b15c9a01c8c1ba
Sven Klemm <sven@timescale.com>
2024-02-14 20:02:42 +0100

    2.14.1 post release
    Adjust update tests to include new version.

--- scripts/test_updates_pg16.sh
+++ scripts/test_updates_pg16.sh
@@ -16,5 +16,5 @@ run_tests "$@" -v8 \
 
 # Run repair tests for >=2.10.x versions due to PR #5441
 run_tests "$@" -r -v8 \
-          2.13.0-pg16 2.13.1-pg16 2.14.0-pg16
+          2.13.0-pg16 2.13.1-pg16 2.14.0-pg16 2.14.1-pg16
 
