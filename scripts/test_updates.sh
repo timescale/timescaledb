3c45d9ea1153c582386a5ec9bb84a3235438a7f9
Sven Klemm <sven@timescale.com>
2024-02-25 21:30:44 +0100

    Refactor update test
    Currently the update test is quite inconvenient to run locally and also
    inconvenient to debug as the different update tests all run in their own
    docker container. This patch refactors the update test to no longer
    require docker and make it easier to debug as it will run in the
    local environment as determined by pg_config.
    This patch also consolidates update/downgrade and repair test since
    they do very similar things and adds support for coredump stacktraces to
    the github action and removes some dead code from the update tests.
    Additionally the versions to be used in the update test are now
    determined from existing git tags so the post release patch no longer
    needs to add newly released versions.

--- scripts/test_updates.sh
+++ scripts/test_updates.sh
@@ -1,106 +1,128 @@
 #!/bin/bash
 
-set -o pipefail
-set +e # Should not exit immediately on failure
+set -eu
 
-SCRIPT_DIR=$(dirname $0)
-TEST_TMPDIR=${TEST_TMPDIR:-$(mktemp -d 2>/dev/null || mktemp -d -t 'timescaledb_update_test' || mkdir -p /tmp/$RANDOM )}
-BASE_DIR=${PWD}/${SCRIPT_DIR}/..
-TAGS=${TAGS:-}
-TEST_VERSION=${TEST_VERSION:-}
-GIT_ID=$(git -C ${BASE_DIR} describe --dirty --always | sed -e "s|/|_|g")
-UPDATE_TO_IMAGE=${UPDATE_TO_IMAGE:-update_test}
-UPDATE_TO_TAG=${UPDATE_TO_TAG:-${GIT_ID}}
-PG_VERSION=${PG_VERSION:-14.3}
+SCRIPT_DIR=$(readlink -f "$(dirname $0)")
+PG_MAJOR_VERSION=$(pg_config --version | awk '{print $2}' | awk -F. '{print $1}')
 
-# This will propagate to the test_update_from_tags.sh script
-export TEST_REPAIR
+PG_EXTENSION_DIR=$(pg_config --sharedir)/extension
+if [ "${CI:-false}" == true ]; then
+  GIT_REF=${GIT_REF:-$(git rev-parse HEAD)}
+else
+  GIT_REF=$(git branch --show-current)
+fi
 
-FAILED_TEST=
-KEEP_TEMP_DIRS=false
-TEST_UPDATE_FROM_TAGS_EXTRA_ARGS=
-TEST_REPAIR=false
+BUILD_DIR="build_update_pg${PG_MAJOR_VERSION}"
+
+VERSIONS=""
+FAILED_VERSIONS=""
+
+ALL_VERSIONS=$(git tag --sort=taggerdate | grep -P '^[2]\.[0-9]+\.[0-9]+$')
+MAX_VERSION=$(grep '^downgrade_to_version ' version.config | awk '{ print $3 }')
+
+# major version is always 2 atm
+max_minor_version=$(echo "${MAX_VERSION}" | awk -F. '{print $2}')
+max_patch_version=$(echo "${MAX_VERSION}" | awk -F. '{print $3}')
+
+# Filter versions depending on the current postgres version
+# Minimum version for valid update paths are as follows:
+# PG 13 v7 2.1+
+# PG 14 v8 2.5+
+# PG 15 v8 2.9+
+# PG 16 v8 2.13+
+for version in ${ALL_VERSIONS}; do
+  minor_version=$(echo "${version}" | awk -F. '{print $2}')
+  patch_version=$(echo "${version}" | awk -F. '{print $3}')
+
+  # skip versions that are newer than the max version
+  # We might have a tag for a newer version defined already but the post release
+  # adjustment have not been merged yet. So we want to skip those versions.
+  if [ "${minor_version}" -gt "${max_minor_version}" ]; then
+    continue
+  elif [ "${minor_version}" -eq "${max_minor_version}" ] && [ "${patch_version}" -gt "${max_patch_version}" ]; then
+    continue
+  fi
+
+  if [ "${minor_version}" -eq 0 ]; then
+    # not part of any valid update path
+    continue
+  elif [ "${minor_version}" -le 4 ]; then
+    continue
+    # on <= 2.4 we need to run v7 version of the update test
+    if [ "${PG_MAJOR_VERSION}" -le 13 ]; then
+        VERSIONS="${VERSIONS} ${version}"
+    fi
+  elif [ "${minor_version}" -le 8 ]; then
+    if [ "${PG_MAJOR_VERSION}" -le 14 ]; then
+        VERSIONS="${VERSIONS} ${version}"
+    fi
+  elif [ "${minor_version}" -le 12 ]; then
+    if [ "${PG_MAJOR_VERSION}" -le 15 ]; then
+        VERSIONS="${VERSIONS} ${version}"
+    fi
+  else
+    VERSIONS="${VERSIONS} ${version}"
+  fi
+done
+
 FAIL_COUNT=0
 
-# Declare a hash table to keep test names keyed by pid
-declare -A tests
-
-while getopts "cd" opt;
-do
-    case $opt in
-        c)
-            echo "Forcing cleanup of build image"
-            docker rmi -f ${UPDATE_TO_IMAGE}:${UPDATE_TO_TAG}
-            ;;
-        d)
-            echo "Keeping temporary directory ${TEST_TMPDIR}"
-            KEEP_TEMP_DIRS=true
-            TEST_UPDATE_FROM_TAGS_EXTRA_ARGS="-d"
-            ;;
-    	*)
-    		echo "Unknown flag '$opt'"
-    		exit 1
-    		;;
-    esac
-done
-
-kill_all_tests() {
-    local exit_code="$?"
-    set +e # do not exit immediately on failure
-    echo "Killing all tests"
-    kill "${!tests[@]}" 2>/dev/null
-    return $exit_code
-}
-
-trap kill_all_tests INT HUP
-
-if [ -z "${TEST_VERSION}" ]; then
-    echo "No TEST_VERSION specified"
-    exit 1
+if [ ! -d "${BUILD_DIR}" ]; then
+  echo "Initializing build directory"
+  BUILD_DIR="${BUILD_DIR}" ./bootstrap -DCMAKE_BUILD_TYPE=Release -DWARNINGS_AS_ERRORS=OFF -DASSERTIONS=ON -DLINTER=OFF -DGENERATE_DOWNGRADE_SCRIPT=ON -DREGRESS_CHECKS=OFF -DTAP_CHECKS=OFF
 fi
 
-if [ -z "${TAGS}" ]; then
-    echo "No TAGS specified"
-    exit 1
-fi
-
-# Build the docker image with current source here so that the parallel
-# tests don't all compete in trying to build it first
-IMAGE_NAME=${UPDATE_TO_IMAGE} TAG_NAME=${UPDATE_TO_TAG} PG_VERSION=${PG_VERSION} bash ${SCRIPT_DIR}/docker-build.sh
-
-# Run update tests in parallel
-for tag in ${TAGS};
-do
-    UPDATE_FROM_TAG=${tag} TEST_VERSION=${TEST_VERSION} "$(dirname $0)/test_update_from_tag.sh" ${TEST_UPDATE_FROM_TAGS_EXTRA_ARGS} > ${TEST_TMPDIR}/${tag}.log 2>&1 &
-
-    tests[$!]=${tag}
-    echo "Launched test ${tag} with pid $!"
+for version in ${VERSIONS}; do
+  if [ ! -f "${PG_EXTENSION_DIR}/timescaledb--${version}.sql" ]; then
+    echo "Building ${version}"
+    git checkout ${version}
+    make -C "${BUILD_DIR}" -j4 > /dev/null
+    sudo make -C "${BUILD_DIR}" install > /dev/null
+    git checkout ${GIT_REF}
+  fi
 done
 
-# Need to wait on each pid in a loop to return the exit status of each
+# We want to use the latest loader for all the tests so we build it last
+git checkout ${GIT_REF}
+make -C "${BUILD_DIR}" -j4
+sudo make -C "${BUILD_DIR}" install
 
-# Since we are iterating a hash table, the tests are not going to be
-# in order started. But it doesn't matter.
-for pid in "${!tests[@]}"
-do
-    echo "Waiting for test pid $pid"
-    wait $pid
-    exit_code=$?
-    echo "Test ${tests[$pid]} (pid $pid) exited with code $exit_code"
+set +e
 
-    if [ $exit_code -ne 0 ]; then
-        FAIL_COUNT=$((FAIL_COUNT + 1))
-        FAILED_TEST=${tests[$pid]}
-        if [ -f ${TEST_TMPDIR}/${FAILED_TEST}.log ]; then
-            echo "###### Failed test log below #####"
-            cat ${TEST_TMPDIR}/${FAILED_TEST}.log
-        fi
+if [ -n "${VERSIONS}" ]; then
+  for version in ${VERSIONS}; do
+    ts_minor_version=$(echo "${version}" | awk -F. '{print $2}')
+
+    if [ "${ts_minor_version}" -le 4 ]; then
+      TEST_VERSION=v7
+    else
+      TEST_VERSION=v8
     fi
-done
 
-if [ "$KEEP_TEMP_DIRS" = "false" ]; then
-    echo "Cleaning up temporary directory"
-    rm -rf ${TEST_TMPDIR}
+    if [ "${ts_minor_version}" -ge 10 ]; then
+      TEST_REPAIR=true
+    else
+      TEST_REPAIR=false
+    fi
+
+    export TEST_VERSION TEST_REPAIR
+
+    FROM_VERSION=${version} "${SCRIPT_DIR}/test_update_from_version.sh"
+    return_code=$?
+    if [ $return_code -ne 0 ]; then
+      FAIL_COUNT=$((FAIL_COUNT + 1))
+      FAILED_VERSIONS="${FAILED_VERSIONS} ${version}"
+    fi
+  done
+fi
+
+echo -e "\nUpdate test finished for ${VERSIONS}\n"
+
+if [ $FAIL_COUNT -gt 0 ]; then
+  echo -e "Failed versions: ${FAILED_VERSIONS}\n"
+else
+  echo -e "All tests succeeded.\n"
 fi
 
 exit $FAIL_COUNT
+
