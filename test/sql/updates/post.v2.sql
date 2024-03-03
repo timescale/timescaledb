5ea5bfc6655326039d82535d57340b94dc9dbb57
Sven Klemm <sven@timescale.com>
2024-03-03 08:52:46 +0100

    Remove obsolete update test files
    The only relevant update test versions are v7 and v8 all previous
    versions are no longeri used in any supported update path so we can
    safely remove those files.

--- test/sql/updates/post.v2.sql
+++
@@ -1,8 +0,0 @@
--- This file and its contents are licensed under the Apache License 2.0.
--- Please see the included NOTICE for copyright information and
--- LICENSE-APACHE for a copy of the license.
-
-\ir post.catalog.sql
-\ir post.insert.sql
-\ir post.integrity_test.sql
-\ir post.functions.sql
