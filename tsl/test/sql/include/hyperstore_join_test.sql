acc2529b7b5baabc9f961f2c93f294895f600e6f
Mats Kindahl <mats@timescale.com>
2024-03-21 13:26:18 +0100

    Add tests for joins with hyperstore
    This adds tests for inner and outer joins for merge join, nested loop
    join, and hash join. We explicitly disable memoization and columnar
    scan to test the basic join functionality and ensure there are no
    unexpected issues.
    
    The addition of nested loop join is to make sure that the rescan method
    works correctly.

---
+++ tsl/test/sql/include/hyperstore_join_test.sql
@@ -0,0 +1,38 @@
+-- This file and its contents are licensed under the Timescale License.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-TIMESCALE for a copy of the license.
+
+\set inner :jointype _join
+\set outer :jointype _outer_join
+
+-- Test inner join to make sure that it works.
+explain (analyze, costs off, timing off, summary off, decompress_cache_stats)
+select * from :chunk1 join the_hyperstore using (device_id);
+
+-- Check that it generates the right result
+select * into :inner from :chunk1 join the_hyperstore using (device_id);
+
+\x on
+select * from :inner r full join expected_inner e on row(r) = row(e)
+where r.device_id is null or e.device_id is null;
+\x off
+
+-- Test outer join (left in this case) to make sure that it works.
+explain (analyze, costs off, timing off, summary off, decompress_cache_stats)
+select created_at, updated_at, o.device_id, i.humidity, o.height
+  from :chunk1 i left join the_hyperstore o
+    on i.created_at = o.updated_at and i.device_id = o.device_id;
+
+select created_at, updated_at, o.device_id, i.humidity, o.height
+  into :outer
+  from :chunk1 i left join the_hyperstore o
+    on i.created_at = o.updated_at and i.device_id = o.device_id;
+
+\x on
+select * from :outer r full join expected_left e on row(r) = row(e)
+where r.device_id is distinct from e.device_id;
+\x off
+
+drop table :inner;
+drop table :outer;
+
