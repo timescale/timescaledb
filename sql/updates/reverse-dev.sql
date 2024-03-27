b4166ce63475e7a540c6037eb340aff0eee0922f
Mats Kindahl <mats@timescale.com>
2024-03-27 10:20:48 +0100

    Replace compressionam with hyperstore
    Replace "compressionam" in all functions and symbols with "hyperstore".

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,4 +1,4 @@
 -- Hyperstore AM
 DROP ACCESS METHOD IF EXISTS hyperstore;
-DROP FUNCTION IF EXISTS ts_compressionam_handler;
+DROP FUNCTION IF EXISTS ts_hyperstore_handler;
 DROP FUNCTION IF EXISTS _timescaledb_debug.is_compressed_tid;
