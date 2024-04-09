69586da4c5790ea9704b18b3f6e1b8ddc9d643f8
Erik Nordström <erik@timescale.com>
2024-04-09 14:24:12 +0200

    Add VACUUM support in hyperstore
    Implement vacuum by internally calling vacuum on both the compressed
    and non-compressed relations.
    
    Since hyperstore indexes are defined on the non-compressed relation,
    vacuuming the compressed relation won't clean up compressed tuples
    from those indexes. To handle this, a proxy index is defined on each
    compressed relation in order to direct index vacuum calls to the
    corresponding indexes on the hyperstore relation. The proxy index also
    translates the encoded TIDs stored in the index to proper TIDs for the
    compressed relation.

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,6 +1,8 @@
 DROP FUNCTION IF EXISTS _timescaledb_functions.cagg_get_bucket_function_info(INTEGER);
 
 -- Hyperstore AM
+DROP ACCESS METHOD IF EXISTS hsproxy;
+DROP FUNCTION IF EXISTS ts_hsproxy_handler;
 DROP ACCESS METHOD IF EXISTS hyperstore;
 DROP FUNCTION IF EXISTS ts_hyperstore_handler;
 DROP FUNCTION IF EXISTS _timescaledb_debug.is_compressed_tid;
