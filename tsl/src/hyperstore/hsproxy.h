a544565c3bc969249228225f8b8e2c0af413b0f2
Erik Nordström <erik@timescale.com>
2024-04-09 14:24:12 +0200

    Add VACUUM support in hyperstore
    Implement vacuum by internally calling vacuum on both the compressed
    and non-compressed relations.
    
    Since hyperstore indexes are defined on the non-compressed relation,
    vacuuming the compressed relation won't clean up compressed tuples
    from those indexes. To handle this, a proxy index is defined on each
    compressed relation in order to direct index vacuum calls to the
    corresponding indexes on the hyperstore relation. The proxy index also
    translates the encoded TIDs stored in the index to proper TIDs for the
    compressed relation.

---
+++ tsl/src/hyperstore/hsproxy.h
@@ -0,0 +1,12 @@
+/*
+ * This file and its contents are licensed under the Timescale License.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-TIMESCALE for a copy of the license.
+ */
+#pragma once
+
+#include <postgres.h>
+#include <fmgr.h>
+
+extern void _hsproxy_init(void);
+extern Datum hsproxy_handler(PG_FUNCTION_ARGS);
