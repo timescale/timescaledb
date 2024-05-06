5b98fc7f1d1f7ae730345859fa9c4ce3a007581f
Mats Kindahl <mats@timescale.com>
2024-05-06 18:56:35 +0200

    Add arrow array functions
    Add functions to build `ArrowArray` instances with buffers and using
    decompression iterators and follow the standard by using the `release`
    function for `ArrowArray` instances in the correct manner and also
    create a `default_decompress_all` that can be used for algorithms that
    do not have a `decompress_all` function.
    
    Fixes #1040

---
+++ tsl/src/hyperstore/arrow_array.h
@@ -0,0 +1,24 @@
+/*
+ * This file and its contents are licensed under the Timescale License.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-TIMESCALE for a copy of the license.
+ */
+
+#ifndef HYPERSTORE_ARROW_ARRAY_H_
+#define HYPERSTORE_ARROW_ARRAY_H_
+
+#include <postgres.h>
+
+#include "compression/compression.h"
+
+extern ArrowArray *arrow_create_with_buffers(MemoryContext mcxt, int n_buffers);
+extern ArrowArray *arrow_from_iterator(MemoryContext mcxt, DecompressionIterator *iterator,
+									   Oid typid);
+extern NullableDatum arrow_get_datum(ArrowArray *array, Oid typid, int64 index);
+extern ArrowArray *default_decompress_all(Datum compressed, Oid element_type,
+										  MemoryContext dest_mctx);
+
+#define TYPLEN_VARLEN (-1)
+#define TYPLEN_CSTRING (-2)
+
+#endif /* HYPERSTORE_ARROW_ARRAY_H_ */
