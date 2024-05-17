b8918ba841cf6eee44d1b8bcfea2821bc8e59d4a
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2024-05-17 14:50:27 +0200

    add an assertion and fix the test

--- tsl/test/sql/compress_dml_copy.sql
+++ tsl/test/sql/compress_dml_copy.sql
@@ -2,6 +2,7 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-TIMESCALE for a copy of the license.
 
+-- Test various cases of COPY with decompression and different chunk layouts.
 
 create table cdmlcopy(filler bigint, ts int, value float, metric text);
 
@@ -11,39 +12,49 @@ alter table cdmlcopy add unique (metric, ts);
 
 alter table cdmlcopy set (timescaledb.compress, timescaledb.compress_segmentby = 'metric');
 
-insert into cdmlcopy values
-    (0, 1, 1.1, 'metric1'),
-    (0, 1, 1.2, 'metric2');
+\copy cdmlcopy from stdin
+0	1	1.1	'metric1'
+0	1	1.2	'metric2'
+\.
 
 select count(compress_chunk(x)) from show_chunks('cdmlcopy') x;
 
 alter table cdmlcopy drop column filler;
 
-insert into cdmlcopy values
-    (1001, 1.1, 'metric1'),
-    (1001, 1.2, 'metric2');
+\copy cdmlcopy from stdin
+1001	1.1	'metric1'
+1001	1.2	'metric2'
+\.
 
 select count(compress_chunk(x)) from show_chunks('cdmlcopy') x;
 
 \set ON_ERROR_STOP 0
-insert into cdmlcopy values
-    (1, 1.1, 'metric1'),
-    (1, 1.2, 'metric2');
+\copy cdmlcopy from stdin
+1	1.1	'metric1'
+1	1.2	'metric2'
+\.
 
-insert into cdmlcopy values
-    (1001, 1.1, 'metric1'),
-    (1001, 1.2, 'metric2');
+\copy cdmlcopy from stdin
+1001	1.1	'metric1'
+1001	1.2	'metric2'
+\.
 \set ON_ERROR_STOP 1
+
 
 -- Also test the code path where the chunk insert state goes out of cache.
 set timescaledb.max_open_chunks_per_insert = 1;
 
-insert into cdmlcopy values
-    (2, 2.1, 'metric1'),
-    (1002, 2.2, 'metric2'),
-    (2, 2.2, 'metric2'),
-    (1002, 2.1, 'metric1');
+select count(compress_chunk(x)) from show_chunks('cdmlcopy') x;
+
+\copy cdmlcopy from stdin
+2	2.1	'metric1'
+1002	2.2	'metric2'
+2	2.2	'metric2'
+1002	2.1	'metric1'
+\.
 
 reset timescaledb.max_open_chunks_per_insert;
+
+select count(compress_chunk(x)) from show_chunks('cdmlcopy') x;
 
 drop table cdmlcopy;
