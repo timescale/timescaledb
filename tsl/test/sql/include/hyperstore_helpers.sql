f2da5c959530ffe36d83e4029913a07eced25229
Mats Kindahl <mats@timescale.com>
2024-05-21 10:41:50 +0200

    Add tests for hash index scans
    Duplicate the existing index tests to use hash index and make sure that
    it works correctly. We rename the existing file to be able to have
    tests for all the different kinds of indexes.
    
    We also rename the existing `explain_anonymize` to
    `explain_analyze_anonymize` and introduce a non-analyze version with
    the original name.
    
    It also modifies the permissions of `_timescaledb_debug` schema and
    `_timescaledb_debug.is_compressed_tid` function to make sure we can use
    it correctly.

--- tsl/test/sql/include/hyperstore_helpers.sql
+++ tsl/test/sql/include/hyperstore_helpers.sql
@@ -6,7 +6,7 @@
 -- emitted plan. This is intended to be used when the structure of the
 -- plan is important, but not the specific chunks scanned nor the
 -- number of heap fetches, rows, loops, etc.
-create function explain_anonymize(text) returns setof text
+create function explain_analyze_anonymize(text) returns setof text
 language plpgsql as
 $$
 declare
@@ -15,11 +15,33 @@ begin
     for ln in
         execute format('explain (analyze, costs off, summary off, timing off, decompress_cache_stats) %s', $1)
     loop
+        if trim(both from ln) like 'Group Key:%' then
+	   continue;
+	end if;
         ln := regexp_replace(ln, 'Arrays read from cache: \d+', 'Arrays read from cache: N');
         ln := regexp_replace(ln, 'Heap Fetches: \d+', 'Heap Fetches: N');
         ln := regexp_replace(ln, 'Workers Launched: \d+', 'Workers Launched: N');
         ln := regexp_replace(ln, 'actual rows=\d+ loops=\d+', 'actual rows=N loops=N');
-	ln := regexp_replace(ln, '_hyper_\d+_\d+_chunk', '_hyper_I_N_chunk');
+        ln := regexp_replace(ln, '_hyper_\d+_\d+_chunk', '_hyper_I_N_chunk', 1, 0);
+        return next ln;
+    end loop;
+end;
+$$;
+
+create function explain_anonymize(text) returns setof text
+language plpgsql as
+$$
+declare
+    ln text;
+begin
+    for ln in
+        execute format('explain (costs off, summary off, timing off) %s', $1)
+    loop
+        ln := regexp_replace(ln, 'Arrays read from cache: \d+', 'Arrays read from cache: N');
+        ln := regexp_replace(ln, 'Heap Fetches: \d+', 'Heap Fetches: N');
+        ln := regexp_replace(ln, 'Workers Launched: \d+', 'Workers Launched: N');
+        ln := regexp_replace(ln, 'actual rows=\d+ loops=\d+', 'actual rows=N loops=N');
+        ln := regexp_replace(ln, '_hyper_\d+_\d+_chunk', '_hyper_I_N_chunk', 1, 0);
         return next ln;
     end loop;
 end;
