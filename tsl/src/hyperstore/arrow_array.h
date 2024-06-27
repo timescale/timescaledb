5194b7974eb41e5b12f8013aaa6303dec219104d
Erik Nordström <erik@timescale.com>
2024-06-28 00:02:41 +0200

    Avoid repeated get_typlen() in Arrow slot
    Calling get_typlen() goes through the syscache repeatedly, which is
    not necessary, and can have a performance impact. It is easy to avoid
    by just passing on the typlen to functions that need it.

--- tsl/src/hyperstore/arrow_array.h
+++ tsl/src/hyperstore/arrow_array.h
@@ -11,6 +11,6 @@
 #include "compression/compression.h"
 
 extern ArrowArray *arrow_create_with_buffers(MemoryContext mcxt, int n_buffers);
-extern NullableDatum arrow_get_datum(ArrowArray *array, Oid typid, int64 index);
+extern NullableDatum arrow_get_datum(ArrowArray *array, Oid typid, int16 typlen, uint16 index);
 extern ArrowArray *arrow_from_compressed(Datum compressed, Oid typid, MemoryContext dest_mcxt,
 										 MemoryContext tmp_mcxt);
