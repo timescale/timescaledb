891bc5ace2127a60ae2f312776bc0fa0d6c97327
Pallavi Sontakke <pallavi@timescale.com>
2024-07-10 16:38:38 +0530

    Receive github user parameters
    From git config, instead of CLI input.

--- scripts/release/ready_fork_for_commit.sh
+++ scripts/release/ready_fork_for_commit.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 set -eu
 
-
-args=("$@")
-
 # GITHUB_USERNAMES
-GH_EMAIL="$1"
-GH_USERNAME="$2"
-GH_FULL_USERNAME="$3"
+GH_EMAIL=$(git config user.email)
+GH_FULL_USERNAME=$(git config user.name)
+GH_USERNAME=$(gh auth status | grep 'Logged in to' |cut -d ' ' -f 9)
 
 echo "GH_EMAIL is $GH_EMAIL"
-echo "GH_USERNAME is $GH_USERNAME"
 echo "GH_FULL_USERNAME is $GH_FULL_USERNAME"
+echo "GH_USERNAME is $GH_USERNAME"
 
 # Folder, where we have cloned repositories' sources
 SOURCES_DIR="sources"
 
 # Derived Variables
 FORK_DIR="$GH_USERNAME-timescaledb"
-PWD=$(pwd)
-SCRIPTS_DIR="$PWD"
 
 echo "---- Updating fork with upstream for user $GH_USERNAME ----"
 
@@ -53,6 +48,3 @@ git push --tags origin main
 # Check the needed branch name here - could it be 2.14.x ?
 # git push -f --tags origin main
 
-cd "$SCRIPTS_DIR"
-sed -i.bak "s/GH_USERNAME/"$GH_USERNAME"/g" create_release_PR_commit.sh
-rm create_release_PR_commit.sh.bak
