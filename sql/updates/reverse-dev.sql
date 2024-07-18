f492bd192a27878f1ae65241a18e61a0268bd870
Pallavi Sontakke <pallavi@timescale.com>
2024-07-18 18:06:56 +0530

    ## 2.16.0
    This release contains performance improvements and bug fixes since
    the 2.15.3 release. We recommend that you upgrade at the next
    available opportunity.
    
    **Features**
    * #6880: Add support for the array operators used for compressed DML batch filtering.
    * #6895: Improve the compressed DML expression pushdown.
    * #6897: Add support for replica identity on compressed hypertables.
    * #6918: Remove support for PG13.
    * #6920: Rework compression activity wal markers.
    * #6989: Add support for foreign keys when converting plain tables to hypertables.
    * #7020: Add support for the chunk column statistics tracking.
    * #7048: Add an index scan for INSERT DML decompression.
    * #7075: Reduce decompression on the compressed INSERT.
    * #7101: Reduce decompressions for the compressed UPDATE/DELETE.
    * #7108 Reduce decompressions for INSERTs with UNIQUE constraints
    
    **Bugfixes**
    * #7018: Fix `search_path` quoting in the compression defaults function.
    * #7046: Prevent locking for compressed tuples.
    * #7055: Fix the `scankey` for `segment by` columns, where the type `constant` is different to `variable`.
    * #7064: Fix the bug in the default `order by` calculation in compression.
    * #7069: Fix the index column name usage.
    * #7074: Fix the bug in the default `segment by` calculation in compression.
    
    **Thanks**

--- sql/updates/reverse-dev.sql
+++
@@ -1,7 +0,0 @@
-DROP FUNCTION IF EXISTS _timescaledb_functions.cagg_get_bucket_function_info(INTEGER);
--- remove chunk column statistics related objects
-DROP FUNCTION IF EXISTS @extschema@.enable_column_stats(REGCLASS, NAME, BOOLEAN);
-DROP FUNCTION IF EXISTS @extschema@.disable_column_stats(REGCLASS, NAME, BOOLEAN);
-ALTER EXTENSION timescaledb DROP TABLE _timescaledb_catalog.chunk_column_stats;
-ALTER EXTENSION timescaledb DROP SEQUENCE _timescaledb_catalog.chunk_column_stats_id_seq;
-DROP TABLE IF EXISTS _timescaledb_catalog.chunk_column_stats;
