aaad486a65cff446dd6488f46a5f3c39a125395c
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2024-08-26 12:02:28 +0200

    ..........

--- scripts/installcheck_32bit.sh
+++ scripts/installcheck_32bit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -xeu
+set -x
 set -o pipefail
 
 # On 32-bit tests, the GitHub Action steps run inside a Docker container as
@@ -7,8 +7,17 @@ set -o pipefail
 # sudo, but this resets the ulimit, so we have to set the ulimit after that so
 # that the core dumps can be collected. This is the purpose of this script.
 
+sudo cat /etc/security/limits.conf
+
 ulimit -c unlimited
 export LANG=C.UTF-8
 whoami
+
+        cat /proc/sys/kernel/core_pattern
+        sleep 10 &
+        killall -SIGSEGV sleep
+        sleep 10
+        ls -lath /tmp
+
 make -k -C build installcheck IGNORES="${IGNORES}" \
   SKIPS="${SKIPS}" PSQL="${HOME}/${PG_INSTALL_DIR}/bin/psql" | tee installcheck.log
