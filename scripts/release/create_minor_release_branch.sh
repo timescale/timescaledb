c2c4a77e3fcd8d9b9fb95ecce421495e73c935ed
Pallavi Sontakke <pallavi@timescale.com>
2024-09-23 17:17:51 +0530

    Edit the way we set some derived variables
    To fix the shellcheck style issue:
    ^-- SC2116 (style): Useless echo? Instead of 'cmd $(echo foo)', just use 'cmd foo'.

--- scripts/release/create_minor_release_branch.sh
+++ scripts/release/create_minor_release_branch.sh
@@ -17,7 +17,7 @@ NEW_PATCH_VERSION="0"
 
 NEW_VERSION=$(head -1 version.config | cut -d ' ' -f 3 | cut -d '-' -f 1)
 
-RELEASE_BRANCH=$(echo "${NEW_VERSION/%.$NEW_PATCH_VERSION/.x}")
+RELEASE_BRANCH="${NEW_VERSION/%.$NEW_PATCH_VERSION/.x}"
 
 echo "RELEASE_BRANCH is $RELEASE_BRANCH"
 echo "NEW_VERSION is $NEW_VERSION"
