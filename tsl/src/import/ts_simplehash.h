ff16ec8a0a30dc332706c581bf69958b4cf56d3c
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2024-10-09 16:54:06 +0200

    license

--- tsl/src/import/ts_simplehash.h
+++ tsl/src/import/ts_simplehash.h
@@ -1,4 +1,10 @@
 /*
+ * This file and its contents are licensed under the Apache License 2.0.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-APACHE for a copy of the license.
+ */
+
+/*
  * simplehash.h
  *
  *	  When included this file generates a "templated" (by way of macros)
