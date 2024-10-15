4c833b79366fefaf93108ad5e4fd6cb6d4411cc4
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2024-10-15 21:32:08 +0100

    license

--- tsl/src/import/ts_simplehash.h
+++ tsl/src/import/ts_simplehash.h
@@ -1,7 +1,14 @@
 /*
- * This file and its contents are licensed under the Apache License 2.0.
+ * This file and its contents are licensed under the Timescale License.
  * Please see the included NOTICE for copyright information and
- * LICENSE-APACHE for a copy of the license.
+ * LICENSE-TIMESCALE for a copy of the license.
+ */
+
+/*
+ * This file contains source code that was copied and/or modified from
+ * the PostgreSQL database, which is licensed under the open-source
+ * PostgreSQL License. Please see the NOTICE at the top level
+ * directory for a copy of the PostgreSQL License.
  */
 
 /*
