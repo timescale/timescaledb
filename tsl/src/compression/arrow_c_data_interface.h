7d83fb0c9da9063d3ac4baf3662cdb82f832eac8
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2024-11-13 13:55:03 +0100

    more cosmetic changes

--- tsl/src/compression/arrow_c_data_interface.h
+++ tsl/src/compression/arrow_c_data_interface.h
@@ -176,8 +176,9 @@ arrow_set_row_validity(uint64 *bitmap, size_t row_number, bool value)
 	const size_t qword_index = row_number / 64;
 	const size_t bit_index = row_number % 64;
 	const uint64 mask = 1ull << bit_index;
+	const uint64 new_bit = (value ? 1ull : 0ull) << bit_index;
 
-	bitmap[qword_index] = (bitmap[qword_index] & ~mask) | ((-(uint64) value) & mask);
+	bitmap[qword_index] = (bitmap[qword_index] & ~mask) | new_bit;
 
 	Assert(arrow_row_is_valid(bitmap, row_number) == value);
 }
@@ -207,7 +208,9 @@ arrow_combine_validity(size_t num_words, uint64 *restrict storage, const uint64 
 	return storage;
 }
 
-/* Increase the `source_value` to be an even multiple of `pad_to`. */
+/*
+ * Increase the `source_value` to be an even multiple of `pad_to`.
+ */
 static inline uint64
 pad_to_multiple(uint64 pad_to, uint64 source_value)
 {
