206b7b7506af910f554176022092258daf2ce450
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2024-11-13 15:21:52 +0100

    test

--- tsl/test/sql/vector_agg_functions.sql
+++ tsl/test/sql/vector_agg_functions.sql
@@ -137,7 +137,8 @@ from
         'cint2 is null']) with ordinality as condition(condition, n),
     unnest(array[
         null,
-        's']) with ordinality as grouping(grouping, n)
+        's',
+        'ss']) with ordinality as grouping(grouping, n)
 where
     true
     and (explain is null /* or condition is null and grouping = 's' */)
