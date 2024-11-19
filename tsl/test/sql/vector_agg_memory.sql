89e53c9ead06871ff5ea2ef645b84634a8554405
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2024-11-19 17:26:59 +0100

    check the decompression memory usage as well

--- tsl/test/sql/vector_agg_memory.sql
+++ tsl/test/sql/vector_agg_memory.sql
@@ -32,8 +32,33 @@ select count(compress_chunk(x)) from show_chunks('mvagg') x;
 create table log(n int, bytes int, a bigint, b bigint, c bigint, d bigint, e bigint);
 
 
--- Run the vectorized aggregation with grouping by segmentby with various number
+-- First, ensure that the underlying decompression has constant memory usage.
+explain (costs off) select distinct on (s0, s1) ts_debug_allocated_bytes() bytes,
+        s0, s1, t
+    from mvagg where t >= -1 and t < 1000000 order by s0, s1, t desc;
+
+truncate log;
+\set ECHO none
+select
+format('insert into log
+    select distinct on (s0, s1) %1$s,
+        ts_debug_allocated_bytes() bytes,
+        0 a, 0 b, 0 c, 0 d, 0 e
+    from mvagg where t >= -1 and t < %1$s
+    order by s0, s1, t desc',
+    pow(10, generate_series(1, 7)))
+\gexec
+\set ECHO all
+
+select * from log where (
+    -- Ideally the memory usage should be constant, but we have to allow for
+    -- small spurious changes to make this test more robust.
+    select regr_slope(bytes, n) > 1/65536 from log
+);
+
+-- Test the vectorized aggregation with grouping by segmentby with various number
 -- of input row. We expect approximately constant memory usage.
+truncate log;
 set max_parallel_workers_per_gather = 0;
 set timescaledb.debug_require_vector_agg = 'require';
 -- Despite the tweaks above, we are unable to force the HashAggregation, because
@@ -51,7 +76,7 @@ format('insert into log
     select %1$s,
         ts_debug_allocated_bytes() bytes,
         count(*) a, count(t) b, sum(t) c, avg(t) d, stddev(t) e
-            from mvagg where t >= -1 and t < %1$s group by s1',
+    from mvagg where t >= -1 and t < %1$s group by s1',
     pow(10, generate_series(1, 7)))
 \gexec
 \set ECHO all
