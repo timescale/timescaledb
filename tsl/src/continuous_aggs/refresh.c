85437c0b2f25d9cab0d5c263f248a6c59572afb6
Alex Kasko <alex@staticlibs.net>
2025-01-01 16:36:36 +0000

    Improve transaction check in CAgg refresh
    Procedures that use multiple transactions cannot be run in a transaction
    block (from a function, from dynamic SQL) or in a subtransaction (from a
    procedure block with an EXCEPTION clause). Such procedures use
    PreventInTransactionBlock function to check whether they can be run.
    
    Though currently such checks are incompete, because
    PreventInTransactionBlock requires isTopLevel argument to throw a
    consistent error when the call originates from a function. This
    isTopLevel flag (that is a bit poorly named - see below) is not readily
    available inside C procedures. The source of truth for it -
    ProcessUtilityContext parameter is passed to ProcessUtility hooks, but
    is not included with the function calls. There is an undocumented
    SPI_inside_nonatomic_context function, that would have been sufficient
    for isTopLevel flag, but it currently returns false when SPI connection
    is absent (that is a valid scenario when C procedures are called from
    top-lelev SQL instead of PLPG procedures or DO blocks) so it cannot be
    used.
    
    To work around this the value of ProcessUtilityContext parameter is
    saved when TS ProcessUtility hook is entered and can be accessed from
    C procedures using new ts_process_utility_is_context_nonatomic function.
    The result is called "non-atomic" instead of "top-level" because the way
    how isTopLevel flag is determined from the ProcessUtilityContext value
    in standard_ProcessUtility is insufficient for C procedures - it
    excludes PROCESS_UTILITY_QUERY_NONATOMIC value (used when called from
    PLPG procedure without an EXCEPTION clause) that is a valid use case for
    C procedures with transactions. See details in the description of
    ExecuteCallStmt function.
    
    It is expected that calls to C procedures are done with CALL and always
    pass though the ProcessUtility hook. The ProcessUtilityContext
    parameter is set to PROCESS_UTILITY_TOPLEVEL value by default. In
    unlikely case when a C procedure is called without passing through
    ProcessUtility hook and the call is done in atomic context, then
    PreventInTransactionBlock checks will pass, but SPI_commit will fail
    when checking that all current active snapshots are portal-owned
    snapshots (the same behaviour that was observed before this change).
    In atomic context there will be an additional snapshot set in
    _SPI_execute_plan, see the snapshot handling invariants description
    in that function.
    
    Closes #6533.

--- tsl/src/continuous_aggs/refresh.c
+++ tsl/src/continuous_aggs/refresh.c
@@ -31,6 +31,7 @@
 #include "invalidation.h"
 #include "invalidation_threshold.h"
 #include "materialize.h"
+#include "process_utility.h"
 #include "refresh.h"
 
 #define CAGG_REFRESH_LOG_LEVEL (callctx == CAGG_REFRESH_POLICY ? LOG : DEBUG1)
@@ -768,6 +769,24 @@ continuous_agg_refresh_internal(const ContinuousAgg *cagg,
 	int32 mat_id = cagg->data.mat_hypertable_id;
 	InternalTimeRange refresh_window = *refresh_window_arg;
 	int64 invalidation_threshold;
+	bool nonatomic = ts_process_utility_is_context_nonatomic();
+
+	/* Reset the saved ProcessUtilityContext value promptly before
+	 * calling Prevent* checks so the potential unsupported (atomic)
+	 * value won't linger there in case of ereport exit.
+	 */
+	ts_process_utility_context_reset();
+
+	PreventCommandIfReadOnly(REFRESH_FUNCTION_NAME);
+
+	/* Prevent running refresh if we're in a transaction block since a refresh
+	 * can run two transactions and might take a long time to release locks if
+	 * there's a lot to materialize. Strictly, it is optional to prohibit
+	 * transaction blocks since there will be only one transaction if the
+	 * invalidation threshold needs no update. However, materialization might
+	 * still take a long time and it is probably best for consistency to always
+	 * prevent transaction blocks.  */
+	PreventInTransactionBlock(nonatomic, REFRESH_FUNCTION_NAME);
 
 	/* Connect to SPI manager due to the underlying SPI calls */
 	int rc = SPI_connect_ext(SPI_OPT_NONATOMIC);
@@ -783,17 +802,6 @@ continuous_agg_refresh_internal(const ContinuousAgg *cagg,
 		aclcheck_error(ACLCHECK_NOT_OWNER,
 					   get_relkind_objtype(get_rel_relkind(cagg->relid)),
 					   get_rel_name(cagg->relid));
-
-	PreventCommandIfReadOnly(REFRESH_FUNCTION_NAME);
-
-	/* Prevent running refresh if we're in a transaction block since a refresh
-	 * can run two transactions and might take a long time to release locks if
-	 * there's a lot to materialize. Strictly, it is optional to prohibit
-	 * transaction blocks since there will be only one transaction if the
-	 * invalidation threshold needs no update. However, materialization might
-	 * still take a long time and it is probably best for consistency to always
-	 * prevent transaction blocks.  */
-	PreventInTransactionBlock(true, REFRESH_FUNCTION_NAME);
 
 	/* No bucketing when open ended */
 	if (!(start_isnull && end_isnull))
