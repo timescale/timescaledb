b613f05d3badefa8323b9b7b302a77b6fa57082a
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-01-06 13:42:39 +0100

    Optimize decompression planning
    The EquivalenceMember lookup is the most costly part, so share it
    between different uses.
    
    Switch batch sorted merge to use the generic pathkey matching code.
    
    Also cache some intermediate data in the CompressionInfo struct.

--- tsl/src/nodes/decompress_chunk/decompress_chunk.h
+++ tsl/src/nodes/decompress_chunk/decompress_chunk.h
@@ -22,6 +22,10 @@ typedef struct CompressionInfo
 	RangeTblEntry *compressed_rte;
 	RangeTblEntry *ht_rte;
 
+	FormData_chunk compressed_fd;
+	Oid compressed_reloid;
+	Oid compression_hypertable_reloid;
+
 	Oid compresseddata_oid;
 
 	CompressionSettings *settings;
@@ -42,15 +46,15 @@ typedef struct CompressionInfo
 	/* compressed chunk attribute numbers for columns that are compressed */
 	Bitmapset *compressed_attnos_in_compressed_chunk;
 
-	bool single_chunk; /* query on explicit chunk */
-	bool has_seq_num;  /* legacy sequence number support */
-
+	bool single_chunk;	  /* query on explicit chunk */
+	bool has_seq_num;	  /* legacy sequence number support */
+	Relids parent_relids; /* relids of the parent hypertable and UNION */
 } CompressionInfo;
 
 typedef struct DecompressChunkPath
 {
 	CustomPath custom_path;
-	CompressionInfo *info;
+	const CompressionInfo *info;
 
 	List *required_compressed_pathkeys;
 	bool needs_sequence_num;
