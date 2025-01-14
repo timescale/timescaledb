c9c4eca112ba83b24f63812ec15243980e5f92e1
Mats Kindahl <mats@timescale.com>
2025-01-14 11:57:33 +0100

    Reduce size of shared test tables
    In order to reduce the runtime of the shared tests we reduce the number
    of rows in the tables used for the shared tests by increasing the
    interval between the time samples. This reduce the number of rows
    without reducing the upper and lower ranges. This changes the number of
    rows in the plans, so we have to update the tests accordingly.

--- tsl/test/shared/sql/include/shared_setup.sql
+++ tsl/test/shared/sql/include/shared_setup.sql
@@ -17,11 +17,11 @@ CREATE TABLE metrics(filler_1 int, filler_2 int, filler_3 int, time timestamptz 
 SELECT create_hypertable('metrics','time',create_default_indexes:=false);
 
 ALTER TABLE metrics DROP COLUMN filler_1;
-INSERT INTO metrics(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1,  device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-01 0:00:00+0'::timestamptz,'2000-01-05 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1,  device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-01 0:00:00+0'::timestamptz,'2000-01-05 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 ALTER TABLE metrics DROP COLUMN filler_2;
-INSERT INTO metrics(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id-1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-06 0:00:00+0'::timestamptz,'2000-01-12 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id-1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-06 0:00:00+0'::timestamptz,'2000-01-12 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 ALTER TABLE metrics DROP COLUMN filler_3;
-INSERT INTO metrics(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-13 0:00:00+0'::timestamptz,'2000-01-19 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-13 0:00:00+0'::timestamptz,'2000-01-19 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 CREATE INDEX ON metrics(time DESC);
 CREATE INDEX ON metrics(device_id,time DESC);
 ANALYZE metrics;
@@ -31,11 +31,11 @@ CREATE TABLE metrics_space(filler_1 int, filler_2 int, filler_3 int, time timest
 SELECT create_hypertable('metrics_space','time','device_id',3,create_default_indexes:=false);
 
 ALTER TABLE metrics_space DROP COLUMN filler_1;
-INSERT INTO metrics_space(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-01 0:00:00+0'::timestamptz,'2000-01-05 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_space(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-01 0:00:00+0'::timestamptz,'2000-01-05 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 ALTER TABLE metrics_space DROP COLUMN filler_2;
-INSERT INTO metrics_space(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-06 0:00:00+0'::timestamptz,'2000-01-12 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_space(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-06 0:00:00+0'::timestamptz,'2000-01-12 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 ALTER TABLE metrics_space DROP COLUMN filler_3;
-INSERT INTO metrics_space(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-13 0:00:00+0'::timestamptz,'2000-01-19 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_space(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-13 0:00:00+0'::timestamptz,'2000-01-19 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 CREATE INDEX ON metrics_space(time);
 CREATE INDEX ON metrics_space(device_id,time);
 ANALYZE metrics_space;
@@ -46,11 +46,11 @@ CREATE TABLE metrics_compressed(filler_1 int, filler_2 int, filler_3 int, time t
 SELECT create_hypertable('metrics_compressed','time',create_default_indexes:=false);
 
 ALTER TABLE metrics_compressed DROP COLUMN filler_1;
-INSERT INTO metrics_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1,  device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-01 0:00:00+0'::timestamptz,'2000-01-05 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1,  device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-01 0:00:00+0'::timestamptz,'2000-01-05 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 ALTER TABLE metrics_compressed DROP COLUMN filler_2;
-INSERT INTO metrics_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id-1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-06 0:00:00+0'::timestamptz,'2000-01-12 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id-1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-06 0:00:00+0'::timestamptz,'2000-01-12 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 ALTER TABLE metrics_compressed DROP COLUMN filler_3;
-INSERT INTO metrics_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-13 0:00:00+0'::timestamptz,'2000-01-19 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-13 0:00:00+0'::timestamptz,'2000-01-19 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 CREATE INDEX ON metrics_compressed(time);
 CREATE INDEX ON metrics_compressed(device_id,time);
 ANALYZE metrics_compressed;
@@ -69,11 +69,11 @@ CREATE TABLE metrics_space_compressed(filler_1 int, filler_2 int, filler_3 int, 
 SELECT create_hypertable('metrics_space_compressed','time','device_id',3,create_default_indexes:=false);
 
 ALTER TABLE metrics_space_compressed DROP COLUMN filler_1;
-INSERT INTO metrics_space_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-01 0:00:00+0'::timestamptz,'2000-01-05 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_space_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-01 0:00:00+0'::timestamptz,'2000-01-05 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 ALTER TABLE metrics_space_compressed DROP COLUMN filler_2;
-INSERT INTO metrics_space_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-06 0:00:00+0'::timestamptz,'2000-01-12 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_space_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-06 0:00:00+0'::timestamptz,'2000-01-12 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 ALTER TABLE metrics_space_compressed DROP COLUMN filler_3;
-INSERT INTO metrics_space_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-13 0:00:00+0'::timestamptz,'2000-01-19 23:55:00+0','2m') gtime(time), generate_series(1,5,1) gdevice(device_id);
+INSERT INTO metrics_space_compressed(time,device_id,v0,v1,v2,v3) SELECT time, device_id, device_id+1, device_id + 2, device_id + 0.5, NULL FROM generate_series('2000-01-13 0:00:00+0'::timestamptz,'2000-01-19 23:55:00+0','10m') gtime(time), generate_series(1,5,1) gdevice(device_id);
 CREATE INDEX ON metrics_space_compressed(time);
 CREATE INDEX ON metrics_space_compressed(device_id,time);
 ANALYZE metrics_space_compressed;
