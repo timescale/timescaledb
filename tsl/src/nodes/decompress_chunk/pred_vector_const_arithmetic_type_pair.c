193431b749637520429eb60365d4bb255c06219d
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-01-22 11:51:32 +0100

    Match the Postgres NaN behavior in vectorized filters (#7598)
    It has some nonstandard rules that don't match the IEEE floats.
    
    (cherry picked from commit f0996a46bca26f4914608eac9c25b21ee8b6c764)

--- tsl/src/nodes/decompress_chunk/pred_vector_const_arithmetic_type_pair.c
+++ tsl/src/nodes/decompress_chunk/pred_vector_const_arithmetic_type_pair.c
@@ -5,31 +5,32 @@
  */
 
 /*
- * Vector-const predicates for one pair of arithmetic types.
+ * Vector-const predicates for one pair of arithmetic types. For NaN comparison,
+ * Postgres has its own nonstandard rules different from the IEEE floats.
  */
 
 #define PREDICATE_NAME GE
-#define PREDICATE_EXPRESSION(X, Y) ((X) >= (Y))
+#define PREDICATE_EXPRESSION(X, Y) (isnan((double) (X)) || (!isnan((double) (Y)) && (X) >= (Y)))
 #include "pred_vector_const_arithmetic_single.c"
 
 #define PREDICATE_NAME LE
-#define PREDICATE_EXPRESSION(X, Y) ((X) <= (Y))
+#define PREDICATE_EXPRESSION(X, Y) (isnan((double) (Y)) || (!isnan((double) (X)) && (X) <= (Y)))
 #include "pred_vector_const_arithmetic_single.c"
 
 #define PREDICATE_NAME LT
-#define PREDICATE_EXPRESSION(X, Y) ((X) < (Y))
+#define PREDICATE_EXPRESSION(X, Y) (!isnan((double) (X)) && (isnan((double) (Y)) || (X) < (Y)))
 #include "pred_vector_const_arithmetic_single.c"
 
 #define PREDICATE_NAME GT
-#define PREDICATE_EXPRESSION(X, Y) ((X) > (Y))
+#define PREDICATE_EXPRESSION(X, Y) (!isnan((double) (Y)) && (isnan((double) (X)) || (X) > (Y)))
 #include "pred_vector_const_arithmetic_single.c"
 
 #define PREDICATE_NAME EQ
-#define PREDICATE_EXPRESSION(X, Y) ((X) == (Y))
+#define PREDICATE_EXPRESSION(X, Y) (isnan((double) (X)) ? isnan((double) (Y)) : ((X) == (Y)))
 #include "pred_vector_const_arithmetic_single.c"
 
 #define PREDICATE_NAME NE
-#define PREDICATE_EXPRESSION(X, Y) ((X) != (Y))
+#define PREDICATE_EXPRESSION(X, Y) (isnan((double) (X)) ? !isnan((double) (Y)) : ((X) != (Y)))
 #include "pred_vector_const_arithmetic_single.c"
 
 #undef VECTOR_CTYPE
