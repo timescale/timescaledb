40263456b3a41e3428139fe76b0c9cb7fdd7a4da
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-01-30 18:59:19 +0100

    or replace

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION _timescaledb_functions.ts_bloom1_matches(bytea, anyelement)
+CREATE OR REPLACE FUNCTION _timescaledb_functions.ts_bloom1_matches(bytea, anyelement)
 RETURNS bool
 AS '@MODULE_PATHNAME@', 'ts_bloom1_matches'
 LANGUAGE C IMMUTABLE STRICT;
