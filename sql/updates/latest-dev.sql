e27915edbd71ae943a2db429898a11a3d9ebc9be
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-01-30 20:17:05 +0100

    placeholder

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE FUNCTION _timescaledb_functions.ts_bloom1_matches(bytea, anyelement)
 RETURNS bool
-AS '@MODULE_PATHNAME@', 'ts_bloom1_matches'
+AS '@MODULE_PATHNAME@', 'ts_update_placeholder'
 LANGUAGE C IMMUTABLE STRICT;
 
