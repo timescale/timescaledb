7a7f40ce470d8b26ebd709b81faf1233d50324b8
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-02-11 14:46:40 +0100

    fixes

--- tsl/test/sql/vector_agg_groupagg.sql
+++ tsl/test/sql/vector_agg_groupagg.sql
@@ -10,19 +10,19 @@
 set max_parallel_workers_per_gather = 0;
 set enable_hashagg to off;
 
-set timescaledb.enable_vectorized_aggregation to off;
-set timescaledb.debug_require_vector_agg to 'forbid';
+--set timescaledb.enable_vectorized_aggregation to off;
+--set timescaledb.debug_require_vector_agg to 'forbid';
 
-create table groupagg(t int, s text, value int);
+create table groupagg(t int, s int, value int);
 select create_hypertable('groupagg', 't', chunk_time_interval => 10000);
 
 insert into groupagg
 select
-    xfast * 100 + xslow,
+    xslow * 10,
     case when xfast = 13 then null else xfast end,
     xfast * 7 + xslow * 3
 from generate_series(10, 99) xfast,
-    generate_series(1, 1000) xslow
+    generate_series(1, 1487) xslow
 ;
 
 alter table groupagg set (timescaledb.compress, timescaledb.compress_segmentby = '',
@@ -31,24 +31,29 @@ select count(compress_chunk(x)) from show_chunks('groupagg') x;
 vacuum analyze groupagg;
 
 
+set timescaledb.debug_require_vector_agg to 'require';
 select s, sum(value) from groupagg group by s order by s limit 10;
 
 -- The hash grouping policies do not support the GroupAggregate mode in the
 -- reverse order.
+set timescaledb.debug_require_vector_agg to 'forbid';
 select s, sum(value) from groupagg group by s order by s desc limit 10;
+reset timescaledb.debug_require_vector_agg;
 
-
-
+-- Also test the NULLS FIRST order.
 select count(decompress_chunk(x)) from show_chunks('groupagg') x;
 alter table groupagg set (timescaledb.compress, timescaledb.compress_segmentby = '',
     timescaledb.compress_orderby = 's nulls first');
 select count(compress_chunk(x)) from show_chunks('groupagg') x;
 vacuum analyze groupagg;
 
-select s , sum(value) from groupagg group by s  order by s  nulls first limit 10;
+set timescaledb.debug_require_vector_agg to 'require';
+select s, sum(value) from groupagg group by s order by s nulls first limit 10;
+reset timescaledb.debug_require_vector_agg;
 
 
--- More tests for dictionary encoding.
+-- More tests for dictionary encoding. This is not vectorized at the moment.
+set timescaledb.debug_require_vector_agg to 'forbid';
 create table text_table(ts int);
 select create_hypertable('text_table', 'ts', chunk_time_interval => 3);
 alter table text_table set (timescaledb.compress);
@@ -69,7 +74,7 @@ insert into text_table select 5, case when x % 2 = 0 then null else 'different-w
 select count(compress_chunk(x)) from show_chunks('text_table') x;
 vacuum analyze text_table;
 
-
+-- The following tests are collation-sensitive, so verify it all matches.
 select table_schema,
        table_name,
        column_name,
@@ -80,26 +85,19 @@ order by table_schema,
          table_name,
          ordinal_position;
 
-select datname,
-       datcollate
-from pg_database
-where datname = current_database();
+select datcollate from pg_database where datname = current_database();
 
 select 'different-with-nulls999' > 'different999';
 
 select count(distinct a) from text_table;
 
 
-explain (verbose, costs off, analyze, timing off, summary off)
-select a, count(*) from text_table group by a order by a limit 10;
+-- Test the GroupAggregate by a text column. Not vectorized for now.
 select a, count(*) from text_table group by a order by a limit 10;
 
 -- The hash grouping policies do not support the GroupAggregate mode in the
 -- reverse order.
-explain (verbose, costs off, analyze, timing off, summary off)
 select a, count(*) from text_table group by a order by a desc limit 10;
-select a, count(*) from text_table group by a order by a desc limit 10;
-
 
 
 -- with NULLS FIRST
