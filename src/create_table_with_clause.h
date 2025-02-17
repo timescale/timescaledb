1f0d03683b7f9d56c7643abb60446bda6e9393ee
Sven Klemm <sven@timescale.com>
2025-02-17 10:07:46 +0100

    Add CREATE TABLE ... WITH (timescaledb.hypertable)
    This is the first patch to add support for CREATE TABLE ... WITH
    for creating a hypertable. This patch only adds hypertable AND
    time_column as options. The other timescaledb related configuration
    settings will be added in followup patches.

---
+++ src/create_table_with_clause.h
@@ -0,0 +1,18 @@
+/*
+ * This file and its contents are licensed under the Apache License 2.0.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-APACHE for a copy of the license.
+ */
+#pragma once
+
+#include <postgres.h>
+
+#include "with_clause_parser.h"
+
+typedef enum CreateTableFlags
+{
+	CreateTableFlagHypertable = 0,
+	CreateTableFlagTimeColumn,
+} CreateTableFlags;
+
+WithClauseResult *ts_create_table_with_clause_parse(const List *defelems);
