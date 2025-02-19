bb49cfc6d626782a214b6b6e343a0666d7c40773
Sven Klemm <sven@timescale.com>
2025-02-19 07:28:32 +0100

    Release 2.18.2
    This release contains performance improvements and bug fixes since
    the 2.18.1 release. We recommend that you upgrade at the next
    available opportunity.
    
    **Bugfixes**
    * #7686 Potential wrong aggregation result when using vectorized aggregation with hash grouping in reverse order
    * #7694 Fix ExplainHook breaking call chain
    * #7695 Block dropping internal compressed chunks with `drop_chunk()`
    * #7711 License error when using hypercore handler
    * #7712 Respect other extensions' ExecutorStart hooks
    
    **Thanks**
    * @davidmehren and @jflambert for reporting an issue with extension hooks
    * @jflambert for reporting a bug with license errors shown in autovacuum

--- sql/updates/reverse-dev.sql
+++
@@ -1,5 +0,0 @@
-UPDATE _timescaledb_internal.bgw_job_stat_history SET succeeded = FALSE WHERE succeeded IS NULL;
-
-ALTER TABLE _timescaledb_internal.bgw_job_stat_history
-    ALTER COLUMN succeeded SET NOT NULL,
-    ALTER COLUMN succeeded SET DEFAULT FALSE;
