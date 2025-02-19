de8276d058349929e86e01b15540e211f834325d
Sven Klemm <sven@timescale.com>
2025-02-19 07:28:32 +0100

    Release 2.18.2
    This release contains performance improvements and bug fixes since
    the 2.18.1 release. We recommend that you upgrade at the next
    available opportunity.
    
    **Bugfixes**
    * #7686 Potential wrong aggregation result when using vectorized aggregation with hash grouping in reverse order
    * #7694 Fix ExplainHook breaking call chain
    * #7695 Block dropping internal compressed chunks with `drop_chunk()`
    * #7711 License error when using hypercore handler
    * #7712 Respect other extensions' ExecutorStart hooks
    
    **Thanks**
    * @davidmehren and @jflambert for reporting an issue with extension hooks
    * @jflambert for reporting a bug with license errors shown in autovacuum

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,7 +1,3 @@
-ALTER TABLE _timescaledb_internal.bgw_job_stat_history
-    ALTER COLUMN succeeded DROP NOT NULL,
-    ALTER COLUMN succeeded DROP DEFAULT;
-
 CREATE FUNCTION _timescaledb_functions.compressed_data_has_nulls(_timescaledb_internal.compressed_data)
     RETURNS BOOL
     LANGUAGE C STRICT IMMUTABLE
