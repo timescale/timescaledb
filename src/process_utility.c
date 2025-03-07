53d1bb0d8dde3913f9d4bbcbf9b308d1e0a9e1dc
Mats Kindahl <mats@timescale.com>
2025-03-07 14:21:15 +0100

    Handle DEFAULT table access name in ALTER TABLE
    If `ALTER TABLE ... SET ACCESS METHOD DEFAULT` is used, the name of the
    table access method is NULL in the `AlterTableCmd`, so add checks that
    the name is not null before using it.
    
    Function `hypercore_alter_access_method_finish` expects a chunk, but
    could be called with a non-chunk, so checking that the relid is a
    chunk.

--- src/process_utility.c
+++ src/process_utility.c
@@ -3834,7 +3834,7 @@ process_set_access_method(AlterTableCmd *cmd, ProcessUtilityArgs *args)
 	Oid relid = AlterTableLookupRelation(stmt, NoLock);
 	Cache *hcache;
 	Hypertable *ht = ts_hypertable_cache_get_cache_and_entry(relid, CACHE_FLAG_MISSING_OK, &hcache);
-	if (ht && (strcmp(cmd->name, TS_HYPERCORE_TAM_NAME) == 0))
+	if (ht && cmd->name && (strcmp(cmd->name, TS_HYPERCORE_TAM_NAME) == 0))
 	{
 		/* For hypertables, we automatically add command to set the
 		 * compression flag if we are setting the access method to be a
