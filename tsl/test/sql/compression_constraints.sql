4851cc4c7be6af9331c1844afcb56b7c49c69077
Sven Klemm <sven@timescale.com>
2025-03-18 11:22:09 +0100

    Allow creating foreign key constraints on compressed tables
    This patch allows creating foreign keys on hypertables with
    compressed chunks. Previously this required decompressing any
    compressed chunks before adding the foreign key constraints.

--- tsl/test/sql/compression_constraints.sql
+++ tsl/test/sql/compression_constraints.sql
@@ -102,6 +102,7 @@ CREATE UNIQUE INDEX metrics_pk ON metrics(time);
 CREATE UNIQUE INDEX metrics_pk ON metrics(time,device);
 \set ON_ERROR_STOP 1
 
+DROP TABLE metrics;
 
 -- test NULL behaviour
 CREATE TABLE dist_null(time timestamptz not null, device text, value float);
@@ -211,6 +212,68 @@ SELECT count(compress_chunk(ch)) FROM show_chunks('dist_null') ch;
 INSERT INTO dist_null SELECT '2025-01-02', 'd', 0.1;
 \set ON_ERROR_STOP 0
 CREATE UNIQUE INDEX dist_null_idx ON dist_null(time,device) NULLS NOT DISTINCT;
+\set ON_ERROR_STOP 1
+ROLLBACK;
+
+-- test foreign keys
+-- hypertable referencing plain table
+CREATE TABLE metrics(time timestamptz NOT NULL, device text, value float, PRIMARY KEY(device,time));
+SELECT create_hypertable('metrics','time');
+INSERT INTO metrics SELECT '2025-01-01','d1',0.1;
+INSERT INTO metrics SELECT '2025-01-01','d2',0.1;
+
+CREATE TABLE device(device TEXT PRIMARY KEY);
+INSERT INTO device SELECT 'd1';
+
+CREATE TABLE events(time timestamptz, device text, data text);
+INSERT INTO events SELECT '2025-02-01', 'd1', '';
+
+\set ON_ERROR_STOP 0
+ALTER TABLE metrics ADD CONSTRAINT device_fk FOREIGN KEY(device) REFERENCES device(device);
+ALTER TABLE events ADD CONSTRAINT events_fk FOREIGN KEY(device,time) REFERENCES metrics(device,time);
+\set ON_ERROR_STOP 1
+
+BEGIN;
+INSERT INTO device SELECT 'd2';
+ALTER TABLE metrics ADD CONSTRAINT device_fk FOREIGN KEY(device) REFERENCES device(device);
+INSERT INTO metrics SELECT '2025-03-01','d2',0.3;
+\set ON_ERROR_STOP 0
+INSERT INTO metrics SELECT '2025-03-01','d3',0.3;
+\set ON_ERROR_STOP 1
+ROLLBACK;
+
+BEGIN;
+INSERT INTO metrics SELECT '2025-02-01','d1',0.1;
+ALTER TABLE events ADD CONSTRAINT events_fk FOREIGN KEY(device,time) REFERENCES metrics(device,time);
+INSERT INTO events SELECT '2025-01-01','d1','';
+\set ON_ERROR_STOP 0
+INSERT INTO events SELECT '2025-03-01','d1','';
+\set ON_ERROR_STOP 1
+ROLLBACK;
+
+ALTER TABLE metrics SET (tsdb.compress, tsdb.compress_orderby='device,time DESC', tsdb.compress_segmentby='');
+SELECT count(compress_chunk(ch)) FROM show_chunks('metrics') ch;
+
+\set ON_ERROR_STOP 0
+ALTER TABLE metrics ADD CONSTRAINT device_fk FOREIGN KEY(device) REFERENCES device(device);
+ALTER TABLE events ADD CONSTRAINT events_fk FOREIGN KEY(device,time) REFERENCES metrics(device,time);
+\set ON_ERROR_STOP 1
+
+BEGIN;
+INSERT INTO device SELECT 'd2';
+ALTER TABLE metrics ADD CONSTRAINT device_fk FOREIGN KEY(device) REFERENCES device(device);
+INSERT INTO metrics SELECT '2025-03-01','d2',0.3;
+\set ON_ERROR_STOP 0
+INSERT INTO metrics SELECT '2025-03-01','d3',0.3;
+\set ON_ERROR_STOP 1
+ROLLBACK;
+
+BEGIN;
+INSERT INTO metrics SELECT '2025-02-01','d1',0.1;
+ALTER TABLE events ADD CONSTRAINT events_fk FOREIGN KEY(device,time) REFERENCES metrics(device,time);
+INSERT INTO events SELECT '2025-01-01','d1','';
+\set ON_ERROR_STOP 0
+INSERT INTO events SELECT '2025-03-01','d1','';
 \set ON_ERROR_STOP 1
 ROLLBACK;
 
