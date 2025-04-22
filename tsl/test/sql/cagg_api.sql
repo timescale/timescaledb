0453ea9ee1ab09ccbb84d716bd9a8770ce646260
Mats Kindahl <mats@timescale.com>
2025-04-22 15:48:09 +0200

    Add API into hypertable invalidation log
    The API is intended to be used to read hypertable invalidations for a
    hypertable that has continuous aggregates and is intended to also
    support the future logical decoding-based invalidation, which affects
    the design.
    
    This commit adds one function and one procedure:
    
    Function `get_hypertable_invalidations` will get the current
    invalidations for buckets of a suitable size. It will also return a
    token that can be used when accepting the invalidations.
    
    Procedure `accept_hypertable_invalidations` will take a token from a
    previous call to `get_hypertable_invalidations` and remove all
    invalidations that are associated with that token.

--- tsl/test/sql/cagg_api.sql
+++ tsl/test/sql/cagg_api.sql
@@ -157,3 +157,145 @@ SELECT * FROM _timescaledb_functions.get_materialization_invalidations(
        'tstz_temperature_15m'::regclass,
        '["2025-04-25","2025-04-26"]'::tstzrange
 );
+-- Generate some invalidations. These new values need to be before the
+-- invalidation threshold, which is set to end time of the insertion
+-- above.
+INSERT INTO hyper_ts
+  SELECT time, ceil(random() * 100)::int
+    FROM generate_series('2025-01-04 10:00:00'::timestamp,
+                         '2025-01-04 11:12:00', '9 minutes') time;
+
+INSERT INTO hyper_tstz
+  SELECT time, ceil(random() * 100)::int
+    FROM generate_series('2025-01-04 12:00:00'::timestamptz,
+                         '2025-01-04 13:12:00', '8 minutes') time;
+
+-- Check the invalidation threshold. If that is not after the
+-- insertions above, nothing will show up in the invalidation log.
+SELECT hypertable_id,
+       _timescaledb_functions.to_timestamp_without_timezone(watermark) AS watermark
+  FROM _timescaledb_catalog.continuous_aggs_invalidation_threshold
+  ORDER BY 1;
+
+-- Check that there indeed is something in the hypertable invalidation
+-- log. If not, this will fail anyway. We show the "raw" timestamps,
+-- which is in UTC if it was originally a timestamp with timezone.
+SELECT hypertable_id,
+       _timescaledb_functions.to_timestamp_without_timezone(lowest_modified_value) AS start,
+       _timescaledb_functions.to_timestamp_without_timezone(greatest_modified_value) AS finish
+  FROM _timescaledb_catalog.continuous_aggs_hypertable_invalidation_log
+  ORDER BY 1;
+
+\set VERBOSITY default
+
+-- Check that we can handle hypertables with single and multiple
+-- dimensions. This function is not part of the official API but we
+-- add tests for it here.
+SELECT _timescaledb_functions.get_hypertable_id('hyper_ts', 'timestamp');
+SELECT _timescaledb_functions.get_hypertable_id('hyper_tstz', 'timestamptz');
+SELECT _timescaledb_functions.get_hypertable_id('hyper_multi', 'timestamptz');
+
+\set ON_ERROR_STOP 0
+-- Not a hypertable
+SELECT _timescaledb_functions.get_hypertable_id('normal_ts', 'timestamp');
+SELECT _timescaledb_functions.get_hypertable_invalidations('normal_ts', null::timestamp, array['15 minutes', '1 hour']::interval[]);
+-- No continuous aggregate connected
+SELECT _timescaledb_functions.get_hypertable_id('hyper_no_cagg', 'timestamp');
+SELECT _timescaledb_functions.get_hypertable_invalidations('hyper_no_cagg', null::timestamp, array['15 minutes', '1 hour']::interval[]);
+-- Wrong type used
+SELECT _timescaledb_functions.get_hypertable_id('hyper_ts', 'timestamptz');
+SELECT _timescaledb_functions.get_hypertable_invalidations('hyper_ts', null::timestamptz, array['15 minutes', '1 hour']::interval[]);
+\set ON_ERROR_STOP 1
+
+SELECT * INTO saved_invalidations_1
+  FROM _timescaledb_functions.get_hypertable_invalidations('hyper_ts', null::timestamp, array['15 minutes', '1 hour']::interval[]);
+
+SELECT bucket_width, invalidations FROM saved_invalidations_1;
+
+-- Calling it twice should return same invalidations since we haven't
+-- inserted anything between.
+SELECT * INTO saved_invalidations_2
+  FROM _timescaledb_functions.get_hypertable_invalidations('hyper_ts', null::timestamp, array['15 minutes', '1 hour']::interval[]);
+
+SELECT * FROM saved_invalidations_2 s1
+    FULL JOIN saved_invalidations_2 s2 ON row(s1.*) = row(s2.*)
+        WHERE s1.token IS NULL OR s2.token IS NULL;
+
+SELECT token FROM saved_invalidations_1 LIMIT 1 \gset
+
+-- Test some error cases
+\set ON_ERROR_STOP 0
+CALL _timescaledb_functions.accept_hypertable_invalidations('normal_ts', :'token');
+CALL _timescaledb_functions.accept_hypertable_invalidations('hyper_no_cagg', :'token');
+CALL _timescaledb_functions.accept_hypertable_invalidations('hyper_ts', 'garbage');
+\set ON_ERROR_STOP 1
+
+-- Accept the invalidations as processed by passing in the token.
+CALL _timescaledb_functions.accept_hypertable_invalidations('hyper_ts', :'token');
+
+-- This should now show no validations.
+SELECT bucket_width, invalidations
+  FROM _timescaledb_functions.get_hypertable_invalidations(
+       'hyper_ts',
+       null::timestamp,
+       array['15 minutes', '1 hour']::interval[]
+  );
+
+-- Check that the invalidations associated with the token are removed
+-- when there are inserts in between.
+
+-- First insert some rows. All invalidations were removed above, so we
+-- should only see these.
+INSERT INTO hyper_ts
+  SELECT time, ceil(random() * 100)::int
+    FROM generate_series('2025-01-03 10:00:00'::timestamp,
+                         '2025-01-03 11:12:00', '10 minutes') time;
+
+-- Get invalidations from the hypertable, with a token.
+SELECT * INTO saved_3
+  FROM _timescaledb_functions.get_hypertable_invalidations(
+	'hyper_ts',
+	null::timestamp,
+	array['15 minutes', '1 hour']::interval[]
+  );
+
+SELECT token FROM saved_3 LIMIT 1 \gset
+
+-- Insert some more rows into the hypertable that are disjoint with
+-- the invalidations associated with the token above. These should be
+-- fully removed.
+INSERT INTO hyper_ts
+  SELECT time, ceil(random() * 100)::int
+    FROM generate_series('2025-01-02 10:00:00'::timestamp,
+                         '2025-01-02 11:12:00', '10 minutes') time;
+
+-- Insert some more rows into the hypertable that are inside the range
+-- associated with the token above. These should remain after
+-- accepting the token.
+INSERT INTO hyper_ts
+  SELECT time, ceil(random() * 100)::int
+    FROM generate_series('2025-01-03 10:30:00'::timestamp,
+                         '2025-01-03 10:50:00', '5 minutes') time;
+
+-- Check that we have them in the invalidation log
+SELECT bucket_width, unnest(invalidations) AS inval
+  FROM _timescaledb_functions.get_hypertable_invalidations(
+	'hyper_ts',
+	null::timestamp,
+	array['15 minutes', '1 hour']::interval[]
+  )
+ORDER BY 1, 2;
+
+-- Accept the original invalidations
+CALL _timescaledb_functions.accept_hypertable_invalidations('hyper_ts', :'token');
+
+-- Check that we only removed the ones associated with the token.
+SELECT bucket_width, unnest(invalidations) AS inval
+  FROM _timescaledb_functions.get_hypertable_invalidations(
+	'hyper_ts',
+	null::timestamp,
+	array['15 minutes', '1 hour']::interval[]
+  )
+ORDER BY 1, 2;
+
+
