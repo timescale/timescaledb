bd74c5b1b3632f259e05e34dabdd0e7161bc9668
Sven Klemm <sven@timescale.com>
2025-04-24 13:13:29 +0200

    Support ALTER TABLE SET (tsdb.chunk_time_interval='1 day')

--- src/with_clause/create_table_with_clause.h
+++ src/with_clause/create_table_with_clause.h
@@ -21,5 +21,5 @@ typedef enum CreateTableFlags
 
 WithClauseResult *ts_create_table_with_clause_parse(const List *defelems);
 
-Datum ts_create_table_parse_chunk_time_interval(WithClauseResult *parsed_options, Oid column_type,
+Datum ts_create_table_parse_chunk_time_interval(WithClauseResult option, Oid column_type,
 												Oid *interval_type);
