42ee6140f0c2e0356c11e15d5f57f558ee21256a
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-05-20 10:56:27 +0200

    Fix sorting transformation with joins (#8152)
    We ended up with the transformation applying the original pathkey from a
    different table, after successfully transforming to a join equivalence
    class that contains both tables. Add a check for that.
    
    (cherry picked from commit 46dcc641e0276f1ea0351ccad8f3612fc7bf3e43)

--- tsl/test/sql/compress_sort_transform.sql
+++ tsl/test/sql/compress_sort_transform.sql
@@ -72,5 +72,17 @@ order by time_bucket('1 minute', time) limit 1;
 select * from ht_metrics_partially_compressed
 order by device, time_bucket('1 minute', time) limit 1;
 
+-- Test incorrect transformation into a Pathkey on different relation through
+-- a join EquivalenceClass.
+set max_parallel_workers_per_gather = 0;
+:PREFIX
+select time_bucket('1 minute', a.time) from ht_metrics_partially_compressed a
+join ht_metrics_partially_compressed b
+on a.time = b.time
+where b.time < '2020-01-07'
+group by 1
+;
+reset max_parallel_workers_per_gather;
+
 reset work_mem;
 reset enable_hashagg;
