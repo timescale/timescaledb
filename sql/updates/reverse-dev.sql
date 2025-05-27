50a741ff6915287e75b1e2d6370168d5d4f2ee33
Mats Kindahl <mats@timescale.com>
2025-05-27 07:11:15 +0200

    Add missing ALTER EXTENSION in reverse-dev

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,3 +1,9 @@
+ALTER EXTENSION timescaledb DROP PROCEDURE _timescaledb_functions.process_hypertable_invalidations(REGCLASS);
+ALTER EXTENSION timescaledb DROP PROCEDURE @extschema@.add_process_hypertable_invalidations_policy(REGCLASS, INTERVAL, BOOL, TIMESTAMPTZ, TEXT);
+ALTER EXTENSION timescaledb DROP PROCEDURE @extschema@.remove_process_hypertable_invalidations_policy(REGCLASS, BOOL);
+ALTER EXTENSION timescaledb DROP PROCEDURE _timescaledb_functions.policy_process_hypertable_invalidations(INTEGER, JSONB);
+ALTER EXTENSION timescaledb DROP FUNCTION _timescaledb_functions.policy_process_hypertable_invalidations_check(JSONB);
+
 DROP PROCEDURE _timescaledb_functions.process_hypertable_invalidations(REGCLASS);
 DROP PROCEDURE @extschema@.add_process_hypertable_invalidations_policy(REGCLASS, INTERVAL, BOOL, TIMESTAMPTZ, TEXT);
 DROP PROCEDURE @extschema@.remove_process_hypertable_invalidations_policy(REGCLASS, BOOL);
