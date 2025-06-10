b1dbb904162ad210e9244af8350f07918d6c3df8
Mats Kindahl <mats@timescale.com>
2025-06-10 11:58:19 +0200

    Add multi-invalidation processing module
    A multi-table invalidation processing module that allows you to collect
    and add invalidations for continuous aggregates. Function will
    automatically compute bucket ranges and produce a multi-range for each
    continuous aggregate that is aligned to the bucket for the continuous
    aggregate.

--- src/utils.h
+++ src/utils.h
@@ -19,6 +19,7 @@
 #include <utils/builtins.h>
 #include <utils/datetime.h>
 #include <utils/jsonb.h>
+#include <utils/rangetypes.h>
 
 #include "compat/compat.h"
 #include "process_utility.h"
@@ -413,3 +414,4 @@ extern TSDLLEXPORT void ts_relation_set_reloption(Relation rel, List *options, L
 extern TSDLLEXPORT bool ts_is_hypercore_am(Oid amoid);
 extern TSDLLEXPORT Jsonb *ts_errdata_to_jsonb(ErrorData *edata, Name proc_schema, Name proc_name);
 extern TSDLLEXPORT char *ts_get_attr_expr(Relation rel, AttrNumber attno);
+extern TSDLLEXPORT RangeType *ts_internal_to_range(int64 lower, int64 upper, Oid dimtype);
