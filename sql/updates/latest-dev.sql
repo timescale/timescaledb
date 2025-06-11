7b30533d4d9b6ce1353b8bac0ba6215be20f2633
Philip Krauss <35487337+philkra@users.noreply.github.com>
2025-06-11 10:38:01 +0200

    Release 2.20.3 (#8248)
    ## 2.20.3 (2025-06-11)
    
    This release contains bug fixes since the 2.20.2 release. We recommend
    that you upgrade at the next available opportunity.
    
    **Bugfixes**
    * [#8107](https://github.com/timescale/timescaledb/pull/8107) Adjust
    SkipScan cost for quals needing full scan of indexed data.
    * [#8211](https://github.com/timescale/timescaledb/pull/8211) Fixed dump
    and restore when chunk skipping is enabled.
    * [#8216](https://github.com/timescale/timescaledb/pull/8216) Fix for
    dropped quals bug in SkipScan.
    * [#8230](https://github.com/timescale/timescaledb/pull/8230) Fix for
    inserting into compressed data when a vectorized check is not available.
    * [#8236](https://github.com/timescale/timescaledb/pull/8236) Fixed the
    snapshot handling in background workers.
    
    **Thanks**
    * @ikaakkola for reporting that SkipScan is slow when non-index quals do
    not match any tuples.

--- sql/updates/latest-dev.sql
+++
@@ -1,5 +0,0 @@
--- Make chunk_id use NULL to mark special entries instead of 0
--- (Invalid chunk) since that doesn't work with the FK constraint on
--- chunk_id.
-ALTER TABLE _timescaledb_catalog.chunk_column_stats ALTER COLUMN chunk_id DROP NOT NULL;
-UPDATE _timescaledb_catalog.chunk_column_stats SET chunk_id = NULL WHERE chunk_id = 0;
