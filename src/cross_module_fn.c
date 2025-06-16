2b4661ebe3cbcb9f3291550d8dc8506f9c81cacc
Poroma Banerjee <poroma.jee@gmail.com>
2025-06-16 14:03:19 -0400

    Add configurable alter settings for sparse indexes
    Previously, sparse indexes ("minmax" and "bloom") relied on internal
    heuristics to be created. This commit adds support for configuring
    sparse indexes manually via ALTER TABLE or CREATE TABLE.
    
    Sparse indexes can now be created on multiple columns, but a single
    column must only have one index type defined.

--- src/cross_module_fn.c
+++ src/cross_module_fn.c
@@ -295,6 +295,13 @@ preprocess_query_tsl_default_fn_community(Query *parse, int *cursor_opts)
 	/* No op in community licensed code */
 }
 
+static PGFunction
+bloom1_get_hash_function_default(Oid type, FmgrInfo **finfo)
+{
+	error_no_default_fn_community();
+	pg_unreachable();
+}
+
 /*
  * Define cross-module functions' default values:
  * If the submodule isn't activated, using one of the cm functions will throw an
@@ -400,6 +407,7 @@ TSDLLEXPORT CrossModuleFunctions ts_cm_functions_default = {
 	.uuid_compressor_append = error_no_default_fn_pg_community,
 	.uuid_compressor_finish = error_no_default_fn_pg_community,
 	.bloom1_contains = error_no_default_fn_pg_community,
+	.bloom1_get_hash_function = bloom1_get_hash_function_default,
 
 	.decompress_batches_for_insert = error_no_default_fn_chunk_insert_state_community,
 	.init_decompress_state_for_insert = error_no_default_fn_chunk_insert_state_community,
