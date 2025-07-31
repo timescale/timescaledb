79329021a3e764db3fc259170718a8718b616b2d
Sven Klemm <sven@timescale.com>
2025-07-31 16:11:38 +0200

    Allow show_chunks to process more than 65535 chunks

--- src/chunk.c
+++ src/chunk.c
@@ -134,7 +134,7 @@ static void chunk_scan_ctx_init(ChunkScanCtx *ctx, const Hypertable *ht, const P
 static void chunk_scan_ctx_destroy(ChunkScanCtx *ctx);
 static void chunk_collision_scan(ChunkScanCtx *scanctx, const Hypercube *cube);
 static int chunk_scan_ctx_foreach_chunk_stub(ChunkScanCtx *ctx, on_chunk_stub_func on_chunk,
-											 uint16 limit);
+											 uint64 limit);
 static Datum show_chunks_return_srf(FunctionCallInfo fcinfo);
 static int chunk_cmp(const void *ch1, const void *ch2);
 static int chunk_point_find_chunk_id(const Hypertable *ht, const Point *p);
@@ -1794,7 +1794,7 @@ chunk_collision_scan(ChunkScanCtx *scanctx, const Hypercube *cube)
  * Returns the number of processed chunks.
  */
 static int
-chunk_scan_ctx_foreach_chunk_stub(ChunkScanCtx *ctx, on_chunk_stub_func on_chunk, uint16 limit)
+chunk_scan_ctx_foreach_chunk_stub(ChunkScanCtx *ctx, on_chunk_stub_func on_chunk, uint64 limit)
 {
 	HASH_SEQ_STATUS status;
 	ChunkScanEntry *entry;
@@ -2277,7 +2277,7 @@ get_chunks_in_time_range(Hypertable *ht, int64 older_than, int64 newer_than, Mem
 
 	/* Get all the chunks from the context */
 	chunk_scan_ctx.data = &data;
-	chunk_scan_ctx_foreach_chunk_stub(&chunk_scan_ctx, chunk_scan_context_add_chunk, -1);
+	chunk_scan_ctx_foreach_chunk_stub(&chunk_scan_ctx, chunk_scan_context_add_chunk, 0);
 	/*
 	 * only affects ctx.htab Got all the chunk already so can now safely
 	 * destroy the context
