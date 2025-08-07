fbbb5cec133ce5b8517dab63122afda527dbe34f
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-08-07 15:24:17 +0200

    Stabilize the transparent_decompression_queries test (#8406)
    The current plan choice is based on an overestimate of the number of
    rows. Actually add more rows there.

--- test/runner_cleanup_output.sh
+++ test/runner_cleanup_output.sh
@@ -6,6 +6,7 @@ set -e
 RUNNER=${1:-""}
 
 sed  -e '/<exclude_from_test>/,/<\/exclude_from_test>/d' \
+     -e 's! Disk: [0-9]\{1,\}kB!!' \
      -e 's! Memory: [0-9]\{1,\}kB!!' \
      -e 's! Memory Usage: [0-9]\{1,\}kB!!' \
      -e 's! Average  Peak Memory: [0-9]\{1,\}kB!!' \
