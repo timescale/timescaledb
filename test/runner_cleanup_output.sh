3e153c2f0e7b2a40f3ce31258a11195495bec1e8
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-08-13 10:20:02 +0200

    remove more

--- test/runner_cleanup_output.sh
+++ test/runner_cleanup_output.sh
@@ -7,12 +7,12 @@ RUNNER=${1:-""}
 
 sed  -e '/<exclude_from_test>/,/<\/exclude_from_test>/d' \
      -e 's! Disk: [0-9]\{1,\}kB!!' \
-     -e 's! Buckets: [0-9]\+!!' \
-     -e 's! Batches: [0-9]\+!!' \
      -e 's! Memory: [0-9]\{1,\}kB!!' \
      -e 's! Memory Usage: [0-9]\{1,\}kB!!' \
      -e 's! Average  Peak Memory: [0-9]\{1,\}kB!!' \
      -e '/Heap Fetches: [0-9]\{1,\}/d' \
+     -e '/Buckets: [0-9]\{1,\}/d' \
+     -e '/Batches: [0-9]\{1,\}/d' \
      -e '/found [0-9]\{1,\} removable, [0-9]\{1,\} nonremovable row versions in [0-9]\{1,\} pages/d' | \
 grep -av 'DEBUG:  rehashing catalog cache id' | \
 grep -av 'DEBUG:  compacted fsync request queue from' | \
