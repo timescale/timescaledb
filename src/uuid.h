16dd264cebdf8b13cdf12ca16c71bf470f0ae1dd
Erik Nordström <erik@tigerdata.com>
2025-08-14 15:30:57 +0200

    Add function to generate UUIDv7 from unixtime
    The new function is created for internal use by breaking up the
    function to generate a UUIDv7 from a timestamp with time zone in two
    pieces: first convert the timestamp with time zone to unixtime, then
    call the unixtime function in second step.
    
    Creating a UUIDv7 from unixtime is useful when implementing native
    partitioning support for UUIDv7, since the internal time used for
    constraint metadata is in unixtime.

--- src/uuid.h
+++ src/uuid.h
@@ -6,7 +6,11 @@
 #pragma once
 
 #include <postgres.h>
+#include <fmgr.h>
 #include <utils/uuid.h>
 
 extern pg_uuid_t *ts_uuid_create(void);
+extern pg_uuid_t *ts_create_uuid_v7_from_unixtime_us(int64 unixtime_us, bool zeroed);
 extern TSDLLEXPORT pg_uuid_t *ts_create_uuid_v7_from_timestamptz(TimestampTz ts, bool zero);
+extern bool ts_uuid_v7_extract_unixtime(const pg_uuid_t *uuid, uint64 *unixtime_ms,
+										uint16 *extra_us);
