039cfb75121a319bedd03ffc98b5b63492199820
Fabrízio de Royes Mello <fabriziomello@gmail.com>
2025-08-28 20:18:11 -0300

    Don't split ranges for refresh after the threshold
    Cap the refresh window at the hypertable invalidation threshold to
    don't produce unnecessary batches for refresh.

--- tsl/src/continuous_aggs/refresh.c
+++ tsl/src/continuous_aggs/refresh.c
@@ -1040,6 +1040,15 @@ continuous_agg_split_refresh_window(ContinuousAgg *cagg, InternalTimeRange *orig
 			compute_inscribed_bucketed_refresh_window(cagg, &refresh_window, bucket_width);
 	}
 
+	/* We must also cap the refresh window at the invalidation threshold. If
+	 * we process invalidations after the threshold, the continuous aggregates
+	 * won't be refreshed when the threshold is moved forward in the
+	 * future. The invalidation threshold should already be aligned on bucket
+	 * boundary. */
+	int64 invalidation_threshold =
+		invalidation_threshold_get(cagg->data.raw_hypertable_id, refresh_window.type);
+	refresh_window.end = MIN(invalidation_threshold, refresh_window.end);
+
 	/* Check if the refresh size is large enough to produce bathes, if not then return no batches */
 	const int64 refresh_window_size = i64abs(refresh_window.end - refresh_window.start);
 	const int64 batch_size = (bucket_width * buckets_per_batch);
