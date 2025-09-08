a7db7ac122afbbfa80782b2ad4e3ac95a9703182
Mats Kindahl <mats@timescale.com>
2025-09-08 14:06:33 +0200

    Add config parameters to history retention job
    Adding new parameters `max_successes_per_job` and
    `max_failures_per_job` to configuration for job retention history
    policy function.
    
    `max_successes_per_job` is the maximum number of job success messages
    for each job. Default is 1000.
    
    `max_failures_per_job` is the maximum number of job failure messages
    for each job. Default is 1000.
    
    Also changing the schedule interval from one month to 1 day to avoid
    filling up the log too much.

--- sql/updates/latest-dev.sql
+++ sql/updates/latest-dev.sql
@@ -1,3 +1,8 @@
+UPDATE _timescaledb_config.bgw_job
+  SET config = config || '{"max_successes_per_job": 1000, "max_failures_per_job": 1000}',
+      schedule_interval = '1 day'
+WHERE proc_schema = '_timescaledb_functions'
+  AND proc_name = 'policy_job_stat_history_retention';
 
 DROP VIEW IF EXISTS timescaledb_information.job_stats;
 
--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,2 +1,8 @@
+UPDATE _timescaledb_config.bgw_job
+  SET config = config - 'max_successes_per_job' - 'max_failures_per_job',
+      schedule_interval = '1 month'
+WHERE proc_schema = '_timescaledb_functions'
+  AND proc_name = 'policy_job_stat_history_retention';
+
 DROP VIEW IF EXISTS timescaledb_information.job_stats;
 
