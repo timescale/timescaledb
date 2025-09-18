1d4898fe36f9800ffafba8ffe7dfe1cc432401f2
Ante Kresic <ante.kresic@gmail.com>
2025-09-18 10:39:19 +0200

    Fix migration issue with sparse index conf
    Migration script for configuration settings was materializing
    orderby sparse indexes into the configuration. This is wrong
    since we assume all hts use default sparse indexing before
    version 2.22. Correct default value is NULL. Also adjusted
    settings equality function to account for this.
    
    (cherry picked from commit 7833bf8)

--- tsl/src/compression/create.c
+++ tsl/src/compression/create.c
@@ -1404,7 +1404,7 @@ compression_settings_set_defaults(Hypertable *ht, CompressionSettings *settings,
 		settings->fd.orderby = obs.orderby;
 		settings->fd.orderby_desc = obs.orderby_desc;
 		settings->fd.orderby_nullsfirst = obs.orderby_nullsfirst;
-		add_orderby_sparse_index = true;
+		add_orderby_sparse_index = settings->fd.index != NULL;
 	}
 
 	if (ts_guc_auto_sparse_indexes && can_set_default_sparse_index(settings))
@@ -1449,7 +1449,7 @@ compression_settings_set_manually_for_alter(Hypertable *ht, CompressionSettings 
 		settings->fd.orderby = obs.orderby;
 		settings->fd.orderby_desc = obs.orderby_desc;
 		settings->fd.orderby_nullsfirst = obs.orderby_nullsfirst;
-		add_orderby_sparse_index = true;
+		add_orderby_sparse_index = settings->fd.index != NULL;
 	}
 
 	if (!with_clause_options[AlterTableFlagIndex].is_default)
@@ -1501,7 +1501,7 @@ compression_settings_set_manually_for_create(Hypertable *ht, CompressionSettings
 		settings->fd.orderby = obs.orderby;
 		settings->fd.orderby_desc = obs.orderby_desc;
 		settings->fd.orderby_nullsfirst = obs.orderby_nullsfirst;
-		add_orderby_sparse_index = true;
+		add_orderby_sparse_index = settings->fd.index != NULL;
 	}
 
 	if (!with_clause_options[CreateTableFlagIndex].is_default)
