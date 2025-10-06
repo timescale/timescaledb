f49a39cad4292c500ac9d946bc005f5fc79dbee5
David Beck <dbeck@timescale.com>
2025-10-06 16:25:12 +0100

    tuple target 4096+1024+1024+1024

--- src/guc.c
+++ src/guc.c
@@ -179,7 +179,7 @@ bool ts_shutdown_bgw = false;
 char *ts_current_timestamp_mock = NULL;
 #endif
 
-int ts_guc_debug_toast_tuple_target = 4096+1024+1024;
+int ts_guc_debug_toast_tuple_target = 4096+1024+1024+1024;
 
 static const struct config_enum_entry debug_require_options[] = { { "allow", DRO_Allow, false },
 																  { "forbid", DRO_Forbid, false },
