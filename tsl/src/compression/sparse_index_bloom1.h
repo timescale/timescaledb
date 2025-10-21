380b4e005922ce6547407970a52341bbaf83971c
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-10-21 11:38:33 +0200

    one column type?

--- tsl/src/compression/sparse_index_bloom1.h
+++ tsl/src/compression/sparse_index_bloom1.h
@@ -12,14 +12,10 @@ Datum bloom1_contains(PG_FUNCTION_ARGS);
 PGFunction bloom1_get_hash_function(Oid type, FmgrInfo **finfo);
 
 /*
- * Confusingly, the column name prefix is now "bl2". We had two possible
+ * Confusingly, the column name prefix is now "bloom2". We had two possible
  * hashes depending on the build configuration, which were incompatible with
  * each other. This led to false negatives if a database was updated to a
- * different build of extension. Now these also have different prefixes.
+ * different build of extension. Now we only have one.
  * The bloom filter is still constructed according to the "bloom1" rules.
  */
-#ifdef TS_USE_UMASH
-#define BLOOM1_COLUMN_PREFIX "bl2u"
-#else
-#define BLOOM1_COLUMN_PREFIX "bl2p"
-#endif
+#define BLOOM1_COLUMN_PREFIX "bloom2"
