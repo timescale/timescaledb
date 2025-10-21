e62bf2466a714451940e77144044d5610a928824
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2025-10-21 14:31:46 +0100

    Allow compression when bloom sparse index is configured but disabled (#8807)
    At the moment the compression would fail with an error which is
    disruptive if we want to temporarily disable the bloom filter indexes on
    a database. Lower the message severity to WARNING. Still treat it as an
    error when configuring new indexes.
    
    (cherry picked from commit 3854a227752aac483280b3372f0bccafb6554fd3)

--- src/with_clause/alter_table_with_clause.c
+++ src/with_clause/alter_table_with_clause.c
@@ -449,7 +449,9 @@ parse_sparse_index_config(JsonbParseState *parse_state, FuncCall *sparse_index_d
 				ereport(ERROR,
 						(errcode(ERRCODE_OBJECT_NOT_IN_PREREQUISITE_STATE),
 						 errmsg("Creating bloom sparse index is disabled"),
-						 errhint("Set \"enable_sparse_index_bloom\" to true.")));
+						 errhint("Either set \"enable_sparse_index_bloom\" to true or remove the "
+								 "bloom filter indexes from \"sparse_index\" configuration of the "
+								 "hypertable.")));
 			}
 			/*
 			 * The column type must be hashable. For some types we use our own hash functions
