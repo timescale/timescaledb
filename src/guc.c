055ade45187fd404c7286cc63bc37851d320fb38
Erik Nordström <erik@tigerdata.com>
2025-11-24 16:15:52 +0100

    Add GUC for default chunk time interval
    When auto-tuning of the chunk time interval is available, we'd like to be
    able to set a small default interval and tune into a bigger one (going
    from big to small is not great because big intervals lock you in). When
    auto-tuning is not available, it makes sense that the default is bigger.
    
    With this change, a default chunk time interval can be set with a new
    GUC `timescaledb.default_chunk_time_interval`. The GUC expects a
    text-format PostgreSQL INTERVAL type (e.g., '1 day' or '1 week'). It is
    only possible to set the default chunk time interval for hypertables
    that use an INTERVAL-compatible time partitioning type, i.e.,
    TIMESTAMP(TZ), DATE, and UUID (version 7). Integer partitioning still
    uses hard-coded defaults. The default value of the GUC is "null", which
    means the current behavior is preserved (i.e., the hard-coded defaults
    are used).

--- src/guc.c
+++ src/guc.c
@@ -6,8 +6,10 @@
 #include <postgres.h>
 #include <miscadmin.h>
 #include <parser/parse_func.h>
+#include <utils/fmgrprotos.h>
 #include <utils/guc.h>
 #include <utils/regproc.h>
+#include <utils/timestamp.h>
 #include <utils/varlena.h>
 
 #include "compat/compat.h"
@@ -202,6 +204,7 @@ static bool ts_guc_enable_hypertable_create = true;
 static bool ts_guc_enable_hypertable_compression = true;
 static bool ts_guc_enable_cagg_create = true;
 static bool ts_guc_enable_policy_create = true;
+static char *ts_guc_default_chunk_time_interval = NULL;
 
 typedef struct
 {
@@ -403,6 +406,73 @@ chunk_skipping_assign_hook(bool newval, void *extra)
 {
 	if (newval)
 		ts_hypertable_cache_invalidate_callback();
+}
+
+#if PG16_LT
+/*
+ * guc_malloc is not public in PostgreSQL < 16.
+ */
+static void *
+guc_malloc(int elevel, size_t size)
+{
+	void *data;
+
+	/* Avoid unportable behavior of malloc(0) */
+	if (size == 0)
+		size = 1;
+	data = malloc(size);
+	if (data == NULL)
+		ereport(elevel, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("out of memory")));
+	return data;
+}
+#endif
+
+static bool
+check_default_chunk_time_interval(char **newval, void **extra, GucSource source)
+{
+	/*
+	 * If GUC is unset, we treat that as a valid value for "no default chunk interval".
+	 * The chunk interval is instead computed in the legacy way using hard-coded defaults.
+	 */
+	if (*newval == NULL)
+	{
+		Assert(*extra == NULL);
+		return true;
+	}
+
+	/* Test that the text value is a valid Interval */
+	LOCAL_FCINFO(fcinfo, 3);
+	InitFunctionCallInfoData(*fcinfo, NULL, 3, InvalidOid, NULL, NULL);
+	fcinfo->args[0].value = CStringGetDatum(*newval);
+	fcinfo->args[0].isnull = false;
+	fcinfo->args[1].value = ObjectIdGetDatum(INTERVALOID);
+	fcinfo->args[1].isnull = false;
+	fcinfo->args[2].value = Int32GetDatum(-1);
+	fcinfo->args[2].isnull = false;
+
+	Datum interval = interval_in(fcinfo);
+
+	if (fcinfo->isnull)
+	{
+		GUC_check_errdetail("The default chunk interval must be a valid INTERVAL.");
+		return false;
+	}
+
+	Interval *parsed = DatumGetIntervalP(interval);
+	/* Save the new Interval in extra. The old extra is freed automatically. */
+	*extra = guc_malloc(ERROR, sizeof(Interval));
+	memcpy(*extra, parsed, sizeof(Interval));
+	pfree(parsed);
+
+	return true;
+}
+
+Interval *default_chunk_time_interval = NULL;
+
+static void
+assign_default_chunk_time_interval(const char *newval, void *extra)
+{
+	default_chunk_time_interval = extra;
 }
 
 void
@@ -1332,6 +1402,22 @@ _guc_init(void)
 							 /* check_hook= */ NULL,
 							 /* assign_hook= */ NULL,
 							 /* show_hook= */ NULL);
+
+	DefineCustomStringVariable(/* name= */ MAKE_EXTOPTION("default_chunk_time_interval"),
+							   /* short_desc= */ "Default chunk time interval for new hypertables",
+							   /* long_desc= */
+							   "Chunk time interval to use for a new hypertable, unless a specific "
+							   "chunk time interval is set on the hypertable. The default chunk "
+							   "interval is only used for hypertables with a compatible time "
+							   "type, e.g., timestamp, date, and UUID (v7). Hypertables using an "
+							   "integer partitioning column have hard-coded defaults.",
+							   /* valueAddr= */ &ts_guc_default_chunk_time_interval,
+							   NULL,
+							   /* context= */ PGC_USERSET,
+							   /* flags= */ 0,
+							   /* check_hook= */ check_default_chunk_time_interval,
+							   /* assign_hook= */ assign_default_chunk_time_interval,
+							   /* show_hook= */ NULL);
 
 #ifdef TS_DEBUG
 	DefineCustomBoolVariable(/* name= */ MAKE_EXTOPTION("shutdown_bgw_scheduler"),
