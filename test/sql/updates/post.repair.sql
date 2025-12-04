399a82ddf0d6eba14f44fadc4d9bdb91fc8adcff
Sven Klemm <sven@timescale.com>
2025-12-04 18:47:10 +0100

    Remove cagg_try_repair
    This is no longer needed and was used to repair caggs created with
    1.7.0

--- test/sql/updates/post.repair.sql
+++ test/sql/updates/post.repair.sql
@@ -2,10 +2,6 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
---Check if the repaired cagg with joins work alright now
-\ir post.repair.cagg_joins.sql
-\ir post.repair.hierarchical_cagg.sql
-
 \z repair_test_int
 \z repair_test_extra
 
