bfb95011d39fbe107feec276c6d65fe96b8835e1
Erik Nordström <erik@tigerdata.com>
2026-01-08 10:14:31 +0100

    Remove duplicate subtract_interval_from_now function
    There were two versions of the same function; one in non-TSL code and
    one in TSL code. Remove the duplicate version in TSL.

--- tsl/src/bgw_policy/policy_utils.h
+++ tsl/src/bgw_policy/policy_utils.h
@@ -10,6 +10,5 @@
 
 int64 subtract_integer_from_now_internal(int64 interval, Oid time_dim_type, Oid now_func,
 										 bool *overflow);
-Datum subtract_interval_from_now(Interval *lag, Oid time_dim_type);
 const Dimension *get_open_dimension_for_hypertable(const Hypertable *ht, bool fail_if_not_found);
 bool policy_get_verbose_log(const Jsonb *config);
