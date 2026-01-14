32fe6938a0e03d1388f7350d0e99c821d0fe715a
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-01-14 14:40:50 +0100

    Compute compressed parallel workers based on uncompressed pages
    Currently we estimate it based on the compressed pages number, which
    doesn't account for the fact that this decompresses into much more rows.

--- tsl/src/nodes/columnar_scan/columnar_scan.c
+++ tsl/src/nodes/columnar_scan/columnar_scan.c
@@ -2410,6 +2410,7 @@ static void
 create_compressed_scan_paths(PlannerInfo *root, RelOptInfo *compressed_rel,
 							 const CompressionInfo *compression_info, const SortInfo *sort_info)
 {
+	mybt();
 	Path *compressed_path;
 	Relids required_outer = compressed_rel->lateral_relids;
 
@@ -2439,8 +2440,18 @@ create_compressed_scan_paths(PlannerInfo *root, RelOptInfo *compressed_rel,
 	 */
 	if (compressed_rel->consider_parallel && required_outer == NULL)
 	{
+		const float compression_ratio_guess = 5.;
+		int effective_pages = compressed_rel->pages
+			/ (float) compressed_rel->reltarget->width
+			* compression_info->chunk_rel->reltarget->width
+			* compression_info->compressed_batch_size
+			/ compression_ratio_guess;
+		effective_pages = Max(effective_pages, 1);
+//		fprintf(stderr, "compressed width %d uncompressed %d\n",
+//			compressed_rel->reltarget->width, compression_info->chunk_rel->reltarget->width);
+//		fprintf(stderr, "compressed pages %d effective %d\n", compressed_rel->pages, effective_pages);
 		int parallel_workers = compute_parallel_worker(compressed_rel,
-													   compressed_rel->pages,
+													   effective_pages,
 													   -1,
 													   max_parallel_workers_per_gather);
 
