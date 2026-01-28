10fb8948114d911858c20e08f2d099b99ee87d7d
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-01-28 12:58:58 +0100

    switch to bools

--- src/nodes/modify_hypertable.h
+++ src/nodes/modify_hypertable.h
@@ -52,6 +52,8 @@ typedef struct ModifyHypertableState
 
 } ModifyHypertableState;
 
+extern bool ts_is_modify_hypertable_plan(Plan *plan);
+
 extern void ts_modify_hypertable_fixup_tlist(Plan *plan);
 extern Path *ts_modify_hypertable_path_create(PlannerInfo *root, ModifyTablePath *mtpath,
 											  RelOptInfo *input_rel);
