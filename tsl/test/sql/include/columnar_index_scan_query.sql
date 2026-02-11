f29edbcb6056794cccc4ac738919f798d3a5ae55
Sven Klemm <sven@timescale.com>
2026-02-11 09:37:48 +0100

    Refactor ColumnarIndexScan planning
    Do ColumnarIndexScan plan generation after postgres planning so we
    dont have to work against setrefs. This commit also changes the
    plan structure for this node. The node will be below a partial
    aggregate node instead of replacing it.
    
    New plan structure:
    
     Finalize Aggregate
       Filter: (sum(compress_hyper_2_5_chunk._ts_meta_count) > 1)
       ->  Append
             ->  Partial Aggregate
                   ->  Custom Scan (ColumnarIndexScan) on _hyper_1_1_chunk metrics
                         ->  Seq Scan on compress_hyper_2_5_chunk
             ->  Partial Aggregate
                   ->  Custom Scan (ColumnarIndexScan) on _hyper_1_9_chunk
                         ->  Seq Scan on compress_hyper_2_42_chunk
             ->  Partial Aggregate
                   ->  Seq Scan on _hyper_1_9_chunk
    
    This patch also adds count(*) as supported aggregate and adds
    support for HAVING clause.

--- tsl/test/sql/include/columnar_index_scan_query.sql
+++ tsl/test/sql/include/columnar_index_scan_query.sql
@@ -5,6 +5,15 @@
 -- canary for test diff
 SHOW timescaledb.enable_columnarindexscan;
 
+-- simple query with count without grouping
+:PREFIX SELECT device, count(*) FROM metrics GROUP BY device;
+
+-- simple query with count with grouping
+:PREFIX SELECT device, count(*) FROM metrics GROUP BY device ORDER BY device;
+
+-- simple query with multiple count with grouping
+:PREFIX SELECT device, count(*), count(*) FROM metrics GROUP BY device ORDER BY device;
+
 -- simple query with 1 max aggregate that can use optimization
 :PREFIX SELECT device, max(time) FROM metrics GROUP BY device;
 
@@ -37,25 +46,24 @@ SHOW timescaledb.enable_columnarindexscan;
 -- test multiple group by columns
 :PREFIX SELECT device, sensor, max(time) FROM metrics GROUP BY device,sensor ORDER BY device,sensor;
 
--- order by does currently prevent optimization
-:PREFIX SELECT device, max(time) FROM metrics GROUP BY device ORDER BY device;
-
 -- filter on segmentby allows optimization
 :PREFIX SELECT device, min(time) FROM metrics WHERE device IN ('d1','d2') GROUP BY device;
 :PREFIX SELECT device, min(time) FROM metrics WHERE device =ANY(ARRAY['d1','d2']) AND sensor='B' GROUP BY device;
 
--- filter on non-segmentby prevents optimization
-:PREFIX SELECT device, max(time) FROM metrics WHERE time <> '2025-01-01'  GROUP BY device;
+-- HAVING on segmentby (pushed down to lower scan)
+:PREFIX SELECT device, min(time) FROM metrics GROUP BY device HAVING device IN ('d1','d2');
+:PREFIX SELECT device, min(time) FROM metrics GROUP BY device,sensor HAVING device =ANY(ARRAY['d1','d2']) AND sensor='B';
+
+-- HAVING with aggregate function
+:PREFIX SELECT device, min(value) FROM metrics GROUP BY device HAVING min(value) > 20;
+:PREFIX SELECT device, max(value) FROM metrics GROUP BY device HAVING min(value) < 25;
+
+-- HAVING
+:PREFIX SELECT device, min(time) FROM metrics GROUP BY device HAVING min(time) > '2025-01-01 00:30:00 PST';
 
 -- tableoid doesnt prevent optimization
---:PREFIX SELECT tableoid, device, max(time) FROM metrics GROUP BY device, tableoid;
-
--- group by on non-segmentby prevents optimization
-:PREFIX SELECT max(time) FROM metrics GROUP BY value;
-:PREFIX SELECT device, max(time) FROM metrics GROUP BY device,value;
-
--- no group by prevents optimization
-:PREFIX SELECT max(time) FROM metrics;
+:PREFIX SELECT tableoid, device, max(time) FROM metrics GROUP BY device, tableoid;
+:PREFIX SELECT device, max(time), min(tableoid) FROM metrics GROUP BY device ORDER BY device;
 
 -- multiple aggregates on same column use optimization
 :PREFIX SELECT device, min(time), max(time) FROM metrics GROUP BY device;
@@ -75,14 +83,8 @@ SHOW timescaledb.enable_columnarindexscan;
 -- multiple aggregates on multiple columns
 :PREFIX SELECT device, min(time), max(time), min(value), max(value) FROM metrics GROUP BY device;
 
--- expression on aggregates (currently not optimized)
+-- expression on aggregates
 :PREFIX SELECT device, max(time), min(time), max(time) - min(time)  FROM metrics GROUP BY device;
-
--- aggregate on segmentby column does not use optimization
-:PREFIX SELECT device, min(sensor) FROM metrics GROUP BY device;
-
--- aggregate on column without metadata does not use optimization
-:PREFIX SELECT device, min(value2) FROM metrics GROUP BY device;
 
 -- test with sort
 :PREFIX SELECT device, sensor, first(time,time), last(time,time) from metrics group by device, sensor order by 1,2;
@@ -94,11 +96,6 @@ SHOW timescaledb.enable_columnarindexscan;
 
 -- test with CTE (also uses SubqueryScan)
 :PREFIX WITH agg_data AS (
-    SELECT device, min(time) as min_time, max(time) as max_time FROM metrics GROUP BY device
-)
-SELECT * FROM agg_data ORDER BY device;
-
-WITH agg_data AS (
     SELECT device, min(time) as min_time, max(time) as max_time FROM metrics GROUP BY device
 )
 SELECT * FROM agg_data ORDER BY device;
@@ -117,4 +114,50 @@ ORDER BY device;
 :PREFIX SELECT device, min(time), max(time) FROM metrics GROUP BY device
 UNION ALL
 SELECT device, max(time), min(time) FROM metrics GROUP BY device ORDER BY 1,2,3;
+
+-- test WINDOW functions
+:PREFIX SELECT device, max(time), lead(device) OVER (PARTITION BY max(time)) from metrics GROUP BY device ORDER BY device;
+:PREFIX SELECT device, max(time), lead(device) OVER (PARTITION BY device) from metrics GROUP BY device ORDER BY device;
+
+-- currently unoptimized queries
+
+-- count on dimension column (could be optimized but currently is not)
+:PREFIX SELECT count(time) FROM metrics;
+
+-- count with segmentby column
+:PREFIX SELECT count(device) FROM metrics;
+
+-- count with other columns
+:PREFIX SELECT count(value) FROM metrics;
+
+-- group by on non-segmentby prevents optimization
+:PREFIX SELECT max(time) FROM metrics GROUP BY value;
+:PREFIX SELECT device, max(time) FROM metrics GROUP BY device,value;
+
+-- unsupported aggregates on any column
+:PREFIX SELECT avg(value) FROM metrics;
+:PREFIX SELECT string_agg(device, ',') FROM metrics;
+
+-- aggregate on segmentby column does not use optimization
+:PREFIX SELECT device, min(sensor) FROM metrics GROUP BY device;
+
+-- aggregate on column without metadata
+:PREFIX SELECT min(value2) FROM metrics GROUP BY device ORDER BY device;
+
+-- aggregates with FILTER clause
+:PREFIX SELECT count(*) FILTER(WHERE device='dev 1') FROM metrics;
+:PREFIX SELECT min(value) FILTER(WHERE device='dev 1') FROM metrics;
+
+-- aggregates with DISTINCT
+:PREFIX SELECT count(DISTINCT device) FROM metrics;
+
+-- aggregates with ORDER
+:PREFIX SELECT min(value ORDER BY time) FROM metrics;
+
+-- WHERE clause on non-segmentby column
+:PREFIX SELECT count(*) FROM metrics WHERE value > 10;
+
+-- first/last with different arg1 and arg2
+:PREFIX SELECT first(value, time) FROM metrics GROUP BY device ORDER BY device;
+:PREFIX SELECT last(value, time) FROM metrics GROUP BY device ORDER BY device;
 
