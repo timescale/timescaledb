c7b38aea9c78877701063e8dae207b67889751f3
David Beck <dbeck@timescale.com>
2026-02-19 18:34:37 +0000

    pickup change

--- src/nodes/modify_hypertable.h
+++ src/nodes/modify_hypertable.h
@@ -53,6 +53,16 @@ typedef struct ModifyHypertableState
 	int64 batches_pruned_by_bloom;
 	int64 batches_without_bloom;
 	int64 batches_bloom_false_positives;
+
+	/*
+	 * When EXPLAIN VERBOSE is used, we temporarily nullify the targetlist of the
+	 * lefttree of the ModifyTable to avoid printing out the full targetlist since
+	 * they can't be resolved by EXPLAIN. To not corrupt cached plans we need to
+	 * restore them to their original value afterwards.
+	 */
+	List *explain_saved_tlist;
+	List *explain_saved_custom_scan_tlist;
+
 } ModifyHypertableState;
 
 extern TSDLLEXPORT bool ts_is_modify_hypertable_plan(Plan *plan);
