58b1c5131d589711cb94e5e1f971161cb21827c1
Sven Klemm <sven@timescale.com>
2026-03-02 07:20:23 +0100

    Ensure search_path is set before anything else in SQL scripts
    Add a new file header.sql that is included before any other files
    when building install/update/downgrade script.
    
    (cherry picked from commit 9a8f7f8bdeb99e6abae0786ffe526791a8628ce3)

---
+++ sql/header.sql
@@ -0,0 +1,6 @@
+-- This file and its contents are licensed under the Apache License 2.0.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-APACHE for a copy of the license.
+
+-- This file is always prepended to all installation and upgrade/downgrade scripts.
+SET LOCAL search_path TO pg_catalog, pg_temp;
