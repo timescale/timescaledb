e0cf0a33807937e176c28c6bcf5a69b2089e3c05
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-03-05 21:05:52 +0100

    a test

---
+++ tsl/test/shared/sql/parameterized_chunkappend.sql
@@ -0,0 +1,46 @@
+-- This file and its contents are licensed under the Timescale License.
+-- Please see the included NOTICE for copyright information and
+-- LICENSE-TIMESCALE for a copy of the license.
+
+-- Tests for parameterized ChunkAppend.
+
+\set PREFIX 'EXPLAIN (COSTS OFF, ANALYZE, TIMING OFF, BUFFERS OFF, SUMMARY OFF)'
+
+SET enable_hashjoin = off;
+SET enable_mergejoin = off;
+SET enable_material = off;
+
+:PREFIX
+WITH win(id, win_start, win_end) AS (VALUES
+    (1, '2000-01-03 00:00'::timestamptz, '2000-01-03 12:00'::timestamptz),
+    (2, '2000-01-08 00:00', '2000-01-08 12:00'),
+    (3, '2000-01-15 00:00', '2000-01-15 12:00'))
+SELECT win.id, count(*)
+FROM win
+INNER JOIN metrics m ON m.time >= win.win_start
+                    AND m.time < win.win_end
+                    AND m.time > now() - interval '100 years'
+GROUP BY win.id
+ORDER BY win.id;
+
+:PREFIX
+WITH win(id, win_start, win_end) AS (VALUES
+    (1, '2000-01-03 00:00'::timestamptz, '2000-01-03 12:00'::timestamptz),
+    (2, '2000-01-08 00:00', '2000-01-08 12:00'),
+    (3, '2000-01-15 00:00', '2000-01-15 12:00'))
+SELECT devices.device_id, win.id, count(*)
+FROM devices, win,
+LATERAL (
+    SELECT * FROM metrics
+    WHERE metrics.device_id = devices.device_id
+      AND metrics.time >= win.win_start
+      AND metrics.time < win.win_end
+      AND metrics.time > now() - interval '100 years'
+) sub
+GROUP BY devices.device_id, win.id
+ORDER BY devices.device_id, win.id
+;
+
+RESET enable_hashjoin;
+RESET enable_mergejoin;
+RESET enable_material;
