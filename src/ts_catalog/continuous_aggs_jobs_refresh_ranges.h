d9f1a1fbe7571689cf8db9db66bb39367399ed6e
Thao Pham <thao@tigerdata.com>
2026-03-06 11:26:47 -0500

    Remove job_id from cagg_jobs_refresh_ranges With current design, we only need pid to know if a refresh range is still active or not. so no need to keep job_id in there

--- src/ts_catalog/continuous_aggs_jobs_refresh_ranges.h
+++ src/ts_catalog/continuous_aggs_jobs_refresh_ranges.h
@@ -11,7 +11,7 @@
 
 extern TSDLLEXPORT void ts_cagg_jobs_refresh_ranges_insert(int32 materialization_id,
 														   int64 start_range, int64 end_range,
-														   int32 job_id, int32 pid);
+														   int32 pid);
 extern TSDLLEXPORT void
 ts_cagg_jobs_refresh_ranges_delete_by_materialization_id(int32 materialization_id);
 extern TSDLLEXPORT void ts_cagg_jobs_refresh_ranges_delete_by_pid(int32 materialization_id,
