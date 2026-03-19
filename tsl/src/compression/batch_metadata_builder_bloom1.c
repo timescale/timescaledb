f2624dbfdd3d4643ca27982007a33cdd2ea2d7ca
Sven Klemm <sven@timescale.com>
2026-03-19 19:57:26 +0100

    Use better error code when extended hash function missing
    Change the bloom errorcode from ERRCODE_INTERNAL_ERROR to
    ERRCODE_UNDEFINED_FUNCTION when extended hash function is missing
    
    (cherry picked from commit 24d432477e27fa0933ce81ae6829c4877d66771c)

--- tsl/src/compression/batch_metadata_builder_bloom1.c
+++ tsl/src/compression/batch_metadata_builder_bloom1.c
@@ -839,7 +839,7 @@ bloom1_hasher_init(Bloom1HasherInternal *hasher, const Oid *type_oids, int num_c
 			bloom1_get_hash_function(type_oids[i], &hasher->hash_function_finfos[i]);
 		if (hasher->hash_functions[i] == NULL)
 			ereport(ERROR,
-					(errcode(ERRCODE_INTERNAL_ERROR),
+					(errcode(ERRCODE_UNDEFINED_FUNCTION),
 					 errmsg("the argument type %s lacks an extended hash function",
 							format_type_be(type_oids[i]))));
 	}
