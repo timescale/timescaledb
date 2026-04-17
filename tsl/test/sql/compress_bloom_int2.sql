4e6e8456c382cc70ca8ac936fea874be62039ee0
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-04-17 16:42:46 +0200

    negative values

--- tsl/test/sql/compress_bloom_int2.sql
+++ tsl/test/sql/compress_bloom_int2.sql
@@ -8,11 +8,14 @@ WITH (tsdb.hypertable, tsdb.partition_column = 'ts',
 ;
 
 INSERT INTO int2_crosstype SELECT i, (i % 100)::smallint, i % 100 FROM generate_series(1, 5000) i;
+INSERT INTO int2_crosstype SELECT i, (-1 * (i % 100))::smallint, i % 100 FROM generate_series(5001, 10000) i;
 SELECT compress_chunk(c) FROM show_chunks('int2_crosstype') c;
 
 SELECT count(*) FROM int2_crosstype WHERE i2 = 1::smallint;
 SELECT count(*) FROM int2_crosstype WHERE i2 = 1::bigint;
 
+SELECT count(*) FROM int2_crosstype WHERE i2 = -1::smallint;
+SELECT count(*) FROM int2_crosstype WHERE i2 = -1::bigint;
 SELECT count(*) FROM int2_crosstype WHERE i2 = 1::smallint AND i4 = 1;
 SELECT count(*) FROM int2_crosstype WHERE i2 = 1::bigint AND i4 = 1;
 
