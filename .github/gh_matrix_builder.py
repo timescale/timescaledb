8de9f1eecb23c2d046a04f18f79bb50cc137c9e7
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-04-21 13:41:24 +0200

    test true

--- .github/gh_matrix_builder.py
+++ .github/gh_matrix_builder.py
@@ -225,7 +225,7 @@ m["include"].append(
         macos_config(
             {
                 "pg": PG18_LATEST,
-                "installcheck": not pull_request,
+                "installcheck": True,
                 "pginstallcheck": not pull_request,
             }
         )
