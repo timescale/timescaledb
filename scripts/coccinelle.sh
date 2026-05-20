8425e0e8a31cb9f3be403add89aaa50fce7a801e
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-05-20 14:46:43 +0200

    Include Postgres header in coccinelle
    When it doesn't see the headers, it can't understand what e.g. `Name`
    is, so some rules don't work.

--- scripts/coccinelle.sh
+++ scripts/coccinelle.sh
@@ -6,8 +6,14 @@ SCRIPT_DIR=$(dirname "${0}")
 FAILED=false
 true > coccinelle.diff
 
+PG_SERVER_INC=$(pg_config --includedir-server)
+
 for f in "${SCRIPT_DIR}"/../coccinelle/*.cocci; do
-  spatch --very-quiet --include-headers --sp-file "$f" --dir "${SCRIPT_DIR}"/.. | tee -a coccinelle.diff
+  spatch --very-quiet \
+        --all-includes --include-headers-for-types \
+        -I "${PG_SERVER_INC}" \
+        --sp-file "$f" --dir "${SCRIPT_DIR}"/.. --include-headers \
+    | tee -a coccinelle.diff
   rc=$?
   if [ $rc -ne 0 ]; then
     FAILED=true
