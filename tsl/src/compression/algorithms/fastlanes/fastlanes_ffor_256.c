48556ac6283531792910114132ba358c99bd6857
David Beck <dbeck@timescale.com>
2026-05-30 22:16:31 +0100

    Modified and reimplemented FastLanes library in C
    There is a background and intro document in src/fl/README.md
    
    The short version is: this change brings a C implementation
    of the FastLanes library to Timescale DB. The original idea
    is from 'The FastLanes Compression Layout: Decoding >100 Billion
    Integers per Second with Scalar Code' by Azim Afroozeh and
    Peter Boncz (https://doi.org/10.14778/3598581.3598587)
    
    Our version departs from the original ideas in a few ways:
    
    - instead of having a single 1024 bit virtual register, I
      introduce multiple smaller register widths, that I call
      'tiered FL', the widths are 8, 16, 32, 64, 128 and 256
    
    - we do not have 1024 bit virtual registers
    
    - our implementation relies on C macros, instead of C++
      templates and code generation
    
    The src/fl/README.md file provides more details about the
    library.
    
    Co-authored-by: Sven Klemm <31455525+svenklemm@users.noreply.github.com>
    Signed-off-by: David Beck <david.beck.priv@gmail.com>

---
+++ tsl/src/compression/algorithms/fastlanes/fastlanes_ffor_256.c
@@ -0,0 +1,31 @@
+/*
+ * This file and its contents are licensed under the Timescale License.
+ * Please see the included NOTICE for copyright information and
+ * LICENSE-TIMESCALE for a copy of the license.
+ */
+
+#include <postgres.h>
+
+#include "fastlanes.h"
+#include "fastlanes_common.h"
+
+/*
+ * These extern declarations are needed because MSVC runs out of the heap space if all FFOR
+ * macro expansion happens in the same C file. The functions are being called from
+ * `fastlanes_ffor.c`, which dispatches between the tiers. These functions are intentionally
+ * not placed in a header file, to discourage direct use.
+ */
+extern size_t fl256_pack_ffor(const void *values, void *packed, uint32 n, uint8 w,
+							  fl_elem_width_t t, uint64 base);
+extern void fl256_unpack_ffor(const void *packed, void *values, uint32 n, uint8 w,
+							  fl_elem_width_t t, uint64 base);
+
+/* clang format would reorder the headers which is not desired here */
+/* clang-format off */
+
+#define FL_TIER 256
+#include "fastlanes_tier_sizing.h"
+#include "fastlanes_tier_ffor_impl.h"
+#undef FL_TIER
+
+/* clang-format on */
