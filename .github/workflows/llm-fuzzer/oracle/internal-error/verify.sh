ff0376c6da2babc45d5fcc2269ee54c350cec84e
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-06-11 16:54:54 +0200

    Refactor LLM Fuzzer workflow to allow multiple oracles
    Add build matrix and split out the oracle-specific code.

---
+++ .github/workflows/llm-fuzzer/oracle/internal-error/verify.sh
@@ -0,0 +1,9 @@
+# This oracle just runs the reproducer script. The later workflow checks will
+# detect the program logic errors: segfault, assertion failure, address
+# sanitizer failure, internal program error (SQLSTATE XX000) and so on.
+# The reproducer script for this oracle must trigger this class of error.
+set -xeu
+
+psql -c "create extension timescaledb;"
+psql -v ON_ERROR_STOP=0 -f ~/llm-fuzzer-repro.sql
+psql -c "select 1"
