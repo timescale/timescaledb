14e8f25ef26ac9a861879e8d61de6cf44a1b29db
Sven Klemm <sven@tigerdata.com>
2026-06-17 07:02:58 +0200

    Fix uncompressed size estimate for text segmentby columns
    The size estimate built a query that called compressed_data_column_size
    on every variable length column. Variable length segmentby columns
    were handled incorrectly and need to use pg_column_size instead of
    compressed_data_column_size.

--- tsl/test/sql/uncompressed_size.sql
+++ tsl/test/sql/uncompressed_size.sql
@@ -22,3 +22,17 @@ INSERT INTO t1 SELECT '2026-01-01'::timestamptz + format('%s ms', i)::interval, 
 SELECT compress_chunk(chunk) FROM show_chunks('t1') AS chunk;
 SELECT _timescaledb_functions.compressed_data_info(time), _timescaledb_functions.compressed_data_info(device), _timescaledb_functions.compressed_data_info(value) FROM _timescaledb_internal.compress_hyper_2_6_chunk;
 SELECT * FROM _timescaledb_functions.estimate_uncompressed_size('_timescaledb_internal.compress_hyper_2_6_chunk');
+
+-- test varlen segmentby column keeps
+CREATE TABLE t3 (time timestamptz, device text, value float) WITH (tsdb.hypertable, tsdb.segmentby = 'device');
+INSERT INTO t3 SELECT '2025-01-01'::timestamptz + format('%s ms', i)::interval, format('Device %s', i%10), 1 + 1/i FROM generate_series(1, 100000) i;
+CREATE INDEX t3_device_idx ON t3 (device);
+VACUUM FULL t3;
+SELECT compress_chunk(chunk) FROM show_chunks('t3') AS chunk;
+
+SELECT l.relation_size > 0 AS relation_positive,
+       l.index_size > 0 AS index_positive,
+       l.total_size = l.relation_size + l.index_size AS total_consistent
+FROM show_chunks('t3') AS chunk
+JOIN _timescaledb_catalog.compression_settings cs ON cs.relid = chunk
+JOIN LATERAL _timescaledb_functions.estimate_uncompressed_size(cs.compress_relid) l ON true;
