ac29d1c36ce633aac5f1e5dc16e6e853fa679d9e
Sven Klemm <sven@tigerdata.com>
2026-06-20 23:09:58 +0200

    Refactor more tests to use compression_settings for compressed relation lookup

--- tsl/test/sql/uncompressed_size.sql
+++ tsl/test/sql/uncompressed_size.sql
@@ -14,14 +14,16 @@ VACUUM FULL t2;
 SELECT compress_chunk(chunk) FROM show_chunks('t1') AS chunk;
 SELECT compress_chunk(chunk) FROM show_chunks('t2') AS chunk;
 
-
 SELECT cs.compress_relid, l.relation_size, l.index_size, l.total_size FROM _timescaledb_catalog.compression_chunk_size ccs JOIN _timescaledb_catalog.chunk uc ON uc.id=ccs.chunk_id JOIN _timescaledb_catalog.compression_settings cs ON cs.relid=format('%I.%I',uc.schema_name,uc.table_name)::regclass JOIN LATERAL (SELECT * FROM _timescaledb_functions.estimate_uncompressed_size(cs.compress_relid)) l ON true;
 
 -- test NULL compression does not error and returns NULL
+
 INSERT INTO t1 SELECT '2026-01-01'::timestamptz + format('%s ms', i)::interval, NULL, NULL FROM generate_series(1, 3000) i;
 SELECT compress_chunk(chunk) FROM show_chunks('t1') AS chunk;
-SELECT _timescaledb_functions.compressed_data_info(time), _timescaledb_functions.compressed_data_info(device), _timescaledb_functions.compressed_data_info(value) FROM _timescaledb_internal.compress_hyper_2_6_chunk;
-SELECT * FROM _timescaledb_functions.estimate_uncompressed_size('_timescaledb_internal.compress_hyper_2_6_chunk');
+SELECT compress_relid AS "COMPRESSED_RELATION" from show_chunks('t1') chunk JOIN _timescaledb_catalog.compression_settings cs ON cs.relid=chunk ORDER BY chunk::text DESC LIMIT 1 \gset
+
+SELECT _timescaledb_functions.compressed_data_info(time), _timescaledb_functions.compressed_data_info(device), _timescaledb_functions.compressed_data_info(value) FROM :COMPRESSED_RELATION;
+SELECT * FROM _timescaledb_functions.estimate_uncompressed_size(:'COMPRESSED_RELATION');
 
 -- test varlen segmentby column keeps
 CREATE TABLE t3 (time timestamptz, device text, value float) WITH (tsdb.hypertable, tsdb.segmentby = 'device');
