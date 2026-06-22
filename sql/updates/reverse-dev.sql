e21fd8bac657400db8f82400c3822be053a82dcc
Ante Kresic <ante.kresic@gmail.com>
2026-06-22 22:07:40 +0000

    Add compact_chunk function
    This new function will compact the chunk by looking for overlapping
    batches and combining them together in order to produce globally
    ordered chunks. This change is a first step towards supporting
    direct compress in production workloads.

--- sql/updates/reverse-dev.sql
+++ sql/updates/reverse-dev.sql
@@ -1,2 +1,3 @@
 DROP FUNCTION IF EXISTS _timescaledb_functions.decompress_batch(record);
 DROP FUNCTION IF EXISTS _timescaledb_functions.estimate_uncompressed_size(regclass, double precision);
+DROP FUNCTION IF EXISTS _timescaledb_functions.compact_chunk(REGCLASS);
