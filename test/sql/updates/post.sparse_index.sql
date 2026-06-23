a145ed779ab7c7f2dbbed0309c27a6245374b7ce
Sven Klemm <sven@tigerdata.com>
2026-06-23 18:30:35 +0200

    Normalize chunk names in updates/downgrade test
    With the removal of compressed chunks as separate chunk entry all
    chunks will have different numbering between fresh install/upgrade.
    Prepare for this by ignoring chunk numbering in update test.

--- test/sql/updates/post.sparse_index.sql
+++ test/sql/updates/post.sparse_index.sql
@@ -2,35 +2,25 @@
 -- Please see the included NOTICE for copyright information and
 -- LICENSE-APACHE for a copy of the license.
 
--- Show chunk column stats after updating the extension. We need to
--- exclude the rows with chunk_id = 0 because we cannot keep those on
--- downgrade due to FK constraint. Showing them would mean a diff in
--- the output. We can still test that 0 chunk_ids are converted to
--- NULL values during upgrades, however.
-
-SELECT
-    cs.compress_relid::text AS chunk
-FROM _timescaledb_catalog.chunk ch
-JOIN _timescaledb_catalog.compression_settings cs
-    ON cs.relid = format('%I.%I', ch.schema_name, ch.table_name)::regclass
-WHERE ch.hypertable_id = (
-    SELECT id
-    FROM _timescaledb_catalog.hypertable
-    WHERE table_name = 'bloom'
-)
-LIMIT 1
-\gset
-
 -- This test checks that the bloom sparse indexes survive the upgrade, so only
 -- look at the bloom columns of the compressed chunk. Dumping the whole chunk
 -- would also pull in the orderby sparse metadata, whose layout depends on the
 -- version that compressed the chunk (minmax vs firstlast) and is not rewritten
 -- on upgrade, which would cause a spurious diff unrelated to bloom indexes.
+-- look at the bloom columns of one compressed chunk of the bloom hypertable.
 \a
 SELECT a.attname, format_type(a.atttypid, a.atttypmod) AS type
 FROM pg_attribute a
 JOIN pg_type t ON t.oid = a.atttypid
-WHERE a.attrelid = :'chunk'::regclass
+WHERE a.attrelid = (
+    SELECT cs.compress_relid
+    FROM _timescaledb_catalog.chunk ch
+    JOIN _timescaledb_catalog.compression_settings cs
+        ON cs.relid = format('%I.%I', ch.schema_name, ch.table_name)::regclass
+    WHERE ch.hypertable_id = (SELECT id FROM _timescaledb_catalog.hypertable WHERE table_name = 'bloom')
+    ORDER BY ch.id
+    LIMIT 1
+  )
   AND a.attnum > 0
   AND NOT a.attisdropped
   AND t.typname = 'bloom1'
