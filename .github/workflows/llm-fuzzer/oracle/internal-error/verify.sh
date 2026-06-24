5a66237170e48300396fc8d8f15d5634e2aff73a
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-06-24 17:42:29 +0200

    Do not consider bugs that require superuser privilege in LLM fuzzer
    It's easy to create weirdly broken opclasses and this has questionable
    utility, similar to other catalog breakages.

--- .github/workflows/llm-fuzzer/oracle/internal-error/verify.sh
+++ .github/workflows/llm-fuzzer/oracle/internal-error/verify.sh
@@ -4,6 +4,8 @@
 # workflow checks will detect the program logic errors: segfault, assertion
 # failure, address sanitizer failure, internal program error (SQLSTATE XX000)
 # and so on. The admissible bug must manifest as this class of error.
+#
+# The repro must not require superuser privileges.
 set -xeu
 
-psql -v ON_ERROR_STOP=0 -f ~/llm-fuzzer-repro.sql
+psql -v ON_ERROR_STOP=0 -f $1
