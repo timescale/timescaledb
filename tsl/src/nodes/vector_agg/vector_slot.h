81baae603445b55f10256d3100f7f3f53666cde9
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-06-25 12:17:33 +0200

    Common subexpression elimination in columnar agg (#9534)
    1) Use interning to find the identical expression subtrees and replace
       them with a canonical pointer.
    1) Count how many times each expression is present in the hierarchy.
    1) For expression that are present many times, compute them only once
       per batch.

--- tsl/src/nodes/vector_agg/vector_slot.h
+++ tsl/src/nodes/vector_agg/vector_slot.h
@@ -30,10 +30,14 @@ vector_slot_get_qual_result(const TupleTableSlot *slot, uint16 *num_rows)
 	return batch_state->vector_qual_result;
 }
 
+struct expr_cache_hash;
+
 /*
- * Return the arrow array or the datum (in case of single scalar value) for a
- * given attribute as a CompressedColumnValues struct.
+ * Compute the arrow array or the datum (in case of single scalar value) for a
+ * given expression as a CompressedColumnValues struct. Optionally use the
+ * common subexpression cache.
  */
 CompressedColumnValues vector_slot_evaluate_expression(DecompressContext *dcontext,
 													   TupleTableSlot *slot, uint64 const *filter,
-													   const Expr *argument);
+													   const Expr *argument,
+													   struct expr_cache_hash *expr_cache);
