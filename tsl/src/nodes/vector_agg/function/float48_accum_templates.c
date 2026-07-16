d331f6c0f1471538d90535b715e84e394b951718
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-07-16 10:51:37 +0200

    Fix potential incorrect stddev(float8/4) in columnar pipeline (#10209)
    Postgres uses the same float8_accum transition function for both avg()
    and stddev() for these types. TimescaleDB used to use a simplified
    implementation for avg(), because it doesn't require tracking sum of
    squares. However, Postgres uses a shared transition state for aggregates
    sharing a transition functions, as given by AggRef.aggtransno. This
    leads to potentially wrong result when both avg() and stddev() of same
    variable are computed in one query.
    
    Change the aggregation states in columnar aggregation to follow
    aggtransno. Remove the "float8 accum with no squares" implementation
    because it cannot be safely used in general case, and checking for
    special cases is complicated.
    
    (cherry picked from commit 58e9840f68170144f50a3399f9d901048ee9c920)

--- tsl/src/nodes/vector_agg/function/float48_accum_templates.c
+++ tsl/src/nodes/vector_agg/function/float48_accum_templates.c
@@ -20,19 +20,5 @@
 #include "template_helper.h"
 #include <compression/arrow_c_data_interface.h>
 
-#ifndef GENERATE_DISPATCH_TABLE
-
-#endif
-
-/*
- * Templated parts for vectorized avg(float).
- */
-#define AGG_NAME accum_no_squares
-#include "float48_accum_types.c"
-
-/*
- * Templated parts for vectorized functions that use the Sxx state (stddev etc).
- */
 #define AGG_NAME accum_with_squares
-#define NEED_SXX
 #include "float48_accum_types.c"
