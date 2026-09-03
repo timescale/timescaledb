03d1829d0200cee576667c4092f283a30d98ca18
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-09-03 11:17:52 +0200

    Speed up code formatting with "make format"
    1. Remove workaround for clang-format not properly parsing
       `my_func(PG_FUNCTION_ARGS)`. It takes a lot of time, and at the
    current version affects only a few places where this is used inside a
    macro.
    1. Improve parallelism of formatting by tweaking `xargs`.

--- scripts/clang_format_all.sh
+++ scripts/clang_format_all.sh
@@ -4,6 +4,11 @@
 SCRIPT_DIR=$(cd "$(dirname $0)" || exit; pwd)
 BASE_DIR=$(dirname $SCRIPT_DIR)
 
+CLANG_FORMAT=${CLANG_FORMAT:-clang-format}
+
+${CLANG_FORMAT} --version
+
 find ${BASE_DIR} \( -path "${BASE_DIR}/src/*" -or -path "${BASE_DIR}/test/*" -or -path "${BASE_DIR}/tsl/*" \) \
     -and -not \( -path "*/.*" -or -path "*CMake*" -or -path "${BASE_DIR}/src/import/*" -or -path "${BASE_DIR}/tsl/src/import/*" \) \
-    -and \( -name '*.c' -or -name '*.h' \) -print0 | xargs -0 ${SCRIPT_DIR}/clang_format_wrapper.sh -style=file -i
+    -and \( -name '*.c' -or -name '*.h' \) -print0 \
+    | xargs -0 -n 16 -P 0 ${CLANG_FORMAT} -Wno-error=unknown -style=file -i
