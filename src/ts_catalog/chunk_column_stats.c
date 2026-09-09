7598b78d920600f29753ea2a1f9b25052f9e6193
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-09-09 15:32:02 +0200

    look here instead

--- src/ts_catalog/chunk_column_stats.c
+++ src/ts_catalog/chunk_column_stats.c
@@ -575,7 +575,6 @@ static Constraint *
 create_col_stats_check_constraint(const Form_chunk_column_stats info, Oid main_table_relid,
 								  const char *name)
 {
-	/* LOOK HERE */
 	Constraint *constr = NULL;
 	Node *rangedef;
 	ColumnRef *colref;
