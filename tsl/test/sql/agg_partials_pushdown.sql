8be844543640e825e40534e32c37eacfa03964cf
Alexander Kuzmenkov <36882414+akuzm@users.noreply.github.com>
2026-09-16 12:26:40 +0200

    more

--- tsl/test/sql/agg_partials_pushdown.sql
+++ tsl/test/sql/agg_partials_pushdown.sql
@@ -90,6 +90,10 @@ SELECT (device_id % 4)::bit(4), sum(v0) FROM testtable GROUP BY 1 ORDER BY 1, 2;
 -- Unsortable grouping
 SELECT (device_id % 2)::text::xid, sum(v0) FROM testtable GROUP BY 1 ORDER BY 2;
 
+SET enable_hashagg to OFF;
+SELECT (device_id % 2)::text::xid, sum(v0) FROM testtable GROUP BY 1 ORDER BY 2;
+RESET enable_hashagg;
+
 
 -- Check chunk exclusion for index scans
 SET enable_seqscan = OFF;
